1cef7aa1bcfacabce3060787bae869c33baf6a3f
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2022-01-20 15:39:31 +0100

    Cleanup tests and update test writing

--- cal/test_views.py
+++ cal/test_views.py
@@ -1,25 +1,18 @@
-from django.http.request import HttpRequest
-from django.test import TestCase
-from django.http import HttpResponse
-from datetime import datetime, timedelta, date
+from datetime import date
 
-import calendar
 from .models import *
 from .views import *
-from .utils import Calendar
-from .forms import EventForm
+
 
 # Create your tests here.
-class CalendarTest(TestCase):
+def test_next_month():
+    d = date(2021, 11, 23)
+    assert next_month(d) == 'month=2021-12'
 
-    def test_next_month(self):
-        d = date(2021, 11, 23)
-        self.assertEqual(next_month(d), 'month=2021-12')
+def test_pre_month():
+    d = date(2021, 11, 23)
+    assert prev_month(d) == 'month=2021-10'
 
-    def test_pre_month(self):
-        d = date(2021, 11, 23)
-        self.assertEqual(prev_month(d), 'month=2021-10')
-
-    def test_get_date(self):
-        d = date(2021, 11, 1)
-        self.assertEqual(get_date("2021-11"), d)
+def test_get_date():
+    d = date(2021, 11, 1)
+    assert get_date("2021-11") == d
