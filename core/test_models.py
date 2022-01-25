4857f7b83a12e9b4c1bcb89f31dc89eb16c7fb5f
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2022-01-25 16:19:17 +0100

    Update

--- core/test_models.py
+++ core/test_models.py
@@ -6,19 +6,18 @@ from asf_rm.settings import ARM_SETTINGS
 from openpyxl import load_workbook
 import pandas as pd
 from django.urls import reverse
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 from datetime import date
 import pytest
 
-"""
+
 @pytest.mark.django_db
 def testAnalysis():
     projectsgroup = ProjectsGroup.objects.create()
-    project=Project.objects.create(parent_group=projectsgroup)
+    project=Project.objects.create(parent_group=projectsgroup, name="Test")
     user = User.objects.create()
     analysis = Analysis.objects.create(
         project = project,
         auditor = user,
     )
     print(analysis) #problem proxy, maybe add str() to project.__str__ etc...
-"""
