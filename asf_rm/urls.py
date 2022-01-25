ba83bad6c40c69844fc0845273d4b2249b2bc398
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-01-25 17:34:36 +0100

    Remove baton dependency

--- asf_rm/urls.py
+++ asf_rm/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 #from django.contrib import admin
-from baton.autodiscover import admin
 from django.urls import include, path
 import core.views as cv
 import general.views as gv
 from django.contrib.auth import views as auth_views
+from django.contrib import admin
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
 
@@ -28,7 +28,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', cv.UserLogin.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
-    path('baton/', include('baton.urls')),
     path('search/', cv.SearchResults.as_view(), name='search'),
     path('', login_required(cv.AnalysisListView.as_view()), name='home'),
     path('staff/x-rays', staff_member_required(gv.ReviewView.as_view()), name='xrays'),
