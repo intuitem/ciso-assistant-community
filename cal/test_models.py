c4ae4ac7ba860d458730973619caff1d10f3330f
nas-tabchiche <71079376+nas-tabchiche@users.noreply.github.com>
2022-01-26 12:06:06 +0100

    Add newline

--- cal/test_models.py
+++ cal/test_models.py
@@ -6,3 +6,4 @@ def test_event():
     Event.objects.create(title="Event", description="A simple event", start_time="2022-1-20", end_time="2022-1-27")
     event = Event.objects.get(title="Event")
     assert event.get_html_url == '<a class="bg-blue-100" href=""> Event </a>'
+    
