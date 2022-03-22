54085cf3d4feefdeb797a50291080ad10754f0eb
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-03-22 17:01:16 +0100

    Add RiskAnalysis and RiskInstance Create ModelForms

--- back_office/forms.py
+++ back_office/forms.py
@@ -1,6 +1,15 @@
+from dataclasses import fields
 from django.forms import ModelForm, Select, Textarea
 
 from core.models import Analysis, RiskInstance
+
+class RiskAnalysisCreateForm(ModelForm):
+    class Meta:
+        model = Analysis
+        fields = ['project', 'auditor', 'is_draft', 'rating_matrix', 'comments']
+        widgets = { # Tailwind Styles go here
+            'comments': Textarea(attrs={'class': 'w-full rounded-md'}),
+        }
 
 class RiskAnalysisUpdateForm(ModelForm):
     class Meta:
@@ -10,11 +19,26 @@ class RiskAnalysisUpdateForm(ModelForm):
             'comments': Textarea(attrs={'class': 'w-full rounded-md'}),
         }
 
+class RiskInstanceCreateForm(ModelForm):
+    class Meta:
+        model = RiskInstance
+        fields = '__all__'
+        widgets = { # Tailwind Styles go here
+            'existing_measures': Textarea(attrs={'class': 'w-full rounded-md text-sm h-32'}),
+            'scenario': Textarea(attrs={'class': 'w-full rounded-md text-sm h-24'}),
+            'comments': Textarea(attrs={'class': 'w-full rounded-md text-sm h-18'}),
+            'parent_risk': Select(attrs={'class': 'w-full rounded-md text-sm'}),
+            'current_proba': Select(attrs={'class': 'w-full rounded-md text-sm'}),
+            'current_impact': Select(attrs={'class': 'w-full rounded-md text-sm'}),
+            'residual_proba': Select(attrs={'class': 'w-full rounded-md text-sm'}),
+            'residual_impact': Select(attrs={'class': 'w-full rounded-md text-sm'}),
+            'treatment': Select(attrs={'class': 'w-full rounded-md text-sm'}),
+        }
+
 class RiskInstanceUpdateForm(ModelForm):
     class Meta:
         model = RiskInstance
-        fields = ['analysis', 'parent_risk', 'title', 'scenario', 'existing_measures', 'comments',
-        'current_proba', 'current_impact', 'residual_proba', 'residual_impact', 'treatment']
+        fields = '__all__'
         widgets = { # Tailwind Styles go here
             'existing_measures': Textarea(attrs={'class': 'w-full rounded-md text-sm h-32'}),
             'scenario': Textarea(attrs={'class': 'w-full rounded-md text-sm h-24'}),
