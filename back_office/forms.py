2652251d9fbfb5682419cd1f510e53275331e313
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-03-29 18:55:07 +0200

    Add ModelForms for Project, ProjectGroup

--- back_office/forms.py
+++ back_office/forms.py
@@ -1,6 +1,7 @@
 from dataclasses import fields
-from django.forms import ModelForm, Select, TextInput, Textarea
+from django.forms import ModelForm, Select, TextInput, Textarea, widgets
 
+from django.contrib.auth.models import User, Group
 from core.models import Analysis, Mitigation, RiskInstance
 from general.models import Project, ProjectsGroup
 
@@ -8,6 +9,14 @@ class RiskAnalysisCreateForm(ModelForm):
     class Meta:
         model = Analysis
         fields = ['project', 'auditor', 'is_draft', 'rating_matrix', 'comments']
+        widgets = { # Tailwind Styles go here
+            'comments': Textarea(attrs={'class': 'w-full rounded-md'}),
+        }
+
+class UserCreateForm(ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
         widgets = { # Tailwind Styles go here
             'comments': Textarea(attrs={'class': 'w-full rounded-md'}),
         }
@@ -23,8 +32,10 @@ class RiskAnalysisUpdateForm(ModelForm):
 class RiskInstanceCreateForm(ModelForm):
     class Meta:
         model = RiskInstance
-        fields = '__all__'
+        exclude = ['analysis', 'residual_level', 'current_level']
+        
         widgets = { # Tailwind Styles go here
+            'analysis': Select(attrs={'class': 'w-auto rounded-md text-sm h-32'}),
             'existing_measures': Textarea(attrs={'class': 'w-full rounded-md text-sm h-32'}),
             'scenario': Textarea(attrs={'class': 'w-full rounded-md text-sm h-24'}),
             'comments': Textarea(attrs={'class': 'w-full rounded-md text-sm h-18'}),
@@ -70,3 +81,15 @@ class ProjectUpdateForm(ModelForm):
     class Meta:
         model = Project
         fields = '__all__'
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = '__all__'
+        widgets = {
+            'name': TextInput(attrs={'class': 'w-full rounded-md text-sm h-32'}),
+            'internal_id': TextInput(attrs={'class': 'w-full rounded-md text-sm h-32'}),
+            'parent_group': Select(attrs={'class': 'w-full rounded-md text-sm h-32'}),
+            'lc_status': Select(attrs={'class': 'w-full rounded-md text-sm h-32'}),
+            'summary': Textarea(attrs={'class': 'w-full rounded-md text-sm h-32'}),
+        }
