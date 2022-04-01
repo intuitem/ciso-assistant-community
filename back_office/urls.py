41c611f9f7d621ceabbc7d0e84fc7f7f40f8eed4
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-04-01 14:12:46 +0200

    Add DeleteView for Project and ProjectsDomain

--- back_office/urls.py
+++ back_office/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # LIST VIEWS
     path('analysis-registry/', login_required(views.RiskAnalysisListView.as_view()), name='ra-list'),
     path('risk-instances/', login_required(views.RiskInstanceListView.as_view()), name='ri-list'),
     path('mitigations/', login_required(views.MitigationListView.as_view()), name='mtg-list'),
@@ -16,6 +18,7 @@ urlpatterns = [
     path('users/', login_required(views.UserListView.as_view()), name='user-list'),
     path('groups/', login_required(views.GroupListView.as_view()), name='group-list'),
 
+    # CREATE VIEWS
     path('RA/create', login_required(views.RiskAnalysisCreateView.as_view()), name='ra-create'),
     path('RA/<int:parent_analysis>/RI/create', login_required(views.RiskInstanceCreateView.as_view()), name='ri-create'),
 
@@ -24,13 +27,18 @@ urlpatterns = [
     
     path('users/create', login_required(views.UserCreateView.as_view()), name='user-create'),
     
-    path('RI/create-modal', login_required(views.RiskInstanceCreateViewModal.as_view()), name='ri-create-modal'),
+    # path('RI/create-modal', login_required(views.RiskInstanceCreateViewModal.as_view()), name='ri-create-modal'),
     
+    # UPDATE VIEWS
     path('RA/<int:pk>', login_required(views.RiskAnalysisUpdateView.as_view()), name='ra-update'),
     path('RI/<int:pk>', login_required(views.RiskInstanceUpdateView.as_view()), name='ri-update'),
     path('MTG/<int:pk>', login_required(views.MitigationUpdateView.as_view()), name='mtg-update'),
+
     path('PD/<int:pk>', login_required(views.ProjectsGroupUpdateView.as_view()), name='pd-update'),
     path('PRJ/<int:pk>', login_required(views.ProjectUpdateView.as_view()), name='project-update'),
     
+    # DELETE VIEWS
     path('RA/<int:pk>/delete/', login_required(views.RiskAnalysisDeleteView.as_view()), name='ra-delete'),
+    path('PRJ/<int:pk>/delete/', login_required(views.ProjectDeleteView.as_view()), name='project-delete'),
+    path('D/<int:pk>/delete/', login_required(views.ProjectsGroupDeleteView.as_view()), name='pd-delete'),
 ]
