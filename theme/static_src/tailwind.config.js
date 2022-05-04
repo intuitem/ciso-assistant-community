8e0028a5cafd120048147dd3be8e18b7c8c1ad6c
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2022-05-04 17:09:17 +0200

    chore: clean custom colors in tailwind.config.js

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -50,40 +50,18 @@ module.exports = {
     ],
     theme: {
         extend: {
+            colors: {
+                // Now we build the full color palette, using all colors available
+                // as shown at this link: https://tailwindcss.com/docs/customizing-colors#color-palette-reference
+                transparent: "transparent",
+                current: 'currentColor',
+                black: "#000",
+                white: "#fff",
+            },
             screens: {
                 'print': { 'raw': 'print' },
                 // => @media print { ... }
             }
-        },
-        colors: {
-            // Now we build the full color palette, using all colors available
-            // as shown at this link: https://tailwindcss.com/docs/customizing-colors#color-palette-reference
-            transparent: "transparent",
-            current: 'currentColor',
-            black: "#000",
-            white: "#fff",
-            bluegray: colors.slate,
-            coolgray: colors.gray,
-            gray: colors.gray,
-            truegray: colors.neutral,
-            warmgray: colors.stone,
-            red: colors.red,
-            orange: colors.orange,
-            amber: colors.amber,
-            yellow: colors.yellow,
-            lime: colors.lime,
-            green: colors.green,
-            emerald: colors.emerald,
-            teal: colors.teal,
-            cyan: colors.cyan,
-            sky: colors.sky,
-            blue: colors.blue,
-            indigo: colors.indigo,
-            violet: colors.violet,
-            purple: colors.purple,
-            fuchsia: colors.fuchsia,
-            pink: colors.pink,
-            rose: colors.rose,
         },
     },
     variants: {
