f2dc485c1ac527aec6139172b2eb9c94f878cc30
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-05-18 22:01:57 +0200

    Analyses List Filters POC fully functional

--- back_office/filters.py
+++ back_office/filters.py
@@ -3,6 +3,11 @@ from django_filters import *
 from core.models import Analysis
 
 class AnalysisFilter(FilterSet):
+    STATUS_CHOICES = (
+        (True, 'Yes'),
+        (False, 'No'),
+    )
+    is_draft = ChoiceFilter(choices=STATUS_CHOICES)
     class Meta:
         model = Analysis
         fields = ['is_draft', 'auditor', 'project']
