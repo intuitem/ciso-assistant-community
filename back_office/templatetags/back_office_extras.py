329fa52161327a2efb098443fbb50b907d32d09e
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-06-30 11:12:57 +0200

    WIP

--- back_office/templatetags/back_office_extras.py
+++ back_office/templatetags/back_office_extras.py
@@ -1,6 +1,12 @@
 from django import template
 
+from asf_rm.settings import VERSION
+
 register = template.Library()
+
+@register.simple_tag()
+def mira_version():
+    return VERSION
 
 @register.filter('class')
 def _class(obj):
