3d6b994cf4e984c103706815ad80ecd0bf2fd905
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-07-01 13:24:51 +0200

    Get allowed hosts from environment if set

--- asf_rm/settings.py
+++ asf_rm/settings.py
@@ -31,7 +31,10 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+if 'DJANGO_ALLOWED_HOSTS' in os.environ:
+    ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS'].split(',')
+else:
+    ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 # ASF RM settings
 
