1e59a3bae3879b309f5ceb4002eba78c9a227dcb
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2022-07-21 11:04:01 +0200

    feat: create installation test

---
+++ asfTest/test/test_installation.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+from playwright.sync_api import *
+from playwright.async_api import *
+from playwright import *
+import time
+import urlpatterns
+
+def test_ASF002(page):
+    # Test case: installation
+	# Step # | action | expected
+	step = 0
+	# 1 | Go to the url | Go on login page
+	step = 1
+	def log_response(intercepted_response):
+		print("a response was received:", intercepted_response.status, intercepted_response.status_text)
+		assert intercepted_response.status != 500 and intercepted_response.status != 404, "Step "+str(step)+": not Ok"
+	page.on("response", log_response)
+	page.goto(urlpatterns.url)
+	message = page.locator('id=hellothere')
+	assert message.is_visible() == True, "Step 1: not Ok"
+	# 2 | Enter an admin username and a password, then click on "Login" | Open home page
+	step = 2
+	page.fill("id=id_username", "root")
+	page.fill("id=id_password", "root")
+	page.click('id=login')
+	assert page.url == urlpatterns.url, "Step "+str(step)+": not Ok"
