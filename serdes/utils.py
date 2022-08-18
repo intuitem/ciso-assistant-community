d854e529590d94b12b5a7bb46f4471d0d7f137e4
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-08-18 09:41:13 +0200

    Create serializer-deserializer
    Create basic serializer-deserializer functions

---
+++ serdes/utils.py
@@ -0,0 +1,44 @@
+from django.core import serializers
+
+def serialize_objects(objects, path, format="json"):
+    '''
+    Serialize objects to a file.
+
+    Parameters:
+    -----------
+    objects: list
+        List of objects to serialize.
+    path: str
+        Path to file to serialize to.
+    format: str
+        Format to serialize to. e.g. json, yaml, xml.
+
+    Returns:
+    --------
+    path: str
+        Path to file to serialize to.
+    '''
+    with open(path, 'w') as outfile:
+        outfile.write(serializers.serialize(format, objects))
+    return path
+
+def deserialize_objects(path, format="json"):
+    '''
+    Deserialize objects from a file.
+
+    Parameters:
+    -----------
+    path: str
+        Path to file to deserialize from.
+    format: str
+        Format to deserialize from. e.g. json, yaml, xml.
+
+    Returns:
+    --------
+    path: str
+        Path to file to deserialize from.
+    '''
+    with open(path, 'r') as infile:
+        for object in serializers.deserialize(format, infile):
+            object.save()
+    return path
