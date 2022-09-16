9966ab6a21627a58a02e6947d4c2efd6879b3acd
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-09-16 18:21:21 +0200

    Remove references to former models

--- general/apps.py
+++ general/apps.py
@@ -7,8 +7,10 @@ def startup():
     import os
     if os.environ.get('RUN_MAIN'):
         from .models import Folder
-        from back_office.models import Role, UserGroup, RoleAssignment
-        from django.contrib.auth.models import Permission, User
+        from back_office.models import RoleAssignment
+        from iam.models import Group, Role
+        from django.contrib.auth.models import Permission
+        from iam.models import User
 
         auditor_permissions = Permission.objects.filter(codename__in=[
             "view_project",
@@ -143,8 +145,8 @@ def startup():
             domain_manager.permissions.set(domain_manager_permissions)
             administrator = Role.objects.create(name="BI-RL-ADM", builtin=True)
             administrator.permissions.set(administrator_permissions)
-        if not UserGroup.objects.filter(name="BI-UG-ADM", folder=Folder.objects.get(content_type=Folder.ContentType.ROOT)).exists():
-            administrators = UserGroup.objects.create(
+        if not Group.objects.filter(name="BI-UG-ADM", folder=Folder.objects.get(content_type=Folder.ContentType.ROOT)).exists():
+            administrators = Group.objects.create(
                 name="BI-UG-ADM", folder=Folder.objects.get(content_type=Folder.ContentType.ROOT), builtin=True)
             ra1 = RoleAssignment.objects.create(
                 user_group=administrators, role=Role.objects.get(name="BI-RL-ADM"), builtin=True,
@@ -152,8 +154,8 @@ def startup():
             ra1.perimeter_folders.add(administrators.folder)
             for superuser in User.objects.filter(is_superuser=True):
                 administrators.user_set.add(superuser)
-        if not UserGroup.objects.filter(name="BI-UG-GAD", folder=Folder.objects.get(content_type=Folder.ContentType.ROOT)).exists():
-            global_auditors = UserGroup.objects.create(name="BI-UG-GAD", folder=Folder.objects.get(
+        if not Group.objects.filter(name="BI-UG-GAD", folder=Folder.objects.get(content_type=Folder.ContentType.ROOT)).exists():
+            global_auditors = Group.objects.create(name="BI-UG-GAD", folder=Folder.objects.get(
                 content_type=Folder.ContentType.ROOT), builtin=True)
             ra2 = RoleAssignment.objects.create(user_group=global_auditors, role=Role.objects.get(
                 name="BI-RL-AUD"), is_recursive=True, builtin=True,
