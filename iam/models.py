d13ef711249c4a25303bfa4d49329bf5ab7f2ddb
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-09-16 18:21:06 +0200

    Create User model

--- iam/models.py
+++ iam/models.py
@@ -1,14 +1,12 @@
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Permission, UserManager
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 
 from general.utils import *
-
-
-class User(AbstractBaseUser):
-    pass
 
 
 class AbstractGroup(models.Model):
@@ -65,3 +63,97 @@ class Role(AbstractGroup):
             return f"{BUILTIN_ROLE_CODENAMES.get(self.name)}"
         return self.name
         
+
+class PermissionsMixin(models.Model):
+    # is_superuser is not used in this project, it was kept for simplicity's
+    # sake and to avoid having to rewrite the UserManager. It will be removed.
+    is_superuser = models.BooleanField(
+        _('superuser status'),
+        default=False,
+        help_text=_(
+            'Designates that this user has all permissions without '
+            'explicitly assigning them.'
+        ),
+    )
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        help_text=_(
+            'The groups this user belongs to. A user will get all permissions '
+            'granted to each of their groups.'
+        ),
+        related_name="user_set",
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        related_name="user_set",
+        related_query_name="user",
+    )
+
+    class Meta:
+        abstract = True
+
+        
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+    )
+    first_name = models.CharField(_('first name'), max_length=150, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    email = models.EmailField(_('email address'), blank=True, unique=True)
+    # is_staff won't be used, but is required by Django's UserManager()
+    # We might ditch the UserManager() and use our own, but for now, we'll
+    # just set is_staff to False.
+    is_staff = models.BooleanField(
+        default=False,
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+
+    # USERNAME_FIELD is used as the unique identifier for the user
+    # and is required by Django to be set to a non-empty value.
+    # See https://docs.djangoproject.com/en/3.2/topics/auth/customizing/#django.contrib.auth.models.CustomUser.USERNAME_FIELD
+    USERNAME_FIELD = 'username'
+    # REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        swappable = 'AUTH_USER_MODEL'
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        full_name = f'{self.first_name} {self.last_name}'
+        return full_name.strip()
+
+    def get_short_name(self):
+        return self.first_name
+
+    def get_username(self) -> str:
+        return self.username
