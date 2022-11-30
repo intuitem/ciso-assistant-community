76deed14d85f78ebf7a501e31a706cf9ae9e14b5
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-11-30 20:39:17 +0100

    Moved tests to appropriate location

--- cal/models_test.py
+++
@@ -1,9 +0,0 @@
-from .models import *
-import pytest
-
-@pytest.mark.django_db
-def test_event():
-    Event.objects.create(name="Event", description="A simple event", start_time="2022-1-20", end_time="2022-1-27")
-    event = Event.objects.get(name="Event")
-    assert event.get_html_url == '<a class="bg-blue-100" href=""> Event </a>'
-    
