13bacc073b4c7d646f1ec87f8d992706fafc4084
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-12-01 13:37:20 +0100

    Replace 'package' with 'library'

--- library/urls.py
+++ library/urls.py
@@ -4,8 +4,6 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    path('packages/', login_required(views.PackageListView.as_view()), name='package-list'),
-    path('PKG/<str:package>/', login_required(views.PackageDetailView.as_view()), name='package-detail'),
-
-    path('import_default_package/<str:package_name>', views.import_default_package, name='import-default-package'),
+    path('libraries/', login_required(views.LibraryListView.as_view()), name='library-list'),
+    path('PKG/<str:library>/', login_required(views.LibraryDetailView.as_view()), name='library-detail'),
 ]
--- library/views.py
+++ library/views.py
@@ -12,18 +12,18 @@ from django.urls import reverse_lazy
 from django.shortcuts import redirect
 from django.utils.translation import gettext_lazy as _
 
-class PackageListView(FormView):
-    template_name = 'library/package_list.html'
+class LibraryListView(FormView):
+    template_name = 'library/libarry_list.html'
     form_class = UploadFileForm
-    success_url = reverse_lazy('package-list')
+    success_url = reverse_lazy('library-list')
 
     def get_queryset(self):
-        qs = get_available_packages()
+        qs = get_available_libraries()
         return qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['packages'] = self.get_queryset()
+        context['libraries'] = self.get_queryset()
         context['change_usergroup'] = RoleAssignment.has_permission(self.request.user, "change_usergroup")
         context['view_user'] = RoleAssignment.has_permission(self.request.user, "view_user")
         context['form'] = UploadFileForm()
@@ -35,45 +35,37 @@ class PackageListView(FormView):
         files = request.FILES.getlist('file')
         if form.is_valid():
             for f in files:
-                package = json.load(f)
-                import_package_view(request, package)
+                library = json.load(f)
+                import_library_view(request, library)
             return self.form_valid(form)
         else:
             messages.error(request, f'Invalid form.')
             return self.form_invalid(form)
 
 
-class PackageDetailView(TemplateView):
-    template_name = 'library/package_detail.html'
+class LibraryDetailView(TemplateView):
+    template_name = 'library/library_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        package = get_package(kwargs['package'])
+        library = get_library(kwargs['library'])
         context['change_usergroup'] = RoleAssignment.has_permission(self.request.user, "change_usergroup")
         context['view_user'] = RoleAssignment.has_permission(self.request.user, "view_user")
-        context['package'] = package
-        context['types'] = self.get_object_types(package)
-        context['matrices'] = self.get_matrices(package)
-        context['crumbs'] = {'package-list': _('Packages')}
+        context['library'] = library
+        context['types'] = self.get_object_types(library)
+        context['matrices'] = self.get_matrices(library)
+        context['crumbs'] = {'library-list': _('Libraries')}
         return context
 
-    def get_object_types(self, package):
+    def get_object_types(self, library):
         types = set()
-        for obj in package.get('objects'):
+        for obj in library.get('objects'):
             types.add(obj['type'])
         return types
 
-    def get_matrices(self, package):
+    def get_matrices(self, library):
         matrices = []
-        for obj in package.get('objects'):
+        for obj in library.get('objects'):
             if obj['type'] == 'matrix':
                 matrices.append(obj)
         return matrices
-
-def import_default_package(request, package_name):
-    try:
-        package = get_package(package_name)
-        import_package_view(request, package)
-    except:
-        return redirect("package-list")
-    return redirect("package-list")
