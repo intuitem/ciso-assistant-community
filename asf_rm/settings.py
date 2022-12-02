20f5bb166adf73dda09ddb999c6175c2b4ac6670
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2022-12-02 18:09:44 +0100

    Remove fallback value for DJANGO_DEBUG

--- asf_rm/settings.py
+++ asf_rm/settings.py
@@ -29,7 +29,7 @@ with open(BASE_DIR / 'asf_rm/VERSION') as f:
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DJANGO_DEBUG') == 'True'
 
 if 'DJANGO_ALLOWED_HOSTS' in os.environ:
     ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS'].split(',')
