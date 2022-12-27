e90fd5e8f8e0096535c394e986d7d95958503739
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2022-12-27 12:28:05 +0100

    chore: improve assertions style in test_installation.py

--- asfTest/test/test_installation.py
+++ asfTest/test/test_installation.py
@@ -2,7 +2,6 @@
 from playwright.sync_api import *
 from playwright.async_api import *
 from playwright import *
-import time
 import urlpatterns
 
 def test_ASF002(page):
@@ -13,11 +12,11 @@ def test_ASF002(page):
 	step = 1
 	def log_response(intercepted_response):
 		print("a response was received:", intercepted_response.status, intercepted_response.status_text)
-		assert intercepted_response.status != 500 and intercepted_response.status != 404, "Step "+str(step)+": not Ok"
+		assert intercepted_response.status not in (500, 404), "Step "+str(step)+": not Ok"
 	page.on("response", log_response)
 	page.goto(urlpatterns.url)
 	message = page.locator('id=hellothere')
-	assert message.is_visible() == True, "Step 1: not Ok"
+	assert message.is_visible() is True, "Step 1: not Ok"
 	# 2 | Enter an admin username and a password, then click on "Login" | Open home page
 	step = 2
 	page.fill("id=id_username", "root@gmail.com")
