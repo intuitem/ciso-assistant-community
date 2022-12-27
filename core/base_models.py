bc6093128ea362decdc28c7a26fef0fc8d320829
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2022-12-27 16:16:36 +0100

    update: add unique field into AbstractBaseModel name

--- core/base_models.py
+++ core/base_models.py
@@ -5,7 +5,7 @@ import uuid
 
 class AbstractBaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=200, verbose_name=_('Name'))
+    name = models.CharField(max_length=200, verbose_name=_('Name'), unique=True)
     description = models.TextField(verbose_name=_('Description'), blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))
 
