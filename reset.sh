6a04aed20558d3abb7aec9c650b8026d7a061d5a
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2023-01-24 00:48:41 +0100

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,11 +1,13 @@
-psql -c "drop database mira;" nassim
-psql -c "create database mira;" nassim
-psql -c "grant all privileges on database mira to mirauser;" nassim
+psql -c "drop database mira;" $POSTGRES_DBLOGIN
+psql -c "create database mira;" $POSTGRES_DBLOGIN
+psql -c "grant all privileges on database mira to mirauser;" $POSTGRES_DBLOGIN
 
 find . -path "*/migrations/*.py" -not -name "__init__.py" -and -not -path "./venv/*" -delete
 find . -path "*/migrations/*.pyc"  -delete
 python manage.py makemigrations
 python manage.py migrate
-python3 manage.py createsuperuser --email root@example.com
-python manage.py runserver
+python manage.py makemessages -i venv -l fr
+python manage.py compilemessages -i venv -l fr
+python manage.py createsuperuser --email root@example.com
+echo python manage.py runserver
 
