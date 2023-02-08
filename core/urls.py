da5165ead1ac6b83e4be3bfcf4a7d1efcbf15485
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2023-02-08 15:39:01 +0100

    feat: password recovery is now working locally

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('password_reset', views.password_reset_request, name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
     path('analyses-registry', login_required(views.AnalysisListView.as_view()), name='analysis_list'),
     path('i18n/', include('django.conf.urls.i18n')),
 
--- core/views.py
+++ core/views.py
@@ -48,6 +48,7 @@ from django.contrib import messages
 from core.utils import RoleCodename, UserGroupCodename
 from iam.models import UserGroup, Role, RoleAssignment
 from django.contrib.auth.views import PasswordChangeView
+from django.views.generic.edit import FormView
 from core.models import Analysis, RiskScenario, SecurityMeasure, RiskAcceptance
 from iam.forms import *
 from core.forms import *
@@ -85,28 +86,27 @@ def password_reset_request(request):
     if request.method == "POST":
         password_reset_form = ResetForm(request.POST)
         if password_reset_form.is_valid():
-            return redirect ("/password_reset/done/")
             data = password_reset_form.cleaned_data['email']
-            associated_users = User.objects.filter(Q(email=data))
-            if associated_users.exists():
-                for user in associated_users:
-                    subject = "Password Reset Requested"
-                    email_template_name = "registration/password_reset_email.txt"
-                    c = {
-                    "email":user.email,
+            associated_users = User.objects.filter(email=data)
+            if associated_users and associated_users.exists():
+                associated_user = associated_users[0]
+                subject = "Password Reset Requested"
+                email_template_name = "registration/password_reset_email.txt"
+                header = {
+                    "email":associated_user.email,
                     'domain':'127.0.0.1:8000',
                     'site_name': 'Website',
-                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
-                    "user": user,
-                    'token': default_token_generator.make_token(user),
+                    "uid": urlsafe_base64_encode(force_bytes(associated_user.pk)),
+                    "user": associated_user,
+                    'token': default_token_generator.make_token(associated_user),
                     'protocol': 'http',
-                    }
-                    email = render_to_string(email_template_name, c)
-                    try:
-                        send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
-                    except BadHeaderError:
-                        return HttpResponse('Invalid header found.')
-                    return redirect ("/password_reset/done/")
+                }
+                email = render_to_string(email_template_name, header)
+                try:
+                    send_mail(subject, email, 'admin@example.com' , [associated_user.email], fail_silently=False)
+                except BadHeaderError:
+                    return HttpResponse('Invalid header found.')
+                return redirect ("/password_reset/done/")
     password_reset_form = ResetForm()
     return render(request=request, template_name="registration/password_reset.html", context={"password_reset_form":password_reset_form})
 
