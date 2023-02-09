286460849b9485be94de7b0ab8e3a8e3acc52ba2
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2023-02-09 12:59:36 +0100

    feat: make Mailhog smtp server works

--- asf_rm/settings.py
+++ asf_rm/settings.py
@@ -81,7 +81,12 @@ ROOT_URLCONF = 'asf_rm.urls'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_PORT = os.environ['EMAIL_PORT']
+# EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+# EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+# EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
+
 
 TEMPLATES = [
     {
