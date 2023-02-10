e5356316c203096599013708a92ee49a1035fd4e
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2023-02-10 15:14:18 +0100

    feat: send magic link when we create a user

--- iam/forms.py
+++ iam/forms.py
@@ -65,18 +65,7 @@ class UserCreationForm(forms.ModelForm):
         'password_mismatch': _('The two password fields didn’t match.'),
     }
     email = forms.EmailField(max_length=100)
-    password1 = forms.CharField(
-        label=_("Password"),
-        strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-        help_text=password_validation.password_validators_help_text_html(),
-    )
-    password2 = forms.CharField(
-        label=_("Password confirmation"),
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-        strip=False,
-        help_text=_("Enter the same password as before, for verification."),
-    )
+    
 
     class Meta:
         model = User
@@ -114,7 +103,7 @@ class UserCreationForm(forms.ModelForm):
 
 class UserCreateForm(UserCreationForm, StyledModelForm):
     """ form to create user """
-    pass
+    
 
 
 class UserUpdateForm(UserChangeForm, StyledModelForm):
