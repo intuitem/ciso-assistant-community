d04e190de38af386eadb8a72c756f2470ea86bf8
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2023-02-19 19:55:05 +0100

    generate django secret key on first use
    when not given as a env parameter, the key is generated automatically on a (hopefully) persistent volume.
    Adapt k8s config accordingly
    Put a warning in create_secrets.py

--- startup.sh
+++ startup.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 # wait for database to be ready
+
+if [ ! -n "$DJANGO_SECRET_KEY" ]; then
+    if [ ! -f /code/db/django_secret_key ]; then
+        cat /proc/sys/kernel/random/uuid > db/django_secret_key
+        echo "generating initial Django secret key"
+    fi
+    export DJANGO_SECRET_KEY=$(<db/django_secret_key)
+    echo "Django secret key read from file"
+fi
+
 while ! python manage.py showmigrations iam > /dev/null 2>&1; do 
     echo "database not ready; waiting"
     sleep 10
@@ -12,4 +22,5 @@ python manage.py compilemessages -i venv -l fr
 if [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
     python manage.py createsuperuser --noinput
 fi
+
 exec gunicorn --chdir asf_rm --bind :8000 --env RUN_MAIN=true asf_rm.wsgi:application
