6d4c7a9ca682897c7b289c9a51c5771fa896aee1
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2023-02-20 23:55:56 +0100

    WIP for cluster controller

---
+++ k8s/mira-cluster-controller/mira-cluster-controller.py
@@ -0,0 +1,1 @@
+from kubernetes import client, config
---
+++ k8s/mira-cluster-controller/startup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# wait for database to be ready
+
+if [ ! -n "$DJANGO_SECRET_KEY" ]; then
+    if [ ! -f /code/db/django_secret_key ]; then
+        cat /proc/sys/kernel/random/uuid > db/django_secret_key
+        echo "generating initial Django secret key"
+    fi
+    export DJANGO_SECRET_KEY=$(<db/django_secret_key)
+    echo "Django secret key read from file"
+fi
+
+while ! python manage.py showmigrations iam > /dev/null 2>&1; do 
+    echo "database not ready; waiting"
+    sleep 10
+done
+
+python manage.py makemigrations cal core iam
+python manage.py migrate
+if [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
+    python manage.py createsuperuser --noinput
+fi
+
+exec gunicorn --chdir mira-cluster-controller  --bind :8000 --env RUN_MAIN=true mira_cluster_controller.wsgi:application
