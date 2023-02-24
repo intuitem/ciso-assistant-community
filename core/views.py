24f8866301c50fba350c3ea31763f0095c06d7ce
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2023-02-24 13:34:32 +0100

    feat: improve mail backend failure message

--- core/views.py
+++ core/views.py
@@ -126,8 +126,10 @@ def password_reset_request(request):
                 email = render_to_string(email_template_name, header)
                 try:
                     send_mail(subject, email, None, [associated_user.email], fail_silently=False)
-                except BadHeaderError:
-                    return HttpResponse('Invalid header found.')
+                except:
+                    messages.error(request, 'An error has occured, please try later.')
+                    password_reset_form = ResetForm()
+                    return render(request=request, template_name="registration/password_reset.html", context={"password_reset_form":password_reset_form})
             return redirect ("/password_reset/done/")
         else:
             messages.error(request, "An invalid email has been entered.")
