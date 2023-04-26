1719aff52aaf7e4d7eb3d69ff666e5e478ee7029
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2023-04-26 19:31:40 +0200

    Now it works

--- core/constants.py
+++ core/constants.py
@@ -2,7 +2,9 @@ from django.db import connection
 from iam.models import Folder
 
 
-def ROOT_FOLDER(): return None if not connection.introspection.table_names() else (
+def _root_folder(): return None if not connection.introspection.table_names() else (
     Folder.objects.get(content_type=Folder.ContentType.ROOT) if Folder.objects.filter(
         content_type=Folder.ContentType.ROOT).exists() else None
 )
+
+ROOT_FOLDER = _root_folder()
