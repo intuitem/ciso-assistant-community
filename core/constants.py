e77e1fc00c4511d14f2b9b45568e19dbc83a65ec
Nassim Tabchiche <nassim.tabchiche@intuitem.com>
2023-04-26 19:16:17 +0200

    Replace get with get_object_or_404 to avoid DoesNotExist

--- core/constants.py
+++ core/constants.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from iam.models import Folder
 
-ROOT_FOLDER = Folder.objects.get(content_type=Folder.ContentType.ROOT)
+ROOT_FOLDER = get_object_or_404(Folder, content_type=Folder.ContentType.ROOT)
