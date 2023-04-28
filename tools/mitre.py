4377a66e6e6a960eafbd8578a3d23dec02f0991e
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2023-04-28 12:10:04 +0200

    Add French translation for Mitre Att&ck
    Using Excel -> word -> Excel for automatic translation, with some manual cleaning

--- tools/mitre.py
+++ tools/mitre.py
@@ -1,5 +1,6 @@
 from mitreattack.stix20 import MitreAttackData
 import json
+import csv
 
 MITRE_COPYRIGHT="""
 Terms of Use
@@ -22,21 +23,41 @@ def main():
     mylib['description'] = "Mitigations from MITRE ATT&CK 2.1"
     mylib["format_version"] = "1.0"
     mylib["copyright"] = MITRE_COPYRIGHT
+    mylib["locale"] = "en"
     mylib["objects"] = []
     for m in mitigations:
         name = m.name
         id = m.external_references[0].external_id
         url = m.external_references[0].url
-        description = m.description
+        description = m.description.strip(" \n")
         print(id, name)
         secfunc = {"type": "security_function", "fields": {
             "name": f"{id} - {name}", 
             "description": description + "\n" + url + "\n", 
             "provider": "MITRE ATT&CK"}}
         mylib["objects"].append(secfunc)
-
     with open("mitre-mitigations.json", "w") as f:
-        json.dump(mylib, f)
+        json.dump(mylib, f, indent=2)
+    with open('measures.csv', 'w', newline='', encoding='utf8') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for m in mitigations:
+            writer.writerow([m.external_references[0].external_id, m.name, m.description.strip(" \n"), m.external_references[0].url])
+    with open('measures_fr.csv', 'r', encoding='utf8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+        mylib['name'] = "MITRE ATT&CK 2.1 - Fonctions de sécurité"
+        mylib['description'] = "Fonctions de sécurité depuis MITRE ATT&CK 2.1"
+        mylib["locale"] = "fr"
+        mylib["objects"] = []
+        for (id, name, description, url) in reader:
+            print(id, name)
+            secfunc = {"type": "security_function", "fields": {
+                "name": f"{id} - {name}", 
+                "description": description + "\n" + url + "\n", 
+                "provider": "MITRE ATT&CK"}}
+            mylib["objects"].append(secfunc)
+        with open("mitre-mitigations-fr.json", "w") as f:
+            json.dump(mylib, f, indent=2)
+
 
     techniques = mitre_attack_data.get_techniques(remove_revoked_deprecated=True)
 
@@ -53,16 +74,34 @@ def main():
         name = t.name
         id = t.external_references[0].external_id
         url = t.external_references[0].url
-        description = t.description
+        description = t.description.strip(" \n")
         print(id, name)
         threat = {"type": "threat", "fields": {
             "name": f"{id} - {name}", 
             "description": description + "\n" + url + "\n",
             "provider": "MITRE ATT&CK"}}
         mylib["objects"].append(threat)
-
     with open("mitre-techniques.json", "w") as f:
-        json.dump(mylib, f)
+        json.dump(mylib, f, indent=2)
+    with open('techniques.csv', 'w', newline='', encoding='utf8') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for t in main_techniques:
+            writer.writerow([t.external_references[0].external_id, t.name, t.description.strip(" \n"), t.external_references[0].url])
+    with open('techniques_fr.csv', 'r', encoding='utf8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+        mylib['name'] = "MITRE ATT&CK 2.1 - Menaces"
+        mylib['description'] = "Menaces depuis MITRE ATT&CK 2.1"
+        mylib["locale"] = "fr"
+        mylib["objects"] = []
+        for (id, name, description, url) in reader:
+            print(id, name)
+            secfunc = {"type": "threat", "fields": {
+                "name": f"{id} - {name}", 
+                "description": description + "\n" + url + "\n", 
+                "provider": "MITRE ATT&CK"}}
+            mylib["objects"].append(secfunc)
+        with open("mitre-techniques-fr.json", "w") as f:
+            json.dump(mylib, f, indent=2)
 
 
 if __name__ == "__main__":
