2e34599b0b214738fd73201dcd2b3ddfc893dfc1
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2023-10-22 02:33:23 +0200

    Add docker-compose configuration with pg database

---
+++ docker-compose-pg.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+if [ -d db/data ] ; then
+    echo "the database seems already created"
+    echo "you should launch docker-compose up -d"
+else
+    POSTGRES_PASSWORD=`uuidgen` docker-compose up -d
+    echo "initialize your superuser account..."
+    docker-compose exec ciso-assistant python manage.py createsuperuser
+    echo "for successive runs you can now use docker compose up"
+fi
