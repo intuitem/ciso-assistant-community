c393383b16dc6384bc5aa43e02605463949df505
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2023-10-23 00:48:50 +0200

    manage pg password using docker secrets
    - manage pg password using docker secrets
    - add POSTGRES_PASSWORD_FILE variable support
    - update doc
    - remove duplicate print

--- docker-compose-pg.sh
+++ docker-compose-pg.sh
@@ -4,7 +4,8 @@ if [ -d db/data ] ; then
     echo "the database seems already created"
     echo "you should launch docker-compose up -d"
 else
-    POSTGRES_PASSWORD=`uuidgen` docker-compose up -d
+    uuidgen > ./db/pg_password.txt
+    docker-compose up -d
     echo "initialize your superuser account..."
     docker-compose exec ciso-assistant python manage.py createsuperuser
     echo "for successive runs you can now use docker compose up"
