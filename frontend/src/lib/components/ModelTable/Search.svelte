06efdb43a68b9d33b6c51dc960c26a30dbb9889b
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-02-15 14:17:24 +0100

    feat: end model tables translation

--- frontend/src/lib/components/ModelTable/Search.svelte
+++ frontend/src/lib/components/ModelTable/Search.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import type { DataHandler } from '@vincjo/datatables';
+	import * as m from '$paraglide/messages';
 	export let handler: DataHandler;
 	let value = '';
 </script>
 
 <input
 	class="input bg-surface-50 max-w-2xl"
-	placeholder="Search..."
+	placeholder={m.searchPlaceholder()}
 	bind:value
 	on:input={() => handler.search(value)}
 />
