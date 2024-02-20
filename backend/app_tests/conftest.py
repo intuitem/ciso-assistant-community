63086d122805edf9fc8adaab09e7ab6b7a5f85f3
Alexis <alexis.bensoussan@intuitem.com>
2024-02-20 16:51:25 +0100

    first test of auditor permissions

--- backend/app_tests/conftest.py
+++ backend/app_tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from rest_framework.test import APIClient
-from iam.models import User, UserGroup
+from iam.models import Folder, User, UserGroup
 from core.apps import startup
 
 
@@ -23,4 +23,14 @@ def authenticated_client(app_config):
     UserGroup.objects.get(name="BI-UG-ADM").user_set.add(admin)
     client = APIClient()
     client.force_login(admin)
+    return client
+
+
+@pytest.fixture
+def authenticated_client_with_role(app_config, request):
+    """Get an authenticated client with a specific role"""
+    user = User.objects.create_user("user@tests.com")
+    UserGroup.objects.get(name=request.param['role'], folder=Folder.objects.get(name=request.param['folder']).id).user_set.add(user)
+    client = APIClient()
+    client.force_login(user)
     return client
