4a6a8ec75a5f817443b5f696d079ee3f98c84a03
Alexis <alexis.bensoussan@intuitem.com>
2024-02-23 12:17:57 +0100

    add security measures api tests for the other user groups

--- backend/app_tests/api/test_api_security_measures.py
+++ backend/app_tests/api/test_api_security_measures.py
@@ -3,6 +3,7 @@ from rest_framework.test import APIClient
 from core.models import SecurityFunction, SecurityMeasure
 from iam.models import Folder
 
+from test_vars import GROUPS_PERMISSIONS
 from test_utils import EndpointTestsQueries
 
 # Generic security measure data for tests
@@ -86,14 +87,15 @@ class TestSecurityMeasuresUnauthenticated:
 
 
 @pytest.mark.django_db
+@pytest.mark.parametrize("test", GROUPS_PERMISSIONS.keys(), ids=[GROUPS_PERMISSIONS[key]["name"] for key in GROUPS_PERMISSIONS.keys()], indirect=True)
 class TestSecurityMeasuresAuthenticated:
     """Perform tests on Security Measures API endpoint with authentication"""
 
-    def test_get_security_measures(self, authenticated_client):
+    def test_get_security_measures(self, test):
         """test to get security measures from the API with authentication"""
 
         EndpointTestsQueries.Auth.get_object(
-            authenticated_client,
+            test.client,
             "Security measures",
             SecurityMeasure,
             {
@@ -104,26 +106,27 @@ class TestSecurityMeasuresAuthenticated:
                 "link": SECURITY_MEASURE_LINK,
                 "eta": SECURITY_MEASURE_ETA,
                 "effort": SECURITY_MEASURE_EFFORT[0],
-                "folder": Folder.get_root_folder(),
+                "folder": test.folder,
             },
             {
-                "folder": {"str": Folder.get_root_folder().name},
+                "folder": {"id": str(test.folder.id), "str": test.folder.name},
                 "security_function": None,
                 "category": SECURITY_MEASURE_CATEGORY[1],
                 "status": SECURITY_MEASURE_STATUS[1],
                 "effort": SECURITY_MEASURE_EFFORT[1],
             },
+            user_group=test.user_group,
         )
 
-    def test_create_security_measures(self, authenticated_client):
+    def test_create_security_measures(self, test):
         """test to create security measures with the API with authentication"""
 
         security_function = SecurityFunction.objects.create(
-            name="test", typical_evidence={}, folder=Folder.objects.create(name="test")
+            name="test", typical_evidence={}, folder=Folder.objects.create(name="test2")
         )
 
         EndpointTestsQueries.Auth.create_object(
-            authenticated_client,
+            test.client,
             "Security measures",
             SecurityMeasure,
             {
@@ -134,26 +137,27 @@ class TestSecurityMeasuresAuthenticated:
                 "link": SECURITY_MEASURE_LINK,
                 "eta": SECURITY_MEASURE_ETA,
                 "effort": SECURITY_MEASURE_EFFORT[0],
-                "folder": str(Folder.get_root_folder().id),
+                "folder": str(test.folder.id),
             },
             {
-                "folder": {"str": Folder.get_root_folder().name},
+                "folder": {"id": str(test.folder.id), "str": test.folder.name},
                 "category": SECURITY_MEASURE_CATEGORY[1],
                 "status": SECURITY_MEASURE_STATUS[1],
                 "effort": SECURITY_MEASURE_EFFORT[1],
             },
+            user_group=test.user_group,
         )
 
-    def test_update_security_measures(self, authenticated_client):
+    def test_update_security_measures(self, test):
         """test to update security measures with the API with authentication"""
 
-        folder = Folder.objects.create(name="test")
+        folder = Folder.objects.create(name="test2")
         security_function = SecurityFunction.objects.create(
             name="test", typical_evidence={}, folder=folder
         )
 
         EndpointTestsQueries.Auth.update_object(
-            authenticated_client,
+            test.client,
             "Security measures",
             SecurityMeasure,
             {
@@ -164,7 +168,7 @@ class TestSecurityMeasuresAuthenticated:
                 "link": SECURITY_MEASURE_LINK,
                 "eta": SECURITY_MEASURE_ETA,
                 "effort": SECURITY_MEASURE_EFFORT[0],
-                "folder": Folder.get_root_folder(),
+                "folder": test.folder,
             },
             {
                 "name": "new " + SECURITY_MEASURE_NAME,
@@ -177,49 +181,57 @@ class TestSecurityMeasuresAuthenticated:
                 "folder": str(folder.id),
             },
             {
-                "folder": {"str": Folder.get_root_folder().name},
+                "folder": {"id": str(test.folder.id), "str": test.folder.name},
                 "category": SECURITY_MEASURE_CATEGORY[1],
                 "status": SECURITY_MEASURE_STATUS[1],
                 "effort": SECURITY_MEASURE_EFFORT[1],
             },
+            user_group=test.user_group,
         )
 
-    def test_delete_security_measures(self, authenticated_client):
+    def test_delete_security_measures(self, test):
         """test to delete security measures with the API with authentication"""
 
         EndpointTestsQueries.Auth.delete_object(
-            authenticated_client,
+            test.client,
             "Security measures",
             SecurityMeasure,
             {
                 "name": SECURITY_MEASURE_NAME,
-                "folder": Folder.objects.create(name="test"),
+                "folder": test.folder,
             },
+            user_group=test.user_group,
         )
 
-    def test_get_effort_choices(self, authenticated_client):
+    def test_get_effort_choices(self, test):
         """test to get security measures effort choices from the API with authentication"""
 
         EndpointTestsQueries.Auth.get_object_options(
-            authenticated_client, "Security measures", "effort", SecurityMeasure.EFFORT
+            test.client, 
+            "Security measures", 
+            "effort", 
+            SecurityMeasure.EFFORT, 
+            user_group=test.user_group
         )
 
-    def test_get_status_choices(self, authenticated_client):
+    def test_get_status_choices(self, test):
         """test to get security measures status choices from the API with authentication"""
 
         EndpointTestsQueries.Auth.get_object_options(
-            authenticated_client,
+            test.client,
             "Security measures",
             "status",
             SecurityMeasure.Status.choices,
+            user_group=test.user_group,
         )
 
-    def test_get_type_choices(self, authenticated_client):
+    def test_get_type_choices(self, test):
         """test to get security measures type choices from the API with authentication"""
 
         EndpointTestsQueries.Auth.get_object_options(
-            authenticated_client,
+            test.client,
             "Security measures",
             "category",
             SecurityMeasure.CATEGORY,
+            user_group=test.user_group,
         )
