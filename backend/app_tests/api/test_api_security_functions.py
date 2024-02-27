1a8ee4807ac2e2c16e4c613bbf1704cc0a5bfe88
Alexis <alexis.bensoussan@intuitem.com>
2024-02-27 17:32:41 +0100

    replace expected http error 400 to 403 in threats and security functions

--- backend/app_tests/api/test_api_security_functions.py
+++ backend/app_tests/api/test_api_security_functions.py
@@ -1,5 +1,5 @@
 import pytest
-from rest_framework import status
+from rest_framework.status import HTTP_403_FORBIDDEN
 from rest_framework.test import APIClient
 from core.models import SecurityFunction
 from iam.models import Folder
@@ -155,7 +155,7 @@ class TestSecurityFunctionsAuthenticated:
                 "folder": {"str": Folder.get_root_folder().name},
             },
             fails=True,
-            expected_status=status.HTTP_400_BAD_REQUEST, # Imported objects cannot be updated
+            expected_status=HTTP_403_FORBIDDEN, # Imported objects cannot be updated
             user_group=test.user_group,
         )
 
