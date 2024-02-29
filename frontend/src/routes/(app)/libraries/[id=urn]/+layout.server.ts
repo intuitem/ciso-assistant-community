8e9e206d8e50e4b385db5d010179d34c59570fad
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-02-29 15:28:55 +0100

    Stream tree promise

--- frontend/src/routes/(app)/libraries/[id=urn]/+layout.server.ts
+++ frontend/src/routes/(app)/libraries/[id=urn]/+layout.server.ts
@@ -5,9 +5,8 @@ import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = async ({ fetch, params }) => {
 	const endpoint = `${BASE_API_URL}/libraries/${params.id}/`;
 
-	const library = await fetch(endpoint).then((res) => res.json());
-	const tree =
-		(await fetch(`${BASE_API_URL}/libraries/${params.id}/tree`).then((res) => res.json())) ?? {};
-
-	return { library, tree };
+	return {
+		tree: fetch(`${BASE_API_URL}/libraries/${params.id}/tree`).then((res) => res.json()) ?? {},
+		library: await fetch(endpoint).then((res) => res.json())
+	};
 };
