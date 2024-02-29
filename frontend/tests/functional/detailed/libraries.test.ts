e7c0b34a422b8e097f71ed3968446fb2b764c403
Alexis <alexis.bensoussan@intuitem.com>
2024-02-29 13:33:28 +0100

    (WIP) add test to delete every libraries

--- frontend/tests/functional/detailed/libraries.test.ts
+++ frontend/tests/functional/detailed/libraries.test.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '../../utils/test-utils.js';
 
+test.describe.configure({ mode: 'serial' });
 test('every libraries can be imported', async ({
 	logedPage,
     librariesPage,
@@ -26,4 +27,53 @@ test('every libraries can be imported', async ({
         }
         expect(previousRemainingLibrary, "An error occured while importing library: " + previousRemainingLibrary).not.toEqual(nextRemainingLibrary);
     }
+});
+
+test('every libraries can be deleted', async ({
+    logedPage,
+    librariesPage,
+    page
+}) => {
+    test.fail(true, "This test is currently failing due to a bug with delete buttons and dependencies. Check JIRA for more information.");
+    test.slow();
+    await librariesPage.goto();
+    await librariesPage.hasUrl();
+    
+    await expect(librariesPage.tab('Imported libraries'), "There is no imported libraries to delete").toBeVisible();
+    if (await librariesPage.tab('Imported libraries').isVisible() && await librariesPage.tab('Imported libraries').getAttribute('aria-selected') === 'false') {
+        await librariesPage.tab('Imported libraries').click();
+        expect(librariesPage.tab('Imported libraries').getAttribute('aria-selected')).toBeTruthy();
+    }
+
+    let previousRemainingLibrary = '';
+    let nextRemainingLibrary = '';
+    let count = 0;
+    do {
+        if (await librariesPage.tab('Imported libraries').isVisible()) {
+            previousRemainingLibrary = nextRemainingLibrary;
+            nextRemainingLibrary = await page.locator('tbody tr td:nth-child(1)').nth(count)?.innerText();
+            expect(previousRemainingLibrary, "An error occured while deleting library: " + previousRemainingLibrary).not.toEqual(nextRemainingLibrary);
+        }
+        else {
+            break;    
+        }
+        
+        if (await librariesPage.deleteItemButton(nextRemainingLibrary).isVisible()) {
+            await librariesPage.deleteItemButton(nextRemainingLibrary).click();
+            await librariesPage.deleteModalConfirmButton.click();
+		    await librariesPage.isToastVisible('Successfully deleted.+', undefined, {
+                timeout: 15000
+            });
+            await expect(librariesPage.getRow(nextRemainingLibrary)).not.toBeVisible();
+            count = 0;
+        }
+        else {
+            count++;
+            continue;
+        }
+        // const toast = page.getByTestId('toast').first();
+		// await toast.getByLabel('Dismiss toast').click();
+		// await expect(toast).toBeHidden();
+        
+    } while (nextRemainingLibrary);
 });
