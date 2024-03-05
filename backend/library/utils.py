aa3b87e6eb7c0de1455d927a6c8011edb2004355
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2024-03-05 20:14:41 +0100

    avoid silk on production branch
    - remove silk stuff
    - add missing migrations

--- backend/library/utils.py
+++ backend/library/utils.py
@@ -18,7 +18,6 @@ from core.models import (
 )
 from django.db import transaction
 from iam.models import Folder
-from silk.profiling.profiler import silk_profile
 
 import structlog
 
@@ -54,7 +53,6 @@ def get_available_library_files():
     return files
 
 
-@silk_profile()
 def get_available_libraries():
     """
     Returns a list of available libraries
