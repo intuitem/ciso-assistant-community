47f85f297fe1bebcb464af3d167dfe72ffcc3694
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-03-06 17:12:47 +0100

    feat: exclude localization_dict from detailed view

--- frontend/src/routes/(app)/[model=urlmodel]/[id=uuid]/+page.svelte
+++ frontend/src/routes/(app)/[model=urlmodel]/[id=uuid]/+page.svelte
@@ -171,7 +171,7 @@
 	{/if}
 	<div class="card px-6 py-4 bg-white flex flex-row space-y-2 justify-between shadow-lg">
 		<div class="flex flex-col space-y-2 whitespace-pre-line">
-			{#each Object.entries(data.data).filter(([key, _]) => !['id', 'is_published'].includes(key)) as [key, value]}
+			{#each Object.entries(data.data).filter(([key, _]) => !['id', 'is_published', 'localization_dict'].includes(key)) as [key, value]}
 				<div class="flex flex-col">
 					<div
 						class="text-sm font-medium text-gray-800"
