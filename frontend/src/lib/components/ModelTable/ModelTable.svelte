d90d7827d75cb54668c592fc01e52cdf9eaa46ae
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2024-03-08 19:33:30 +0100

    avoid deletion of library objects

--- frontend/src/lib/components/ModelTable/ModelTable.svelte
+++ frontend/src/lib/components/ModelTable/ModelTable.svelte
@@ -262,7 +262,7 @@
                 {row}
                 hasBody={$$slots.actionsBody}
                 {identifierField}
-                preventDelete={row.meta.preventDelete ?? false}
+                preventDelete={(row.meta.builtin || row.meta.urn) || (row.meta.preventDelete ?? false)}
               >
                 <svelte:fragment slot="head">
                   {#if $$slots.actionsHead}
