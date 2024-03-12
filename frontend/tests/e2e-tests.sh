788cedbe31622603ee4fc86bc43e67a3c43aad75
Alexis <alexis.bensoussan@intuitem.com>
2024-03-12 15:01:39 +0100

    change backend port to a less common one

--- frontend/tests/e2e-tests.sh
+++ frontend/tests/e2e-tests.sh
@@ -7,7 +7,7 @@ SCRIPT_LONG_ARGS=()
 SCRIPT_SHORT_ARGS=()
 TEST_PATHS=()
 
-BACKEND_PORT=8080
+BACKEND_PORT=8173
 
 for arg in "$@"
 do
@@ -28,17 +28,19 @@ if [[ " ${SCRIPT_SHORT_ARGS[@]} " =~ " -h " ]] || [[ " ${SCRIPT_LONG_ARGS[@]} " 
     echo "Options:"
     echo "  --browser=NAME          Run the tests in the specified browser (chromium, firefox, webkit)"
     echo "  --global-timeout=MS     Maximum time this test suite can run in milliseconds (default: unlimited)"
+    echo "  --grep=SEARCH           Only run tests matching this regular expression (default: \".*\")"
     echo "  --headed                Run the tests in headful mode"
     echo "  -h                      Show this help message and exit"
     echo "  --list                  List all the tests"
-    echo "  --port=PORT             Run the backend server on the specified port (default: 8080)"
+    echo "  --port=PORT             Run the backend server on the specified port (default: $BACKEND_PORT)"
     echo "  --project=NAME          Run the tests in the specified project"
     echo "  -q                      Quick mode: execute only the tests 1 time with no retries and only 1 project"
     echo "  --repeat-each=COUNT     Run the tests the specified number of times (default: 1)"
     echo "  --retries=COUNT         Set the number of retries for the tests"
     echo "  --timeout=MS            Set the timeout for the tests in milliseconds"
     echo "  -v                      Show the output of the backend server"
-    echo -e "  --workers=COUNT         Number of concurrent workers or percentage of logical CPU cores, use 1 to run in a single worker (default: 1)\nBe aware that increasing the number of workers may reduce tests accuracy and stability."
+    echo -e "  --workers=COUNT         Number of concurrent workers or percentage of logical CPU cores, use 1 to run in a single worker (default: 1)"
+    echo "                          Be aware that increasing the number of workers may reduce tests accuracy and stability"
     exit 0
 fi
 
@@ -116,9 +118,9 @@ else
     nohup python manage.py runserver $BACKEND_PORT > /dev/null 2>&1 &
 fi
 BACKEND_PID=$!
-echo "test backend server started on port $BACKEND_PORT (PID: $BACKEND_PID)"
+echo "Test backend server started on port $BACKEND_PORT (PID: $BACKEND_PID)"
 
-echo "starting playwright tests"
+echo "Starting playwright tests"
 export ORIGIN=http://localhost:4173
 export PUBLIC_BACKEND_API_URL=http://localhost:$BACKEND_PORT/api
 
