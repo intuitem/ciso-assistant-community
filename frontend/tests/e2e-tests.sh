3771280cad81c8303e8d192c7811192d13d46d9e
Alexis <alexis.bensoussan@intuitem.com>
2024-03-15 16:09:14 +0100

    add mailer service to the e2e-tests script with option to either use a dedicated test mailer service or an already started one

--- frontend/tests/e2e-tests.sh
+++ frontend/tests/e2e-tests.sh
@@ -8,11 +8,29 @@ SCRIPT_SHORT_ARGS=()
 TEST_PATHS=()
 
 BACKEND_PORT=8173
+MAILER_WEB_SERVER_PORT=8073
+MAILER_SMTP_SERVER_PORT=1073
 
 for arg in "$@"
 do
-    if [[ $arg == --port=* ]]; then
-        BACKEND_PORT="${arg#*=}"
+    if [[ $arg == --port* ]]; then
+        if [[ "${arg#*=}" =~ ^[0-9]+$ ]]; then
+            BACKEND_PORT="${arg#*=}"
+        else
+            echo "Invalid format for --port argument. Please use --port=PORT"
+            exit 1
+        fi
+    elif [[ $arg == --mailer* ]]; then
+        MAILER_PORTS="${arg#*=}"
+        if [[ $MAILER_PORTS =~ ^[0-9]+/[0-9]+$ ]]; then
+            IFS='/' read -ra PORTS <<< "$MAILER_PORTS"
+            MAILER_SMTP_SERVER_PORT="${PORTS[0]}"
+            MAILER_WEB_SERVER_PORT="${PORTS[1]}"
+            SCRIPT_SHORT_ARGS+=("-m")
+        else
+            echo "Invalid format for --mailer argument. Please use --mailer=PORT/PORT"
+            exit 1
+        fi
     elif [[ $arg == --* ]]; then
         SCRIPT_LONG_ARGS+=("$arg")
     elif [[ $arg == -* ]]; then
@@ -32,6 +50,7 @@ if [[ " ${SCRIPT_SHORT_ARGS[@]} " =~ " -h " ]] || [[ " ${SCRIPT_LONG_ARGS[@]} " 
     echo "  --headed                Run the tests in headful mode"
     echo "  -h                      Show this help message and exit"
     echo "  --list                  List all the tests"
+    echo "  -m, --mailer=PORT/PORT  Use an existing mailer service on the optionally defined ports (default: $MAILER_SMTP_SERVER_PORT/$MAILER_WEB_SERVER_PORT)"
     echo "  --port=PORT             Run the backend server on the specified port (default: $BACKEND_PORT)"
     echo "  --project=NAME          Run the tests in the specified project"
     echo "  -q                      Quick mode: execute only the tests 1 time with no retries and only 1 project"
@@ -44,17 +63,37 @@ if [[ " ${SCRIPT_SHORT_ARGS[@]} " =~ " -h " ]] || [[ " ${SCRIPT_LONG_ARGS[@]} " 
     exit 0
 fi
 
-if python -c "import socket;exit(0 if 0 == socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex(('127.0.0.1',$BACKEND_PORT)) else 1)" ; then
+if python -c "import socket;exit(0 if 0 == socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex(('localhost',$BACKEND_PORT)) else 1)" ; then
     echo "The port $BACKEND_PORT is already in use!"
     echo "Please stop the running process using the port or change the backend test server port using --port=PORT and try again."
     exit 1
 fi
 
+for PORT in $MAILER_WEB_SERVER_PORT $MAILER_SMTP_SERVER_PORT ; do
+    if python -c "import socket;exit(0 if 0 == socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex(('localhost',$PORT)) else 1)" ; then
+        if [[ ! " ${SCRIPT_SHORT_ARGS[@]} " =~ " -m " ]] ; then
+            echo "The port $PORT is already in use!"
+            echo "Please stop the running process using the port or change the mailer port and try again."
+            exit 1
+        fi
+    elif [[ " ${SCRIPT_SHORT_ARGS[@]} " =~ " -m " ]] ; then
+        echo "No mailer service is running on port $PORT!"
+        echo "Please start a mailer service on port $PORT or change the mailer port using --mailer=PORT/PORT and try again."
+        echo "You can also use the isolated test mailer service by removing the -m option."
+        exit 1
+    fi
+done
+
 cleanup() {
     echo -e "\nCleaning up..."
-    if [ -n $BACKEND_PID ] ; then
+    if [ -n "$BACKEND_PID" ] ; then
         kill $BACKEND_PID > /dev/null 2>&1
         echo "| backend server stopped"
+    fi
+    if [ -n "$MAILER_PID" ] ; then
+        docker stop $MAILER_PID > /dev/null 2>&1
+        docker rm $MAILER_PID > /dev/null 2>&1
+        echo "| mailer service stopped"
     fi
     if [ -f $DB_DIR/$DB_NAME ] ; then
         rm $DB_DIR/$DB_NAME
@@ -85,6 +124,11 @@ export DJANGO_DEBUG=True
 export DJANGO_SUPERUSER_EMAIL=admin@tests.com
 export DJANGO_SUPERUSER_PASSWORD=1234
 export SQLITE_FILE=db/$DB_NAME
+export EMAIL_HOST_USER=tests@tests.com
+export DEFAULT_FROM_EMAIL='ciso-assistant@tests.net'
+export EMAIL_HOST=localhost
+export EMAIL_HOST_PASSWORD=pwd
+export EMAIL_PORT=$MAILER_SMTP_SERVER_PORT
 
 cd $APP_DIR/backend/
 python manage.py makemigrations
@@ -99,9 +143,24 @@ fi
 BACKEND_PID=$!
 echo "Test backend server started on port $BACKEND_PORT (PID: $BACKEND_PID)"
 
+if [[ ! " ${SCRIPT_SHORT_ARGS[@]} " =~ " -m " ]] ; then
+    if command -v docker > /dev/null 2>&1 ; then
+        echo "Starting mailer service..."
+        MAILER_PID=$(docker run -d -p $MAILER_SMTP_SERVER_PORT:1025 -p $MAILER_WEB_SERVER_PORT:8025 mailhog/mailhog)
+        echo "Mailer service started on ports $MAILER_SMTP_SERVER_PORT/$MAILER_WEB_SERVER_PORT (Container ID: ${MAILER_PID:0:6})"
+    else
+        echo "Docker is not installed!"
+        echo "Please install Docker to use the isolated test mailer service or use -m to tell the tests to use an existing one."
+        exit 1
+    fi
+else
+    echo "Using an existing mailer service on ports $MAILER_SMTP_SERVER_PORT/$MAILER_WEB_SERVER_PORT"
+fi
+
 echo "Starting playwright tests"
 export ORIGIN=http://localhost:4173
 export PUBLIC_BACKEND_API_URL=http://localhost:$BACKEND_PORT/api
+export MAILER_WEB_SERVER_PORT=$MAILER_WEB_SERVER_PORT
 
 cd $APP_DIR/frontend/
 
