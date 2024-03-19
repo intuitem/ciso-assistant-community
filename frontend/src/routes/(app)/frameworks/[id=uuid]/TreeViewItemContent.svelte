4d76b373156cd1f8ca19edb8d80ba7360b19ee08
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2024-03-19 01:21:56 +0100

    increase max width
    65ch is very limited on a modern computer

--- frontend/src/routes/(app)/frameworks/[id=uuid]/TreeViewItemContent.svelte
+++ frontend/src/routes/(app)/frameworks/[id=uuid]/TreeViewItemContent.svelte
@@ -21,7 +21,7 @@
 
 <div>
 	<span class="whitespace-pre-line" style="font-weight: 300};">
-		<p class="max-w-[65ch]">
+		<p class="max-w-[120ch]">
 			{#if title} <span style="font-weight: 600;">{title}</span>&nbsp;&nbsp;{/if}
 			{#if description}{description}{/if}
 		</p>
