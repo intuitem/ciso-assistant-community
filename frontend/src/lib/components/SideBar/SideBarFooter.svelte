937a268afe19096a03f32a0f4115ddca427a8dc0
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-03-30 04:42:44 +0100

    Update functional tests and add test to check that docs button is displayed

--- frontend/src/lib/components/SideBar/SideBarFooter.svelte
+++ frontend/src/lib/components/SideBar/SideBarFooter.svelte
@@ -97,7 +97,7 @@
 				href="https://intuitem.gitbook.io/ciso-assistant"
 				target="_blank"
 				class="unstyled cursor-pointer flex items-center gap-2 w-full px-4 py-2.5 text-left text-sm hover:bg-gray-100 disabled:text-gray-500 text-gray-800"
-				data-testid="profile-button"><i class="fa-solid fa-book mr-2" />{m.onlineDocs()}</a
+				data-testid="docs-button"><i class="fa-solid fa-book mr-2" />{m.onlineDocs()}</a
 			>
 			<form action="/logout" method="POST">
 				<button class="w-full" type="submit" data-testid="logout-button">
