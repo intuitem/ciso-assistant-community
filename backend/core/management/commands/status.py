a1c1601cec0e088f97bbd81d6446ff6f7b9a87b4
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2024-04-24 18:13:40 +0200

    Update status.py
    ruff

--- backend/core/management/commands/status.py
+++ backend/core/management/commands/status.py
@@ -22,7 +22,9 @@ class Command(BaseCommand):
         nb_risk_scenarios = RiskScenario.objects.all().count()
         nb_risk_acceptances = RiskAcceptance.objects.all().count()
         created_at = Folder.get_root_folder().created_at
-        last_login = max(x['last_login'] for x in User.objects.all().values('last_login'))
+        last_login = max(
+            x["last_login"] for x in User.objects.all().values("last_login")
+        )
         self.stdout.write(
             f"created_at={created_at.strftime('%Y-%m-%dT%H:%M')} last_login={last_login.strftime('%Y-%m-%dT%H:%M')} "
             + f"users={nb_users} first_logins={nb_first_login} libraries={nb_libraries} "
