722e039c3d35f7b5bb9daa119c4aec599d192e64
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-04-26 18:51:02 +0200

    Refactor isScored state management
    Just some good ol' de-duplication

--- frontend/src/lib/components/Forms/Score.svelte
+++ frontend/src/lib/components/Forms/Score.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import { formFieldProxy, type SuperForm } from 'sveltekit-superforms/client';
-	import { ProgressRadial, SlideToggle } from '@skeletonlabs/skeleton';
-	import { RangeSlider } from '@skeletonlabs/skeleton';
-	import * as m from '$paraglide/messages';
 	import { displayScoreColor, formatScoreValue } from '$lib/utils/helpers';
+	import * as m from '$paraglide/messages';
+	import { ProgressRadial, RangeSlider, SlideToggle } from '@skeletonlabs/skeleton';
+	import { formFieldProxy, type SuperForm } from 'sveltekit-superforms/client';
 
 	export let label: string | undefined = undefined;
 	export let field: string;
@@ -53,63 +52,44 @@
 	<div class="flex flex-row w-full items-center justify-evenly space-x-4">
 		{#if isApplicable}
 			<div class="flex w-full items-center justify-center">
-				{#if $isScored}
-					<RangeSlider
-						class="w-full"
-						name="range-slider"
-						bind:value={$value}
-						min={min_score}
-						max={max_score}
-						step={score_step}
-						ticked
-					>
-						<div class="flex justify-between space-x-8 items-center">
-							<SlideToggle
-								bind:checked={$isScored}
-								class="shrink-0"
-								active="bg-primary-500"
-								name="score-slider"
-							>
-								<p class="text-sm text-gray-500">{m.scoringHelpText()}</p></SlideToggle
-							>
-							{#if score_definition && $value !== null}
-								{#each score_definition as definition}
-									{#if definition.score === $value}
-										<p class="w-full max-w-[80ch]">
-											{definition.name}: {definition.description}
-										</p>
-									{/if}
-								{/each}
-							{/if}
-							<ProgressRadial
-								stroke={100}
-								meter={displayScoreColor($value, max_score)}
-								value={formatScoreValue($value, max_score)}
-								font={150}
-								class="shrink-0"
-								width={'w-12'}>{$value}</ProgressRadial
-							>
-						</div>
-					</RangeSlider>
-				{:else}
-					<RangeSlider
-						disabled
-						class="w-full"
-						name="range-slider"
-						value={$value}
-						min={min_score}
-						max={max_score}
-						step={score_step}
-						ticked
-					>
-						<div class="flex justify-between items-center">
-							<SlideToggle bind:checked={$isScored} active="bg-primary-500" name="score-slider">
-								<p class="text-sm text-gray-500">{m.scoringHelpText()}</p></SlideToggle
-							>
-							<ProgressRadial stroke={100} value={0} font={150} width={'w-12'}>--</ProgressRadial>
-						</div>
-					</RangeSlider>
-				{/if}
+				<RangeSlider
+					class="w-full"
+					name="range-slider"
+					bind:value={$value}
+					min={min_score}
+					max={max_score}
+					step={score_step}
+					ticked
+					disabled={!$isScored}
+				>
+					<div class="flex justify-between space-x-8 items-center">
+						<SlideToggle
+							bind:checked={$isScored}
+							class="shrink-0"
+							active="bg-primary-500"
+							name="score-slider"
+						>
+							<p class="text-sm text-gray-500">{m.scoringHelpText()}</p></SlideToggle
+						>
+						{#if score_definition && $value !== null}
+							{#each score_definition as definition}
+								{#if definition.score === $value}
+									<p class="w-full max-w-[80ch]">
+										{definition.name}: {definition.description}
+									</p>
+								{/if}
+							{/each}
+						{/if}
+						<ProgressRadial
+							stroke={100}
+							meter={displayScoreColor($value, max_score)}
+							value={$isScored ? formatScoreValue($value, max_score) : 0}
+							font={150}
+							class="shrink-0"
+							width={'w-12'}>{$isScored ? $value : '--'}</ProgressRadial
+						>
+					</div>
+				</RangeSlider>
 			</div>
 		{:else}
 			<p class="text-sm text-gray-500">
