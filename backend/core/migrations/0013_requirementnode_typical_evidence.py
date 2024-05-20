84d8a8102a96ec5d5d2c38b825c6a116cda1e9c6
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2024-05-20 13:08:22 +0200

    ruff

--- backend/core/migrations/0013_requirementnode_typical_evidence.py
+++ backend/core/migrations/0013_requirementnode_typical_evidence.py
@@ -4,15 +4,16 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('core', '0012_alter_appliedcontrol_updated_at_and_more'),
+        ("core", "0012_alter_appliedcontrol_updated_at_and_more"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='requirementnode',
-            name='typical_evidence',
-            field=models.TextField(blank=True, null=True, verbose_name='Typical evidence'),
+            model_name="requirementnode",
+            name="typical_evidence",
+            field=models.TextField(
+                blank=True, null=True, verbose_name="Typical evidence"
+            ),
         ),
     ]
