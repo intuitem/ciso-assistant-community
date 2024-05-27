3fa32d12a7c35707237c86be129b20563763fbe2
monsieurswag <mael.joumier@intuitem.com>
2024-05-27 21:18:44 +0200

    Pass backend messages to frontend

--- frontend/src/routes/(app)/libraries/[id=urn]/+page.server.ts
+++ frontend/src/routes/(app)/libraries/[id=urn]/+page.server.ts
@@ -24,15 +24,33 @@ export const actions: Actions = {
 	upgrade: async(event) => {
 		const endpoint = `${BASE_API_URL}/loaded-libraries/${event.params.id}/upgrade/`;
 		const res = await event.fetch(endpoint); // We will have to make this a PATCH later (we should use PATCH when modifying an object)
+		const resText = await res.text();
 
-		console.log(`Response: ${res.status} ${res.statusText}`);
+		if (!res.ok) {
+			/*
+			Error message list :
 
-		setFlash(
-			{
-				type: 'success',
-				message: "Request successfully processed" // Temporary message for debugging purposes
-			},
-			event
-		);
+			- "Library not found."
+			- "This library has no update."
+			- "Dependency not found."
+			- "A library update isn't allowed to remove a dependency."
+			- "Invalid library update."
+			*/
+			setFlash(
+				{
+					type: 'error',
+					message: resText.substring(1,resText.length-1) // To remove the double quotes around the message, django add double quotes for no reason, we can make this cleaner later
+				},
+				event
+			);
+		} else {
+			setFlash(
+				{
+					type: 'success',
+					message: "Library successfully updated." // Translate this too
+				},
+				event
+			);
+		}
 	}
 };
