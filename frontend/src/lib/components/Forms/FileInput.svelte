6f17971bb4a406b2ad8dceb4531ce8b8798359f1
monsieurswag <mael.joumier@intuitem.com>
2024-06-04 12:24:35 +0200

    Formatter

--- frontend/src/lib/components/Forms/FileInput.svelte
+++ frontend/src/lib/components/Forms/FileInput.svelte
@@ -17,30 +17,25 @@
 
 	$: classesTextField = (errors: string[] | undefined) => (errors ? 'input-error' : '');
 
-	const allowedExtensions = new Set(
-		["jpg","jpeg","png","svg","webp","gif","bmp","tiff"]
-	);
+	const allowedExtensions = new Set(['jpg', 'jpeg', 'png', 'svg', 'webp', 'gif', 'bmp', 'tiff']);
 
 	function getShortenPreciseType(preciseType: string): string {
 		const shortPreciseTypeResult = /^[a-z0-9]+/.exec(preciseType);
-		if (shortPreciseTypeResult === null)
-			return "";
+		if (shortPreciseTypeResult === null) return '';
 		return shortPreciseTypeResult[0];
 	}
 
 	function getAppropriateExtension(mimeType: string): string | null {
-		const [mainType,preciseType] = mimeType.toLocaleLowerCase().split("/");
+		const [mainType, preciseType] = mimeType.toLocaleLowerCase().split('/');
 		const shortPreciseType = getShortenPreciseType(preciseType);
 
-		if (mainType === "image" && allowedExtensions.has(shortPreciseType))
-			return shortPreciseType;
+		if (mainType === 'image' && allowedExtensions.has(shortPreciseType)) return shortPreciseType;
 		return null;
 	}
 
 	function generateFilename(mimeType: string): string | null {
 		const extension = getAppropriateExtension(mimeType);
-		if (extension === null)
-			return null;
+		if (extension === null) return null;
 		// We could implement some contextual data in the filename (for example the evidence name or the name of an object this evidence is related to etc...)
 		const date = new Date();
 		return `${date.getDay()}-${date.getMonth()}-${date.getFullYear()}_${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}_${date.getMilliseconds()}.${extension}`;
@@ -51,19 +46,18 @@
 		const items = event.clipboardData?.items;
 		if (!items) return;
 		for (const item of items) {
-			if (item.kind === "file") {
+			if (item.kind === 'file') {
 				const blob = item.getAsFile();
 				if (blob === null) continue;
 				const filename = generateFilename(blob.type);
-				if (filename === null)
-					continue;
+				if (filename === null) continue;
 
-				const file = new File([blob],filename,{type: blob.type});
+				const file = new File([blob], filename, { type: blob.type });
 				const dataTransfer = new DataTransfer();
 				dataTransfer.items.add(file);
 				fileInput.files = dataTransfer.files; // It seems to work fine even with the superforms fileProxy.
 
-				const event = new Event("change", { bubbles: true }); // Do we really need bubbles: true ?
+				const event = new Event('change', { bubbles: true }); // Do we really need bubbles: true ?
 				fileInput.dispatchEvent(event);
 
 				// A toast should appear to tell the user the Paste operation was successfull.
@@ -75,7 +69,7 @@
 	}
 </script>
 
-<svelte:document on:paste={onPaste}/>
+<svelte:document on:paste={onPaste} />
 
 <div>
 	{#if label !== undefined}
