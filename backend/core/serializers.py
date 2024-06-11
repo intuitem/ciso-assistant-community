05af7ddfd74f991c50d4dd203e1a805fe6acb1d9
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-06-11 11:53:18 +0200

    Fix import error

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,15 +1,17 @@
 from typing import Any
-
-import structlog
 from ciso_assistant.settings import EMAIL_HOST, EMAIL_HOST_RESCUE
-from django.contrib.auth import get_user_model
-from django.db import models
-from iam.models import *
-from rest_framework import serializers
-from rest_framework.exceptions import PermissionDenied
+
 
 from core.models import *
+from iam.models import *
+
+from rest_framework import serializers
+from rest_framework.exceptions import PermissionDenied
+from django.contrib.auth import get_user_model
+from django.db import models
 from core.serializer_fields import FieldsRelatedField
+
+import structlog
 
 logger = structlog.get_logger(__name__)
 
