c80a3421862ad90feb9b97909a13e9a332f09b24
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-06-13 11:58:34 +0200

    Refactor IdentityProviderWriteSerializer

--- backend/iam/sso/serializers.py
+++ backend/iam/sso/serializers.py
@@ -75,77 +75,85 @@ class IdentityProviderWriteSerializer(BaseModelSerializer):
 
     def create(self, validated_data):
         if validated_data.get("provider") == "saml":
-            settings = {
-                "attribute_mapping": {
-                    "uid": validated_data.pop("attribute_mapping_uid", None),
-                    "email_verified": validated_data.pop(
-                        "attribute_mapping_email_verified", None
-                    ),
-                    "email": validated_data.pop("attribute_mapping_email", None),
-                },
-                "idp": {
-                    "entity_id": validated_data.pop("idp_entity_id", None),
-                    "metadata_url": validated_data.pop("metadata_url", None),
-                    "sso_url": validated_data.pop("sso_url", None),
-                    "slo_url": validated_data.pop("slo_url", None),
-                    "x509cert": validated_data.pop("x509cert", None),
-                },
-                "sp": {
-                    # Optional entity ID of the SP. If not set, defaults to the `saml_metadata` urlpattern
-                    "entity_id": validated_data.pop("sp_entity_id", None),
-                },
-                # Advanced settings.
-                "advanced": {
-                    "allow_repeat_attribute_name": validated_data.pop(
-                        "allow_repeat_attribute_name", True
-                    ),
-                    "allow_single_label_domains": validated_data.pop(
-                        "allow_single_label_domains", False
-                    ),
-                    "authn_request_signed": validated_data.pop(
-                        "authn_request_signed", False
-                    ),
-                    "digest_algorithm": validated_data.pop(
-                        "digest_algorithm",
-                        "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256",
-                    ),
-                    "logout_request_signed": validated_data.pop(
-                        "logout_request_signed", False
-                    ),
-                    "logout_response_signed": validated_data.pop(
-                        "logout_response_signed", False
-                    ),
-                    "metadata_signed": validated_data.pop("metadata_signed", False),
-                    "name_id_encrypted": validated_data.pop("name_id_encrypted", False),
-                    "reject_deprecated_algorithm": validated_data.pop(
-                        "reject_deprecated_algorithm", True
-                    ),
-                    # Due to security concerns, IdP initiated SSO is rejected by default.
-                    "reject_idp_initiated_sso": validated_data.pop(
-                        "reject_idp_initiated_sso", False
-                    ),
-                    "signature_algorithm": validated_data.pop(
-                        "signature_algorithm",
-                        "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256",
-                    ),
-                    "want_assertion_encrypted": validated_data.pop(
-                        "want_assertion_encrypted", False
-                    ),
-                    "want_assertion_signed": validated_data.pop(
-                        "want_assertion_signed", False
-                    ),
-                    "want_attribute_statement": validated_data.pop(
-                        "want_attribute_statement", True
-                    ),
-                    "want_message_signed": validated_data.pop(
-                        "want_message_signed", False
-                    ),
-                    "want_name_id": validated_data.pop("want_name_id", False),
-                    "want_name_id_encrypted": validated_data.pop(
-                        "want_name_id_encrypted", False
-                    ),
-                },
-            }
+            settings = self.build_saml_settings(validated_data)
             validated_data["settings"] = settings
             pprint(validated_data)
         return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if validated_data.get("provider") == "saml":
+            settings = self.build_saml_settings(validated_data)
+            validated_data["settings"] = settings
+            pprint(validated_data)
+        return super().update(instance, validated_data)
+
+    def build_saml_settings(self, validated_data):
+        return {
+            "attribute_mapping": {
+                "uid": validated_data.pop("attribute_mapping_uid", None),
+                "email_verified": validated_data.pop(
+                    "attribute_mapping_email_verified", None
+                ),
+                "email": validated_data.pop("attribute_mapping_email", None),
+            },
+            "idp": {
+                "entity_id": validated_data.pop("idp_entity_id", None),
+                "metadata_url": validated_data.pop("metadata_url", None),
+                "sso_url": validated_data.pop("sso_url", None),
+                "slo_url": validated_data.pop("slo_url", None),
+                "x509cert": validated_data.pop("x509cert", ""),
+            },
+            "sp": {
+                # Optional entity ID of the SP. If not set, defaults to the `saml_metadata` urlpattern
+                "entity_id": validated_data.pop("sp_entity_id", None),
+            },
+            # Advanced settings.
+            "advanced": {
+                "allow_repeat_attribute_name": validated_data.pop(
+                    "allow_repeat_attribute_name", True
+                ),
+                "allow_single_label_domains": validated_data.pop(
+                    "allow_single_label_domains", False
+                ),
+                "authn_request_signed": validated_data.pop(
+                    "authn_request_signed", False
+                ),
+                "digest_algorithm": validated_data.pop(
+                    "digest_algorithm",
+                    "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256",
+                ),
+                "logout_request_signed": validated_data.pop(
+                    "logout_request_signed", False
+                ),
+                "logout_response_signed": validated_data.pop(
+                    "logout_response_signed", False
+                ),
+                "metadata_signed": validated_data.pop("metadata_signed", False),
+                "name_id_encrypted": validated_data.pop("name_id_encrypted", False),
+                "reject_deprecated_algorithm": validated_data.pop(
+                    "reject_deprecated_algorithm", True
+                ),
+                # Due to security concerns, IdP initiated SSO is rejected by default.
+                "reject_idp_initiated_sso": validated_data.pop(
+                    "reject_idp_initiated_sso", False
+                ),
+                "signature_algorithm": validated_data.pop(
+                    "signature_algorithm",
+                    "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256",
+                ),
+                "want_assertion_encrypted": validated_data.pop(
+                    "want_assertion_encrypted", False
+                ),
+                "want_assertion_signed": validated_data.pop(
+                    "want_assertion_signed", False
+                ),
+                "want_attribute_statement": validated_data.pop(
+                    "want_attribute_statement", True
+                ),
+                "want_message_signed": validated_data.pop("want_message_signed", False),
+                "want_name_id": validated_data.pop("want_name_id", False),
+                "want_name_id_encrypted": validated_data.pop(
+                    "want_name_id_encrypted", False
+                ),
+            },
+        }
