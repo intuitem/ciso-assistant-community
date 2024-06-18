75f603d0a175de7fb74b3321f5f1171e09c9d41a
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-06-18 18:19:07 +0200

    chore: Run prettier

--- frontend/src/lib/allauth.js
+++ frontend/src/lib/allauth.js
@@ -1,100 +1,99 @@
-import { getCSRFToken } from './django.js'
+import { getCSRFToken } from './django.js';
 import { BASE_API_URL } from '$lib/utils/constants';
 
 const Client = Object.freeze({
-  APP: 'app',
-  BROWSER: 'browser'
-})
+	APP: 'app',
+	BROWSER: 'browser'
+});
 
-const CLIENT = Client.BROWSER
+const CLIENT = Client.BROWSER;
 
-const BASE_URL = `${BASE_API_URL}/_allauth/${CLIENT}/v1`
+const BASE_URL = `${BASE_API_URL}/_allauth/${CLIENT}/v1`;
 const ACCEPT_JSON = {
-  accept: 'application/json'
-}
+	accept: 'application/json'
+};
 
 export const AuthProcess = Object.freeze({
-  LOGIN: 'login',
-  CONNECT: 'connect'
-})
+	LOGIN: 'login',
+	CONNECT: 'connect'
+});
 
 export const Flows = Object.freeze({
-  VERIFY_EMAIL: 'verify_email',
-  LOGIN: 'login',
-  LOGIN_BY_CODE: 'login_by_code',
-  SIGNUP: 'signup',
-  PROVIDER_REDIRECT: 'provider_redirect',
-  PROVIDER_SIGNUP: 'provider_signup',
-  MFA_AUTHENTICATE: 'mfa_authenticate',
-  REAUTHENTICATE: 'reauthenticate',
-  MFA_REAUTHENTICATE: 'mfa_reauthenticate'
-})
+	VERIFY_EMAIL: 'verify_email',
+	LOGIN: 'login',
+	LOGIN_BY_CODE: 'login_by_code',
+	SIGNUP: 'signup',
+	PROVIDER_REDIRECT: 'provider_redirect',
+	PROVIDER_SIGNUP: 'provider_signup',
+	MFA_AUTHENTICATE: 'mfa_authenticate',
+	REAUTHENTICATE: 'reauthenticate',
+	MFA_REAUTHENTICATE: 'mfa_reauthenticate'
+});
 
 export const URLs = Object.freeze({
-  // Meta
-  CONFIG: BASE_URL + '/config',
+	// Meta
+	CONFIG: BASE_URL + '/config',
 
-  // Account management
-  CHANGE_PASSWORD: BASE_URL + '/account/password/change',
-  EMAIL: BASE_URL + '/account/email',
-  PROVIDERS: BASE_URL + '/account/providers',
+	// Account management
+	CHANGE_PASSWORD: BASE_URL + '/account/password/change',
+	EMAIL: BASE_URL + '/account/email',
+	PROVIDERS: BASE_URL + '/account/providers',
 
-  // Account management: 2FA
-  AUTHENTICATORS: BASE_URL + '/account/authenticators',
-  RECOVERY_CODES: BASE_URL + '/account/authenticators/recovery-codes',
-  TOTP_AUTHENTICATOR: BASE_URL + '/account/authenticators/totp',
+	// Account management: 2FA
+	AUTHENTICATORS: BASE_URL + '/account/authenticators',
+	RECOVERY_CODES: BASE_URL + '/account/authenticators/recovery-codes',
+	TOTP_AUTHENTICATOR: BASE_URL + '/account/authenticators/totp',
 
-  // Auth: Basics
-  LOGIN: BASE_URL + '/auth/login',
-  REQUEST_LOGIN_CODE: BASE_URL + '/auth/code/request',
-  CONFIRM_LOGIN_CODE: BASE_URL + '/auth/code/confirm',
-  SESSION: BASE_URL + '/auth/session',
-  REAUTHENTICATE: BASE_URL + '/auth/reauthenticate',
-  REQUEST_PASSWORD_RESET: BASE_URL + '/auth/password/request',
-  RESET_PASSWORD: BASE_URL + '/auth/password/reset',
-  SIGNUP: BASE_URL + '/auth/signup',
-  VERIFY_EMAIL: BASE_URL + '/auth/email/verify',
+	// Auth: Basics
+	LOGIN: BASE_URL + '/auth/login',
+	REQUEST_LOGIN_CODE: BASE_URL + '/auth/code/request',
+	CONFIRM_LOGIN_CODE: BASE_URL + '/auth/code/confirm',
+	SESSION: BASE_URL + '/auth/session',
+	REAUTHENTICATE: BASE_URL + '/auth/reauthenticate',
+	REQUEST_PASSWORD_RESET: BASE_URL + '/auth/password/request',
+	RESET_PASSWORD: BASE_URL + '/auth/password/reset',
+	SIGNUP: BASE_URL + '/auth/signup',
+	VERIFY_EMAIL: BASE_URL + '/auth/email/verify',
 
-  // Auth: 2FA
-  MFA_AUTHENTICATE: BASE_URL + '/auth/2fa/authenticate',
-  MFA_REAUTHENTICATE: BASE_URL + '/auth/2fa/reauthenticate',
+	// Auth: 2FA
+	MFA_AUTHENTICATE: BASE_URL + '/auth/2fa/authenticate',
+	MFA_REAUTHENTICATE: BASE_URL + '/auth/2fa/reauthenticate',
 
-  // Auth: Social
-  PROVIDER_SIGNUP: BASE_URL + '/auth/provider/signup',
-  REDIRECT_TO_PROVIDER: BASE_URL + '/auth/provider/redirect',
-  PROVIDER_TOKEN: BASE_URL + '/auth/provider/token',
+	// Auth: Social
+	PROVIDER_SIGNUP: BASE_URL + '/auth/provider/signup',
+	REDIRECT_TO_PROVIDER: BASE_URL + '/auth/provider/redirect',
+	PROVIDER_TOKEN: BASE_URL + '/auth/provider/token',
 
-  // Auth: Sessions
-  SESSIONS: BASE_URL + '/auth/sessions'
-})
+	// Auth: Sessions
+	SESSIONS: BASE_URL + '/auth/sessions'
+});
 
 export const AuthenticatorType = Object.freeze({
-  TOTP: 'totp',
-  RECOVERY_CODES: 'recovery_codes'
-})
+	TOTP: 'totp',
+	RECOVERY_CODES: 'recovery_codes'
+});
 
-function postForm (action, data) {
-  const f = document.createElement('form')
-  f.method = 'POST'
-  f.action = action
+function postForm(action, data) {
+	const f = document.createElement('form');
+	f.method = 'POST';
+	f.action = action;
 
-  for (const key in data) {
-    const d = document.createElement('input')
-    d.type = 'hidden'
-    d.name = key
-    d.value = data[key]
-    f.appendChild(d)
-  }
-  document.body.appendChild(f)
-  f.submit()
+	for (const key in data) {
+		const d = document.createElement('input');
+		d.type = 'hidden';
+		d.name = key;
+		d.value = data[key];
+		f.appendChild(d);
+	}
+	document.body.appendChild(f);
+	f.submit();
 }
 
-
-export function redirectToProvider (providerId, callbackURL, process = AuthProcess.LOGIN) {
-  postForm(URLs.REDIRECT_TO_PROVIDER, {
-    provider: providerId,
-    process,
-    callback_url: callbackURL,
-    csrfmiddlewaretoken: getCSRFToken()
-  })
+export function redirectToProvider(providerId, callbackURL, process = AuthProcess.LOGIN) {
+	postForm(URLs.REDIRECT_TO_PROVIDER, {
+		provider: providerId,
+		process,
+		callback_url: callbackURL,
+		csrfmiddlewaretoken: getCSRFToken()
+	});
 }
