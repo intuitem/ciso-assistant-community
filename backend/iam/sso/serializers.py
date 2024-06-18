01e5b941a84917072dc0d035a428eba0814ef997
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-06-18 18:01:15 +0200

    Fix settings serialization

--- backend/iam/sso/serializers.py
+++ backend/iam/sso/serializers.py
@@ -1,5 +1,7 @@
 from allauth.socialaccount.providers.saml.provider import SAMLProvider
 from rest_framework import serializers
+
+from settings.models import GlobalSettings
 from .models import SSOSettings
 
 from core.serializers import BaseModelSerializer
@@ -12,95 +14,183 @@ class SSOSettingsReadSerializer(BaseModelSerializer):
 
     class Meta:
         model = SSOSettings
-        fields = "__all__"
+        exclude = ["value"]
 
 
 class SSOSettingsWriteSerializer(BaseModelSerializer):
+    provider = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        allow_null=True,
+    )
+    provider_id = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        allow_null=True,
+    )
+    client_id = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        allow_null=True,
+    )
+    provider_name = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        allow_null=True,
+        source="settings.name",
+    )
     attribute_mapping_uid = serializers.ListField(
         child=serializers.CharField(
-            required=False, allow_blank=True, allow_null=True, write_only=True
+            required=False,
+            allow_blank=True,
+            allow_null=True,
         ),
-        write_only=True,
         required=False,
         allow_null=True,
+        source="settings.attribute_mapping.uid",
     )
     attribute_mapping_email_verified = serializers.ListField(
         child=serializers.CharField(
-            required=False, allow_blank=True, allow_null=True, write_only=True
+            required=False,
+            allow_blank=True,
+            allow_null=True,
         ),
-        write_only=True,
         required=False,
         allow_null=True,
+        source="settings.attribute_mapping.email_verified",
     )
     attribute_mapping_email = serializers.ListField(
         child=serializers.CharField(
-            required=False, allow_blank=True, allow_null=True, write_only=True
+            required=False,
+            allow_blank=True,
+            allow_null=True,
         ),
-        write_only=True,
         required=False,
         allow_null=True,
+        source="settings.attribute_mapping.email",
     )
     idp_entity_id = serializers.CharField(
-        required=False, allow_blank=True, allow_null=True, write_only=True
+        required=False,
+        allow_blank=True,
+        allow_null=True,
+        source="settings.idp.entity_id",
     )
     metadata_url = serializers.CharField(
-        required=False, allow_blank=True, allow_null=True, write_only=True
+        required=False,
+        allow_blank=True,
+        allow_null=True,
+        source="settings.idp.metadata_url",
     )
     sso_url = serializers.CharField(
-        required=False, allow_blank=True, allow_null=True, write_only=True
+        required=False,
+        allow_blank=True,
+        allow_null=True,
+        source="settings.idp.sso_url",
     )
     slo_url = serializers.CharField(
-        required=False, allow_blank=True, allow_null=True, write_only=True
+        required=False,
+        allow_blank=True,
+        allow_null=True,
+        source="settings.idp.slo_url",
     )
     x509cert = serializers.CharField(
-        required=False, allow_blank=True, allow_null=True, write_only=True
+        required=False,
+        allow_blank=True,
+        allow_null=True,
+        source="settings.idp.x509cert",
     )
     sp_entity_id = serializers.CharField(
-        required=False, allow_blank=True, allow_null=True, write_only=True
+        required=False,
+        allow_blank=True,
+        allow_null=True,
+        source="settings.sp.entity_id",
     )
     allow_repeat_attribute_name = serializers.BooleanField(
-        required=False, write_only=True
+        required=False,
+        source="settings.advanced.allow_repeat_attribute_name",
     )
     allow_single_label_domains = serializers.BooleanField(
-        required=False, write_only=True
+        required=False,
+        source="settings.advanced.allow_single_label_domains",
     )
-    authn_request_signed = serializers.BooleanField(required=False, write_only=True)
+    authn_request_signed = serializers.BooleanField(
+        required=False,
+        source="settings.advanced.authn_request_signed",
+    )
     digest_algorithm = serializers.CharField(
-        required=False, allow_blank=True, allow_null=True, write_only=True
+        required=False,
+        allow_blank=True,
+        allow_null=True,
+        source="settings.advanced.digest_algorithm",
     )
-    logout_request_signed = serializers.BooleanField(required=False, write_only=True)
-    logout_response_signed = serializers.BooleanField(required=False, write_only=True)
-    metadata_signed = serializers.BooleanField(required=False, write_only=True)
-    name_id_encrypted = serializers.BooleanField(required=False, write_only=True)
+    logout_request_signed = serializers.BooleanField(
+        required=False,
+        source="settings.advanced.logout_request_signed",
+    )
+    logout_response_signed = serializers.BooleanField(
+        required=False,
+        source="settings.advanced.logout_response_signed",
+    )
+    metadata_signed = serializers.BooleanField(
+        required=False,
+        source="settings.advanced.metadata_signed",
+    )
+    name_id_encrypted = serializers.BooleanField(
+        required=False,
+        source="settings.advanced.name_id_encrypted",
+    )
     reject_deprecated_algorithm = serializers.BooleanField(
-        required=False, write_only=True
+        required=False,
+        source="settings.advanced.reject_deprecated_algorithm",
     )
-    reject_idp_initiated_sso = serializers.BooleanField(required=False, write_only=True)
+    reject_idp_initiated_sso = serializers.BooleanField(
+        required=False,
+        source="settings.advanced.reject_idp_initiated_sso",
+    )
     signature_algorithm = serializers.CharField(
-        required=False, allow_blank=True, allow_null=True, write_only=True
+        required=False,
+        allow_blank=True,
+        allow_null=True,
+        source="settings.advanced.signature_algorithm",
     )
-    want_assertion_encrypted = serializers.BooleanField(required=False, write_only=True)
-    want_assertion_signed = serializers.BooleanField(required=False, write_only=True)
-    want_attribute_statement = serializers.BooleanField(required=False, write_only=True)
-    want_message_signed = serializers.BooleanField(required=False, write_only=True)
-    want_name_id = serializers.BooleanField(required=False, write_only=True)
-    want_name_id_encrypted = serializers.BooleanField(required=False, write_only=True)
+    want_assertion_encrypted = serializers.BooleanField(
+        required=False,
+        source="settings.advanced.want_assertion_encrypted",
+    )
+    want_assertion_signed = serializers.BooleanField(
+        required=False,
+        source="settings.advanced.want_assertion_signed",
+    )
+    want_attribute_statement = serializers.BooleanField(
+        required=False,
+        source="settings.advanced.want_attribute_statement",
+    )
+    want_message_signed = serializers.BooleanField(
+        required=False,
+        source="settings.advanced.want_message_signed",
+    )
+    want_name_id = serializers.BooleanField(
+        required=False,
+        source="settings.advanced.want_name_id",
+    )
+    want_name_id_encrypted = serializers.BooleanField(
+        required=False,
+        source="settings.advanced.want_name_id_encrypted",
+    )
 
     class Meta:
         model = SSOSettings
-        fields = "__all__"
-
-    def create(self, validated_data):
-        if validated_data.get("provider") == "saml":
-            settings = self.build_saml_settings(validated_data)
-            validated_data["settings"] = settings
-        return super().create(validated_data)
+        exclude = ["value"]
 
     def update(self, instance, validated_data):
-        if validated_data.get("provider") == "saml":
-            settings = self.build_saml_settings(validated_data)
-            validated_data["settings"] = settings
-        return super().update(instance, validated_data)
+        # if validated_data.get("provider") == "saml":
+        #     settings = self.build_saml_settings(validated_data)
+        #     validated_data["settings"] = settings
+        settings_object = GlobalSettings.objects.get(name=GlobalSettings.Names.SSO)
+        settings_object.value = validated_data
+        # print(settings_object.value)
+        settings_object.save()
+        return instance
 
     def build_saml_settings(self, validated_data):
         default_attribute_mapping = SAMLProvider.default_attribute_mapping
