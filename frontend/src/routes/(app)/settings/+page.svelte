6f05f020917a133147e438a205d4072ff91b6651
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-06-18 18:02:30 +0200

    Fix SSO Settings form

--- frontend/src/routes/(app)/settings/+page.svelte
+++ frontend/src/routes/(app)/settings/+page.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
 	import ModelForm from '$lib/components/Forms/ModelForm.svelte';
-	import { getModelInfo } from '$lib/utils/crud';
 	import { IdentityProviderSchema } from '$lib/utils/schemas';
 
 	export let data;
 </script>
 
-<ModelForm
-	form={data.form}
-	schema={IdentityProviderSchema}
-	model={getModelInfo('identity-providers')}
-/>
+<ModelForm form={data.form} schema={IdentityProviderSchema} model={data.model} />
