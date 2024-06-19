f608269675907cb33d37ad91ee0811916801a518
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-06-19 15:57:28 +0200

    fix: overwrite is_safe_url

--- backend/iam/adapter.py
+++ backend/iam/adapter.py
@@ -3,12 +3,15 @@ from allauth.socialaccount.helpers import ImmediateHttpResponse
 from django.conf import settings
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.core import context
 from django.dispatch import receiver
 from allauth.socialaccount.signals import pre_social_login
 from django.contrib.auth import login, get_user_model
 from rest_framework.response import Response
 from rest_framework.status import HTTP_401_UNAUTHORIZED
 from knox.views import LoginView
+from django.utils.http import url_has_allowed_host_and_scheme
+from urllib.parse import urlparse
 
 User = get_user_model()
 
@@ -16,6 +19,15 @@ User = get_user_model()
 class MyAccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
         return False
+    
+    def is_safe_url(self, url):
+        
+        allowed_hosts = {urlparse(settings.CISO_ASSISTANT_URL).hostname} | set(settings.ALLOWED_HOSTS)
+        
+        if urlparse(url).port:
+            url = url.replace(':' + str(urlparse(url).port), '')
+            
+        return url_has_allowed_host_and_scheme(url, allowed_hosts=allowed_hosts)
 
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
