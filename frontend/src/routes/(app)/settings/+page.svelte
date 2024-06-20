c322c9b85509c831e68fbb0c9582bc5c63babaa1
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-06-20 20:24:59 +0200

    Revert "fix: add page proxy settings for ci"

---
+++ frontend/src/routes/(app)/settings/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import ModelForm from '$lib/components/Forms/ModelForm.svelte';
+	import { IdentityProviderSchema } from '$lib/utils/schemas';
+
+	export let data;
+</script>
+
+<ModelForm form={data.form} schema={IdentityProviderSchema} model={data.model} />
