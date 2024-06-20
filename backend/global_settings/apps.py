83f6f10d5f2f832eea7d351c1a3f8fed36f99a1f
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-06-20 18:25:10 +0200

    chore: run format

--- backend/global_settings/apps.py
+++ backend/global_settings/apps.py
@@ -2,6 +2,7 @@ import os
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
+
 class SettingsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "global_settings"
