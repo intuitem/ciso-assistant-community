01a974f46a2fa57925c4a4b5e59baf0bb0461fb6
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-06-21 13:32:30 +0200

    style: improve settings UI

--- frontend/src/routes/(app)/settings/+page.svelte
+++ frontend/src/routes/(app)/settings/+page.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
 	import ModelForm from '$lib/components/Forms/ModelForm.svelte';
 	import { IdentityProviderSchema } from '$lib/utils/schemas';
+	import SettingsLayout from './SettingsLayout.svelte';
+	import * as m from '$paraglide/messages';
 
 	export let data;
 </script>
 
-<ModelForm form={data.form} schema={IdentityProviderSchema} model={data.model} />
+<SettingsLayout>
+	<div class="p-4" slot="sso">
+		<span class="text-gray-500">{m.ssoSettingsDescription()}</span>
+		<ModelForm form={data.form} schema={IdentityProviderSchema} model={data.model} cancelButton={false}/>
+	</div>
+</SettingsLayout>
---
+++ frontend/src/routes/(app)/settings/SettingsLayout.svelte
@@ -0,0 +1,22 @@
+<script>
+
+	import { TabGroup, Tab } from "@skeletonlabs/skeleton";
+    import * as m from '$paraglide/messages';
+
+    let tabSet = 0;
+
+</script>
+
+<div class="card bg-white shadow-lg p-4">
+    <TabGroup
+    active="bg-primary-100 text-primary-800 border-b border-primary-800"
+    >
+        <Tab bind:group={tabSet} name="ssoSettings" value={0}
+            >{m.ssoSettings()}</Tab
+        >
+    </TabGroup>
+    {#if tabSet === 0}
+        <slot name="sso"/>
+    {/if}
+</div>
+
