539e4ac06bebee68d97306e9ee5b71136b4ef934
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-06-21 09:23:42 +0200

    Re-enable CSRF middleware

--- backend/ciso_assistant/settings.py
+++ backend/ciso_assistant/settings.py
@@ -144,7 +144,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
-    # "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
