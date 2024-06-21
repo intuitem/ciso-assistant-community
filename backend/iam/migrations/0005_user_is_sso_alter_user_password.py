93e8cf36faf3a5764e5e6bf3f68c693a4d867e5c
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-06-21 20:33:59 +0200

    chore: Run ruff format

--- backend/iam/migrations/0005_user_is_sso_alter_user_password.py
+++ backend/iam/migrations/0005_user_is_sso_alter_user_password.py
@@ -4,20 +4,21 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('iam', '0004_ssosettings'),
+        ("iam", "0004_ssosettings"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='is_sso',
+            model_name="user",
+            name="is_sso",
             field=models.BooleanField(default=False),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='password',
-            field=models.CharField(blank=True, max_length=128, null=True, verbose_name='password'),
+            model_name="user",
+            name="password",
+            field=models.CharField(
+                blank=True, max_length=128, null=True, verbose_name="password"
+            ),
         ),
     ]
