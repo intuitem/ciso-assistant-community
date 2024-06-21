4e61b7ce0c12ca1158b2ba734ffb1115d1fc35bd
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-06-21 17:40:35 +0200

    Use +layout.svelte instead of SettingsLayout component

---
+++ frontend/src/routes/(app)/settings/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<div class="card bg-white shadow-lg p-4">
+	<slot />
+</div>
--- frontend/src/routes/(app)/settings/+page.svelte
+++ frontend/src/routes/(app)/settings/+page.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
 	import ModelForm from '$lib/components/Forms/ModelForm.svelte';
 	import { IdentityProviderSchema } from '$lib/utils/schemas';
-	import SettingsLayout from './SettingsLayout.svelte';
 	import * as m from '$paraglide/messages';
+	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+
+	let tabSet = 0;
 
 	export let data;
 </script>
 
-<SettingsLayout>
-	<div class="p-4" slot="sso">
+<TabGroup active="bg-primary-100 text-primary-800 border-b border-primary-800">
+	<Tab bind:group={tabSet} name="ssoSettings" value={0}><i class="fa-solid fa-key" /> {m.sso()}</Tab
+	>
+</TabGroup>
+{#if tabSet === 0}
+	<div>
 		<span class="text-gray-500">{m.ssoSettingsDescription()}</span>
 		<ModelForm
 			form={data.form}
@@ -17,4 +23,4 @@
 			cancelButton={false}
 		/>
 	</div>
-</SettingsLayout>
+{/if}
