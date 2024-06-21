f4114512f91ef8d95298ddd748b896ed5a6dcef2
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-06-21 13:42:39 +0200

    style: improve sso tab

--- frontend/src/routes/(app)/settings/SettingsLayout.svelte
+++ frontend/src/routes/(app)/settings/SettingsLayout.svelte
@@ -7,7 +7,7 @@
 
 <div class="card bg-white shadow-lg p-4">
 	<TabGroup active="bg-primary-100 text-primary-800 border-b border-primary-800">
-		<Tab bind:group={tabSet} name="ssoSettings" value={0}>{m.ssoSettings()}</Tab>
+		<Tab bind:group={tabSet} name="ssoSettings" value={0}><i class="fa-solid fa-key"></i> {m.sso()}</Tab>
 	</TabGroup>
 	{#if tabSet === 0}
 		<slot name="sso" />
