e5697fd9d495f30c010006abe2195180d73a5c7d
Abderrahmane Smimite <smimite@gmail.com>
2024-06-24 00:28:51 +0200

    format

--- frontend/src/routes/(app)/[model=urlmodel]/+page.svelte
+++ frontend/src/routes/(app)/[model=urlmodel]/+page.svelte
@@ -47,27 +47,27 @@
 {#if data.table}
 	<div class="shadow-lg">
 		{#key URLModel}
-		<ModelTable source={data.table} deleteForm={data.deleteForm} URLModel={URLModel}>
-			<div slot="addButton">
-				{#if !['risk-matrices', 'frameworks', 'user-groups', 'role-assignments'].includes(URLModel)}
-					<button
-						class="btn variant-filled-primary self-end"
-						data-testid="add-button"
-						on:click={modalCreateForm}
-						><i class="fa-solid fa-plus mr-2" />
-						{m['add' + capitalizeFirstLetter(data.model.localName)]()}
-					</button>
-				{:else if URLModel === 'risk-matrices'}
-					<a href="/libraries" class="btn variant-filled-primary" data-testid="add-button"
-						><i class="fa-solid fa-file-import mr-2" />{m.importMatrices()}</a
-					>
-				{:else if URLModel === 'frameworks'}
-					<a href="/libraries" class="btn variant-filled-primary" data-testid="add-button"
-						><i class="fa-solid fa-file-import mr-2" />{m.importFrameworks()}</a
-					>
-				{/if}
-			</div>
-		</ModelTable>
+			<ModelTable source={data.table} deleteForm={data.deleteForm} {URLModel}>
+				<div slot="addButton">
+					{#if !['risk-matrices', 'frameworks', 'user-groups', 'role-assignments'].includes(URLModel)}
+						<button
+							class="btn variant-filled-primary self-end"
+							data-testid="add-button"
+							on:click={modalCreateForm}
+							><i class="fa-solid fa-plus mr-2" />
+							{m['add' + capitalizeFirstLetter(data.model.localName)]()}
+						</button>
+					{:else if URLModel === 'risk-matrices'}
+						<a href="/libraries" class="btn variant-filled-primary" data-testid="add-button"
+							><i class="fa-solid fa-file-import mr-2" />{m.importMatrices()}</a
+						>
+					{:else if URLModel === 'frameworks'}
+						<a href="/libraries" class="btn variant-filled-primary" data-testid="add-button"
+							><i class="fa-solid fa-file-import mr-2" />{m.importFrameworks()}</a
+						>
+					{/if}
+				</div>
+			</ModelTable>
 		{/key}
 	</div>
 {/if}
