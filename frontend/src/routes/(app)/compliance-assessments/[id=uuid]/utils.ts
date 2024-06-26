c5108415885e1554d448b228d7b7e7402c894d14
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-06-26 10:40:58 +0200

    Revert non_compliant_minor and non_compliant_major to partially_compliant and non_compliant

--- frontend/src/routes/(app)/compliance-assessments/[id=uuid]/utils.ts
+++ frontend/src/routes/(app)/compliance-assessments/[id=uuid]/utils.ts
@@ -1,9 +1,9 @@
 export const complianceColorMap = {
 	in_progress: '#3b82f6',
-	non_compliant_major: '#f87171',
+	non_compliant: '#f87171',
 	to_do: '#d1d5db',
 	not_assessed: '#d1d5db',
-	non_compliant_minor: '#fde047',
+	partially_compliant: '#fde047',
 	not_applicable: '#000000',
 	compliant: '#86efac'
 };
