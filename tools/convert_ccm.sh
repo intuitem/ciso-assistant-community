3ffc0a00a862b123ba4d34dec9492f77799285a1
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2024-07-04 00:28:47 +0200

    Add CAIQ to CCM
    Add CAIQ questions as annotations
    simplify and relocate script convert_ccm.sh

--- tools/convert_ccm.sh
+++
@@ -1,6 +0,0 @@
-file="CCMv4.0.12.xlsx"
-packager="personal"
-python ccm/convert_ccm.py $file $packager
-intermediate="ccm-controls-v4.xlsx"
-python convert_library.py $intermediate
-echo "Resulting file is available at cis/cis-controls-v8.yaml"
