82ef438b17cb6755c152ad53308966639d68ad8b
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-07-04 12:44:52 +0200

    chore: run format

--- backend/iam/migrations/0005_alter_user_managers.py
+++ backend/iam/migrations/0005_alter_user_managers.py
@@ -5,16 +5,15 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('iam', '0004_ssosettings_user_is_sso'),
+        ("iam", "0004_ssosettings_user_is_sso"),
     ]
 
     operations = [
         migrations.AlterModelManagers(
-            name='user',
+            name="user",
             managers=[
-                ('objects', iam.models.CaseInsensitiveUserManager()),
+                ("objects", iam.models.CaseInsensitiveUserManager()),
             ],
         ),
     ]
