2e826aac56801bcaea7dccc4bb6ad658b221da78
monsieurswag <mael.joumier@intuitem.com>
2024-07-12 23:18:46 +0200

    Remove debug code from tests

--- frontend/tests/functional/detailed/login.test.ts
+++ frontend/tests/functional/detailed/login.test.ts
@@ -38,9 +38,7 @@ test('login invalid message is showing properly', async ({ loginPage, page }) =>
 	await loginPage.hasUrl();
 });
 
-test('forgot password process is working properly', {
-	tag: "@forgotpassword"
-}, async ({
+test('forgot password process is working properly', async ({
 	logedPage,
 	usersPage,
 	sideBar,
@@ -55,9 +53,7 @@ test('forgot password process is working properly', {
 	});
 
 	await usersPage.editItemButton(email).click();
-	await new Promise(res => setTimeout(res,2000));
 	await page.getByTestId('set-password-btn').click();
-	await new Promise(res => setTimeout(res,2000));
 	await expect(page).toHaveURL(/.*\/users\/.+\/edit\/set-password/);
 	await usersPage.form.fill({
 		new_password: testData.user.password,
