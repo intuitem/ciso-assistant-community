6e95029f34b747fcaf3ba92e8422c8a5484538d9
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-07-16 17:07:38 +0200

    feat: use RA str with version to avoir name confusion

--- frontend/src/routes/(app)/analytics/ComposerSelect.svelte
+++ frontend/src/routes/(app)/analytics/ComposerSelect.svelte
@@ -15,7 +15,7 @@
 
 	onMount(async () => {
 		const riskAssessments = await fetch('/risk-assessments').then((res) => res.json());
-		options = getOptions({ objects: riskAssessments, extra_fields: [['project', 'str']] });
+		options = getOptions({ objects: riskAssessments, label: 'str', extra_fields: [['project', 'str']] });
 	});
 </script>
 
