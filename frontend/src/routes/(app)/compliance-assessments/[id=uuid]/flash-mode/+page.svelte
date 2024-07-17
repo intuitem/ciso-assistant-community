2a9c05b6b50ab8bf815861778de480f380ab2464
Gildas NOEL <g.noel@ackwa.fr>
2024-07-17 14:13:42 +0200

    Proposition de nouvelle mise en page du mode "Flash"

--- frontend/src/routes/(app)/compliance-assessments/[id=uuid]/flash-mode/+page.svelte
+++ frontend/src/routes/(app)/compliance-assessments/[id=uuid]/flash-mode/+page.svelte
@@ -76,7 +76,7 @@
 	>
 		{#if data.requirement_assessments[currentIndex]}
 			<div class="flex flex-col w-full h-full space-y-4">
-				<div class="flex justify-between h-1/6">
+				<div class="flex justify-between">
 					<div class="">
 						<a
 							href="/compliance-assessments/{data.compliance_assessment.id}"
@@ -88,37 +88,39 @@
 					</div>
 					<div class="font-semibold">{currentIndex + 1}/{data.requirement_assessments.length}</div>
 				</div>
-				<div class="flex flex-col h-1/2 items-center text-center justify-center">
+				<div class="flex flex-col items-center text-center justify-center">
 					<p class="font-semibold">{title}</p>
+				</div>
+				<div class="flex flex-col items-center justify-center">
 					{#if data.requirement_assessments[currentIndex].description}
 						{data.requirement_assessments[currentIndex].description}
 					{/if}
 				</div>
-				<div class="items-center">
-					<div>
-						<form id="flashModeForm" action="?/updateRequirementAssessment" method="post">
-							<ul
-								class=" items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-							>
-								<RadioGroup class="w-full flex-wrap items-center">
-									{#each possible_options as option}
-										<RadioItem
-											class="h-full"
-											id={option.id}
-											value={option.id}
-											bind:group={result}
-											name="result"
-											style="border-color: {color}"
-											on:change={updateResult}>{option.label}</RadioItem
-										>
-									{/each}
-								</RadioGroup>
-							</ul>
-						</form>
-					</div>
-				</div>
 			</div>
-			<div class="flex justify-between">
+			<div class="items-center my-4">
+			<div>
+				<form id="flashModeForm" action="?/updateRequirementAssessment" method="post">
+					<ul
+						class=" items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+					>
+						<RadioGroup class="w-full flex-wrap items-center">
+							{#each possible_options as option}
+								<RadioItem
+									class="h-full"
+									id={option.id}
+									value={option.id}
+									bind:group={result}
+									name="result"
+									style="border-color: {color}"
+									on:change={updateResult}>{option.label}</RadioItem
+								>
+							{/each}
+						</RadioGroup>
+					</ul>
+				</form>
+			</div>
+		</div>
+		<div class="flex justify-between">
 				<button class="bg-gray-400 text-white px-4 py-2 rounded" on:click={previousItem}>
 					{m.previous()}
 				</button>
