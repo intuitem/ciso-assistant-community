bc76f936debd39787616d966f6ecb0c13822ab90
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-07-22 12:00:12 +0200

    feat: create get_referential_translation() helper

--- frontend/src/routes/(app)/libraries/[id=urn]/tree/+server.ts
+++ frontend/src/routes/(app)/libraries/[id=urn]/tree/+server.ts
@@ -2,11 +2,18 @@ import { BASE_API_URL } from '$lib/utils/constants';
 
 import { error, type NumericRange } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { languageTag } from '$paraglide/runtime';
+
 
 export const GET: RequestHandler = async ({ fetch, params, url }) => {
 	const URLModel = url.searchParams.has('loaded') ? 'loaded-libraries' : 'stored-libraries';
 	const endpoint = `${BASE_API_URL}/${URLModel}/${params.id}/tree`;
-	const res = await fetch(endpoint);
+	const res = await fetch(endpoint, {
+		headers: {
+			'Accept-Language': languageTag(),
+		},
+	}
+	);
 
 	if (!res.ok) error(res.status as NumericRange<400, 599>, await res.json());
 
