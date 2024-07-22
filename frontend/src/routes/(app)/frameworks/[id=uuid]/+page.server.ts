a97988713e43af19dd77fad6028ee220de98d690
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-07-22 20:09:40 +0200

    feat: translate front compliance objects

--- frontend/src/routes/(app)/frameworks/[id=uuid]/+page.server.ts
+++ frontend/src/routes/(app)/frameworks/[id=uuid]/+page.server.ts
@@ -1,15 +1,24 @@
 import { BASE_API_URL } from '$lib/utils/constants';
 import type { PageServerLoad } from './$types';
+import { languageTag } from '$paraglide/runtime';
 
 export const load = (async ({ fetch, params }) => {
 	const URLModel = 'frameworks';
 	const endpoint = `${BASE_API_URL}/${URLModel}/${params.id}/object`;
 
-	const res = await fetch(endpoint);
+	const res = await fetch(endpoint, {
+		headers: {
+			'Accept-Language': languageTag()
+		}
+	
+	});
 	const framework = await res.json();
-	const tree = await fetch(`${BASE_API_URL}/${URLModel}/${params.id}/tree`).then((res) =>
+	const tree = await fetch(`${BASE_API_URL}/${URLModel}/${params.id}/tree`, {
+		headers: {
+			'Accept-Language': languageTag()
+		}
+	}).then((res) =>
 		res.json()
 	);
-
 	return { URLModel, framework, tree };
 }) satisfies PageServerLoad;
