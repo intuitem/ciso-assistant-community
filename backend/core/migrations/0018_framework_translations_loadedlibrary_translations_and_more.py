5b07426433ef9242fd54712c0aa2f6c262e38022
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-07-23 14:10:32 +0200

    fix: serializers as in main branch

--- backend/core/migrations/0018_framework_translations_loadedlibrary_translations_and_more.py
+++ backend/core/migrations/0018_framework_translations_loadedlibrary_translations_and_more.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('core', '0017_requirementassessment_mapping_inference_and_more'),
+        ("core", "0017_requirementassessment_mapping_inference_and_more"),
     ]
 
     operations = [
