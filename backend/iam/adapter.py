ea01500941124e81a605ce4376bfb40538263474
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-08-02 21:05:07 +0200

    chore: Organize imports

--- backend/iam/adapter.py
+++ backend/iam/adapter.py
@@ -1,29 +1,23 @@
-from allauth.account.utils import perform_login
-from allauth.socialaccount.helpers import ImmediateHttpResponse
-from allauth.socialaccount.models import app_settings
-from django.conf import settings
+from urllib.parse import urlparse
+
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import (
     DefaultSocialAccountAdapter,
     MultipleObjectsReturned,
     warnings,
 )
+from allauth.socialaccount.models import app_settings
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db.models import Q
-from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from allauth.core import context
-from django.dispatch import receiver
-from allauth.socialaccount.signals import pre_social_login
-from django.contrib.auth import login, get_user_model
+from django.utils.http import url_has_allowed_host_and_scheme
 from rest_framework.response import Response
 from rest_framework.status import HTTP_401_UNAUTHORIZED
-from knox.views import LoginView
-from django.utils.http import url_has_allowed_host_and_scheme
-from urllib.parse import urlparse
 
 User = get_user_model()
 
 
-class MyAccountAdapter(DefaultAccountAdapter):
+class AccountAdapter(DefaultAccountAdapter):
     def is_safe_url(self, url):
         allowed_hosts = {urlparse(settings.CISO_ASSISTANT_URL).hostname} | set(
             settings.ALLOWED_HOSTS
@@ -35,7 +29,7 @@ class MyAccountAdapter(DefaultAccountAdapter):
         return url_has_allowed_host_and_scheme(url, allowed_hosts=allowed_hosts)
 
 
-class MySocialAccountAdapter(DefaultSocialAccountAdapter):
+class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
         email_address = next(iter(sociallogin.account.extra_data.values()))[0]
         try:
