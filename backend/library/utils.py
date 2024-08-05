f6b80912e23f030198fdcedd5f882ffd461f256a
Abderrahmane Smimite <smimite@gmail.com>
2024-08-05 22:46:37 +0200

    Add extra log useful for mapping issues

--- backend/library/utils.py
+++ backend/library/utils.py
@@ -128,6 +128,9 @@ class RequirementMappingImporter:
                 urn=self.data["target_requirement_urn"], default_locale=True
             )
         except RequirementNode.DoesNotExist:
+            print(
+                f"ERROR: source requirement with URN {self.data['target_requirement_urn']} does not exist"
+            )
             raise Http404(
                 "ERROR: target requirement with URN {} does not exist".format(
                     self.data["target_requirement_urn"]
@@ -139,6 +142,9 @@ class RequirementMappingImporter:
                 urn=self.data["source_requirement_urn"], default_locale=True
             )
         except RequirementNode.DoesNotExist:
+            print(
+                f"ERROR: source requirement with URN {self.data['source_requirement_urn']} does not exist"
+            )
             raise Http404(
                 "ERROR: source requirement with URN {} does not exist".format(
                     self.data["source_requirement"]
@@ -581,7 +587,6 @@ class LibraryImporter:
 
     def check_and_import_dependencies(self):
         """Check and import library dependencies."""
-
         if not self._library.dependencies:
             return None
         for dependency_urn in self._library.dependencies:
@@ -660,8 +665,9 @@ class LibraryImporter:
         """Main method to import a library."""
         if (error_message := self.init()) is not None:
             return error_message  # This error check should be done when storing the Library but no after.
-
+        print(":: Getting Dependencies :::")
         error_msg = self.check_and_import_dependencies()
+        print(":::Dependencies are ok :::")
         if error_msg is not None:
             return error_msg
 
