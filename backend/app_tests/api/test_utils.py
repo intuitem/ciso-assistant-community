0bce41554cd31c69b9c25133a37f3e702f65c431
monsieurswag <mael.joumier@intuitem.com>
2024-08-08 14:30:57 +0200

    Formatter
    Delete useless file

--- backend/app_tests/api/test_utils.py
+++ backend/app_tests/api/test_utils.py
@@ -1027,7 +1027,7 @@ class EndpointTestsQueries:
             ), "reference endpoint is not accessible"
 
             content = json.loads(reference.content)
-            if isinstance(content, str) :
+            if isinstance(content, str):
                 content = json.loads(content)
 
             for object in content["framework"][object_name.lower().replace(" ", "_")][
