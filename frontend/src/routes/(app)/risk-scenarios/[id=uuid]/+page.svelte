79aec2c90de5f10e688db89d05c655bfd91d5104
monsieurswag <mael.joumier@intuitem.com>
2024-08-14 17:34:45 +0200

    Formatter + clean some code

--- frontend/src/routes/(app)/risk-scenarios/[id=uuid]/+page.svelte
+++ frontend/src/routes/(app)/risk-scenarios/[id=uuid]/+page.svelte
@@ -234,7 +234,9 @@
 			<p class="text-sm font-semibold text-gray-400">{m.qualification()}</p>
 			<p>
 				<span class="font-semibold">
-					{localItems()[toCamelCase(data.scenario.qualification)] || data.scenario.qualification || localItems()["undefined"]}
+					{localItems()[toCamelCase(data.scenario.qualification)] ||
+						data.scenario.qualification ||
+						localItems()['undefined']}
 				</span>
 			</p>
 		</div>
