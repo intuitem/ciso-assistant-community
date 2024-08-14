c1ffd9b639f67ddf28164e7018ec9b49ea56cb21
monsieurswag <mael.joumier@intuitem.com>
2024-08-14 17:34:45 +0200

    Formatter + clean some code

--- frontend/src/lib/components/RiskMatrix/RiskScenarioItem.svelte
+++ frontend/src/lib/components/RiskMatrix/RiskScenarioItem.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <p class="whitespace-nowrap">
-	{#if data.strength_of_knowledge && data.strength_of_knowledge.symbol !== undefined}<sup
-			class="font-mono text-lg">{data.strength_of_knowledge.symbol}</sup
-		>{/if}{data.rid}
+	{#if data.strength_of_knowledge && data.strength_of_knowledge.symbol !== undefined}
+		<sup class="font-mono text-lg">{data.strength_of_knowledge.symbol}</sup>
+	{/if}
+	<span>{data.rid}</span>
 </p>
