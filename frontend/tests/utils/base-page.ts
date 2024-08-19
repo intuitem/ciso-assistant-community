d661ddb3f7de46b314e586c9d275b407b33d8be5
monsieurswag <mael.joumier@intuitem.com>
2024-08-19 10:12:23 +0200

    Attempt to fix codacy test

--- frontend/tests/utils/base-page.ts
+++ frontend/tests/utils/base-page.ts
@@ -29,7 +29,8 @@ export abstract class BasePage {
 	}
 
 	async hasUrl() {
-		await expect(this.page).toHaveURL(RegExp(`${this.url}((\\?|#).*)?`));
+		const regex = `${this.url}((\\?|#).*)?`;
+		await expect(this.page).toHaveURL(new RegExp(regex));
 	}
 
 	async hasBreadcrumbPath(paths: (string | RegExp)[], fullPath = true, origin = 'Home') {
