47630e179a00c594394f5d02e4987099bf895667
Abderrahmane Smimite <smimite@gmail.com>
2024-08-28 13:09:37 +0200

    Model change: add control owner

--- backend/core/models.py
+++ backend/core/models.py
@@ -1238,6 +1238,12 @@ class AppliedControl(NameDescriptionMixin, FolderMixin, PublishInRootFolderMixin
         blank=True,
         verbose_name=_("Status"),
     )
+    owner = models.ManyToManyField(
+        User,
+        blank=True,
+        verbose_name=_("Owner"),
+        related_name="applied_controls",
+    )
     eta = models.DateField(
         blank=True,
         null=True,
