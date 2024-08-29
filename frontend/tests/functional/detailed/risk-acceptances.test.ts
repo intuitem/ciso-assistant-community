639edbd8ff2fafa394e5cbfec809aa091d990ce8
Alexis <alexis.bensoussan@efrei.net>
2024-08-29 20:30:04 +0200

    Lint

--- frontend/tests/functional/detailed/risk-acceptances.test.ts
+++ frontend/tests/functional/detailed/risk-acceptances.test.ts
@@ -3,93 +3,103 @@ import { TestContent, test, expect } from '../../utils/test-utils.js';
 let vars = TestContent.generateTestVars();
 let testObjectsData: { [k: string]: any } = TestContent.itemBuilder(vars);
 
-test('risk acceptances can be processed', async ({ logedPage, pages, usersPage, riskAcceptancesPage, page }) => {
-    const testRequirements = ["users", "folders", "projects", "riskAssessments", "riskScenarios"];
-    
-    for (let requirement of testRequirements) {
-        requirement += "Page";
-        const requiredPage = pages[requirement];
+test('risk acceptances can be processed', async ({
+	logedPage,
+	pages,
+	usersPage,
+	riskAcceptancesPage,
+	page
+}) => {
+	const testRequirements = ['users', 'folders', 'projects', 'riskAssessments', 'riskScenarios'];
 
-        await requiredPage.goto();
-        await requiredPage.hasUrl();
+	for (let requirement of testRequirements) {
+		requirement += 'Page';
+		const requiredPage = pages[requirement];
 
-        await requiredPage.createItem(
-            testObjectsData[requirement].build,
-            'dependency' in testObjectsData[requirement] ? testObjectsData[requirement].dependency : null
-        );
-    };
+		await requiredPage.goto();
+		await requiredPage.hasUrl();
 
-    await usersPage.goto();
-    await usersPage.editItemButton(vars.user.email).click();
-    await usersPage.form.fill({
-            user_groups: [
-                `${vars.folderName} - ${vars.usergroups.reader.name}`,
-            ]
-        });
-        await usersPage.form.saveButton.click();
-        await usersPage.isToastVisible(
-            'The user: ' + vars.user.email + ' has been successfully updated.+'
-    );
+		await requiredPage.createItem(
+			testObjectsData[requirement].build,
+			'dependency' in testObjectsData[requirement] ? testObjectsData[requirement].dependency : null
+		);
+	}
 
-    await riskAcceptancesPage.goto();
-    await riskAcceptancesPage.hasUrl();
+	await usersPage.goto();
+	await usersPage.editItemButton(vars.user.email).click();
+	await usersPage.form.fill({
+		user_groups: [`${vars.folderName} - ${vars.usergroups.reader.name}`]
+	});
+	await usersPage.form.saveButton.click();
+	await usersPage.isToastVisible(
+		'The user: ' + vars.user.email + ' has been successfully updated.+'
+	);
 
-    await riskAcceptancesPage.createItem({
-        ...testObjectsData.riskAcceptancesPage.build, 
-        name: testObjectsData.riskAcceptancesPage.build.name + ' accepted'
-    });
-    await riskAcceptancesPage.createItem({
-        ...testObjectsData.riskAcceptancesPage.build, 
-        name: testObjectsData.riskAcceptancesPage.build.name + ' rejected'
-    });
+	await riskAcceptancesPage.goto();
+	await riskAcceptancesPage.hasUrl();
 
-    // Check that a non approver user can't accept the risk acceptance
-    await riskAcceptancesPage.addButton.click();
-    await page.getByTestId('form-input-approver').click();
-    await expect(page.getByRole('option', { name: vars.user.email, exact: true }).first()).toBeHidden();
-    await riskAcceptancesPage.goto();
-    
-    // Accept
-    await riskAcceptancesPage.viewItemDetail(testObjectsData.riskAcceptancesPage.build.name + ' accepted');
-    await expect(riskAcceptancesPage.itemDetail.statusAcceptButton).toBeVisible();
-    await riskAcceptancesPage.itemDetail.statusAcceptButton.click();
+	await riskAcceptancesPage.createItem({
+		...testObjectsData.riskAcceptancesPage.build,
+		name: testObjectsData.riskAcceptancesPage.build.name + ' accepted'
+	});
+	await riskAcceptancesPage.createItem({
+		...testObjectsData.riskAcceptancesPage.build,
+		name: testObjectsData.riskAcceptancesPage.build.name + ' rejected'
+	});
 
-    await expect(riskAcceptancesPage.modalTitle).toBeVisible();
-    await expect(riskAcceptancesPage.modalCancelButton).toBeVisible();
-    await expect(riskAcceptancesPage.modalConfirmButton).toBeVisible();
-    await riskAcceptancesPage.modalConfirmButton.click();
+	// Check that a non approver user can't accept the risk acceptance
+	await riskAcceptancesPage.addButton.click();
+	await page.getByTestId('form-input-approver').click();
+	await expect(
+		page.getByRole('option', { name: vars.user.email, exact: true }).first()
+	).toBeHidden();
+	await riskAcceptancesPage.goto();
 
-    expect(await page.getByTestId('accepted-at-field-value')).toHaveText(/\d+\/\d+\/\d+,.+/);
-    await expect(riskAcceptancesPage.itemDetail.statusAcceptButton).not.toBeVisible();
-    await expect(riskAcceptancesPage.itemDetail.statusRejectButton).not.toBeVisible();
-    await expect(riskAcceptancesPage.itemDetail.statusRevokeButton).toBeVisible();
-    
-    // Revoke
-    await riskAcceptancesPage.itemDetail.statusRevokeButton.click();
+	// Accept
+	await riskAcceptancesPage.viewItemDetail(
+		testObjectsData.riskAcceptancesPage.build.name + ' accepted'
+	);
+	await expect(riskAcceptancesPage.itemDetail.statusAcceptButton).toBeVisible();
+	await riskAcceptancesPage.itemDetail.statusAcceptButton.click();
 
-    await expect(riskAcceptancesPage.modalTitle).toBeVisible();
-    await expect(riskAcceptancesPage.modalCancelButton).toBeVisible();
-    await expect(riskAcceptancesPage.modalConfirmButton).toBeVisible();
-    await riskAcceptancesPage.modalConfirmButton.click();
+	await expect(riskAcceptancesPage.modalTitle).toBeVisible();
+	await expect(riskAcceptancesPage.modalCancelButton).toBeVisible();
+	await expect(riskAcceptancesPage.modalConfirmButton).toBeVisible();
+	await riskAcceptancesPage.modalConfirmButton.click();
 
-    expect(await page.getByTestId('revoked-at-field-value')).toHaveText(/\d+\/\d+\/\d+,.+/);
-    await expect(riskAcceptancesPage.itemDetail.statusAcceptButton).not.toBeVisible();
-    await expect(riskAcceptancesPage.itemDetail.statusRejectButton).not.toBeVisible();
-    await expect(riskAcceptancesPage.itemDetail.statusRevokeButton).not.toBeVisible();
-    
-    // Reject
-    await riskAcceptancesPage.goto();
-    await riskAcceptancesPage.viewItemDetail(testObjectsData.riskAcceptancesPage.build.name + ' rejected');
-    await expect(riskAcceptancesPage.itemDetail.statusRejectButton).toBeVisible();
-    await riskAcceptancesPage.itemDetail.statusRejectButton.click();
-    
-    await expect(riskAcceptancesPage.modalTitle).toBeVisible();
-    await expect(riskAcceptancesPage.modalCancelButton).toBeVisible();
-    await expect(riskAcceptancesPage.modalConfirmButton).toBeVisible();
-    await riskAcceptancesPage.modalConfirmButton.click();
-    
-    expect(await page.getByTestId('rejected-at-field-value')).toHaveText(/\d+\/\d+\/\d+,.+/);
-    await expect(riskAcceptancesPage.itemDetail.statusAcceptButton).not.toBeVisible();
-    await expect(riskAcceptancesPage.itemDetail.statusRejectButton).not.toBeVisible();
-    await expect(riskAcceptancesPage.itemDetail.statusRevokeButton).not.toBeVisible();
+	expect(await page.getByTestId('accepted-at-field-value')).toHaveText(/\d+\/\d+\/\d+,.+/);
+	await expect(riskAcceptancesPage.itemDetail.statusAcceptButton).not.toBeVisible();
+	await expect(riskAcceptancesPage.itemDetail.statusRejectButton).not.toBeVisible();
+	await expect(riskAcceptancesPage.itemDetail.statusRevokeButton).toBeVisible();
+
+	// Revoke
+	await riskAcceptancesPage.itemDetail.statusRevokeButton.click();
+
+	await expect(riskAcceptancesPage.modalTitle).toBeVisible();
+	await expect(riskAcceptancesPage.modalCancelButton).toBeVisible();
+	await expect(riskAcceptancesPage.modalConfirmButton).toBeVisible();
+	await riskAcceptancesPage.modalConfirmButton.click();
+
+	expect(await page.getByTestId('revoked-at-field-value')).toHaveText(/\d+\/\d+\/\d+,.+/);
+	await expect(riskAcceptancesPage.itemDetail.statusAcceptButton).not.toBeVisible();
+	await expect(riskAcceptancesPage.itemDetail.statusRejectButton).not.toBeVisible();
+	await expect(riskAcceptancesPage.itemDetail.statusRevokeButton).not.toBeVisible();
+
+	// Reject
+	await riskAcceptancesPage.goto();
+	await riskAcceptancesPage.viewItemDetail(
+		testObjectsData.riskAcceptancesPage.build.name + ' rejected'
+	);
+	await expect(riskAcceptancesPage.itemDetail.statusRejectButton).toBeVisible();
+	await riskAcceptancesPage.itemDetail.statusRejectButton.click();
+
+	await expect(riskAcceptancesPage.modalTitle).toBeVisible();
+	await expect(riskAcceptancesPage.modalCancelButton).toBeVisible();
+	await expect(riskAcceptancesPage.modalConfirmButton).toBeVisible();
+	await riskAcceptancesPage.modalConfirmButton.click();
+
+	expect(await page.getByTestId('rejected-at-field-value')).toHaveText(/\d+\/\d+\/\d+,.+/);
+	await expect(riskAcceptancesPage.itemDetail.statusAcceptButton).not.toBeVisible();
+	await expect(riskAcceptancesPage.itemDetail.statusRejectButton).not.toBeVisible();
+	await expect(riskAcceptancesPage.itemDetail.statusRevokeButton).not.toBeVisible();
 });
