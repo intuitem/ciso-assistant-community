5279e48776ba2536e09a4e64c1469e0ec4f873f6
Abderrahmane Smimite <smimite@gmail.com>
2024-09-04 00:28:33 +0200

    format

--- frontend/src/routes/(app)/compliance-assessments/[id=uuid]/export/csv/+server.ts
+++ frontend/src/routes/(app)/compliance-assessments/[id=uuid]/export/csv/+server.ts
@@ -3,20 +3,20 @@ import { BASE_API_URL } from '$lib/utils/constants';
 import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 export const GET: RequestHandler = async ({ fetch, params }) => {
-  const URLModel = 'compliance-assessments';
-  const endpoint = `${BASE_API_URL}/${URLModel}/${params.id}/compliance_assessment_csv/`;
+	const URLModel = 'compliance-assessments';
+	const endpoint = `${BASE_API_URL}/${URLModel}/${params.id}/compliance_assessment_csv/`;
 
-  const res = await fetch(endpoint);
-  if (!res.ok) {
-    error(400, 'Error fetching the CSV file');
-  }
+	const res = await fetch(endpoint);
+	if (!res.ok) {
+		error(400, 'Error fetching the CSV file');
+	}
 
-  const fileName = `audit-${params.id}-${new Date().toISOString()}.csv`;
+	const fileName = `audit-${params.id}-${new Date().toISOString()}.csv`;
 
-  return new Response(await res.blob(), {
-    headers: {
-      'Content-Type': 'text/csv',
-      'Content-Disposition': `attachment; filename="${fileName}"`
-    }
-  });
+	return new Response(await res.blob(), {
+		headers: {
+			'Content-Type': 'text/csv',
+			'Content-Disposition': `attachment; filename="${fileName}"`
+		}
+	});
 };
