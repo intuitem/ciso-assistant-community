e93df07ef402ece5d6ed341ca42b4fb455ce4afe
Abderrahmane Smimite <smimite@gmail.com>
2024-09-04 00:26:36 +0200

    Add CSV export support for Audit

---
+++ frontend/src/routes/(app)/compliance-assessments/[id=uuid]/export/csv/+server.ts
@@ -0,0 +1,22 @@
+import { BASE_API_URL } from '$lib/utils/constants';
+
+import { error } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+export const GET: RequestHandler = async ({ fetch, params }) => {
+  const URLModel = 'compliance-assessments';
+  const endpoint = `${BASE_API_URL}/${URLModel}/${params.id}/compliance_assessment_csv/`;
+
+  const res = await fetch(endpoint);
+  if (!res.ok) {
+    error(400, 'Error fetching the CSV file');
+  }
+
+  const fileName = `audit-${params.id}-${new Date().toISOString()}.csv`;
+
+  return new Response(await res.blob(), {
+    headers: {
+      'Content-Type': 'text/csv',
+      'Content-Disposition': `attachment; filename="${fileName}"`
+    }
+  });
+};
