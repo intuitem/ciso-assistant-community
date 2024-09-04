7c542c86c6fd9fb2a0fb8c6d2a021b7e5899dc6f
Abderrahmane Smimite <smimite@gmail.com>
2024-09-04 19:58:19 +0200

    Linter warnings fixup

--- frontend/src/routes/(app)/x-rays/inspect/+page.svelte
+++ frontend/src/routes/(app)/x-rays/inspect/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import * as m from '$paraglide/messages';
 	export let data: PageData;
 	import TreeChart from '$lib/components/Chart/TreeChart.svelte';
-	import TreemapChart from '$lib/components/Chart/TreemapChart.svelte';
 </script>
 
 <div class="card bg-white p-6 shadow flex flex-col space-y-4 w-screen">
