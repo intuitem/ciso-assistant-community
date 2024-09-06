14b3179ac07758e908ec31b6341909ca2721f95e
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-09-06 16:34:19 +0200

    feat: add questionnaire/assessment mode in table mode

--- frontend/src/routes/(app)/compliance-assessments/[id=uuid]/table-mode/+page.svelte
+++ frontend/src/routes/(app)/compliance-assessments/[id=uuid]/table-mode/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+	import { RadioGroup, RadioItem, SlideToggle } from '@skeletonlabs/skeleton';
 	import * as m from '$paraglide/messages';
 	import { breadcrumbObject } from '$lib/utils/stores';
 	import {
@@ -67,12 +67,36 @@
 	function addColor(result: string, map: Record<string, string>) {
 		return map[result];
 	}
+
+	let questionnaireMode = true;
 </script>
 
 <div class="flex flex-col space-y-4 whitespace-pre-line">
 	<div
 		class="card px-6 py-4 bg-white flex flex-col justify-between shadow-lg w-full h-full space-y-2"
 	>
+	<div class="flex items-center justify-center space-x-4">
+		{#if questionnaireMode}
+			<p class="font-bold text-sm">{m.assessmentMode()}</p>
+		{:else}
+			<p class="font-bold text-sm text-green-500">{m.assessmentMode()}</p>
+		{/if}
+		<SlideToggle
+		name="questionnaireToggle"
+		class="flex flex-row items-center justify-center"
+		active="bg-primary-500"
+		background="bg-green-500"
+		bind:checked={questionnaireMode}
+		on:click={() => (questionnaireMode = !questionnaireMode)}
+		>
+		{#if questionnaireMode}
+			<p class="font-bold text-sm text-primary-500">{m.questionnaireMode()}</p>
+		{:else}
+			<p class="font-bold text-sm">{m.questionnaireMode()}</p>
+		{/if}
+		</SlideToggle>
+	</div>
+	
 		{#each data.requirement_assessments as requirementAssessment}
 			<div
 				class="flex flex-col items-center justify-center border pb-2 px-2 shadow-lg rounded-md space-y-2"
@@ -84,48 +108,50 @@
 					action="?/updateRequirementAssessment"
 					method="post"
 				>
-					<div class="flex flex-row w-full space-x-2">
-						<div class="flex flex-col items-center w-1/2">
-							<p class="flex items-center font-semibold">{m.status()}</p>
-							<RadioGroup class="w-full flex-wrap items-center">
-								{#each status_options as option}
-									<RadioItem
-										class="h-full"
-										id={option.id}
-										active={addColor(
-											requirementAssessment.status,
-											complianceStatusTailwindColorMap
-										)}
-										value={option.id}
-										bind:group={requirementAssessment.status}
-										name="status"
-										on:change={(event) => update(event, requirementAssessment, 'status')}
-										>{option.label}</RadioItem
-									>
-								{/each}
-							</RadioGroup>
+					{#if !questionnaireMode}
+						<div class="flex flex-row w-full space-x-2">
+							<div class="flex flex-col items-center w-1/2">
+								<p class="flex items-center font-semibold">{m.status()}</p>
+								<RadioGroup class="w-full flex-wrap items-center">
+									{#each status_options as option}
+										<RadioItem
+											class="h-full"
+											id={option.id}
+											active={addColor(
+												requirementAssessment.status,
+												complianceStatusTailwindColorMap
+											)}
+											value={option.id}
+											bind:group={requirementAssessment.status}
+											name="status"
+											on:change={(event) => update(event, requirementAssessment, 'status')}
+											>{option.label}</RadioItem
+										>
+									{/each}
+								</RadioGroup>
+							</div>
+							<div class="flex flex-col items-center w-1/2">
+								<p class="flex items-center font-semibold">{m.result()}</p>
+								<RadioGroup class="w-full flex-wrap items-center">
+									{#each result_options as option}
+										<RadioItem
+											class="h-full"
+											active={addColor(
+												requirementAssessment.result,
+												complianceResultTailwindColorMap
+											)}
+											id={option.id}
+											value={option.id}
+											bind:group={requirementAssessment.result}
+											name="result"
+											on:change={(event) => update(event, requirementAssessment, 'result')}
+											>{option.label}</RadioItem
+										>
+									{/each}
+								</RadioGroup>
+							</div>
 						</div>
-						<div class="flex flex-col items-center w-1/2">
-							<p class="flex items-center font-semibold">{m.result()}</p>
-							<RadioGroup class="w-full flex-wrap items-center">
-								{#each result_options as option}
-									<RadioItem
-										class="h-full"
-										active={addColor(
-											requirementAssessment.result,
-											complianceResultTailwindColorMap
-										)}
-										id={option.id}
-										value={option.id}
-										bind:group={requirementAssessment.result}
-										name="result"
-										on:change={(event) => update(event, requirementAssessment, 'result')}
-										>{option.label}</RadioItem
-									>
-								{/each}
-							</RadioGroup>
-						</div>
-					</div>
+					{/if}
 					<div class="flex flex-col w-full">
 						<p class="flex font-semibold">{m.description()}</p>
 						{requirementAssessment.description}
