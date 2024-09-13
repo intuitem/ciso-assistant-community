2b4aba876765c91dc24f8f0b72be6aba8ecfe202
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-09-13 12:20:34 +0200

    fix: confirm delete modal

--- frontend/src/lib/components/Modals/DeleteConfirmModal.svelte
+++ frontend/src/lib/components/Modals/DeleteConfirmModal.svelte
@@ -15,7 +15,7 @@
 
 	export let _form;
 	export let URLModel: urlModel;
-	export let formAction: string = '?delete'
+	export let formAction: string = '?/delete'
 	export let invalidateAll = true; // set to false to keep form data using muliple forms on a page
 	export let id: string;
 	import { superForm } from 'sveltekit-superforms';
@@ -38,7 +38,7 @@
 		</header>
 		<article>{$modalStore[0].body ?? '(body missing)'}</article>
 		<!-- Enable for debugging: -->
-		<form method="POST" action={formAction} class="modal-form {cForm}" use:enhance>
+		<form method="POST" action={formAction} class="modal-form {cForm}">
 			<!-- prettier-ignore -->
 			<footer class="modal-footer {parent.regionFooter}">
         <button type="button" class="btn {parent.buttonNeutral}" data-testid="delete-cancel-button" on:click={parent.onClose}>{m.cancel()}</button>
