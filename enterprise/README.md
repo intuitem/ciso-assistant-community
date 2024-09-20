2e4c4526e0cf9f0ee842e95a0b1fb9158df9f4e2
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2024-09-20 23:28:13 +0200

    fix/simplify docker compose for enterprise
    Use separate script for enterprise

--- enterprise/README.md
+++ enterprise/README.md
@@ -2,18 +2,18 @@
 
 To run CISO Assistant Enterprise locally in a straightforward way, you can use Docker compose.
 
-1. Make sure you are located in the top-level directory of the repository
+1. Make sure you are located in the enterprise directory of the repository
 
 2. Launch docker-compose script for prebuilt images:
 
 ```sh
-./docker-compose.sh -f enterprise/docker-compose.yml
+./docker-compose.sh
 ```
 
 _Alternatively_, you can use this variant to build the docker images for your specific architecture:
 
 ```sh
-./docker-compose-build.sh -f enterprise/docker-compose-build.yml
+./docker-compose-build.sh
 ```
 
 When asked for, enter your email and password for your superuser.
--- enterprise/docker-compose-build.sh
+++ enterprise/docker-compose-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DOCKER_COMPOSE_FILE=docker-compose-build.yml
+DOCKER_COMPOSE_FILE=enterprise/docker-compose-build.yml
 
 prepare_meta_file() {
 	VERSION=$(git describe --tags --always)
@@ -14,21 +14,27 @@ prepare_meta_file() {
 }
 
 # Check if the database already exists
+echo "building CISO Asisstant Enterprise version..."
+echo "this version is governed by intuitem's commercial license"
+
 if [ -f db/ciso-assistant.sqlite3 ]; then
     echo "The database seems already created."
-    echo "For successive runs, you can now use 'docker compose up'."
+    echo "For successive runs, you can now use 'docker compose -p enterprise up'."
 else
+	pushd ..
 	prepare_meta_file
 
 	# Build and start the containers
-	docker compose -f "${DOCKER_COMPOSE_FILE}" build
-	docker compose up -d
+	docker compose -p enterprise -f "${DOCKER_COMPOSE_FILE}" build
+	docker compose -p enterprise -f "${DOCKER_COMPOSE_FILE}" up -d
+
     echo "Giving some time for the database to be ready, please wait ..."
 	sleep 30
+
 	# Initialize the superuser account
 	echo "Initialize your superuser account..."
-	docker compose exec backend python manage.py createsuperuser
+	docker compose -p enterprise exec backend poetry run python manage.py createsuperuser
 
 	echo "Connect to CISO Assistant on https://localhost:8443"
-	echo "For successive runs, you can now use 'docker compose up'."
+	popd
 fi
