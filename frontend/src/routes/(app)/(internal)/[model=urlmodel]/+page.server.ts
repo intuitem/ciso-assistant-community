b4b7bf8bbb365d791bc947292d79ab9dc5d64c5f
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-09-22 12:35:19 +0200

    Use defaultWriteFormAction for default model route

--- frontend/src/routes/(app)/(internal)/[model=urlmodel]/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/[model=urlmodel]/+page.server.ts
@@ -16,6 +16,7 @@ import { setError, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { z } from 'zod';
 import type { PageServerLoad } from './$types';
+import { defaultWriteFormAction } from '$lib/utils/actions';
 
 export const load: PageServerLoad = async ({ params, fetch }) => {
 	const schema = z.object({ id: z.string().uuid() });
@@ -67,107 +68,7 @@ export const load: PageServerLoad = async ({ params, fetch }) => {
 
 export const actions: Actions = {
 	create: async (event) => {
-		const formData = await event.request.formData();
-
-		if (!formData) {
-			return fail(400, { form: null });
-		}
-
-		const schema = modelSchema(event.params.model!);
-		const form = await superValidate(formData, zod(schema));
-
-		if (!form.valid) {
-			console.error(form.errors);
-			return fail(400, { form: form });
-		}
-
-		const endpoint = `${BASE_API_URL}/${event.params.model}/`;
-
-		const model = getModelInfo(event.params.model!);
-
-		const fileFields: Record<string, File> = Object.fromEntries(
-			Object.entries(form.data).filter(([key]) => model.fileFields?.includes(key) ?? false)
-		);
-
-		Object.keys(fileFields).forEach((key) => {
-			form.data[key] = undefined;
-		});
-
-		const requestInitOptions: RequestInit = {
-			method: 'POST',
-			body: JSON.stringify(form.data)
-		};
-
-		const res = await event.fetch(endpoint, requestInitOptions);
-
-		if (!res.ok) {
-			const response: Record<string, any> = await res.json();
-			console.error(response);
-			if (response.warning) {
-				setFlash({ type: 'warning', message: response.warning }, event);
-				return { createForm: form };
-			}
-			if (response.error) {
-				setFlash({ type: 'error', message: response.error }, event);
-				return { createForm: form };
-			}
-			Object.entries(response).forEach(([key, value]) => {
-				setError(form, key, value);
-			});
-			return fail(400, { form: form });
-		}
-
-		const createdObject = await res.json();
-
-		if (fileFields) {
-			for (const [, file] of Object.entries(fileFields)) {
-				if (!file) continue;
-				if (file.size <= 0) continue;
-				const fileUploadEndpoint = `${BASE_API_URL}/${event.params.model}/${createdObject.id}/upload/`;
-				const fileUploadRequestInitOptions: RequestInit = {
-					headers: {
-						'Content-Disposition': `attachment; filename=${encodeURIComponent(file.name)}`
-					},
-					method: 'POST',
-					body: file
-				};
-				const fileUploadRes = await event.fetch(fileUploadEndpoint, fileUploadRequestInitOptions);
-				if (!fileUploadRes.ok) {
-					const response = await fileUploadRes.json();
-					console.error(response);
-					if (response.non_field_errors) {
-						setError(form, 'non_field_errors', response.non_field_errors);
-					}
-					return fail(400, { form: form });
-				}
-			}
-		}
-
-		const modelVerboseName: string = event.params.model
-			? urlParamModelVerboseName(event.params.model)
-			: '';
-		// TODO: reference newly created object
-		if (modelVerboseName === 'User') {
-			setFlash(
-				{
-					type: 'success',
-					message: m.successfullyCreatedObject({
-						object: safeTranslate(modelVerboseName).toLowerCase()
-					})
-				},
-				event
-			);
-		}
-		setFlash(
-			{
-				type: 'success',
-				message: m.successfullyCreatedObject({
-					object: safeTranslate(modelVerboseName).toLowerCase()
-				})
-			},
-			event
-		);
-		return { createForm: form };
+		return defaultWriteFormAction({ event, urlModel: event.params.model!, action: 'create' });
 	},
 	delete: async (event) => {
 		const formData = await event.request.formData();
