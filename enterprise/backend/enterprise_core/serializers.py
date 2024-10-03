705173fa2af9a8cacf70674eb5b11979a8ea2ecf
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-10-03 19:42:56 +0200

    DRY enterprise user write serializer

--- enterprise/backend/enterprise_core/serializers.py
+++ enterprise/backend/enterprise_core/serializers.py
@@ -21,11 +21,29 @@ class FolderWriteSerializer(BaseModelSerializer):
         ]
 
 
-class UserWriteSerializer(CommunityUserWriteSerializer):
-    def update(self, instance: User, validated_data):
+class EditorPermissionMixin:
+    @staticmethod
+    def check_editor_permissions(instance, group):
         editor_prefixes = {"add_", "change_", "delete_"}
         editors = User.get_editors()
         seats = settings.LICENSE_SEATS
+
+        perms = group.permissions
+        if any(perm.startswith(prefix) for prefix in editor_prefixes for perm in perms):
+            logger.info("Adding editor permissions to user", user=instance, group=group)
+            if instance not in editors and len(editors) >= seats:
+                logger.error(
+                    "License seats exceeded, cannot add editor user groups to user",
+                    user=instance,
+                    seats=seats,
+                )
+                raise serializers.ValidationError(
+                    {"user_groups": "errorLicenseSeatsExceeded"}
+                )
+
+
+class UserWriteSerializer(CommunityUserWriteSerializer, EditorPermissionMixin):
+    def _update_user_groups(self, instance, validated_data):
         if validated_data.get("user_groups"):
             logger.info(
                 "Updating user groups",
@@ -33,55 +51,14 @@ class UserWriteSerializer(CommunityUserWriteSerializer):
                 groups=validated_data["user_groups"],
             )
             for group in validated_data["user_groups"]:
-                perms = group.permissions
-                if any(
-                    perm.startswith(prefix)
-                    for prefix in editor_prefixes
-                    for perm in perms
-                ):
-                    logger.info(
-                        "Adding editor permissions to user", user=instance, group=group
-                    )
-                    if instance not in editors and len(editors) >= seats:
-                        logger.error(
-                            "License seats exceeded, cannot add editor user groups to user",
-                            user=instance,
-                            seats=seats,
-                        )
-                        raise serializers.ValidationError(
-                            {"user_groups": "errorLicenseSeatsExceeded"}
-                        )
+                self.check_editor_permissions(instance, group)
+
+    def update(self, instance: User, validated_data):
+        self._update_user_groups(instance, validated_data)
         return super().update(instance, validated_data)
 
     def partial_update(self, instance, validated_data):
-        editor_prefixes = {"add_", "change_", "delete_"}
-        editors = User.get_editors()
-        seats = settings.LICENSE_SEATS
-        if validated_data.get("user_groups"):
-            logger.info(
-                "Updating user groups",
-                user=instance,
-                groups=validated_data["user_groups"],
-            )
-            for group in validated_data["user_groups"]:
-                perms = group.permissions
-                if any(
-                    perm.startswith(prefix)
-                    for prefix in editor_prefixes
-                    for perm in perms
-                ):
-                    logger.info(
-                        "Adding editor permissions to user", user=instance, group=group
-                    )
-                    if instance not in editors and len(editors) >= seats:
-                        logger.error(
-                            "License seats exceeded, cannot add editor user groups to user",
-                            user=instance,
-                            seats=seats,
-                        )
-                        raise serializers.ValidationError(
-                            {"user_groups": "errorLicenseSeatsExceeded"}
-                        )
+        self._update_user_groups(instance, validated_data)
         return super().partial_update(instance, validated_data)
 
 
