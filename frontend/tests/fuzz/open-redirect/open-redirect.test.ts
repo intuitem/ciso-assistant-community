e704a210ac0baeffb351f15aae1f7f287dbd5b3a
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-10-07 07:35:43 +0200

    Improve open redirect fuzz tests
    - Set expect timeout to 1 second
    - Display progress at each individual step
    - Add test.slow() annotation

--- frontend/tests/fuzz/open-redirect/open-redirect.test.ts
+++ frontend/tests/fuzz/open-redirect/open-redirect.test.ts
@@ -2,7 +2,9 @@ import { expect, test } from '../../utils/test-utils.js';
 
 import { readFileSync } from 'fs';
 
-test('open redirect fuzz tests', async ({ logedPage }) => {
+test('prepare open redirect fuzz tests', async ({ logedPage }) => {
+	test.slow();
+
 	await test.step('fuzz open redirect', async () => {
 		await logedPage.page.getByRole('button', { name: 'Organization' }).click();
 		await logedPage.page.getByTestId('accordion-item-folders').click();
@@ -11,24 +13,28 @@ test('open redirect fuzz tests', async ({ logedPage }) => {
 			.getByTestId('form-input-name')
 			.fill('Irure commodo consequat fugiat elit mollit in aute et incididunt et tempor.');
 		await logedPage.page.getByTestId('save-button').click();
+	});
 
-		const payloadsFile = './tests/fuzz/open-redirect/payloads.txt';
-		const payloads = readFileSync(payloadsFile, 'utf8').split('\n');
+	const payloadsFile = './tests/fuzz/open-redirect/payloads.txt';
+	const payloads = readFileSync(payloadsFile, 'utf8').split('\n');
 
-		const href = await logedPage.page
-			.getByTestId('tablerow-edit-button')
-			.getAttribute('href')
-			.then((href) => href!.split('?')[0]);
+	const href = await logedPage.page
+		.getByTestId('tablerow-edit-button')
+		.getAttribute('href')
+		.then((href) => href!.split('?')[0]);
 
-		const currentURL = logedPage.page.url();
-		const parsedURL = new URL(currentURL);
-		const hostname = parsedURL.hostname;
+	const currentURL = logedPage.page.url();
+	const parsedURL = new URL(currentURL);
+	const hostname = parsedURL.hostname;
 
-		for (const payload of payloads) {
+	for await (const [index, payload] of payloads.entries()) {
+		await test.step(`fuzz open redirect with payload: ${payload} (${index + 1}/${payloads.length})`, async () => {
 			await logedPage.page.goto(`${href}?next=${payload}`);
 			await logedPage.page.getByTestId('cancel-button').click();
 			// Redirecting to next MUST not redirect to another domain
-			await expect(logedPage.page).toHaveURL(new RegExp(`^.*${hostname}.*$`));
-		}
-	});
+			await expect
+				.soft(logedPage.page)
+				.toHaveURL(new RegExp(`^.*${hostname}.*$`), { timeout: 1000 });
+		});
+	}
 });
