44d8912e023e0cd6417258cbcea82539c059369e
monsieurswag <mael.joumier@intuitem.com>
2024-10-09 11:28:19 +0200

    Add version check during backup import and fix broken instance when loading corrupted backup

--- backend/serdes/views.py
+++ backend/serdes/views.py
@@ -2,6 +2,7 @@ import gzip
 import io
 import json
 import sys
+import re
 from datetime import datetime
 
 from django.core import management
@@ -12,8 +13,10 @@ from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ciso_assistant.settings import VERSION
+from ciso_assistant.settings import VERSION, SQLITE_FILE
 from serdes.serializers import LoadBackupSerializer
+
+GZIP_MAGIC_NUMBER = b"\x1f\x8b"
 
 
 class ExportBackupView(APIView):
@@ -56,33 +59,83 @@ class LoadBackupView(APIView):
 
     def post(self, request, *args, **kwargs):
         if not request.user.has_backup_permission:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+            return Response({}, status=status.HTTP_403_FORBIDDEN)
         if request.data:
             backup_file = request.data["file"]
-            is_json = backup_file.name.split(".")[-1].lower() == "json"
             data = backup_file.read()
-            decompressed_data = data if is_json else gzip.decompress(data)
+            is_gzip = data.startswith(GZIP_MAGIC_NUMBER)
+            full_decompressed_data = gzip.decompress(data) if is_gzip else data
             # Performances could be improved (by avoiding the json.loads + json.dumps calls with a direct raw manipulation on the JSON body)
             # But performances of the backup loading is not that much important.
-            decompressed_data = json.loads(decompressed_data)[1]
+            full_decompressed_data = json.loads(full_decompressed_data)
+            metadata, decompressed_data = full_decompressed_data
+            metadata = metadata["meta"]
+
+            backup_version = None
+            for metadata_part in metadata:
+                backup_version = metadata_part.get("media_version")
+                if backup_version is not None:
+                    break
+
+            if backup_version is None:
+                return Response({}, status=status.HTTP_400_BAD_REQUEST)
+
+            VERSION_REGEX = r"^v[0-9]+\.[0-9]+\.[0-9]+"
+            match = re.match(VERSION_REGEX, backup_version)
+            if match is None:
+                return Response({}, status=status.HTTP_400_BAD_REQUEST)
+
+            backup_version = match.group()
+            current_version = VERSION.split("-")[0]
+
+            if current_version.lower() == "dev":
+                current_version = "v0.0.0"
+
+            backup_version = [int(num) for num in backup_version.lstrip("v").split(".")]
+            current_version = [
+                int(num) for num in current_version.lstrip("v").split(".")
+            ]
+            # All versions are composed of 3 numbers (see git tag)
+            for i in range(3):
+                if backup_version[i] > current_version[i]:
+                    # Refuse to import the backup and ask to update the instance before importing the backup
+                    return Response(
+                        {"error": "GreaterBackupVersion"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+
             decompressed_data = json.dumps(decompressed_data)
+
+            with open(SQLITE_FILE, "rb") as database_file:
+                database_recover_data = database_file.read()
 
             sys.stdin = io.StringIO(decompressed_data)
             request.session.flush()
             management.call_command("flush", interactive=False)
-            # Here we load the data from stdin
-            management.call_command(
-                loaddata.Command(),
-                "-",
-                format="json",
-                verbosity=0,
-                exclude=[
-                    "contenttypes",
-                    "auth.permission",
-                    "sessions.session",
-                    "iam.ssosettings",
-                    "knox.authtoken",
-                ],
-            )
-            return Response(status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+            try:
+                # Here we load the data from stdin
+                management.call_command(
+                    loaddata.Command(),
+                    "-",
+                    format="json",
+                    verbosity=0,
+                    exclude=[
+                        "contenttypes",
+                        "auth.permission",
+                        "sessions.session",
+                        "iam.ssosettings",
+                        "knox.authtoken",
+                    ],
+                )
+            except:
+                with open(SQLITE_FILE, "wb") as database_file:
+                    database_file.write(database_recover_data)
+
+                if backup_version != current_version:
+                    return Response(
+                        {"error": "LowerBackupVersion"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+                return Response({}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({}, status=status.HTTP_200_OK)
+        return Response({}, status=status.HTTP_400_BAD_REQUEST)
