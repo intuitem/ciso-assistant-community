0fbd3f77d574dbe1ae80b67f3419c129d538155d
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-10-10 14:17:09 +0200

    Implement use_images_unauthenticated in frontend

--- enterprise/frontend/src/lib/utils/client-settings.ts
+++ enterprise/frontend/src/lib/utils/client-settings.ts
@@ -2,7 +2,8 @@ import { z } from 'zod';
 
 export const ClientSettingsSchema = z.object({
 	id: z.string().uuid(),
-	name: z.string().optional().nullable(),
+	name: z.string().optional().nullable().default(''),
 	logo: z.any().optional().nullable(),
-	favicon: z.any().optional().nullable()
+	favicon: z.any().optional().nullable(),
+  show_images_unauthenticated: z.boolean().default(true),
 });
