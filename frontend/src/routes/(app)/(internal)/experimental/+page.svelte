457a61b67f95edae9f55ad449815c634e153d36e
Abderrahmane Smimite <smimite@gmail.com>
2024-10-12 11:13:12 +0200

    prettier view for experimental features

--- frontend/src/routes/(app)/(internal)/experimental/+page.svelte
+++ frontend/src/routes/(app)/(internal)/experimental/+page.svelte
@@ -1,16 +1,18 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import Article from '$lib/components/DataViz/Article.svelte';
+</script>
 
-<div class="bg-white grid p-4 grid-cols-3 space-x-4">
-	<a
-		class="bg-purple-600 text-white text-lg p-4 rounded-lg shadow-lg hover:bg-purple-500"
-		href="experimental/graph">Applied controls graph</a
-	>
-	<a
-		class="bg-purple-600 text-white text-lg p-4 rounded-lg shadow-lg hover:bg-purple-500"
-		href="experimental/priority">Priority scatter</a
-	>
-	<a
-		class="bg-purple-600 text-white text-lg p-4 rounded-lg shadow-lg hover:bg-purple-500"
-		href="experimental/timeline">Timeline</a
-	>
+<div class="bg-white grid p-8 grid-cols-2 space-x-8">
+	<Article
+		title="Controls Graph"
+		desc="Visualize dependencies and the impact of controls"
+		link="experimental/graph"
+		tags={['priority', 'controls']}
+	></Article>
+	<Article
+		title="Controls Timeline"
+		desc="Visualize applied controls as a timeline if start date and ETA are provided"
+		link="experimental/timeline"
+		tags={['schedule', 'controls']}
+	/>
 </div>
