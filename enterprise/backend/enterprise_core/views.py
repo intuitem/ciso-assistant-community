2f1052abbcd85bfbddb3b1499836d4e17be447f6
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-10-14 16:52:26 +0200

    Reset main entity name to default when client settings name is cleared

--- enterprise/backend/enterprise_core/views.py
+++ enterprise/backend/enterprise_core/views.py
@@ -14,6 +14,7 @@ from rest_framework.response import Response
 from django.conf import settings
 
 from core.views import BaseModelViewSet
+from core.utils import MAIN_ENTITY_DEFAULT_NAME
 from iam.models import User
 from tprm.models import Entity
 
@@ -42,16 +43,28 @@ class ClientSettingsViewSet(BaseModelViewSet):
 
     def perform_update(self, serializer):
         instance = serializer.save()
+        self._update_main_entity_name(instance)
+
+    def _update_main_entity_name(self, instance):
+        main_entity = Entity.get_main_entity()
+
         if instance.name:
-            main_entity = Entity.get_main_entity()
-            logger.info(
-                "Updating main entity name", entity=main_entity, name=instance.name
-            )
-            main_entity.name = instance.name
+            self._set_main_entity_name(main_entity, instance.name)
+        elif main_entity.name != MAIN_ENTITY_DEFAULT_NAME:
+            self._set_main_entity_name(main_entity, MAIN_ENTITY_DEFAULT_NAME)
+
+    def _set_main_entity_name(self, main_entity, new_name):
+        if main_entity.name == new_name:
+            return
+
+        logger.info("Updating main entity name", entity=main_entity, name=new_name)
+        try:
+            main_entity.name = new_name
             main_entity.save()
-            logger.info(
-                "Main entity name updated", entity=main_entity, name=instance.name
-            )
+            logger.info("Main entity name updated", entity=main_entity, name=new_name)
+        except Exception as e:
+            logger.error("An error occurred while renaming main entity", exc_info=e)
+            raise
 
     @action(methods=["get"], detail=False, permission_classes=[AllowAny])
     def info(self, request):
