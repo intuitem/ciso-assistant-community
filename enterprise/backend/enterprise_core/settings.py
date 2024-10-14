91decf4c919401ce7524484c577f18929502ce26
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-10-14 12:32:25 +0200

    Switch to timezone naive timestamps to manage license expiration

--- enterprise/backend/enterprise_core/settings.py
+++ enterprise/backend/enterprise_core/settings.py
@@ -397,10 +397,14 @@ MODULES["enterprise_core"] = {
 }
 
 logger.info(
-    "Enterprise startup info", feature_flags=FEATURE_FLAGS, module_paths=MODULE_PATHS
+    "Enterprise startup information",
+    feature_flags=FEATURE_FLAGS,
+    module_paths=MODULE_PATHS,
 )
 
 LICENSE_SEATS = int(os.environ.get("LICENSE_SEATS", 1))
 LICENSE_EXPIRATION = os.environ.get("LICENSE_EXPIRATION", "unset")
+
+logger.info("License information", seats=LICENSE_SEATS, expiration=LICENSE_EXPIRATION)
 
 INSTALLED_APPS.append("enterprise_core")
