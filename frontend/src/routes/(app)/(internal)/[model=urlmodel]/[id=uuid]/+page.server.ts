fc7135bee3dd1a665d31b57105307d57500f054b
monsieurswag <mael.joumier@intuitem.com>
2024-10-15 11:19:19 +0200

    Formatter

--- frontend/src/routes/(app)/(internal)/[model=urlmodel]/[id=uuid]/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/[model=urlmodel]/[id=uuid]/+page.server.ts
@@ -10,7 +10,11 @@ import { message, setError, superValidate } from 'sveltekit-superforms';
 import { setFlash } from 'sveltekit-flash-message/server';
 import { zod } from 'sveltekit-superforms/adapters';
 import { z } from 'zod';
-import { nestedDeleteFormAction, nestedWriteFormAction, handleErrorResponse } from '$lib/utils/actions';
+import {
+	nestedDeleteFormAction,
+	nestedWriteFormAction,
+	handleErrorResponse
+} from '$lib/utils/actions';
 import { modelSchema } from '$lib/utils/schemas';
 
 import { loadDetail } from '$lib/utils/load';
