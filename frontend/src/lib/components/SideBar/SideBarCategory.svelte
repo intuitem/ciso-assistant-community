ad4ede89c290b79a8214b217d439c61b23a5109f
Axxiar <galoualexis@gmail.com>
2024-10-15 15:35:52 +0200

    french thirdParty translation

--- frontend/src/lib/components/SideBar/SideBarCategory.svelte
+++ frontend/src/lib/components/SideBar/SideBarCategory.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	export let item: any; // Specify the type of 'item' as a key of 'Sidebar'
 	import { safeTranslate } from '$lib/utils/i18n';
-	import { localItems } from '$lib/utils/locales';
-	import { languageTag } from '$paraglide/runtime';
 </script>
 
 <span class="whitespace-nowrap text-primary-800 font-semibold uppercase tracking-tighter text-xs">
