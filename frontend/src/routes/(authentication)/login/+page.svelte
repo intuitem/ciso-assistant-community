4fb1a1634780270cadc0ba630cf678d314aea695
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-10-24 21:04:20 +0200

    Implement mfa_authenticate flow

--- frontend/src/routes/(authentication)/login/+page.svelte
+++ frontend/src/routes/(authentication)/login/+page.svelte
@@ -1,10 +1,40 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import type { ActionData, PageData } from './$types';
 	import Logo from '$lib/components/Logo/Logo.svelte';
 	import Greetings from './Greetings.svelte';
 	import FormCard from './FormCard.svelte';
+	import MfaAuthenticateModal from './mfa/components/MFAAuthenticateModal.svelte';
+	import {
+		getModalStore,
+		type ModalComponent,
+		type ModalSettings,
+		type ModalStore
+	} from '@skeletonlabs/skeleton';
 
 	export let data: PageData;
+	export let form: ActionData;
+
+	const modalStore: ModalStore = getModalStore();
+
+	function modalMFAAuthenticate(): void {
+		const modalComponent: ModalComponent = {
+			ref: MfaAuthenticateModal,
+			props: {
+				_form: data.mfaAuthenticateForm,
+				formAction: '?/mfaAuthenticate'
+			}
+		};
+		const modal: ModalSettings = {
+			type: 'component',
+			component: modalComponent,
+			// Data
+			title: '_mfaAuthenticateTitle',
+			body: '_mfaAuthenticateMessage'
+		};
+		modalStore.trigger(modal);
+	}
+
+	$: form && form.mfaFlow ? modalMFAAuthenticate() : null;
 </script>
 
 <div class="relative h-screen w-screen bg-slate-200">
