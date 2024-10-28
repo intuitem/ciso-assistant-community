753da135e690f526aeeeea125738f042576360d3
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-10-28 11:33:35 +0100

    feat: prepare front global setting

---
+++ frontend/src/lib/components/Forms/ModelForm/globalSettingForm.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Select from "../Select.svelte";
+
+    // export let form: SuperValidated<any>;
+	// export let model: ModelInfo;
+	// export let cacheLocks: Record<string, CacheLock> = {};
+	// export let formDataCache: Record<string, any> = {};
+	// export let data: any = {};
+</script>
+
+hello
+
+<!-- <Select
+    form={form}
+    field="provider"
+    cacheLock={cacheLocks['provider']}
+    bind:cachedValue={formDataCache['provider']}
+    options={model.selectOptions['provider']}
+    label={m.provider()}
+    disabled={!data.is_enabled}
+/> -->
