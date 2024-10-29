75d351d69d54664c5609dbf27e0fddebe84491b9
Abderrahmane Smimite <smimite@gmail.com>
2024-10-29 07:52:51 +0100

    wip

---
+++ frontend/src/routes/(app)/(internal)/assets/graph/+page.server.ts
@@ -0,0 +1,13 @@
+
+import { BASE_API_URL } from '$lib/utils/constants';
+
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ fetch }) => {
+  const endpoint = `${BASE_API_URL}/assets/tree/`;
+
+  const res = await fetch(endpoint);
+  const data = await res.json();
+
+  return { data };
+}) satisfies PageServerLoad;
