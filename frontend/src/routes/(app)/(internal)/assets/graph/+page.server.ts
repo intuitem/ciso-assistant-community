38e7efd1c640b144ee349ae45bfda06f310fdae6
Abderrahmane Smimite <smimite@gmail.com>
2024-10-31 11:22:04 +0100

    Run formatter

--- frontend/src/routes/(app)/(internal)/assets/graph/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/assets/graph/+page.server.ts
@@ -1,13 +1,12 @@
-
 import { BASE_API_URL } from '$lib/utils/constants';
 
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ fetch }) => {
-  const endpoint = `${BASE_API_URL}/assets/graph/`;
+	const endpoint = `${BASE_API_URL}/assets/graph/`;
 
-  const res = await fetch(endpoint);
-  const data = await res.json();
+	const res = await fetch(endpoint);
+	const data = await res.json();
 
-  return { data };
+	return { data };
 }) satisfies PageServerLoad;
