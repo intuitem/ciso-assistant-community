a8a9bcd1b4003c090412837f49c7576270ac9c7f
Abderrahmane Smimite <smimite@gmail.com>
2024-10-31 10:30:42 +0100

    Basis for Assets graph

--- frontend/src/routes/(app)/(internal)/assets/graph/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/assets/graph/+page.server.ts
@@ -4,7 +4,7 @@ import { BASE_API_URL } from '$lib/utils/constants';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ fetch }) => {
-  const endpoint = `${BASE_API_URL}/assets/tree/`;
+  const endpoint = `${BASE_API_URL}/assets/graph/`;
 
   const res = await fetch(endpoint);
   const data = await res.json();
