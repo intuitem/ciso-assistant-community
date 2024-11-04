5540be4dfc07a55b5dc29f4ff2d96222f3577983
Abderrahmane Smimite <smimite@gmail.com>
2024-11-04 20:08:49 +0100

    presentable point

--- frontend/src/routes/(app)/(internal)/my-assignments/+page.svelte
+++ frontend/src/routes/(app)/(internal)/my-assignments/+page.svelte
@@ -1,60 +1,135 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	export let data: PageData;
+	import { safeTranslate } from '$lib/utils/i18n';
+	import { formatDateOrDateTime } from '$lib/utils/datetime';
+	import { languageTag } from '$paraglide/runtime';
 </script>
 
 {@debug data}
-<div class="grid gap-2 p-4">
-	<div>assigned controls</div>
-	<table>
-		<thead>
-			<th>name</th>
-		</thead>
-		<tbody>
-			{#each data.data.controls as ac}
+<div class="p-4 text-lg font-black underline underline-offset-4 decoration-4 decoration-pink-500">
+	Controls
+</div>
+<div class="p-4 text-left">
+	<div class="relative overflow-x-auto shadow-lg rounded-lg">
+		<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+			<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
 				<tr>
-					<td>{ac.name}</td>
+					<th scope="col" class="px-6 py-3"> Name </th>
+					<th scope="col" class="px-6 py-3"> Function </th>
+					<th scope="col" class="px-6 py-3"> Status </th>
+					<th scope="col" class="px-6 py-3"> ETA </th>
 				</tr>
-			{/each}</tbody
-		>
-	</table>
-	<div>assigned audits</div>
-	<table>
-		<thead>
-			<th>name</th>
-		</thead>
-		<tbody>
-			{#each data.data.audits as ca}
+			</thead>
+			<tbody>
+				{#each data.data.controls as ac}
+					<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+						<th
+							scope="row"
+							class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+						>
+							<a href="/applied-controls/{ac.id}" class="hover:text-violet-400">{ac.name}</a>
+						</th>
+						<td class="px-6 py-4"> {ac.csf_function ?? '-'} </td>
+						<td class="px-6 py-4"> {safeTranslate(ac.status) ?? '-'} </td>
+						<td class="px-6 py-4"> {formatDateOrDateTime(ac.eta, languageTag()) ?? '-'} </td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+</div>
+
+<div class="p-4 text-lg font-black underline underline-offset-4 decoration-4 decoration-green-300">
+	Audits
+</div>
+<div class="p-4 text-left">
+	<div class="relative overflow-x-auto shadow-lg rounded-lg">
+		<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+			<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
 				<tr>
-					<td>{ca.name}</td>
+					<th scope="col" class="px-6 py-3"> Name </th>
+					<th scope="col" class="px-6 py-3"> Status </th>
+					<th scope="col" class="px-6 py-3"> ETA </th>
 				</tr>
-			{/each}</tbody
-		>
-	</table>
-	<div>assigned risk assessments</div>
-	<table>
-		<thead>
-			<th>name</th>
-		</thead>
-		<tbody>
-			{#each data.data.risk_assessments as ra}
+			</thead>
+			<tbody>
+				{#each data.data.audits as ca}
+					<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+						<th
+							scope="row"
+							class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+						>
+							<a href="/compliance-assessments/{ca.id}" class="hover:text-violet-400">{ca.name}</a>
+						</th>
+						<td class="px-6 py-4"> {safeTranslate(ca.status) ?? '-'} </td>
+						<td class="px-6 py-4"> {formatDateOrDateTime(ca.eta, languageTag()) ?? '-'} </td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+</div>
+
+<div class="p-4 text-lg font-black underline underline-offset-4 decoration-4 decoration-orange-300">
+	Risk assessments
+</div>
+<div class="p-4 text-left">
+	<div class="relative overflow-x-auto shadow-lg rounded-lg">
+		<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+			<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
 				<tr>
-					<td>{ra.name}</td>
+					<th scope="col" class="px-6 py-3"> Name </th>
+					<th scope="col" class="px-6 py-3"> Status </th>
+					<th scope="col" class="px-6 py-3"> ETA </th>
 				</tr>
-			{/each}</tbody
-		>
-	</table>
-	<div>assigned scenarios</div>
-	<table>
-		<thead>
-			<th>name</th>
-		</thead>
-		<tbody>
-			{#each data.data.risk_scenarios as rs}
+			</thead>
+			<tbody>
+				{#each data.data.risk_assessments as ra}
+					<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+						<th
+							scope="row"
+							class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+						>
+							<a href="/risk-assessments/{ra.id}" class="hover:text-violet-400">{ra.name}</a>
+						</th>
+						<td class="px-6 py-4"> {safeTranslate(ra.status) ?? '-'} </td>
+						<td class="px-6 py-4"> {formatDateOrDateTime(ra.eta, languageTag()) ?? '-'} </td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+</div>
+<div class="p-4 text-lg font-black underline underline-offset-4 decoration-4 decoration-purple-300">
+	Owned risks
+</div>
+<div class="p-4 text-left">
+	<div class="relative overflow-x-auto shadow-lg rounded-lg">
+		<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+			<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
 				<tr>
-					<td>{rs.name}</td>
+					<th scope="col" class="px-6 py-3"> Name </th>
+					<th scope="col" class="px-6 py-3"> Status </th>
+					<th scope="col" class="px-6 py-3"> Current level </th>
+					<th scope="col" class="px-6 py-3"> Residual level </th>
 				</tr>
-			{/each}</tbody
-		>
-	</table>
+			</thead>
+			<tbody>
+				{#each data.data.risk_scenarios as rs}
+					<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+						<th
+							scope="row"
+							class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+						>
+							<a href="/risk-scenarios/{rs.id}" class="hover:text-violet-400">{rs.name}</a>
+						</th>
+						<td class="px-6 py-4"> {safeTranslate(rs.treatment) ?? '-'} </td>
+						<td class="px-6 py-4"> {safeTranslate(rs.current_level.name) ?? '-'}</td>
+						<td class="px-6 py-4"> {safeTranslate(rs.residual_level.name) ?? '-'}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
 </div>
