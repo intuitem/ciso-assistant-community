fba2d9dcf747927b9a292685f6bdd3d1b0281123
Abderrahmane Smimite <smimite@gmail.com>
2024-11-16 22:17:00 +0100

    Formatter

--- frontend/src/routes/(app)/(internal)/risk-scenarios/[id=uuid]/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/risk-scenarios/[id=uuid]/+page.server.ts
@@ -7,62 +7,62 @@ import type { urlModel } from '$lib/utils/types';
 import { table } from 'console';
 
 export const load = (async ({ fetch, params }) => {
-  const URLModel = 'risk-scenarios';
-  const baseEndpoint = `${BASE_API_URL}/${URLModel}/${params.id}/`;
-  const objectEndpoint = `${BASE_API_URL}/${URLModel}/${params.id}/object/`;
-  const object = await fetch(objectEndpoint).then((res) => res.json());
-  const scenario = await fetch(baseEndpoint).then((res) => res.json());
+	const URLModel = 'risk-scenarios';
+	const baseEndpoint = `${BASE_API_URL}/${URLModel}/${params.id}/`;
+	const objectEndpoint = `${BASE_API_URL}/${URLModel}/${params.id}/object/`;
+	const object = await fetch(objectEndpoint).then((res) => res.json());
+	const scenario = await fetch(baseEndpoint).then((res) => res.json());
 
-  const tables: Record<string, any> = {};
+	const tables: Record<string, any> = {};
 
-  await Promise.all(
-    ['assets', 'threats', 'vulnerabilities'].map(async (key) => {
-      const keyEndpoint = `${BASE_API_URL}/${key}/?risk_scenarios=${params.id}`;
-      const response = await fetch(keyEndpoint);
-      if (response.ok) {
-        const data = await response.json().then((data) => data.results);
+	await Promise.all(
+		['assets', 'threats', 'vulnerabilities'].map(async (key) => {
+			const keyEndpoint = `${BASE_API_URL}/${key}/?risk_scenarios=${params.id}`;
+			const response = await fetch(keyEndpoint);
+			if (response.ok) {
+				const data = await response.json().then((data) => data.results);
 
-        const metaData = tableSourceMapper(data, ['id', 'status']);
+				const metaData = tableSourceMapper(data, ['id', 'status']);
 
-        const bodyData = tableSourceMapper(data, listViewFields[key].body);
+				const bodyData = tableSourceMapper(data, listViewFields[key].body);
 
-        const table: TableSource = {
-          head: listViewFields[key].head,
-          body: bodyData,
-          meta: metaData
-        };
-        tables[key] = table;
-      } else {
-        console.error(`Failed to fetch data for ${key}: ${response.statusText}`);
-      }
-    })
-  );
-  await Promise.all(
-    ['risk_scenarios', 'risk_scenarios_e'].map(async (key) => {
-      const keyEndpoint = `${BASE_API_URL}/applied-controls/?${key}=${params.id}`;
-      const response = await fetch(keyEndpoint);
-      if (response.ok) {
-        const data = await response.json().then((data) => data.results);
+				const table: TableSource = {
+					head: listViewFields[key].head,
+					body: bodyData,
+					meta: metaData
+				};
+				tables[key] = table;
+			} else {
+				console.error(`Failed to fetch data for ${key}: ${response.statusText}`);
+			}
+		})
+	);
+	await Promise.all(
+		['risk_scenarios', 'risk_scenarios_e'].map(async (key) => {
+			const keyEndpoint = `${BASE_API_URL}/applied-controls/?${key}=${params.id}`;
+			const response = await fetch(keyEndpoint);
+			if (response.ok) {
+				const data = await response.json().then((data) => data.results);
 
-        const metaData = tableSourceMapper(data, ['id', 'status']);
+				const metaData = tableSourceMapper(data, ['id', 'status']);
 
-        const bodyData = tableSourceMapper(data, listViewFields['applied-controls'].body);
+				const bodyData = tableSourceMapper(data, listViewFields['applied-controls'].body);
 
-        const table: TableSource = {
-          head: listViewFields['applied-controls'].head,
-          body: bodyData,
-          meta: metaData
-        };
-        tables[key] = table;
-      } else {
-        console.error(`Failed to fetch data for ${key}: ${response.statusText}`);
-      }
-    })
-  );
+				const table: TableSource = {
+					head: listViewFields['applied-controls'].head,
+					body: bodyData,
+					meta: metaData
+				};
+				tables[key] = table;
+			} else {
+				console.error(`Failed to fetch data for ${key}: ${response.statusText}`);
+			}
+		})
+	);
 
-  const riskMatrix = await fetch(`${BASE_API_URL}/risk-matrices/${object.risk_matrix}/`)
-    .then((res) => res.json())
-    .then((res) => JSON.parse(res.json_definition));
+	const riskMatrix = await fetch(`${BASE_API_URL}/risk-matrices/${object.risk_matrix}/`)
+		.then((res) => res.json())
+		.then((res) => JSON.parse(res.json_definition));
 
-  return { scenario, tables, riskMatrix };
+	return { scenario, tables, riskMatrix };
 }) satisfies PageServerLoad;
