c26822fc989ebb04e95787c602a3e7601ed15259
Abderrahmane Smimite <smimite@gmail.com>
2024-11-16 22:16:16 +0100

    working point - naming to improve

--- frontend/src/routes/(app)/(internal)/risk-scenarios/[id=uuid]/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/risk-scenarios/[id=uuid]/+page.server.ts
@@ -4,42 +4,65 @@ import { BASE_API_URL } from '$lib/utils/constants';
 import { tableSourceMapper, type TableSource } from '@skeletonlabs/skeleton';
 import { listViewFields } from '$lib/utils/table';
 import type { urlModel } from '$lib/utils/types';
+import { table } from 'console';
 
 export const load = (async ({ fetch, params }) => {
-	const URLModel = 'risk-scenarios';
-	const baseEndpoint = `${BASE_API_URL}/${URLModel}/${params.id}/`;
-	const objectEndpoint = `${BASE_API_URL}/${URLModel}/${params.id}/object/`;
-	const object = await fetch(objectEndpoint).then((res) => res.json());
-	const scenario = await fetch(baseEndpoint).then((res) => res.json());
+  const URLModel = 'risk-scenarios';
+  const baseEndpoint = `${BASE_API_URL}/${URLModel}/${params.id}/`;
+  const objectEndpoint = `${BASE_API_URL}/${URLModel}/${params.id}/object/`;
+  const object = await fetch(objectEndpoint).then((res) => res.json());
+  const scenario = await fetch(baseEndpoint).then((res) => res.json());
 
-	const tables: Record<string, any> = {};
+  const tables: Record<string, any> = {};
 
-	await Promise.all(
-		['assets', 'threats', 'applied-controls', 'vulnerabilities'].map(async (key) => {
-			const keyEndpoint = `${BASE_API_URL}/${key}/?risk_scenarios=${params.id}`;
-			const response = await fetch(keyEndpoint);
-			if (response.ok) {
-				const data = await response.json().then((data) => data.results);
+  await Promise.all(
+    ['assets', 'threats', 'vulnerabilities'].map(async (key) => {
+      const keyEndpoint = `${BASE_API_URL}/${key}/?risk_scenarios=${params.id}`;
+      const response = await fetch(keyEndpoint);
+      if (response.ok) {
+        const data = await response.json().then((data) => data.results);
 
-				const metaData = tableSourceMapper(data, ['id', 'status']);
+        const metaData = tableSourceMapper(data, ['id', 'status']);
 
-				const bodyData = tableSourceMapper(data, listViewFields[key].body);
+        const bodyData = tableSourceMapper(data, listViewFields[key].body);
 
-				const table: TableSource = {
-					head: listViewFields[key].head,
-					body: bodyData,
-					meta: metaData
-				};
-				tables[key] = table;
-			} else {
-				console.error(`Failed to fetch data for ${key}: ${response.statusText}`);
-			}
-		})
-	);
+        const table: TableSource = {
+          head: listViewFields[key].head,
+          body: bodyData,
+          meta: metaData
+        };
+        tables[key] = table;
+      } else {
+        console.error(`Failed to fetch data for ${key}: ${response.statusText}`);
+      }
+    })
+  );
+  await Promise.all(
+    ['risk_scenarios', 'risk_scenarios_e'].map(async (key) => {
+      const keyEndpoint = `${BASE_API_URL}/applied-controls/?${key}=${params.id}`;
+      const response = await fetch(keyEndpoint);
+      if (response.ok) {
+        const data = await response.json().then((data) => data.results);
 
-	const riskMatrix = await fetch(`${BASE_API_URL}/risk-matrices/${object.risk_matrix}/`)
-		.then((res) => res.json())
-		.then((res) => JSON.parse(res.json_definition));
+        const metaData = tableSourceMapper(data, ['id', 'status']);
 
-	return { scenario, tables, riskMatrix };
+        const bodyData = tableSourceMapper(data, listViewFields['applied-controls'].body);
+
+        const table: TableSource = {
+          head: listViewFields['applied-controls'].head,
+          body: bodyData,
+          meta: metaData
+        };
+        tables[key] = table;
+      } else {
+        console.error(`Failed to fetch data for ${key}: ${response.statusText}`);
+      }
+    })
+  );
+
+  const riskMatrix = await fetch(`${BASE_API_URL}/risk-matrices/${object.risk_matrix}/`)
+    .then((res) => res.json())
+    .then((res) => JSON.parse(res.json_definition));
+
+  return { scenario, tables, riskMatrix };
 }) satisfies PageServerLoad;
--- frontend/src/routes/(app)/(internal)/risk-scenarios/[id=uuid]/+page.svelte
+++ frontend/src/routes/(app)/(internal)/risk-scenarios/[id=uuid]/+page.svelte
@@ -31,6 +31,7 @@
 	};
 </script>
 
+{@debug data}
 <div class="flex flex-col space-y-3">
 	<div class="flex flex-row card justify-between px-4 py-2 bg-white shadow-lg">
 		<div class="flex flex-col space-y-4">
@@ -107,13 +108,13 @@
 		</div>
 	</div>
 	<div class="flex flex-row space-x-2">
-		<div class="card px-4 py-2 bg-white shadow-lg space-y-4 w-3/5 max-h-96 overflow-y-scroll">
-			<h4 class="h4 font-semibold">{m.threats()}</h4>
-			<ModelTable source={data.tables['threats']} hideFilters={true} URLModel="threats" />
-		</div>
 		<div class="card px-4 py-2 bg-white shadow-lg w-2/5 max-h-96 overflow-y-scroll">
 			<h4 class="h4 font-semibold">{m.assets()}</h4>
 			<ModelTable source={data.tables['assets']} hideFilters={true} URLModel="assets" />
+		</div>
+		<div class="card px-4 py-2 bg-white shadow-lg space-y-4 w-3/5 max-h-96 overflow-y-scroll">
+			<h4 class="h4 font-semibold">{m.threats()}</h4>
+			<ModelTable source={data.tables['threats']} hideFilters={true} URLModel="threats" />
 		</div>
 	</div>
 	<div class="card px-4 py-2 bg-white shadow-lg max-w-full max-h-96 overflow-y-scroll">
@@ -135,6 +136,11 @@
 			{:else}
 				<p class="text-gray-400 italic text-sm">{m.noExistingControls()}</p>
 			{/if}
+			<ModelTable
+				source={data.tables['risk_scenarios_e']}
+				hideFilters={true}
+				URLModel="applied-controls"
+			/>
 		</div>
 		<div class="flex flex-row space-x-4 my-auto items-center justify-center w-fit h-full">
 			<p class="flex flex-col">
@@ -177,7 +183,7 @@
 			<h4 class="h4 font-semibold">{m.residualRisk()}</h4>
 			<p class="text-sm font-semibold text-gray-400">{m.appliedControls()}</p>
 			<ModelTable
-				source={data.tables['applied-controls']}
+				source={data.tables['risk_scenarios']}
 				hideFilters={true}
 				URLModel="applied-controls"
 			/>
