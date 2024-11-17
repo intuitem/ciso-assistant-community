f449165cce91ba00438421fa52f3bf06bfd287e3
Abderrahmane Smimite <smimite@gmail.com>
2024-11-17 09:31:07 +0100

    better styling

--- frontend/src/routes/(app)/(internal)/risk-scenarios/[id=uuid]/+page.svelte
+++ frontend/src/routes/(app)/(internal)/risk-scenarios/[id=uuid]/+page.svelte
@@ -126,23 +126,22 @@
 		/>
 	</div>
 	<div class="flex flex-row space-x-4 card px-4 py-2 bg-white shadow-lg justify-between">
-		<div class="flex flex-col w-fit">
+		<div class="flex flex-col w-1/2">
 			<h4 class="h4 font-semibold">{m.currentRisk()}</h4>
-			<p class="text-sm font-semibold text-gray-400">{m.existingControls()}</p>
 			{#if data.scenario.existing_controls}
-				<p>
+				<p class="text-sm font-semibold text-gray-400">{m.context()}</p>
+				<p class="mt-1 mb-2">
 					{data.scenario.existing_controls}
 				</p>
-			{:else}
-				<p class="text-gray-400 italic text-sm">{m.noExistingControls()}</p>
 			{/if}
+			<p class="text-sm font-semibold text-gray-400">{m.existingControls()}</p>
 			<ModelTable
 				source={data.tables['risk_scenarios_e']}
 				hideFilters={true}
 				URLModel="applied-controls"
 			/>
 		</div>
-		<div class="flex flex-row space-x-4 my-auto items-center justify-center w-fit h-full">
+		<div class="flex flex-row space-x-4 my-auto items-center justify-center w-1/2 h-full">
 			<p class="flex flex-col">
 				<span class="text-sm font-semibold text-gray-400">{m.probability()}</span>
 				<span
@@ -179,7 +178,7 @@
 		</div>
 	</div>
 	<div class="flex flex-row space-x-4 card px-4 py-2 bg-white shadow-lg justify-between">
-		<div class="flex flex-col w-fit">
+		<div class="flex flex-col w-1/2">
 			<h4 class="h4 font-semibold">{m.residualRisk()}</h4>
 			<p class="text-sm font-semibold text-gray-400">{m.appliedControls()}</p>
 			<ModelTable
@@ -188,7 +187,7 @@
 				URLModel="applied-controls"
 			/>
 		</div>
-		<div class="flex flex-row space-x-4 my-auto items-center justify-center w-fit">
+		<div class="flex flex-row space-x-4 my-auto items-center justify-center w-1/2">
 			<p class="flex flex-col">
 				<span class="text-sm font-semibold text-gray-400">{m.probability()}</span>
 				<span
