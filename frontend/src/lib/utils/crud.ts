05ff0683869d516ba667833c3203ea054211b62a
melinoix <titouan.ameline@gmail.com>
2024-11-19 10:30:32 +0100

    corrected format

--- frontend/src/lib/utils/crud.ts
+++ frontend/src/lib/utils/crud.ts
@@ -659,11 +659,11 @@ export const FIELD_COLORED_TAG_MAP: FieldColoredTagMap = {
 					'--': { text: 'undefined', cssClasses: 'badge bg-gray-300' }
 				},
 				priority: {
-					P1:{text:'', cssClasses: 'fa-solid fa-flag text-red-500'},
-					P2:{text:'', cssClasses: 'fa-solid fa-flag text-orange-500'},
-					P3:{text:'', cssClasses: 'fa-solid fa-flag text-blue-500'},
-					P4:{text:'', cssClasses: 'fa-solid fa-flag text-gray-500'},
-					'--':{text:'', cssClasses: ''}
+					P1: { text: '', cssClasses: 'fa-solid fa-flag text-red-500' },
+					P2: { text: '', cssClasses: 'fa-solid fa-flag text-orange-500' },
+					P3: { text: '', cssClasses: 'fa-solid fa-flag text-blue-500' },
+					P4: { text: '', cssClasses: 'fa-solid fa-flag text-gray-500' },
+					'--': { text: '', cssClasses: '' }
 				}
 			}
 		}
