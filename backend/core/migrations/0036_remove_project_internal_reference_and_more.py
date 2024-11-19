f490aa539f9701bb010a6aee63d3ad8c85cfc13a
melinoix <titouan.ameline@gmail.com>
2024-11-19 11:25:29 +0100

    correct format

--- backend/core/migrations/0036_remove_project_internal_reference_and_more.py
+++ backend/core/migrations/0036_remove_project_internal_reference_and_more.py
@@ -4,35 +4,40 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('core', '0035_riskscenario_existing_applied_controls'),
+        ("core", "0035_riskscenario_existing_applied_controls"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='project',
-            old_name='internal_reference',
-            new_name='ref_id'
+            model_name="project", old_name="internal_reference", new_name="ref_id"
         ),
         migrations.AlterField(
-            model_name='project',
-            name='ref_id',
-            field=models.CharField(blank=True, max_length=100, null=True, verbose_name='reference id'),
+            model_name="project",
+            name="ref_id",
+            field=models.CharField(
+                blank=True, max_length=100, null=True, verbose_name="reference id"
+            ),
         ),
         migrations.AddField(
-            model_name='appliedcontrol',
-            name='ref_id',
-            field=models.CharField(blank=True, max_length=100, null=True, verbose_name='reference id'),
+            model_name="appliedcontrol",
+            name="ref_id",
+            field=models.CharField(
+                blank=True, max_length=100, null=True, verbose_name="reference id"
+            ),
         ),
         migrations.AddField(
-            model_name='complianceassessment',
-            name='ref_id',
-            field=models.CharField(blank=True, max_length=100, null=True, verbose_name='reference id'),
+            model_name="complianceassessment",
+            name="ref_id",
+            field=models.CharField(
+                blank=True, max_length=100, null=True, verbose_name="reference id"
+            ),
         ),
         migrations.AddField(
-            model_name='riskassessment',
-            name='ref_id',
-            field=models.CharField(blank=True, max_length=100, null=True, verbose_name='reference id'),
+            model_name="riskassessment",
+            name="ref_id",
+            field=models.CharField(
+                blank=True, max_length=100, null=True, verbose_name="reference id"
+            ),
         ),
     ]
