c8da9097a1a5b5cd0b74e6f605c894274aff5212
melinoix <titouan.ameline@gmail.com>
2024-11-22 12:44:21 +0100

    Corrected fetching & migrations

--- backend/core/migrations/0039_riskscenario_ref_id.py
+++ backend/core/migrations/0039_riskscenario_ref_id.py
@@ -12,7 +12,7 @@ def populate_default_ref_ids(apps, schema_editor):
             x.ref_id for x in risk_assessment.risk_scenarios.all() if x.ref_id
         ]
         nb_scenarios = len(scenarios_ref_ids) + 1
-        candidates = [f"R.{i}" for i in range(nb_scenarios)]
+        candidates = [f"R.{i+1}" for i in range(nb_scenarios)]
         return next(x for x in candidates if x not in scenarios_ref_ids)
 
     for risk_assessment in RiskAssessment.objects.all():
@@ -23,7 +23,7 @@ def populate_default_ref_ids(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("core", "0036_asset_owner"),
+        ("core", "0038_asset_disaster_recovery_objectives_and_more"),
     ]
 
     operations = [
