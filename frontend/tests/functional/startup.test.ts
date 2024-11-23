18f898be41c1df7e5259e37ab1a02218b6100c92
monsieurswag <mael.joumier@intuitem.com>
2024-11-23 01:34:47 +0100

    Attempt to fix startup tests

--- frontend/tests/functional/startup.test.ts
+++ frontend/tests/functional/startup.test.ts
@@ -9,7 +9,7 @@ test('startup tests', async ({ loginPage, analyticsPage, page }) => {
 	});
 
 	await test.step('proper redirection to the analytics page after login', async () => {
-		await analyticsPage.hasUrl();
+		await analyticsPage.hasUrl(false);
 		await analyticsPage.hasTitle();
 	});
 });
