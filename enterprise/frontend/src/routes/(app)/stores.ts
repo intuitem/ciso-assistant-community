fcc326d9e0bc88eac72806f892e8db384550e352
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-11-25 00:30:40 +0100

    Remove unused store

--- enterprise/frontend/src/routes/(app)/stores.ts
+++
@@ -1,5 +0,0 @@
-import { persisted } from 'svelte-persisted-store';
-
-export const licenseAboutToExpireToastShown = persisted('licence_about_to_expire_toast_shown', false, {
-  storage: 'session'
-});
