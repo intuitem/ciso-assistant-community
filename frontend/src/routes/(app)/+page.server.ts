900f1bb55ae517d164cba8d3e6c3003fb8029736
monsieurswag <mael.joumier@intuitem.com>
2024-11-29 07:04:17 +0100

    Remove debug logging

--- frontend/src/routes/(app)/+page.server.ts
+++ frontend/src/routes/(app)/+page.server.ts
@@ -3,9 +3,7 @@ import type { PageServerLoad } from './$types';
 import { CI_TEST } from '$lib/utils/env_constants';
 
 const redirectURL = CI_TEST ? '/analytics' : '/analytics?refresh=1';
-console.log(`[CI_TEST_VALUE:1] ${CI_TEST}`);
 
 export const load: PageServerLoad = async () => {
-	console.log(`[CI_TEST_VALUE:2] ${CI_TEST}`);
 	redirect(301, redirectURL);
 };
