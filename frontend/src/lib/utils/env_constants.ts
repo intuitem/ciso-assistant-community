7e52bd117b6be5c43f786d32ae9869012558b495
monsieurswag <mael.joumier@intuitem.com>
2024-11-29 11:58:32 +0100

    Precise env_constants utility

--- frontend/src/lib/utils/env_constants.ts
+++ frontend/src/lib/utils/env_constants.ts
@@ -1,3 +1,5 @@
 import * as env from '$env/static/private';
 
+// This file is separate from constant.ts because '$env/static/private' can only be imported on the server-side.
+
 export const CI_TEST: boolean = Object.hasOwn(env, 'CI_TEST');
