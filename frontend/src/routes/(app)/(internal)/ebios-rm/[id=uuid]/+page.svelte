1df01ad76bf7e5e49b054b92eb7d31d390aa4920
Abderrahmane Smimite <smimite@gmail.com>
2024-11-29 15:02:51 +0100

    Basic tile styling

--- frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/+page.svelte
+++ frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/+page.svelte
@@ -1,3 +1,31 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import * as m from '$paraglide/messages';
+	import Tile from './Tile.svelte';
+	const data = { ws1_1: [{ k: 'v' }, { k: 'v' }, { k: 'v' }] };
+</script>
 
-<div></div>
+<div class="h-screen w-full p-8">
+	<div class="card bg-white shadow-lg w-full h-full grid grid-cols-3 grid-rows-3 gap-8 p-8">
+		<div class="border col-span-3 grid grid-cols-3 gap-4 p-4 bg-pink-600">
+			<Tile title={m.ebiosWs1_1()} status="to_do" meta={data.ws1_1} />
+			<Tile title={m.ebiosWs1_2()} status="in_progress" />
+			<Tile title={m.ebiosWs1_3()} status="done" />
+		</div>
+		<div class="border bg-fuchsia-900 grid gap-4 p-4">
+			<Tile title={m.ebiosWs2()} status="to_do" />
+		</div>
+		<div class="border col-span-2 grid grid-cols-2 gap-4 p-4 bg-teal-500">
+			<Tile title={m.ebiosWs3_1()} status="to_do" />
+			<Tile title={m.ebiosWs3_2()} status="done" />
+		</div>
+		<div class="border bg-yellow-600 grid gap-4 p-4">
+			<Tile title={m.ebiosWs4()} status="in_progress" />
+		</div>
+		<div class="border bg-red-500 grid gap-4 p-4">
+			<Tile title={m.ebiosWs5()} status="to_do" />
+		</div>
+		<div class="border bg-purple-800 grid gap-4 p-4">
+			<Tile title={m.summary()} />
+		</div>
+	</div>
+</div>
---
+++ frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/Tile.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import * as m from '$paraglide/messages';
+
+	export let title = 'activity';
+	export let status = '';
+	export let meta = [];
+</script>
+
+<div class="rounded bg-white p-4 flex flex-col justify-between">
+	<div class="flex justify-between mb-2">
+		<div class="font-semibold">{title}</div>
+		<div>
+			{#if status == 'to_do'}
+				<i class="fa-solid fa-exclamation"></i>
+			{:else if status == 'in_progress'}
+				<i class="fa-solid fa-spinner"></i>
+			{:else if status == 'done'}
+				<i class="fa-solid fa-check"></i>
+			{/if}
+		</div>
+	</div>
+	{#each meta as kv}
+		<div class="mt-2">{kv}</div>
+	{/each}
+	<div class="justify-end flex">
+		<div class="text-xl text-zinc-700"><i class="fa-solid fa-play"></i></div>
+	</div>
+</div>
