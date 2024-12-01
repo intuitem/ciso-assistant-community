ece218a3e6364c9f68a8eb1908d93a66205c9b35
Abderrahmane Smimite <smimite@gmail.com>
2024-12-01 20:27:14 +0100

    Closer to the training materials

--- frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/+page.svelte
+++ frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/+page.svelte
@@ -1,31 +1,47 @@
 <script lang="ts">
 	import * as m from '$paraglide/messages';
+	import { safeTranslate } from '$lib/utils/i18n';
 	import Tile from './Tile.svelte';
-	const data = { ws1_1: [{ k: 'v' }, { k: 'v' }, { k: 'v' }] };
+	const data = {
+		ws1: [
+			{ title: safeTranslate(m.ebiosWs1_1()), status: 'done', href: '#' },
+			{ title: safeTranslate(m.ebiosWs1_2()), status: 'done', href: '#' },
+			{ title: safeTranslate(m.ebiosWs1_3()), status: 'to_do', href: '#' },
+			{ title: safeTranslate(m.ebiosWs1_4()), status: 'to_do', href: '#' }
+		],
+		ws2: [
+			{ title: safeTranslate(m.ebiosWs2_1()), status: 'to_do', href: '#' },
+			{ title: safeTranslate(m.ebiosWs2_2()), status: 'to_do', href: '#' },
+			{ title: safeTranslate(m.ebiosWs2_3()), status: 'to_do', href: '#' }
+		],
+		ws3: [
+			{ title: safeTranslate(m.ebiosWs3_1()), status: 'to_do', href: '#' },
+			{ title: safeTranslate(m.ebiosWs3_2()), status: 'to_do', href: '#' },
+			{ title: safeTranslate(m.ebiosWs3_3()), status: 'done', href: '#' }
+		],
+		ws4: [
+			{ title: safeTranslate(m.ebiosWs4_1()), status: 'to_do', href: '#' },
+			{ title: safeTranslate(m.ebiosWs4_2()), status: 'to_do', href: '#' }
+		],
+		ws5: [
+			{ title: safeTranslate(m.ebiosWs5_1()), status: 'done', href: '#' },
+			{ title: safeTranslate(m.ebiosWs5_2()), status: 'done', href: '#' },
+			{ title: safeTranslate(m.ebiosWs5_3()), status: 'to_do', href: '#' },
+			{ title: safeTranslate(m.ebiosWs5_4()), status: 'to_do', href: '#' },
+			{ title: safeTranslate(m.ebiosWs5_5()), status: 'done', href: '#' }
+		]
+	};
 </script>
 
 <div class="h-screen w-full p-8">
-	<div class="card bg-white shadow-lg w-full h-full grid grid-cols-3 grid-rows-3 gap-8 p-8">
-		<div class="border col-span-3 grid grid-cols-3 gap-4 p-4 bg-pink-600">
-			<Tile title={m.ebiosWs1_1()} status="to_do" meta={data.ws1_1} />
-			<Tile title={m.ebiosWs1_2()} status="in_progress" />
-			<Tile title={m.ebiosWs1_3()} status="done" />
-		</div>
-		<div class="border bg-fuchsia-900 grid gap-4 p-4">
-			<Tile title={m.ebiosWs2()} status="to_do" />
-		</div>
-		<div class="border col-span-2 grid grid-cols-2 gap-4 p-4 bg-teal-500">
-			<Tile title={m.ebiosWs3_1()} status="to_do" />
-			<Tile title={m.ebiosWs3_2()} status="done" />
-		</div>
-		<div class="border bg-yellow-600 grid gap-4 p-4">
-			<Tile title={m.ebiosWs4()} status="in_progress" />
-		</div>
-		<div class="border bg-red-500 grid gap-4 p-4">
-			<Tile title={m.ebiosWs5()} status="to_do" />
-		</div>
-		<div class="border bg-purple-800 grid gap-4 p-4">
-			<Tile title={m.summary()} />
-		</div>
+	<div
+		class="card bg-white shadow-lg w-full h-full grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 gap-8 p-8"
+	>
+		<Tile title={m.ebiosWs1()} accent_color="bg-pink-600" status="to_do" meta={data.ws1} />
+		<Tile title={m.ebiosWs2()} accent_color="bg-fuchsia-900" status="to_do" meta={data.ws2} />
+		<Tile title={m.ebiosWs3()} accent_color="bg-teal-500" status="to_do" meta={data.ws3} />
+		<Tile title={m.ebiosWs4()} accent_color="bg-yellow-600" status="to_do" meta={data.ws4} />
+		<Tile title={m.ebiosWs5()} accent_color="bg-red-500" status="to_do" meta={data.ws5} />
+		<Tile title={m.summary()} accent_color="bg-purple-800" status="to_do" />
 	</div>
 </div>
