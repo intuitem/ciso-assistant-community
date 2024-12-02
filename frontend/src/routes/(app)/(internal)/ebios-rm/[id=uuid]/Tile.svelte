4b803b3b9758f3404f65966d4fac6a82c318d036
Abder <smimite@gmail.com>
2024-12-02 21:02:31 +0100

    Ebios RM tiles (#1099)

---
+++ frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/Tile.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import * as m from '$paraglide/messages';
+	import { safeTranslate } from '$lib/utils/i18n';
+
+	export let title = 'activity';
+	export let status = '';
+	export let meta = null;
+	export let href = '#';
+	export let accent_color = '';
+</script>
+
+<div class="p-5 {accent_color}">
+	<div class="rounded-lg bg-white p-4 flex flex-col justify-between h-full">
+		<div class="flex justify-between mb-2">
+			<div class="font-semibold">{title}</div>
+			<div class="text-xl" title={safeTranslate(status)}>
+				{#if status == 'to_do'}
+					<i class="fa-solid fa-exclamation"></i>
+				{:else if status == 'in_progress'}
+					<i class="fa-solid fa-spinner"></i>
+				{:else if status == 'done'}
+					<i class="fa-solid fa-check"></i>
+				{/if}
+			</div>
+		</div>
+		{#if meta}
+			<div class="flex mx-auto">
+				<div>
+					<ol class="relative text-gray-500 border-s border-gray-200">
+						{#each meta as step, i}
+							{#if step.status == 'done'}
+								<li class="mb-10 ms-6">
+									<a href={step.href} class="hover:text-purple-800">
+										<span
+											class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -start-4 ring-4 ring-white"
+										>
+											<i class="fa-solid fa-check"></i>
+										</span>
+										<h3 class="font-medium leading-tight">{m.activity()} {i + 1}</h3>
+										<p class="text-sm">{step.title}</p>
+									</a>
+								</li>
+							{:else}
+								<li class="mb-10 ms-6">
+									<a href={step.href} class="hover:text-purple-800">
+										<span
+											class="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -start-4 ring-4 ring-white"
+										>
+											<i class="fa-solid fa-clipboard-check"></i>
+										</span>
+										<h3 class="font-medium leading-tight">{m.activity()} {i + 1}</h3>
+										<p class="text-sm">{step.title}</p>
+									</a>
+								</li>
+							{/if}
+						{/each}
+					</ol>
+				</div>
+			</div>
+		{/if}
+		<div class="justify-end flex"></div>
+	</div>
+</div>
