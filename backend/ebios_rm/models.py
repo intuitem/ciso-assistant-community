598851f98a64621d0d069623dcb26b3c7a32ac71
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-12-02 16:03:17 +0100

    Write RO/TO model

--- backend/ebios_rm/models.py
+++ backend/ebios_rm/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from core.base_models import NameDescriptionMixin, ETADueDateMixin
+from core.base_models import AbstractBaseModel, NameDescriptionMixin, ETADueDateMixin
 from iam.models import FolderMixin, User
 
 
@@ -46,4 +46,35 @@ class EbiosRMStudy(NameDescriptionMixin, ETADueDateMixin, FolderMixin):
     class Meta:
         verbose_name = _("Ebios RM Study")
         verbose_name_plural = _("Ebios RM Studies")
+        ordering = ["created_at"]
+
+
+class ROTO(AbstractBaseModel):
+    class RiskOrigin(models.TextChoices):
+        STATE = "state", _("State")
+        ORGANIZED_CRIME = "organized_crime", _("Organized crime")
+        TERRORIST = "terrorist", _("Terrorist")
+        ACTIVIST = "activist", _("Activist")
+        PROFESSIONAL = "professional", _("Professional")
+        AMATEUR = "amateur", _("Amateur")
+        AVENGER = "avenger", _("Avenger")
+        PATHOLOGICAL = "pathological", _("Pathological")
+
+    study = models.ForeignKey(
+        EbiosRMStudy, verbose_name=_("EBIOS RM study"), on_delete=models.CASCADE
+    )
+    risk_origin = models.CharField(max_length=200, verbose_name=_("Risk origin"))
+    target_objective = models.CharField(
+        max_length=200, verbose_name=_("Target objective")
+    )
+    motivation = models.PositiveSmallIntegerField(verbose_name=_("Motivation"))
+    resources = models.PositiveSmallIntegerField(verbose_name=_("Resources"))
+    pertinence = models.PositiveSmallIntegerField(verbose_name=_("Pertinence"))
+    activity = models.PositiveSmallIntegerField(verbose_name=_("Activity"))
+    is_selected = models.BooleanField(verbose_name=_("Is selected"))
+    justification = models.TextField(verbose_name=_("Justification"))
+
+    class Meta:
+        verbose_name = _("RO/TO couple")
+        verbose_name_plural = _("RO/TO couples")
         ordering = ["created_at"]
