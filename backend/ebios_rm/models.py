ef20b8d47ed494310b6bef15f0a92fec63cd79fb
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-12-02 11:54:13 +0100

    Initialize ebios_rm app

---
+++ backend/ebios_rm/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from core.base_models import NameDescriptionMixin, ETADueDateMixin
+from iam.models import FolderMixin, User
+
+
+class EbiosRMStudy(NameDescriptionMixin, ETADueDateMixin, FolderMixin):
+    class Status(models.TextChoices):
+        PLANNED = "planned", _("Planned")
+        IN_PROGRESS = "in_progress", _("In progress")
+        IN_REVIEW = "in_review", _("In review")
+        DONE = "done", _("Done")
+        DEPRECATED = "deprecated", _("Deprecated")
+
+    ref_id = models.CharField(max_length=100, unique=True)
+    version = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        help_text=_("Version of the Ebios RM study (eg. 1.0, 2.0, etc.)"),
+        verbose_name=_("Version"),
+        default="1.0",
+    )
+    status = models.CharField(
+        max_length=100,
+        choices=Status.choices,
+        default=Status.PLANNED,
+        verbose_name=_("Status"),
+        blank=True,
+        null=True,
+    )
+    authors = models.ManyToManyField(
+        User,
+        blank=True,
+        verbose_name=_("Authors"),
+        related_name="%(class)s_authors",
+    )
+    reviewers = models.ManyToManyField(
+        User,
+        blank=True,
+        verbose_name=_("Reviewers"),
+        related_name="%(class)s_reviewers",
+    )
+    observation = models.TextField(null=True, blank=True, verbose_name=_("Observation"))
