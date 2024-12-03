8051e5d997ff2f08d757b635d76567514574cff2
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-12-03 16:53:57 +0100

    Write EbiosRMStudy ViewSet and Serializer classes
    Co-authored-by: Mohamed-Hacene <mohamedhacene.b@gmail.com>

---
+++ backend/ebios_rm/serializers.py
@@ -0,0 +1,47 @@
+from core.serializers import (
+    BaseModelSerializer,
+    FieldsRelatedField,
+    AssessmentReadSerializer,
+)
+from core.models import StoredLibrary, RiskMatrix
+from .models import EbiosRMStudy
+from rest_framework import serializers
+import logging
+
+
+class EbiosRMStudyWriteSerializer(BaseModelSerializer):
+    def create(self, validated_data):
+        if not validated_data.get("risk_matrix"):
+            try:
+                ebios_matrix_library = StoredLibrary.objects.get(
+                    urn="urn:intuitem:risk:library:risk-matrix-4x4-ebios-rm"
+                )
+                ebios_matrix_library.load()
+
+                validated_data["risk_matrix"] = RiskMatrix.objects.get(
+                    urn="urn:intuitem:risk:matrix:risk-matrix-4x4-ebios-rm"
+                )
+            except (StoredLibrary.DoesNotExist, RiskMatrix.DoesNotExist) as e:
+                logging.error(f"Error loading risk matrix: {str(e)}")
+                raise serializers.ValidationError(
+                    "An error occurred while loading the risk matrix."
+                )
+        return super().create(validated_data)
+
+    class Meta:
+        model = EbiosRMStudy
+        exclude = ["created_at", "updated_at"]
+
+
+class EbiosRMStudyReadSerializer(AssessmentReadSerializer):
+    str = serializers.CharField(source="__str__")
+    project = FieldsRelatedField(["id", "folder"])
+    folder = FieldsRelatedField()
+    risk_matrix = FieldsRelatedField()
+    assets = FieldsRelatedField(many=True)
+    compliance_assessments = FieldsRelatedField(many=True)
+    risk_assessments = FieldsRelatedField(many=True)
+
+    class Meta:
+        model = EbiosRMStudy
+        fields = "__all__"
