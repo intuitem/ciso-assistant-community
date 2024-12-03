adc243d4d8c9809cfadbc45ebe9bb12b7529c350
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-12-03 19:45:46 +0100

    Add feared-events routes

--- backend/ebios_rm/urls.py
+++ backend/ebios_rm/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from ebios_rm.views import EbiosRMStudyViewSet
+from ebios_rm.views import EbiosRMStudyViewSet, FearedEventViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r"studies", EbiosRMStudyViewSet, basename="studies")
+router.register(r"feared-events", FearedEventViewSet, basename="feared-events")
 
 urlpatterns = [
     path("", include(router.urls)),
