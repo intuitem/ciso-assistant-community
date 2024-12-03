e04bc0177be3683994f22a09034809685238ea77
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-12-03 16:58:40 +0100

    Include ebios-rm routes to core urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -83,6 +83,7 @@ urlpatterns = [
     path("iam/", include("iam.urls")),
     path("serdes/", include("serdes.urls")),
     path("settings/", include("global_settings.urls")),
+    path("ebios-rm/", include("ebios_rm.urls")),
     path("csrf/", get_csrf_token, name="get_csrf_token"),
     path("build/", get_build, name="get_build"),
     path("evidences/<uuid:pk>/upload/", UploadAttachmentView.as_view(), name="upload"),
