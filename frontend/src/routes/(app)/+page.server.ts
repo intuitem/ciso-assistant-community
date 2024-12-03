144a8006ddedf8d972a59d8def02289aba361bd4
monsieurswag <mael.joumier@intuitem.com>
2024-12-03 16:41:32 +0100

    Formatter

--- frontend/src/routes/(app)/+page.server.ts
+++ frontend/src/routes/(app)/+page.server.ts
@@ -2,6 +2,6 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url }) => {
-	const queryParams = url.searchParams.has("refresh") ? "?refresh=1" : "";
+	const queryParams = url.searchParams.has('refresh') ? '?refresh=1' : '';
 	redirect(301, `/analytics${queryParams}`);
 };
