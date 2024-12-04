82255bb3f12731106140f19f2114b7954031b9ea
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-12-04 15:25:04 +0100

    Create Stakeholder endpoints

--- backend/ebios_rm/urls.py
+++ backend/ebios_rm/urls.py
@@ -1,13 +1,19 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from ebios_rm.views import EbiosRMStudyViewSet, FearedEventViewSet, RoToViewSet
+from ebios_rm.views import (
+    EbiosRMStudyViewSet,
+    FearedEventViewSet,
+    RoToViewSet,
+    StakeholderViewSet,
+)
 
 router = routers.DefaultRouter()
 
 router.register(r"studies", EbiosRMStudyViewSet, basename="studies")
 router.register(r"feared-events", FearedEventViewSet, basename="feared-events")
 router.register(r"ro-to", RoToViewSet, basename="ro-to")
+router.register(r"stakeholders", StakeholderViewSet, basename="stakeholders")
 
 urlpatterns = [
     path("", include(router.urls)),
