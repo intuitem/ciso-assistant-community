df2679527640c056c5fe4408703ebf924adfd29c
monsieurswag <143810744+monsieurswag@users.noreply.github.com>
2024-12-04 18:02:25 +0100

    Make language choice persistent for users (#1075)

--- frontend/src/routes/(app)/+page.server.ts
+++ frontend/src/routes/(app)/+page.server.ts
@@ -1,6 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
-	redirect(301, '/analytics');
+export const load: PageServerLoad = async ({ url }) => {
+	const queryParams = url.searchParams.has('refresh') ? '?refresh=1' : '';
+	redirect(301, `/analytics${queryParams}`);
 };
