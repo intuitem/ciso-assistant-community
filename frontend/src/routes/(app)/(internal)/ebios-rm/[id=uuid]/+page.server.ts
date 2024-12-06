403ef9be0b0f51e7bd572b0e41b97b5f2be71428
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-12-06 14:37:41 +0100

    feat: create risk analysis from ebios detailed view

--- frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/+page.server.ts
@@ -1,7 +1,45 @@
-import { loadDetail } from '$lib/utils/load';
+import { defaultDeleteFormAction, defaultWriteFormAction } from '$lib/utils/actions';
+import { BASE_API_URL } from '$lib/utils/constants';
+import {
+	getModelInfo,
+	urlParamModelForeignKeyFields,
+	urlParamModelSelectFields
+} from '$lib/utils/crud';
+import { modelSchema } from '$lib/utils/schemas';
+import type { ModelInfo } from '$lib/utils/types';
+import { type Actions } from '@sveltejs/kit';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
 import type { PageServerLoad } from './$types';
-import { getModelInfo } from '$lib/utils/crud';
 
-export const load: PageServerLoad = async (event) => {
-	return await loadDetail({ event, model: getModelInfo('ebios-rm'), id: event.params.id });
+export const load: PageServerLoad = async ({ params, fetch }) => {
+	const URLModel = 'ebios-rm';
+	const model: ModelInfo = getModelInfo(URLModel);
+
+    const endpoint = model.endpointUrl
+		? `${BASE_API_URL}/${model.endpointUrl}/${params.id}/`
+		: `${BASE_API_URL}/${model.urlModel}/${params.id}/`;
+    const res = await fetch(endpoint);
+    const data = await res.json();
+
+    const initialData = {
+        risk_matrix: data.risk_matrix.id
+    }
+
+    const createSchema = modelSchema('risk-assessments');
+	const createRiskAnalysisForm = await superValidate(initialData, zod(createSchema), { errors: false });
+
+	return { createRiskAnalysisForm, model: getModelInfo('risk-assessments') };
+};
+
+export const actions: Actions = {
+	create: async (event) => {
+		// const redirectToWrittenObject = Boolean(event.params.model === 'entity-assessments');
+		return defaultWriteFormAction({
+			event,
+			urlModel: 'risk-assessments',
+			action: 'create'
+			// redirectToWrittenObject: redirectToWrittenObject
+		});
+	},
 };
