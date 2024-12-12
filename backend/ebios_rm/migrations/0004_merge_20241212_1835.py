15d48c772936dc73a611904583c6e0133b8ef950
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-12-12 19:37:15 +0100

    chore: format

--- backend/ebios_rm/migrations/0004_merge_20241212_1835.py
+++ backend/ebios_rm/migrations/0004_merge_20241212_1835.py
@@ -4,11 +4,9 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('ebios_rm', '0003_remove_ebiosrmstudy_risk_assessments'),
-        ('ebios_rm', '0003_remove_roto_pertinence_alter_roto_feared_events'),
+        ("ebios_rm", "0003_remove_ebiosrmstudy_risk_assessments"),
+        ("ebios_rm", "0003_remove_roto_pertinence_alter_roto_feared_events"),
     ]
 
-    operations = [
-    ]
+    operations = []
