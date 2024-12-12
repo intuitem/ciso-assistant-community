5d14b9e44d9372b9ffe82822c3ecfdbff2909695
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2024-12-12 19:19:51 +0100

    style: improve ro to detailed view

--- frontend/src/routes/(app)/(internal)/ro-to/[id=uuid]/+page.svelte
+++ frontend/src/routes/(app)/(internal)/ro-to/[id=uuid]/+page.svelte
@@ -4,6 +4,7 @@
 	import { page } from '$app/stores';
 	import { pageTitle } from '$lib/utils/stores';
 	import { safeTranslate } from '$lib/utils/i18n';
+	import ModelTable from '$lib/components/ModelTable/ModelTable.svelte';
 
 	export let data: PageData;
 
@@ -21,16 +22,17 @@
 			activeActivity = 'three';
 		}
 	});
+
+	const pertinenceColor = {
+		undefined: 'bg-gray-200 text-gray-700',
+		irrelevant: 'bg-green-200 text-green-700',
+		'partially relevant': 'bg-yellow-200 text-yellow-700',
+		fairly_relevant: 'bg-orange-200 text-orange-700',
+		higly_relevant: 'bg-red-200 text-red-700'
+	};
 </script>
 
 <div class="card p-4 bg-white shadow-lg">
-	<a
-		href={`${$page.url.pathname}/edit?activity=${activeActivity}&next=${$page.url.pathname}?activity=${activeActivity}`}
-		class="btn variant-filled-primary h-fit"
-	>
-		<i class="fa-solid fa-pen-to-square mr-2" data-testid="edit-button" />
-		{m.edit()}
-	</a>
 	<div class="flex flex-col space-y-4">
 		<div
 			id="activityOne"
@@ -44,14 +46,28 @@
 					? 'text-primary-500'
 					: 'text-gray-500'}">{m.activityOne()}</span
 			>
-			<div class="flex flex-col items-center">
-				<span class="badge bg-blue-200 text-blue-700">{m.riskOrigin()}</span>
-				<span class="font-bold">{roto.risk_origin}</span>
+			<h1
+				class="font-bold text-xl {activeActivity === 'one' ? 'text-primary-500' : 'text-gray-500'}"
+			>
+				{m.identifyRoTo()}
+			</h1>
+			<div class="flex flex-row space-x-1">
+				<p class="flex flex-col items-center">
+					<span class="text-xs text-gray-500">{m.riskOrigin()}</span>
+					<span class="font-bold">{roto.risk_origin} /</span>
+				</p>
+				<p class="flex flex-col items-center">
+					<span class="text-xs text-gray-500">{m.targetObjective()}</span>
+					<span class="font-bold">{roto.target_objective}</span>
+				</p>
 			</div>
-			<div class="flex flex-col items-center">
-				<span class="badge bg-cyan-200 text-cyan-700">{m.targetObjective()}</span>
-				<span class="font-bold">{roto.target_objective}</span>
-			</div>
+			<a
+				href={`${$page.url.pathname}/edit?activity=${activeActivity}&next=${$page.url.pathname}?activity=${activeActivity}`}
+				class="btn variant-filled-primary h-fit absolute top-2 right-4"
+			>
+				<i class="fa-solid fa-pen-to-square mr-2" data-testid="edit-button" />
+				{m.edit()}
+			</a>
 		</div>
 		<div
 			id="activityTwo"
@@ -65,6 +81,11 @@
 					? 'text-primary-500'
 					: 'text-gray-500'}">{m.activityTwo()}</span
 			>
+			<h1
+				class="font-bold text-xl {activeActivity === 'two' ? 'text-primary-500' : 'text-gray-500'}"
+			>
+				{m.evaluateRoTo()}
+			</h1>
 			<div class="flex space-x-6">
 				<p class="flex flex-col items-center">
 					<span class="text-xs text-gray-500">{m.motivation()}</span>
@@ -78,7 +99,9 @@
 				<span>=</span>
 				<p class="flex flex-col items-center">
 					<span class="text-xs text-gray-500">{m.pertinence()}</span>
-					<span class="badge text-sm font-bold">{safeTranslate(roto.pertinence)}</span>
+					<span class="badge text-sm font-bold {pertinenceColor[roto.pertinence]}"
+						>{safeTranslate(roto.pertinence)}</span
+					>
 				</p>
 			</div>
 			<p>
@@ -99,54 +122,33 @@
 					? 'text-primary-500'
 					: 'text-gray-500'}">{m.activityThree()}</span
 			>
+			<h1
+				class="font-bold text-xl {activeActivity === 'three'
+					? 'text-primary-500'
+					: 'text-gray-500'}"
+			>
+				{m.selectRoTo()}
+			</h1>
 			<p>
-				{#if roto.selected}
+				{#if roto.is_selected}
 					<span class="badge bg-green-200 text-green-700">{m.selected()}</span>
 				{:else}
 					<span class="badge bg-red-200 text-red-700">{m.notSelected()}</span>
 				{/if}
 			</p>
-			<!-- {#if Object.keys(data.relatedModels).length > 0}
-                <div class="card shadow-lg mt-8 bg-white w-full">
-                    <TabGroup justify="justify-center">
-                        {#each Object.entries(data.relatedModels) as [urlmodel, model], index}
-                            <Tab bind:group={tabSet} value={index} name={`${urlmodel}_tab`}>
-                                {safeTranslate(model.info.localNamePlural)}
-                                {#if model.table.body.length > 0}
-                                    <span class="badge variant-soft-secondary">{model.table.body.length}</span>
-                                {/if}
-                            </Tab>
-                        {/each}
-                        <svelte:fragment slot="panel">
-                            {#each Object.entries(data.relatedModels) as [urlmodel, model], index}
-                                {#if tabSet === index}
-                                    <div class="flex flex-row justify-between px-4 py-2">
-                                        <h4 class="font-semibold lowercase capitalize-first my-auto">
-                                            {safeTranslate('associated-' + model.info.localNamePlural)}
-                                        </h4>
-                                    </div>
-                                    {#if model.table}
-                                        <ModelTable
-                                            source={model.table}
-                                            deleteForm={model.deleteForm}
-                                            URLModel={urlmodel}
-                                        >
-                                            <button
-                                                slot="addButton"
-                                                class="btn variant-filled-primary self-end my-auto"
-                                                on:click={(_) => modalCreateForm(model)}
-                                                ><i class="fa-solid fa-plus mr-2 lowercase" />{safeTranslate(
-                                                    'add-' + model.info.localName
-                                                )}</button
-                                            >
-                                        </ModelTable>
-                                    {/if}
-                                {/if}
-                            {/each}
-                        </svelte:fragment>
-                    </TabGroup>
-                </div>
-            {/if} -->
+			<div class="w-full p-4 bg-gray-50 border rounded-md shadow-sm">
+				<h3 class="font-semibold text-lg text-gray-700 flex items-center space-x-2">
+					<i class="fa-solid fa-table text-gray-500 opacity-75"></i>
+					<span>{m.fearedEvents()}</span>
+				</h3>
+				<ModelTable
+					backgroundColor="bg-gray-50"
+					regionBody="bg-gray-50"
+					regionHeadCell="uppercase bg-gray-50 text-gray-700"
+					source={data.table}
+					URLModel="feared-events"
+				></ModelTable>
+			</div>
 			<div class="w-full p-4 bg-gray-50 border rounded-md shadow-sm">
 				<h3 class="font-semibold text-lg text-gray-700 flex items-center space-x-2">
 					<i class="fa-solid fa-eye text-gray-500 opacity-75"></i>
