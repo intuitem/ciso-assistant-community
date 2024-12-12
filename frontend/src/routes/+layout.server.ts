dfe64c082fe72a5a48599cbe45885dd1825b8dcb
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2024-12-12 23:59:40 +0100

    translate emails for TPRM and pwd reset (#1176)
    Co-authored-by: Abder <smimite@gmail.com>

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,5 +1,8 @@
 import type { LayoutServerLoad } from './$types';
+import { setLanguageTag } from '$paraglide/runtime';
+import { DEFAULT_LANGUAGE } from '$lib/utils/constants';
 
-export const load: LayoutServerLoad = async ({ locals }) => {
+export const load: LayoutServerLoad = async ({ locals, cookies }) => {
+	setLanguageTag(cookies.get('ciso_lang') || DEFAULT_LANGUAGE);
 	return { featureFlags: locals.featureFlags };
 };
