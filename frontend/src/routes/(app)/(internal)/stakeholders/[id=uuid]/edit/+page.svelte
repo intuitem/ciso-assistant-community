b5aaf44e1130f57d6b278cee24f013616d40ab6b
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-12-13 19:06:33 +0100

    Allow creating applied controls inside stakeholder edit view

---
+++ frontend/src/routes/(app)/(internal)/stakeholders/[id=uuid]/edit/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import ModelForm from '$lib/components/Forms/ModelForm.svelte';
+	import { breadcrumbObject } from '$lib/utils/stores';
+
+	export let data: PageData;
+
+	breadcrumbObject.set(data.object);
+</script>
+
+<ModelForm
+	form={data.form}
+	action="?/updateStakeholder"
+	object={data.object}
+	selectOptions={data.selectOptions}
+	foreignKeys={data.foreignKeys}
+	model={data.model}
+	context="edit"
+/>
