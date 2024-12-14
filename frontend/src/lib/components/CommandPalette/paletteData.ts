5f27e35e38d03489d45b82cc4d4b6cb6e47edc39
Abderrahmane Smimite <smimite@gmail.com>
2024-12-14 22:18:47 +0100

    formatter

--- frontend/src/lib/components/CommandPalette/paletteData.ts
+++ frontend/src/lib/components/CommandPalette/paletteData.ts
@@ -2,17 +2,17 @@ import { goto } from '$app/navigation';
 import * as m from '$paraglide/messages';
 
 export interface NavigationLink {
-  label: string;
-  href: string;
+	label: string;
+	href: string;
 }
 
 export const navigationLinks: NavigationLink[] = [
-  {
-    label: m.analytics(),
-    href: '/',
-  },
-  {
-    label: m.settings(),
-    href: '/settings',
-  }
+	{
+		label: m.analytics(),
+		href: '/'
+	},
+	{
+		label: m.settings(),
+		href: '/settings'
+	}
 ];
