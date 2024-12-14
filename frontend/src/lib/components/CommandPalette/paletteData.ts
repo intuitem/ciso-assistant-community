eea052c26ca62944416f57759092439d6840dee6
Abderrahmane Smimite <smimite@gmail.com>
2024-12-14 22:18:23 +0100

    Move links outside and manage i18n

---
+++ frontend/src/lib/components/CommandPalette/paletteData.ts
@@ -0,0 +1,18 @@
+import { goto } from '$app/navigation';
+import * as m from '$paraglide/messages';
+
+export interface NavigationLink {
+  label: string;
+  href: string;
+}
+
+export const navigationLinks: NavigationLink[] = [
+  {
+    label: m.analytics(),
+    href: '/',
+  },
+  {
+    label: m.settings(),
+    href: '/settings',
+  }
+];
