3e795614be404f19cb5fd6d879f9fbb84433cae9
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-12-16 17:46:22 +0100

    Set max width for breadcrumb items

--- frontend/src/lib/components/Breadcrumbs/Breadcrumbs.svelte
+++ frontend/src/lib/components/Breadcrumbs/Breadcrumbs.svelte
@@ -3,10 +3,13 @@
 	import { safeTranslate } from '$lib/utils/i18n';
 </script>
 
-<ol class="breadcrumb-nonresponsive">
+<ol class="breadcrumb-nonresponsive h-6 overflow-hidden whitespace-nowrap">
 	{#each $breadcrumbs as c, i}
 		{#if i == $breadcrumbs.length - 1}
-			<span class="text-sm text-gray-500 font-semibold antialiased" data-testid="crumb-item">
+			<span
+				class="max-w-[64ch] overflow-hidden text-sm text-gray-500 font-semibold antialiased"
+				data-testid="crumb-item"
+			>
 				{#if c.icon}
 					<i class={c.icon} />
 				{/if}
@@ -16,7 +19,7 @@
 			<li class="crumb">
 				{#if c.href}
 					<a
-						class="unstyled text-sm hover:text-primary-500 font-semibold antialiased whitespace-nowrap"
+						class="max-w-[64ch] overflow-hidden unstyled text-sm hover:text-primary-500 font-semibold antialiased whitespace-nowrap"
 						data-testid="crumb-item"
 						href={c.href}
 						on:click={() => breadcrumbs.slice(i)}
@@ -27,7 +30,10 @@
 						{safeTranslate(c.label)}
 					</a>
 				{:else}
-					<span class="text-sm text-gray-500 font-semibold antialiased" data-testid="crumb-item">
+					<span
+						class="max-w-[64ch] overflow-hidden text-sm text-gray-500 font-semibold antialiased"
+						data-testid="crumb-item"
+					>
 						{#if c.icon}
 							<i class={c.icon} />
 						{/if}
