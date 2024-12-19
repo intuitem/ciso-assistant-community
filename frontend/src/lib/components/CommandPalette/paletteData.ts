7e1029a66468e0558f04ad1311dbb73de3b15561
Abderrahmane Smimite <smimite@gmail.com>
2024-12-19 14:08:27 +0100

    translations - extra

--- frontend/src/lib/components/CommandPalette/paletteData.ts
+++ frontend/src/lib/components/CommandPalette/paletteData.ts
@@ -1,65 +1,69 @@
 import * as m from '$paraglide/messages';
 
 export interface NavigationLink {
-	label: string;
-	href: string;
+  label: string;
+  href: string;
 }
 
 export const navigationLinks: NavigationLink[] = [
-	{
-		label: 'analytics',
-		href: '/'
-	},
-	{
-		label: 'myAssignments',
-		href: '/my-assignments'
-	},
-	{
-		label: 'domains',
-		href: '/folders'
-	},
-	{
-		label: 'assets',
-		href: '/assets'
-	},
-	{
-		label: 'complianceAssessments',
-		href: '/compliance-assessments'
-	},
-	{
-		label: 'riskAssessments',
-		href: '/risk-assessments'
-	},
-	{
-		label: 'riskScenarios',
-		href: '/risk-scenarios'
-	},
-	{
-		label: 'actionPlan',
-		href: '/applied-controls'
-	},
-	{
-		label: 'evidences',
-		href: '/evidences'
-	},
-	{
-		label: 'xRays',
-		href: '/x-rays'
-	},
-	{
-		label: 'tpSolutions',
-		href: '/solutions'
-	},
-	{
-		label: 'libraries',
-		href: '/libraries'
-	},
-	{
-		label: 'myProfile',
-		href: '/my-profile'
-	},
-	{
-		label: 'settings',
-		href: '/settings'
-	}
+  {
+    label: 'analytics',
+    href: '/'
+  },
+  {
+    label: 'myAssignments',
+    href: '/my-assignments'
+  },
+  {
+    label: 'domains',
+    href: '/folders'
+  },
+  {
+    label: 'assets',
+    href: '/assets'
+  },
+  {
+    label: 'complianceAssessments',
+    href: '/compliance-assessments'
+  },
+  {
+    label: 'riskAssessments',
+    href: '/risk-assessments'
+  },
+  {
+    label: 'riskScenarios',
+    href: '/risk-scenarios'
+  },
+  {
+    label: 'ebiosRM',
+    href: '/ebios-rm'
+  },
+  {
+    label: 'actionPlan',
+    href: '/applied-controls'
+  },
+  {
+    label: 'evidences',
+    href: '/evidences'
+  },
+  {
+    label: 'xRays',
+    href: '/x-rays'
+  },
+  {
+    label: 'tpSolutions',
+    href: '/solutions'
+  },
+  {
+    label: 'libraries',
+    href: '/libraries'
+  },
+  {
+    label: 'myProfile',
+    href: '/my-profile'
+  },
+  {
+    label: 'settings',
+    href: '/settings'
+  }
 ];
