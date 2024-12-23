18b082ffb7bab4e068b32118dc9cc9b156afba1f
Abderrahmane Smimite <smimite@gmail.com>
2024-12-23 14:26:51 +0100

    formatter

--- frontend/src/routes/(app)/(internal)/insights/controls-impact/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/insights/controls-impact/+page.server.ts
@@ -1,13 +1,12 @@
-
 import { BASE_API_URL } from '$lib/utils/constants';
 
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params, fetch }) => {
-  const endpoint = `${BASE_API_URL}/applied-controls/impact_graph/`;
+	const endpoint = `${BASE_API_URL}/applied-controls/impact_graph/`;
 
-  const res = await fetch(endpoint);
-  const data = await res.json();
+	const res = await fetch(endpoint);
+	const data = await res.json();
 
-  return { data };
+	return { data };
 };
