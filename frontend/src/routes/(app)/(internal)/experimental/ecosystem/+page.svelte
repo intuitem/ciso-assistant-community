6b79c8ae57210d33c3fffe63acd3f25857b6c9ce
Abderrahmane Smimite <smimite@gmail.com>
2024-12-25 15:56:04 +0100

    revert changes on unrelated file

--- frontend/src/routes/(app)/(internal)/experimental/ecosystem/+page.svelte
+++ frontend/src/routes/(app)/(internal)/experimental/ecosystem/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	export let data: PageData;
-	import PriorityRadarChart from '$lib/components/Chart/PriorityRadarChart.svelte';
+	import EcosystemRadarChart from '$lib/components/Chart/EcosystemRadarChart.svelte';
 </script>
 
 <div class="bg-white shadow flex overflow-x-auto">
 	<div class="w-full h-screen">
-		<PriorityRadarChart title="Ecosystem - current" name="c_ecosystem" data={data.data.current} />
+		<EcosystemRadarChart title="Ecosystem - current" name="c_ecosystem" data={data.data.current} />
+		<EcosystemRadarChart
+			title="Ecosystem - residual"
+			name="r_ecosystem"
+			data={data.data.residual}
+		/>
 	</div>
 </div>
