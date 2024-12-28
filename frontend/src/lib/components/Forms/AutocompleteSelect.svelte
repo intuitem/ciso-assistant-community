ef136afeb6000def6544e9cc6c374c7b31c8efc4
Abderrahmane Smimite <smimite@gmail.com>
2024-12-28 10:04:33 +0100

    redundant check

--- frontend/src/lib/components/Forms/AutocompleteSelect.svelte
+++ frontend/src/lib/components/Forms/AutocompleteSelect.svelte
@@ -68,8 +68,6 @@
 		const set1 = new Set(arr1);
 		const set2 = new Set(arr2);
 
-		if (set1.size !== set2.size) return false;
-
 		for (const value of set1) {
 			if (!set2.has(value)) return false;
 		}
