e1eebff4527fdae1f25712d32714d97be067a72d
Abderrahmane Smimite <smimite@gmail.com>
2024-12-29 19:05:40 +0100

    Increase wait time for initialisation to cover slow devices

--- enterprise/docker-compose.sh
+++ enterprise/docker-compose.sh
@@ -8,7 +8,7 @@ else
   docker rmi ghcr.io/intuitem/ciso-assistant-enterprise-backend:latest ghcr.io/intuitem/ciso-assistant-enterprise-frontend:latest 2>/dev/null
   docker compose up -d
   echo "Giving some time for the database to be ready, please wait ..."
-  sleep 30
+  sleep 50
   echo "initialize your superuser account..."
   docker compose exec backend poetry run python manage.py createsuperuser
   echo "connect to ciso assistant on https://localhost:9443"
