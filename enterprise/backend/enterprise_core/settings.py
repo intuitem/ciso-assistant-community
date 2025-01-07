89f9012ce149bea5fbdfa37ec2f6457f014c4548
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-01-07 17:27:57 +0100

    Switch to limit offset pagination in the backend

--- enterprise/backend/enterprise_core/settings.py
+++ enterprise/backend/enterprise_core/settings.py
@@ -214,8 +214,7 @@ REST_FRAMEWORK = {
         "enterprise_core.permissions.LicensePermission",
     ],
     "DEFAULT_FILTER_CLASSES": ["django_filters.rest_framework.DjangoFilterBackend"],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": PAGINATE_BY,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "EXCEPTION_HANDLER": "core.helpers.handle",
 }
