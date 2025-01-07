a328e35cb1a8c523b3e0b4a3fb2db61ad062ae43
Abderrahmane Smimite <smimite@gmail.com>
2025-01-07 22:41:40 +0100

    Refactor the steps outside the component

--- frontend/src/lib/components/SideBar/SideBar.svelte
+++ frontend/src/lib/components/SideBar/SideBar.svelte
@@ -5,81 +5,25 @@
 	import SideBarToggle from './SideBarToggle.svelte';
 	import { onMount } from 'svelte';
 	export let open: boolean;
+	import { steps } from './guidedTourData.js';
+	export let firstTime = false; // this needs to come from the db ; we also need to make room for variable about the specialized guided tours
 
 	$: classesSidebarOpen = (open: boolean) => (open ? '' : '-ml-[14rem] pointer-events-none');
 
 	import { driver } from 'driver.js';
 	import 'driver.js/dist/driver.css';
 
-	const driverObj = driver();
-	onMount(() => {
+	function triggerVisit() {
 		const driverObj = driver({
 			showProgress: true,
-			steps: [
-				{
-					element: 'none',
-					popover: {
-						title: 'Welcome !',
-						description:
-							'The quick guided tour will help setup the main parts to get started with CISO Assistant.'
-					}
-				},
-				{
-					element: '#organization',
-					popover: {
-						title: 'Click to unfold',
-						description: 'This section will help you define the scopes of your work.'
-					}
-				},
-				{
-					element: '#domains',
-					popover: {
-						title: 'Click here',
-						description: 'You will need to create a first domain to get started'
-					}
-				},
-				{
-					element: '#add-button',
-					popover: {
-						title: 'Click here',
-						description: 'You will need to create a first domain to get started'
-					}
-				},
-				{
-					element: 'none',
-					popover: {
-						title: 'The catalog',
-						description:
-							'The library of CISO Assistant is quite comprehensive and contain multiple objects: frameworks, threats, matrices.'
-					}
-				},
-				{
-					element: '#catalog',
-					popover: { title: 'Click to unfold' }
-				},
-				{ element: '#frameworks', popover: { title: 'Title', description: 'Description' } },
-				{
-					element: '#add-button',
-					popover: { title: 'Click to import one', description: 'Description' }
-				},
-				{
-					element: '#riskMatrices',
-					popover: {
-						title: 'Title',
-						description: 'You need a framework and a matrix to get  started'
-					}
-				},
-				{ element: '#compliance', popover: { title: 'Title', description: 'Description' } },
-				{
-					element: '#complianceAssessments',
-					popover: { title: 'Title', description: 'Description' }
-				},
-				{ element: '#risk', popover: { title: 'Title', description: 'Description' } },
-				{ element: '#riskAssessments', popover: { title: 'Title', description: 'Description' } }
-			]
+			steps: steps
 		});
-
 		driverObj.drive();
+	}
+	onMount(() => {
+		if (firstTime) {
+			triggerVisit();
+		}
 	});
 </script>
 
