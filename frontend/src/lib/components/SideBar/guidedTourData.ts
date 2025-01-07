7646270c1af2bc593eb4021de9c24b56a3fbd262
Abderrahmane Smimite <smimite@gmail.com>
2025-01-07 22:44:12 +0100

    Formatter

--- frontend/src/lib/components/SideBar/guidedTourData.ts
+++ frontend/src/lib/components/SideBar/guidedTourData.ts
@@ -1,63 +1,62 @@
-
 export const steps = [
-  {
-    element: 'none',
-    popover: {
-      title: 'Welcome !',
-      description:
-        'The quick guided tour will help setup the main parts to get started with CISO Assistant.'
-    }
-  },
-  {
-    element: '#organization',
-    popover: {
-      title: 'Click to unfold',
-      description: 'This section will help you define the scopes of your work.'
-    }
-  },
-  {
-    element: '#domains',
-    popover: {
-      title: 'Click here',
-      description: 'You will need to create a first domain to get started'
-    }
-  },
-  {
-    element: '#add-button',
-    popover: {
-      title: 'Click here',
-      description: 'You will need to create a first domain to get started'
-    }
-  },
-  {
-    element: 'none',
-    popover: {
-      title: 'The catalog',
-      description:
-        'The library of CISO Assistant is quite comprehensive and contain multiple objects: frameworks, threats, matrices.'
-    }
-  },
-  {
-    element: '#catalog',
-    popover: { title: 'Click to unfold' }
-  },
-  { element: '#frameworks', popover: { title: 'Title', description: 'Description' } },
-  {
-    element: '#add-button',
-    popover: { title: 'Click to import one', description: 'Description' }
-  },
-  {
-    element: '#riskMatrices',
-    popover: {
-      title: 'Title',
-      description: 'You need a framework and a matrix to get  started'
-    }
-  },
-  { element: '#compliance', popover: { title: 'Title', description: 'Description' } },
-  {
-    element: '#complianceAssessments',
-    popover: { title: 'Title', description: 'Description' }
-  },
-  { element: '#risk', popover: { title: 'Title', description: 'Description' } },
-  { element: '#riskAssessments', popover: { title: 'Title', description: 'Description' } }
+	{
+		element: 'none',
+		popover: {
+			title: 'Welcome !',
+			description:
+				'The quick guided tour will help setup the main parts to get started with CISO Assistant.'
+		}
+	},
+	{
+		element: '#organization',
+		popover: {
+			title: 'Click to unfold',
+			description: 'This section will help you define the scopes of your work.'
+		}
+	},
+	{
+		element: '#domains',
+		popover: {
+			title: 'Click here',
+			description: 'You will need to create a first domain to get started'
+		}
+	},
+	{
+		element: '#add-button',
+		popover: {
+			title: 'Click here',
+			description: 'You will need to create a first domain to get started'
+		}
+	},
+	{
+		element: 'none',
+		popover: {
+			title: 'The catalog',
+			description:
+				'The library of CISO Assistant is quite comprehensive and contain multiple objects: frameworks, threats, matrices.'
+		}
+	},
+	{
+		element: '#catalog',
+		popover: { title: 'Click to unfold' }
+	},
+	{ element: '#frameworks', popover: { title: 'Title', description: 'Description' } },
+	{
+		element: '#add-button',
+		popover: { title: 'Click to import one', description: 'Description' }
+	},
+	{
+		element: '#riskMatrices',
+		popover: {
+			title: 'Title',
+			description: 'You need a framework and a matrix to get  started'
+		}
+	},
+	{ element: '#compliance', popover: { title: 'Title', description: 'Description' } },
+	{
+		element: '#complianceAssessments',
+		popover: { title: 'Title', description: 'Description' }
+	},
+	{ element: '#risk', popover: { title: 'Title', description: 'Description' } },
+	{ element: '#riskAssessments', popover: { title: 'Title', description: 'Description' } }
 ];
