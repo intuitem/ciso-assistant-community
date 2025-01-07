4efa795722f72ed6be4a1709d9383b522709d3d5
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-01-07 11:08:36 +0100

    Set framework page title to framework name

--- frontend/src/routes/(app)/(internal)/frameworks/[id=uuid]/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/frameworks/[id=uuid]/+page.server.ts
@@ -10,5 +10,5 @@ export const load = (async ({ fetch, params }) => {
 	const tree = await fetch(`${BASE_API_URL}/${URLModel}/${params.id}/tree`).then((res) =>
 		res.json()
 	);
-	return { URLModel, framework, tree };
+	return { URLModel, framework, tree, title: framework.name };
 }) satisfies PageServerLoad;
