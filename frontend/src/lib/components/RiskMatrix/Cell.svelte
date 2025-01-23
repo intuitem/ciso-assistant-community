c38599331593b6115c019880fac6e4f973adfc39
Axxiar <galoualexis@gmail.com>
2025-01-23 15:32:45 +0100

    fix(ui): scrollable tooltip and minor ui fix

--- frontend/src/lib/components/RiskMatrix/Cell.svelte
+++ frontend/src/lib/components/RiskMatrix/Cell.svelte
@@ -40,7 +40,7 @@
 
 {#if cellData.length >= bubbleMinCount && dataItemComponent}
 	<div
-		class="flex flex-wrap items-center space-x-1 justify-center h-full cursor-pointer whitespace-normal overflow-y-scroll hide-scrollbar {classesCellText(
+		class="flex flex-wrap items-center space-x-1 justify-center h-full cursor-pointer whitespace-normal overflow-y-scroll hide-scrollbar group {classesCellText(
 			cell.level.hexcolor
 		)}"
 		style="background-color: {cell.level.hexcolor};"
@@ -48,19 +48,20 @@
 		use:popup={popupClick}
 	>
 		<div
-			class="bg-surface-900/80 rounded-full flex justify-center items-center text-center text-white"
+			class="bg-surface-900/70 rounded-full flex justify-center items-center text-center text-white transition-colors group-hover:bg-surface-900/100 duration-300"
 			style="{classesBubbleSize(cellData.length)};"
 		>
 			{cellData.length}
 		</div>
 		<div
-			class="card bg-black text-gray-200 p-4 z-20"
+			class="card bg-surface-300 text-black p-4 z-20 max-h-56 overflow-y-scroll"
 			data-popup={'popup' + 'data' + '-' + cellData.map((e) => e.ref_id).join('-')}
 		>
 			{#each cellData as item}
 				<svelte:component this={dataItemComponent} data={item} />
 			{/each}
-			<div class="arrow bg-black" />
+			<!-- not displayed because of overflow-y-scroll on parent -->
+			<div class="arrow bg-surface-300" />
 		</div>
 	</div>
 {:else}
