d738d1de0775398f0a50cb3f08f0a1687d8d561d
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-01-29 19:47:06 +0100

    Add null checks

--- frontend/src/lib/components/ModelTable/ModelTable.svelte
+++ frontend/src/lib/components/ModelTable/ModelTable.svelte
@@ -4,8 +4,8 @@
 
 	export let source: TableSource;
 
-	const next = source.meta.next;
-	const previous = source.meta.previous;
+	const next = source.meta?.next;
+	const previous = source.meta?.previous;
 
 	export let server = Boolean(next || previous);
 
@@ -18,7 +18,7 @@
 		});
 	});
 
-	$: _source = { ...source, meta: source.meta.results || source.meta };
+	$: _source = { ...source, meta: source.meta?.results || source.meta };
 </script>
 
 <svelte:component this={modelTable} source={_source} {...$$restProps}>
