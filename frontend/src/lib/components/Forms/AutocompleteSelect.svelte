502ab0b10384dec95f3d0b2a3024d0b4e403a46a
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2025-01-29 18:04:26 +0100

    style: add spinner during AutocompleteSelect loading

--- frontend/src/lib/components/Forms/AutocompleteSelect.svelte
+++ frontend/src/lib/components/Forms/AutocompleteSelect.svelte
@@ -99,17 +99,21 @@
 	};
 
 	const dispatch = createEventDispatcher();
+	// Add loading state
+	let isLoading = false;
 
 	async function fetchOptions() {
+		isLoading = true;
 		try {
-			const response = await fetch(`/${endpoint}`);
+			const response = await fetch(`/${endpoint}/`);
 			if (response.ok) {
 				const data = await response.json();
 				options = processOptions(data);
-				console.log(data)
 			}
 		} catch (error) {
 			console.error(`Error fetching ${endpoint}:`, error);
+		} finally {
+			isLoading = false;
 		}
 	}
 
@@ -242,7 +246,7 @@
 			{/each}
 		</div>
 	{/if}
-	<div class="control overflow-x-clip" data-testid="form-input-{field.replaceAll('_', '-')}">
+	<div class="control overflow-x-clip flex items-center space-x-2" data-testid="form-input-{field.replaceAll('_', '-')}">
 		<input type="hidden" name={field} value={$value ? $value : ''} />
 		<MultiSelect
 			bind:selected
@@ -263,6 +267,12 @@
 				{option.label || option}
 			{/if}
 		</MultiSelect>
+		{#if isLoading}
+			<svg class="animate-spin h-5 w-5 text-primary-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+				<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+				<path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+			</svg>
+		{/if}
 	</div>
 	{#if helpText}
 		<p class="text-sm text-gray-500">{helpText}</p>
