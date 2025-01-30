3c0a0de59939a435d738fdd2e8a8f5a0234105b7
Abderrahmane Smimite <smimite@gmail.com>
2025-01-30 19:37:24 +0100

    Cleaner look for Applied controls forms

--- frontend/src/lib/components/Forms/ModelForm/AppliedControlPolicyForm.svelte
+++ frontend/src/lib/components/Forms/ModelForm/AppliedControlPolicyForm.svelte
@@ -4,6 +4,7 @@
 	import Checkbox from '$lib/components/Forms/Checkbox.svelte';
 	import TextField from '$lib/components/Forms/TextField.svelte';
 	import NumberField from '$lib/components/Forms/NumberField.svelte';
+	import Dropdown from '$lib/components/Dropdown/Dropdown.svelte';
 	import Score from '$lib/components/Forms/Score.svelte';
 	import { getOptions } from '$lib/utils/crud';
 	import type { SuperValidated } from 'sveltekit-superforms';
@@ -25,7 +26,6 @@
 	}
 </script>
 
-toto
 {#if !duplicate}
 	<AutocompleteSelect
 		{form}
@@ -61,95 +61,96 @@ toto
 		cacheLock={cacheLocks['eta']}
 		bind:cachedValue={formDataCache['eta']}
 	/>
-	More
-	<TextField
-		{form}
-		field="ref_id"
-		label={m.refId()}
-		cacheLock={cacheLocks['ref_id']}
-		bind:cachedValue={formDataCache['ref_id']}
-	/>
-	<Select
-		{form}
-		options={model.selectOptions['priority']}
-		field="priority"
-		label={m.priority()}
-		cacheLock={cacheLocks['priority']}
-		bind:cachedValue={formDataCache['priority']}
-	/>
-	<AutocompleteSelect
-		{form}
-		multiple
-		options={getOptions({
-			objects: model.foreignKeys['evidences'],
-			extra_fields: [['folder', 'str']]
-		})}
-		field="evidences"
-		cacheLock={cacheLocks['evidences']}
-		bind:cachedValue={formDataCache['evidences']}
-		label={m.evidences()}
-	/>
-	{#if schema.shape.category}
+	<Dropdown open={false} style="hover:text-primary-700" icon="fa-solid fa-list" header={m.more()}>
+		<TextField
+			{form}
+			field="ref_id"
+			label={m.refId()}
+			cacheLock={cacheLocks['ref_id']}
+			bind:cachedValue={formDataCache['ref_id']}
+		/>
 		<Select
 			{form}
-			options={model.selectOptions['category']}
-			field="category"
-			label={m.category()}
-			cacheLock={cacheLocks['category']}
-			bind:cachedValue={formDataCache['category']}
+			options={model.selectOptions['priority']}
+			field="priority"
+			label={m.priority()}
+			cacheLock={cacheLocks['priority']}
+			bind:cachedValue={formDataCache['priority']}
 		/>
-	{/if}
-	<Select
-		{form}
-		options={model.selectOptions['csf_function']}
-		field="csf_function"
-		label={m.csfFunction()}
-		cacheLock={cacheLocks['csf_function']}
-		bind:cachedValue={formDataCache['csf_function']}
-	/>
-	<TextField
-		type="date"
-		{form}
-		field="start_date"
-		label={m.startDate()}
-		helpText={m.startDateHelpText()}
-		cacheLock={cacheLocks['start_date']}
-		bind:cachedValue={formDataCache['start_date']}
-	/>
-	<TextField
-		type="date"
-		{form}
-		field="expiry_date"
-		label={m.expiryDate()}
-		helpText={m.expiryDateHelpText()}
-		cacheLock={cacheLocks['expiry_date']}
-		bind:cachedValue={formDataCache['expiry_date']}
-	/>
-	<Select
-		{form}
-		options={model.selectOptions['effort']}
-		field="effort"
-		label={m.effort()}
-		helpText={m.effortHelpText()}
-		cacheLock={cacheLocks['effort']}
-		bind:cachedValue={formDataCache['effort']}
-	/>
-	<NumberField
-		{form}
-		field="cost"
-		label={m.cost()}
-		helpText={m.costHelpText()}
-		cacheLock={cacheLocks['cost']}
-		bind:cachedValue={formDataCache['cost']}
-	/>
-	<TextField
-		{form}
-		field="link"
-		label={m.link()}
-		helpText={m.linkHelpText()}
-		cacheLock={cacheLocks['link']}
-		bind:cachedValue={formDataCache['link']}
-	/>
+		<AutocompleteSelect
+			{form}
+			multiple
+			options={getOptions({
+				objects: model.foreignKeys['evidences'],
+				extra_fields: [['folder', 'str']]
+			})}
+			field="evidences"
+			cacheLock={cacheLocks['evidences']}
+			bind:cachedValue={formDataCache['evidences']}
+			label={m.evidences()}
+		/>
+		{#if schema.shape.category}
+			<Select
+				{form}
+				options={model.selectOptions['category']}
+				field="category"
+				label={m.category()}
+				cacheLock={cacheLocks['category']}
+				bind:cachedValue={formDataCache['category']}
+			/>
+		{/if}
+		<Select
+			{form}
+			options={model.selectOptions['csf_function']}
+			field="csf_function"
+			label={m.csfFunction()}
+			cacheLock={cacheLocks['csf_function']}
+			bind:cachedValue={formDataCache['csf_function']}
+		/>
+		<TextField
+			type="date"
+			{form}
+			field="start_date"
+			label={m.startDate()}
+			helpText={m.startDateHelpText()}
+			cacheLock={cacheLocks['start_date']}
+			bind:cachedValue={formDataCache['start_date']}
+		/>
+		<TextField
+			type="date"
+			{form}
+			field="expiry_date"
+			label={m.expiryDate()}
+			helpText={m.expiryDateHelpText()}
+			cacheLock={cacheLocks['expiry_date']}
+			bind:cachedValue={formDataCache['expiry_date']}
+		/>
+		<Select
+			{form}
+			options={model.selectOptions['effort']}
+			field="effort"
+			label={m.effort()}
+			helpText={m.effortHelpText()}
+			cacheLock={cacheLocks['effort']}
+			bind:cachedValue={formDataCache['effort']}
+		/>
+		<NumberField
+			{form}
+			field="cost"
+			label={m.cost()}
+			helpText={m.costHelpText()}
+			cacheLock={cacheLocks['cost']}
+			bind:cachedValue={formDataCache['cost']}
+		/>
+		<TextField
+			{form}
+			field="link"
+			label={m.link()}
+			helpText={m.linkHelpText()}
+			cacheLock={cacheLocks['link']}
+			bind:cachedValue={formDataCache['link']}
+		/>
+	</Dropdown>
 {/if}
 
 {#if duplicate}
