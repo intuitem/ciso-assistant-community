3dac9fe6c2875be7469cb695957e101a45bbff0e
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2025-02-05 14:33:34 +0100

    refactor!: rename project to perimeter as preparation for upcoming features (#1446)
    * refactor project -> perimeter
    
    * ruff+perttier
    
    * fix pytest failing
    
    * update migrations
    
    * ruff
    
    * Use migrations.RenameField to preserve existing links with projects
    
    * Revert testInfo.perimeter to testInfo.project
    
    * Close first connection modal if it opens
    
    * Fix translations
    
    * remove redundant lines
    
    * Update ar.json
    
    prettier
    
    * Remove useless translation
    
    ChatGPT hallucinated solution would probably not work anyway
    
    ---------
    
    Co-authored-by: Abder <smimite@gmail.com>
    Co-authored-by: Nassim Tabchiche <n.tabchiche2@gmail.com>

--- frontend/src/routes/(app)/(internal)/[model=urlmodel]/[id=uuid]/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/[model=urlmodel]/[id=uuid]/+page.server.ts
@@ -73,7 +73,7 @@ export const load: PageServerLoad = async (event) => {
 
 export const actions: Actions = {
 	create: async (event) => {
-		const redirectToWrittenObject = Boolean(event.params.model === 'projects');
+		const redirectToWrittenObject = Boolean(event.params.model === 'perimeters');
 		return nestedWriteFormAction({ event, action: 'create', redirectToWrittenObject });
 	},
 	delete: async (event) => {
