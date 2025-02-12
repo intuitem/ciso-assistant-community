87ffbeaefa5545b2688892009450cf195e7aa951
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2025-02-12 11:54:38 +0100

    perf: query set optimization (#1491)
    * feat: fetch options in AutocompleteSelect instead of load function
    
    * style: add spinner during AutocompleteSelect loading
    
    * chore: format
    
    * chore: remove checkConstraints in page.svelte
    
    * feat: check constraints inside AutocompleteSelect
    
    * feat: globalize AutocompleteSelect fetching
    
    * chore: format
    
    * chore: cleaning useles foreignKey fetches and old missing constraints logic
    
    * fix: typo in ThreatForm optionsEndpoint
    
    * fix: use optionsLabel str for risk assessment field in scenario form
    
    * feat: add optionsLabelField auto for assets threats and feared events
    
    * feat: sort options to put suggestions first
    
    * chore: add missing indent
    
    * chore: remove unused getOptions
    
    * chore: give default values to avoid warnings in frameworks detailed view
    
    * feat: add entity assessment folder extra label for perimeter field
    
    * chore: nitpicking
    
    * feat: add detailed url parameters in AutocompleteSelect fetchOptions
    
    * feat: exclude self from options for parent assets and folder
    
    * feat: refresh AutocompleteSelect when applied control/evidence is created inside requirement assessment
    
    * chore: format front
    
    * chore: remove useless foreignKeys fetch in risk assessment page
    
    * chore: remove the latest foreignKeys fetches on server side
    
    * Improve performance
    
    - optimize get_accessible_object_ids by first calculating the folders that match, and only then retrieving (optimally) the objects from these folders. This should be way more scalable.
    - use scope_folder_id parameter to scope the retrieved objects
    
    * prettier/ruff
    
    * Fix regression
    
    special case of folder for get_accessible_object_ids
    
    * fix published objects not visible
    
    ---------
    
    Co-authored-by: Mohamed-Hacene <mohamedhacene.b@gmail.com>

--- frontend/src/routes/(app)/(third-party)/requirement-assessments/[id=uuid]/edit/+page.svelte
+++ frontend/src/routes/(app)/(third-party)/requirement-assessments/[id=uuid]/edit/+page.svelte
@@ -410,6 +410,9 @@
 										multiple
 										{form}
 										optionsEndpoint="applied-controls"
+										optionsDetailedUrlParameters={[
+											['scope_folder_id', $page.data.requirementAssessment.folder.id]
+										]}
 										optionsExtraFields={[['folder', 'str']]}
 										field="applied_controls"
 									/>
@@ -442,6 +445,9 @@
 										{form}
 										optionsEndpoint="evidences"
 										optionsExtraFields={[['folder', 'str']]}
+										optionsDetailedUrlParameters={[
+											['scope_folder_id', $page.data.requirementAssessment.folder.id]
+										]}
 										field="evidences"
 									/>
 								{/key}
