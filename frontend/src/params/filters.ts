a41b04a2d660d1f1383c23cb4e366252e8747fec
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-02-17 11:43:31 +0100

    Forward static filter choices to frontend

---
+++ frontend/src/params/filters.ts
@@ -0,0 +1,18 @@
+import { listViewFields } from '$lib/utils/table';
+import type { ParamMatcher } from '@sveltejs/kit';
+
+export const match = ((param) => {
+	const filterKeys = new Set<string>();
+
+	Object.values(listViewFields).forEach((field) => {
+		if ('filters' in field && field.filters) {
+			Object.keys(field.filters).forEach((filterKey) => filterKeys.add(filterKey));
+		}
+	});
+
+	console.log(filterKeys);
+
+	// Example output: ["folder", "lc_status", "filtering_labels"]
+
+	return filterKeys.has(param);
+}) satisfies ParamMatcher;
