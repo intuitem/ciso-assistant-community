33b463962240e50be0caf86dd2c8b490d05315fc
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2025-02-21 22:26:09 +0100

    Update 0055_alter_storedlibrary_content.py
    ruff

--- backend/core/migrations/0055_alter_storedlibrary_content.py
+++ backend/core/migrations/0055_alter_storedlibrary_content.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('core', '0054_alter_appliedcontrol_is_published_and_more'),
+        ("core", "0054_alter_appliedcontrol_is_published_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='storedlibrary',
-            name='content',
+            model_name="storedlibrary",
+            name="content",
             field=models.JSONField(),
         ),
     ]
