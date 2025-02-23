a6b34b12ec48d72796e518589e9ba9b6139decdc
Abderrahmane Smimite <smimite@gmail.com>
2025-02-23 23:20:26 +0100

    fixup

--- config/docker-compose.sh
+++ config/docker-compose.sh
@@ -8,12 +8,12 @@ fi
 
 if [ -d ./db ]; then
   echo "The database seems already created. You should launch 'docker compose up -d' instead."
-  echo "\nFor a clean start, you can remove the db folder, and then run 'docker compose rm -fs' and start over"
+  echo "For a clean start, you can remove the db folder, and then run 'docker compose rm -fs' and start over"
   exit 1
 fi
 echo "Starting CISO Assistant services..."
 docker compose -f ./docker-compose.yml pull
-echo "Initializing the database. This can take a minute, please wait.."
+echo "Initializing the database. This can take up to 2 minutes, please wait.."
 docker compose -f ./docker-compose.yml up -d
 
 echo "Waiting for CISO Assistant backend to be ready..."
@@ -22,9 +22,9 @@ until docker compose -f ./docker-compose.yml exec -T backend curl -f http://loca
   sleep 10
 done
 
-echo -e "\nBackend is ready!"
+echo -e "Backend is ready!"
 echo "Creating superuser..."
 docker compose -f ./docker-compose.yml exec backend poetry run python manage.py createsuperuser
 
-echo -e "\nInitialization complete!"
+echo -e "Initialization complete!"
 echo "You can now access CISO Assistant at https://localhost:8443 (or the host:port you've specified)"
