e998c41a05c41c3a45dd07b0d7a340540e911041
Mohamed-Hacene <90701924+Mohamed-Hacene@users.noreply.github.com>
2025-02-27 18:22:40 +0100

    fix: stakeholder edit view (#1534)
    * chore: remove dead code leading to 404 error
    
    * chore: format

--- frontend/src/routes/(app)/(internal)/stakeholders/[id=uuid]/edit/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/stakeholders/[id=uuid]/edit/+page.server.ts
@@ -64,7 +64,7 @@ export const load: LayoutServerLoad = async (event) => {
 			}
 		}
 	}
-	model.foreignKeys = foreignKeys;
+
 	model.selectOptions = selectOptions;
 
 	const measureCreateSchema = modelSchema('applied-controls');
