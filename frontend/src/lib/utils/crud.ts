1cad0ddbb2f2202ff1c396fa4f2ac9cd43dd3386
Axxiar <70155662+Axxiar@users.noreply.github.com>
2025-02-28 19:07:25 +0100

    feat: add assets for audits (#1538)
    * feat: added assets for audits
    
    * feat: reverseForeignKey for audits in asset
    
    * fix: migration rename for merge
    
    * fix: missing filters
    
    * fix: update migration number
    
    * chore: ruff format

--- frontend/src/lib/utils/crud.ts
+++ frontend/src/lib/utils/crud.ts
@@ -383,6 +383,9 @@ export const URL_MODEL_MAP: ModelMap = {
 		localNamePlural: 'assets',
 		verboseName: 'Asset',
 		verboseNamePlural: 'Assets',
+		reverseForeignKeyFields: [
+			{ field: 'assets', urlModel: 'compliance-assessments', disableAddDeleteButtons: true }
+		],
 		foreignKeyFields: [
 			{ field: 'parent_assets', urlModel: 'assets' },
 			{ field: 'owner', urlModel: 'users' },
@@ -474,7 +477,8 @@ export const URL_MODEL_MAP: ModelMap = {
 			{ field: 'authors', urlModel: 'users' },
 			{ field: 'reviewers', urlModel: 'users', urlParams: 'is_third_party=false' },
 			{ field: 'baseline', urlModel: 'compliance-assessments' },
-			{ field: 'ebios_rm_studies', urlModel: 'ebios-rm' }
+			{ field: 'ebios_rm_studies', urlModel: 'ebios-rm' },
+			{ field: 'assets', urlModel: 'assets' }
 		],
 		selectFields: [{ field: 'status' }],
 		filters: [{ field: 'status' }]
