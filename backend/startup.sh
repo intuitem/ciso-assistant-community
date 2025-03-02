310df309a94b8b976605eeb0fd470e70246c732f
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2025-03-02 21:12:01 +0100

    WIP

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-cat /etc/passwd
-cat /etc/groups
-ls -alR /code
 # wait for database to be ready
 if [ ! -n "$DJANGO_SETTINGS_MODULE" ]; then
   export DJANGO_SETTINGS_MODULE=ciso_assistant.settings
