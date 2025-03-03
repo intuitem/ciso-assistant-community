316e8aff8121637423370c41b8ba1ffaf6057c85
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-03-03 15:03:18 +0100

    fix: update autocomplete on applied controls creation from suggestions

--- backend/core/views.py
+++ backend/core/views.py
@@ -4064,9 +4064,15 @@ class ComplianceAssessmentViewSet(BaseModelViewSet):
         ):
             return Response(status=status.HTTP_403_FORBIDDEN)
         requirement_assessments = compliance_assessment.requirement_assessments.all()
+        controls = []
         for requirement_assessment in requirement_assessments:
-            requirement_assessment.create_applied_controls_from_suggestions()
-        return Response(status=status.HTTP_200_OK)
+            controls.append(
+                requirement_assessment.create_applied_controls_from_suggestions()
+            )
+        return Response(
+            AppliedControlReadSerializer(chain.from_iterable(controls), many=True).data,
+            status=status.HTTP_200_OK,
+        )
 
     @action(detail=True, methods=["get"], url_path="progress_ts")
     def progress_ts(self, request, pk):
@@ -4200,8 +4206,11 @@ class RequirementAssessmentViewSet(BaseModelViewSet):
             folder=requirement_assessment.folder,
         ):
             return Response(status=status.HTTP_403_FORBIDDEN)
-        requirement_assessment.create_applied_controls_from_suggestions()
-        return Response(status=status.HTTP_200_OK)
+        controls = requirement_assessment.create_applied_controls_from_suggestions()
+        return Response(
+            AppliedControlReadSerializer(controls, many=True).data,
+            status=status.HTTP_200_OK,
+        )
 
 
 class RequirementMappingSetViewSet(BaseModelViewSet):
