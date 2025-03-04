62bb59190efe938b7cb3eae8a4ebb89715b4e4b0
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2025-03-04 22:58:18 +0100

    WIP

--- docker-compose.sh
+++ docker-compose.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 if [ -d ./db ]; then
-  echo "The database seems already created. You should launch 'docker compose -f ./docker-compose-default.yml up -d' instead."
-  echo "For a clean start, you can remove the db folder, and then run 'docker compose -f ./docker-compose-default.yml rm -fs' and start over"
+  echo "The database seems already created. You should launch 'docker compose up -d' instead."
+  echo "For a clean start, you can remove the db folder, and then run 'docker compose rm -fs' and start over"
   exit 1
 fi
 mkdir db
@@ -12,19 +12,19 @@ if [[ "$(uname -s)" == "Linux" ]]; then
   sudo chown 1001:1001 db
 fi
 echo "Starting CISO Assistant services..."
-docker compose -f ./docker-compose-default.yml pull
+docker compose pull
 echo "Initializing the database. This can take up to 2 minutes, please wait.."
-docker compose -f ./docker-compose-default.yml up -d
+docker compose up -d
 
 echo "Waiting for CISO Assistant backend to be ready..."
-until docker compose -f ./docker-compose-default.yml exec -T backend curl -f http://localhost:8000/api/build >/dev/null 2>&1; do
+until docker compose exec -T backend curl -f http://localhost:8000/api/build >/dev/null 2>&1; do
   echo "Backend is not ready - waiting 10s..."
   sleep 10
 done
 
 echo -e "Backend is ready!"
 echo "Creating superuser..."
-docker compose -f ./docker-compose-default.yml exec backend poetry run python manage.py createsuperuser
+docker compose exec backend poetry run python manage.py createsuperuser
 
 echo -e "Initialization complete!"
 echo "You can now access CISO Assistant at https://localhost:8443 (or the host:port you've specified)"
