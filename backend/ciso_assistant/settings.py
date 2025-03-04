54f282d22c218b5ee144a396517864f5e41778aa
Abderrahmane Smimite <smimite@gmail.com>
2025-03-04 13:11:48 +0100

    wip

--- backend/ciso_assistant/settings.py
+++ backend/ciso_assistant/settings.py
@@ -172,6 +172,7 @@ MIDDLEWARE = [
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
+    "ca_audit_log.middleware_request.UserMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
