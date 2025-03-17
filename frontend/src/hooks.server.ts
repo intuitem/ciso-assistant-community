aeee9d130a2fa88d324a520f610a69e5a36eea70
Abderrahmane Smimite <smimite@gmail.com>
2025-03-17 19:54:24 +0100

    align case

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -90,10 +90,10 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 export const handleFetch: HandleFetch = async ({ request, fetch, event: { cookies } }) => {
 	const unsafeMethods = new Set(['POST', 'PUT', 'PATCH', 'DELETE']);
-	const current_lang = cookies.get('PARAGLIDE_LOCALE') || DEFAULT_LANGUAGE;
+	const currentLang = cookies.get('PARAGLIDE_LOCALE') || DEFAULT_LANGUAGE;
 	if (request.url.startsWith(BASE_API_URL)) {
 		request.headers.set('Content-Type', 'application/json');
-		request.headers.set('Accept-Language', current_lang);
+		request.headers.set('Accept-Language', currentLang);
 
 		const token = cookies.get('token');
 		const csrfToken = cookies.get('csrftoken');
