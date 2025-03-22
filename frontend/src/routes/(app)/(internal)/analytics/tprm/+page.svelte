37c950c6bb140bfe144b83c8ef0f58f3340548d6
Abder <smimite@gmail.com>
2025-03-22 09:56:55 +0100

    feat: new tprm dashboard (#1732)
    * TPRM analytics: starting point
    
    * wip
    
    * wip
    
    * wip
    
    * error management
    
    * metric for answers collection progress
    
    * flippable card - starting
    
    * tailwind
    
    * fixup
    
    * Add sidebar entry
    
    * fixup

---
+++ frontend/src/routes/(app)/(internal)/analytics/tprm/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import FlippableCard from './FlippableCard.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+{@debug data}
+<div class="card bg-white bg-stripes-pink">
+	<div class="p-6 bg-white bg-opacity-85">
+		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+			{#each data.data as entity_assessment}
+				<FlippableCard {entity_assessment} />
+			{/each}
+		</div>
+	</div>
+</div>
+
+<style>
+	.bg-stripes-pink {
+		background-image: linear-gradient(
+			135deg,
+			#07275e 10%,
+			transparent 0,
+			transparent 50%,
+			#07275e 0,
+			#07275e 60%,
+			transparent 0,
+			transparent
+		);
+		background-size: 7.07px 7.07px;
+	}
+</style>
