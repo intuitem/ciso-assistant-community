538b86c54588232bc996b4377334bec148f711b9
Abder <smimite@gmail.com>
2025-03-24 06:36:51 +0100

    feat(ui): data vizualisation for findings followup (#1741)
    * follow-up backend metrics
    
    * getting data to frontend
    
    * room for widgets
    
    * data ready
    
    * basic data viz
    
    * remove debug
    
    * fixup
    
    * fixup
    
    * fixup
    
    * fixup

--- frontend/src/routes/(app)/(internal)/analytics/tprm/+page.svelte
+++ frontend/src/routes/(app)/(internal)/analytics/tprm/+page.svelte
@@ -5,16 +5,19 @@
 	export let data: PageData;
 </script>
 
-{@debug data}
-<div class="card bg-white bg-stripes-pink">
-	<div class="p-6 bg-white bg-opacity-85">
-		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-			{#each data.data as entity_assessment}
-				<FlippableCard {entity_assessment} />
-			{/each}
+{#if data.data && data.data.length > 0}
+	<div class="card bg-white bg-stripes-pink">
+		<div class="p-6 bg-white bg-opacity-85">
+			<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+				{#each data.data as entity_assessment}
+					<FlippableCard {entity_assessment} />
+				{/each}
+			</div>
 		</div>
 	</div>
-</div>
+{:else}
+	<div class="p-4">No data available for now.</div>
+{/if}
 
 <style>
 	.bg-stripes-pink {
