685ce6d6bef911b81e41d1085e64cd80608595b4
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-03-26 13:00:00 +0100

    wip

--- dispatcher/tests/integration/test_messages.py
+++ dispatcher/tests/integration/test_messages.py
@@ -11,48 +11,41 @@ import pytest
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 BACKEND_DIR = BASE_DIR.parent / "backend"
 
-
-@pytest.fixture(scope="session")
-def redpanda():
-    with RedpandaContainer() as redpanda:
-        connection = redpanda.get_bootstrap_server()
-        print(connection)
-        yield redpanda
-
-
-@pytest.fixture(scope="session")
-def dispatcher():
-    with DockerImage(path=BASE_DIR) as image:
-        with DockerContainer(image=str(image)) as container:
-            for i in range(20):
-                sleep(1)
-                logs = container.get_logs()
-                print(logs)
-            delay = wait_for_logs(container, "Starting consumer")
-            print(delay)
-            yield container
-
-
+# @pytest.fixture(scope="session")
+# def redpanda():
+#     with RedpandaContainer() as redpanda:
+#         connection = redpanda.get_bootstrap_server()
+#         print(f"Redpanda running at {connection}")
+#         yield redpanda
+#
+#
 # @pytest.fixture(scope="session")
 # def dispatcher():
-with DockerImage(path=BACKEND_DIR) as image:
-    DJANGO_SUPERUSER_PASSWORD = str(uuid.uuid4())
-    with (
-        DockerContainer(image=str(image))
-        .with_env("ALLOWED_HOSTS", "backend,localhost")
-        .with_env("DJANGO_DEBUG", "True")
-        .with_env("DJANGO_SUPERUSER_EMAIL", "admin@tests.com")
-        .with_env("DJANGO_SUPERUSER_PASSWORD", DJANGO_SUPERUSER_PASSWORD)
-        .with_volume_mapping(
-            str(BACKEND_DIR / "db"),
-            "/code/db",
-            "rw",
-        )
-        .with_exposed_ports(8000) as container
-    ):
-        for i in range(10):
-            sleep(1)
-            logs = container.get_logs()
-            print(logs[0].decode())
-        delay = wait_for_logs(container, "Starting consumer")
-        print(delay)
+#     with DockerImage(path=BASE_DIR) as image:
+#         with DockerContainer(image=str(image)) as container:
+#             wait_for_logs(container, "Starting consumer")
+#             yield container
+
+
+@pytest.fixture(scope="session")
+def api():
+    with DockerImage(path=BACKEND_DIR) as image:
+        DJANGO_SUPERUSER_PASSWORD = str(uuid.uuid4())
+        with (
+            DockerContainer(image=str(image))
+            .with_env("ALLOWED_HOSTS", "backend,localhost")
+            .with_env("DJANGO_DEBUG", "True")
+            .with_env("DJANGO_SUPERUSER_EMAIL", "admin@tests.com")
+            .with_env("DJANGO_SUPERUSER_PASSWORD", DJANGO_SUPERUSER_PASSWORD)
+            .with_volume_mapping(
+                str(BACKEND_DIR / "db"),
+                "/code/db",
+                "rw",
+            )
+            .with_exposed_ports(8000) as container
+        ):
+            for i in range(10):
+                sleep(1)
+                logs = container.get_logs()
+                print(logs[0].decode())
+            yield container
