835d111072cff52878939fb580af76e0b37677ef
Abder <smimite@gmail.com>
2025-03-29 01:57:15 +0100

    feat: GDPR Register Of Processing Activities  (#1721)
    * feat: GDPR Processings register
    
    * migration file
    
    * ruff
    
    * wip
    
    * y
    
    * starting point
    
    * barebone
    
    * wip
    
    * wip
    
    * fix: permissions with migration order
    
    * chore: remove duplicated observation field
    
    * feat: add front base pages
    
    * chore: format
    
    * nav entry
    
    * wip
    
    * fix permissions
    
    * fixup
    
    * wip
    
    * dedicated route for customization
    
    * strings
    
    * wip
    
    * wip
    
    * wip
    
    * fixup
    
    * fixup
    
    * clean up
    
    * wip
    
    * fixup
    
    * fixup
    
    * wip
    
    * wip
    
    * wip
    
    * testable
    
    * fixup
    
    * wip
    
    * fixup
    
    * wip
    
    * wip
    
    * i18n
    
    * wip
    
    * wip
    
    * fix typo
    
    * Add processing nature field
    
    * cleanup
    
    * Update ProcessingForm.svelte
    
    Fix localized string
    
    * Update ProcessingForm.svelte
    
    * fixup
    
    ---------
    
    Co-authored-by: Mohamed-Hacene <mohamedhacene.b@gmail.com>
    Co-authored-by: eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>

---
+++ frontend/src/lib/components/DataViz/WorldMap.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Orientation, Scale, TopoJSONMap, MapProjection } from '@unovis/ts';
+	import { WorldMapTopoJSON } from '@unovis/ts/maps';
+	import { VisSingleContainer, VisTopoJSONMap, VisTooltip } from '@unovis/svelte';
+
+	export let data;
+	const mapData = { areas: data };
+
+	const tooltipTriggers = {
+		[TopoJSONMap.selectors.feature]: (d) =>
+			`${d.properties.name}: ${d.data.count ? d.data.count : 'no data'}`
+	};
+</script>
+
+<div class="">
+	<VisSingleContainer data={mapData} height={600} duration={0}>
+		<VisTopoJSONMap topojson={WorldMapTopoJSON} projection={MapProjection.EqualEarth()} />
+		<VisTooltip triggers={tooltipTriggers} />
+	</VisSingleContainer>
+</div>
