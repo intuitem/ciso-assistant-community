44c4744cc6a7016c1bc3f00bd6389e46048d9593
Mohamed-Hacene <90701924+Mohamed-Hacene@users.noreply.github.com>
2025-03-29 12:56:57 +0100

    Update DetailView.svelte

--- frontend/src/lib/components/DetailView/DetailView.svelte
+++ frontend/src/lib/components/DetailView/DetailView.svelte
@@ -483,7 +483,7 @@
 	</div>
 </div>
 
-{#if relatedModels.length > 0}
+{#if relatedModels.length > 0 && displayModelTable}
 	<div class="card shadow-lg mt-8 bg-white">
 		<TabGroup justify="justify-center">
 			{#each relatedModels as [urlmodel, model], index}
