a474b14512b7ef58fda0ea847d0c363782fd75c1
melinoix <titouan.ameline@gmail.com>
2025-04-16 21:19:41 +0200

    feat: display warning message in strategic scenarios list if no attack path is defined (#1846)
    * erase the used from front form
    
    * erased backend is_used filter
    
    * warning message on no attack path defined
    
    * correct old commit
    
    * correct old commit
    
    * correct backend
    
    * simplify fetching of strategic scenarios with no attack paths
    
    * use warning variant rather than yellow
    
    ---------
    
    Co-authored-by: Nassim Tabchiche <n.tabchiche2@gmail.com>

--- frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/workshop-3/strategic-scenarios/+page.svelte
+++ frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/workshop-3/strategic-scenarios/+page.svelte
@@ -6,6 +6,7 @@
 	import { getModalStore } from '@skeletonlabs/skeleton';
 	import CreateModal from '$lib/components/Modals/CreateModal.svelte';
 	import { page } from '$app/stores';
+	import { m } from '$paraglide/messages';
 
 	const modalStore: ModalStore = getModalStore();
 
@@ -31,6 +32,31 @@
 	}
 </script>
 
+{#if data.scenariosWithoutAttackPath.count > 0}
+	{@const missingScenarios = data.scenariosWithoutAttackPath.results}
+	<section class="my-6">
+		<div
+			class="flex items-start gap-3 rounded-xl border border-warning-300 bg-warning-100 p-4 shadow-sm"
+		>
+			<div class="text-warning-600 mt-1">
+				<i class="fa-solid fa-triangle-exclamation text-xl" />
+			</div>
+			<div>
+				<h2 class="font-semibold text-warning-800 text-md mb-1">
+					{m.reminderWarningStrategicScenarios()}
+				</h2>
+				<p class="text-warning-700 text-sm leading-snug mb-1">
+					{m.addAttackPathToDoOperationalScenarios()}
+				</p>
+				<ul class="list-disc list-inside text-warning-700 text-sm">
+					{#each missingScenarios as scenario}
+						<li>{scenario.name ?? `ID: ${scenario.id}`}</li>
+					{/each}
+				</ul>
+			</div>
+		</div>
+	</section>
+{/if}
 <ModelTable
 	source={data.table}
 	deleteForm={data.deleteForm}
