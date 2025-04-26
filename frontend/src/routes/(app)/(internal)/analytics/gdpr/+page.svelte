bbe90ccdc844af4428652470eb16213b814b5b76
Abderrahmane Smimite <smimite@gmail.com>
2025-04-26 11:52:25 +0200

    fix: condition for geomap rendering

--- frontend/src/routes/(app)/(internal)/analytics/gdpr/+page.svelte
+++ frontend/src/routes/(app)/(internal)/analytics/gdpr/+page.svelte
@@ -7,6 +7,7 @@
 	export let data: PageData;
 </script>
 
+{@debug data}
 <div class="grid grid-cols-12">
 	<div class="grid grid-cols-4 bg-slate-100 p-4 gap-4 col-span-12">
 		<Card
@@ -27,7 +28,7 @@
 		<!-- <Card icon="fa-solid fa-circle-exclamation" text="Incidents" count={2} /> -->
 	</div>
 	<div class="col-span-7">
-		{#if data?.data?.length > 0}
+		{#if data?.data?.countries?.length > 0}
 			<WorldMap data={data.data.countries} />
 		{:else}
 			<div class="h-12 flex items-center justify-center text-slate-700">{m.noDataAvailable()}</div>
