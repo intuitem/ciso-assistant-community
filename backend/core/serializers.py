672f213d5b5ab13f0bd4333b6e4c230147ca5c58
Abder <smimite@gmail.com>
2025-05-03 19:39:33 +0200

    fix: findings creation inherit the folder (#1934)
    * fix: findings creation inherit the folder
    
    * correct deletion issue
    
    * add migration/add protection
    
    add migration
    avoid creating a finding if no assessment given
    
    * fixup
    
    ---------
    
    Co-authored-by: eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1241,7 +1241,15 @@ class FindingsAssessmentReadSerializer(AssessmentReadSerializer):
 class FindingWriteSerializer(BaseModelSerializer):
     class Meta:
         model = Finding
-        exclude = ["created_at", "updated_at"]
+        exclude = ["created_at", "updated_at", "folder"]
+
+    def create(self, validated_data):
+        findings_assessment = validated_data.get("findings_assessment")
+        if not findings_assessment:
+            raise serializers.ValidationError({"findings_assessment": "mandatory"})
+        validated_data["folder"] = findings_assessment.folder
+
+        return super().create(validated_data)
 
 
 class FindingReadSerializer(FindingWriteSerializer):
@@ -1251,6 +1259,9 @@ class FindingReadSerializer(FindingWriteSerializer):
     reference_controls = FieldsRelatedField(many=True)
     applied_controls = FieldsRelatedField(many=True)
     filtering_labels = FieldsRelatedField(many=True)
+    perimeter = FieldsRelatedField(
+        source="findings_assessment.perimeter", fields=["id", "name", "folder"]
+    )
     folder = FieldsRelatedField()
     severity = serializers.CharField(source="get_severity_display")
 
