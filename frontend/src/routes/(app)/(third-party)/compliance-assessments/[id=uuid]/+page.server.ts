f217ce9071980845a933e8040fcbb972bc79f295
Abder <smimite@gmail.com>
2025-05-03 21:36:23 +0200

    feat: sync requirement assessment to applied controls status (#1900)
    * feat: sync audit to applied controls status
    
    * wip
    
    * backend logic
    
    * fixup
    
    * wip
    
    * fixup
    
    * wip
    
    * wip
    
    * fixup
    
    * fixup
    
    * wip
    
    * fixup
    
    * fixup
    
    * dry run draft
    
    * translate requirement assessment results
    
    * run the actual sync on form submit
    
    * serialize requirement string representation
    
    * unwrap values
    
    * more conservative algorithm
    
    * translations
    
    * fix sync to actions displaying loading radial when suggesting controls
    
    * fix regression with suggestions
    
    * color scheme attempt 1
    
    * colors scheme attempt 2
    
    * clean up
    
    * avoid repetition and switch to atomic
    
    ---------
    
    Co-authored-by: Nassim Tabchiche <n.tabchiche2@gmail.com>

--- frontend/src/routes/(app)/(third-party)/compliance-assessments/[id=uuid]/+page.server.ts
+++ frontend/src/routes/(app)/(third-party)/compliance-assessments/[id=uuid]/+page.server.ts
@@ -123,5 +123,43 @@ export const actions: Actions = {
 			);
 		}
 		return { form };
+	},
+	syncToActions: async (event) => {
+		const formData = await event.request.formData();
+
+		if (!formData) {
+			return fail(400, { form: null });
+		}
+
+		const schema = z.object({ id: z.string().uuid() });
+		const form = await superValidate(formData, zod(schema));
+
+		const response = await event.fetch(
+			`${BASE_API_URL}/compliance-assessments/${event.params.id}/syncToActions/?dry_run=false`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		);
+		if (response.ok) {
+			setFlash(
+				{
+					type: 'success',
+					message: m.syncToAppliedControlsSuccess()
+				},
+				event
+			);
+		} else {
+			setFlash(
+				{
+					type: 'error',
+					message: m.syncToAppliedControlsError()
+				},
+				event
+			);
+		}
+		return { form, message: { requirementAssessmentsSync: await response.json() } };
 	}
 };
