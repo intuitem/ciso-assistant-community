e37748d25bf9c0417f1aa3e88dff2482751e898c
Abder <smimite@gmail.com>
2025-05-09 23:49:14 +0200

    feat: business impact analysis (bia) (#1940)
    * feat: BIA management
    * permissions for analyst and domain manager

--- frontend/src/lib/utils/crud.ts
+++ frontend/src/lib/utils/crud.ts
@@ -111,6 +111,8 @@ interface SelectField {
 	field: string;
 	detail?: boolean;
 	valueType?: 'string' | 'number';
+	endpointUrl?: string;
+	formNestedField?: string;
 }
 
 export interface ModelMapEntry {
@@ -644,6 +646,66 @@ export const URL_MODEL_MAP: ModelMap = {
 		localNamePlural: 'qualifications',
 		verboseName: 'Qualification',
 		verboseNamePlural: 'Qualifications'
+	},
+	'business-impact-analysis': {
+		endpointUrl: 'resilience/business-impact-analysis',
+		name: 'businessimpactanalysis',
+		localName: 'businessImpactAnalysis',
+		localNamePlural: 'businessImpactAnalysis',
+		verboseName: 'businessimpactanalysis',
+		verboseNamePlural: 'businessimpactanalysis',
+		foreignKeyFields: [
+			{ field: 'folder', urlModel: 'folders', urlParams: 'content_type=DO' },
+			{ field: 'perimeter', urlModel: 'perimeters' },
+			{ field: 'authors', urlModel: 'users' },
+			{ field: 'reviewers', urlModel: 'users', urlParams: 'is_third_party=false' },
+			{ field: 'risk_matrix', urlModel: 'risk-matrices' }
+		],
+		reverseForeignKeyFields: [{ field: 'bia', urlModel: 'asset-assessments' }],
+		selectFields: [{ field: 'status' }],
+		filters: [{ field: 'perimeter' }, { field: 'auditor' }, { field: 'status' }]
+	},
+	'asset-assessments': {
+		endpointUrl: 'resilience/asset-assessments',
+		name: 'assetassessment',
+		localName: 'assetAssessment',
+		localNamePlural: 'assetAssessments',
+		verboseName: 'assetassessment',
+		verboseNamePlural: 'assetassessments',
+		reverseForeignKeyFields: [{ field: 'asset_assessment', urlModel: 'escalation-thresholds' }],
+		foreignKeyFields: [
+			{ field: 'asset', urlModel: 'assets' },
+			{
+				field: 'bia',
+				urlModel: 'business-impact-analysis',
+				endpointUrl: 'business-impact-analysis'
+			}
+		]
+	},
+	'escalation-thresholds': {
+		endpointUrl: 'resilience/escalation-thresholds',
+		name: 'escalationthreshold',
+		localName: 'escalationThreshold',
+		localNamePlural: 'escalationThresholds',
+		verboseName: 'escalationthreshold',
+		verboseNamePlural: 'escalationthresholds',
+		selectFields: [
+			{ field: 'quant_unit' },
+			{
+				field: 'quali_impact',
+				valueType: 'number',
+				detail: true,
+				endpointUrl: 'resilience/asset-assessments',
+				formNestedField: 'asset_assessment'
+			} //this is for edit only
+		],
+		foreignKeyFields: [
+			{
+				field: 'asset_assessment',
+				urlModel: 'asset-assessments',
+				endpointUrl: 'asset-assessments'
+			}
+		]
 	},
 	processings: {
 		endpointUrl: 'privacy/processings',
