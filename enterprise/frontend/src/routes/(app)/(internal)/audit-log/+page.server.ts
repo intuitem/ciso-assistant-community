7f913aaac976a3b3ce8056f10092760a7f2d3797
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-05-12 19:48:43 +0200

    translate audit log list page title

--- enterprise/frontend/src/routes/(app)/(internal)/audit-log/+page.server.ts
+++ enterprise/frontend/src/routes/(app)/(internal)/audit-log/+page.server.ts
@@ -2,5 +2,5 @@ import { m } from '$paraglide/messages';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-	return { title: 'm.auditLog()' };
+	return { title: m.auditLog() };
 }) satisfies PageServerLoad;
