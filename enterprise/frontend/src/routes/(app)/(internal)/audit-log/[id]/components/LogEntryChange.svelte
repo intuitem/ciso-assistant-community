aef93ce8a10f8396e7b67a1d626710f794607a52
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-05-13 17:05:20 +0200

    simplify logentry component

--- enterprise/frontend/src/routes/(app)/(internal)/audit-log/[id]/components/LogEntryChange.svelte
+++ enterprise/frontend/src/routes/(app)/(internal)/audit-log/[id]/components/LogEntryChange.svelte
@@ -8,36 +8,22 @@
   export let after: ProcessedValue;
 </script>
 
-<div class="px-4 grid grid-cols-6 gap-4 even:bg-gray-50">
+<div class="px-4 grid grid-cols-6">
   <dt
     class="font-medium text-gray-900"
     data-testid="{field.replace('_', '-')}-field-title"
   >
     {safeTranslate(field)}
   </dt>
-  <dd class="text-gray-700 col-span-2">
-    <ul>
-      <li
-        class="list-none whitespace-pre-line"
-        data-testid={!(before instanceof Array)
-          ? field.replace('_', '-') + '-before-value'
-          : null}
-      >
-      {before}
-      </li>
-    </ul>
+  <dd class="text-gray-700 col-span-2 whitespace-pre-line">
+      <span>
+        {before}
+      </span>
   </dd>
   <i class="fa-solid fa-arrow-right my-auto" />
-  <dd class="text-gray-700 col-span-2">
-    <ul>
-      <li
-        class="list-none whitespace-pre-line"
-        data-testid={!(before instanceof Array)
-          ? field.replace('_', '-') + '-after-value'
-          : null}
-      >
-      {after}
-      </li>
-    </ul>
+  <dd class="text-gray-700 col-span-2 whitespace-pre-line">
+      <span>
+        {after}
+      </span>
   </dd>
 </div>
