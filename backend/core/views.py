f04b88572f193e54de1059e5d2003ae25615ab2b
Abder <smimite@gmail.com>
2025-05-21 12:34:55 +0200

    feat: add reporting timestamp and detection channel to incidents (#1999)
    * feat: add reporting timestamp and detection channel to incidents
    
    * datetime formatting
    
    * fixup
    
    * fixup

--- backend/core/views.py
+++ backend/core/views.py
@@ -5241,7 +5241,7 @@ class FindingViewSet(BaseModelViewSet):
 class IncidentViewSet(BaseModelViewSet):
     model = Incident
     search_fields = ["name", "description", "ref_id"]
-    filterset_fields = ["folder", "status", "severity", "qualifications"]
+    filterset_fields = ["folder", "status", "severity", "qualifications", "detection"]
 
     @method_decorator(cache_page(60 * LONG_CACHE_TTL))
     @action(detail=False, name="Get status choices")
@@ -5252,6 +5252,11 @@ class IncidentViewSet(BaseModelViewSet):
     @action(detail=False, name="Get severity choices")
     def severity(self, request):
         return Response(dict(Incident.Severity.choices))
+
+    @method_decorator(cache_page(60 * LONG_CACHE_TTL))
+    @action(detail=False, name="Get detection channel choices")
+    def detection(self, request):
+        return Response(dict(Incident.Detection.choices))
 
     def perform_update(self, serializer):
         previous_instance = self.get_object()
