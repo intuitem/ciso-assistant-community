0e6ccfecc17a190f8084fa36de19b6a7d4e7ee85
Axxiar <galoualexis@gmail.com>
2025-05-21 18:07:32 +0200

    create dynamic zod object to validate forms

--- frontend/src/lib/components/ModelTable/ModelTable.svelte
+++ frontend/src/lib/components/ModelTable/ModelTable.svelte
@@ -149,24 +149,6 @@
 		(Object.hasOwn(row?.meta, 'reference_count') && row?.meta?.reference_count > 0) ||
 		['severity_changed', 'status_changed'].includes(row?.meta?.entry_type);
 
-	const filterInitialData: Record<string, string[]> = {};
-	// convert URL search params to filter initial data
-	for (const [key, value] of $page.url.searchParams) {
-		filterInitialData[key] ??= [];
-		filterInitialData[key].push(value);
-	}
-
-	const _form = superForm(defaults(filterInitialData, zod(z.object({}))), {
-		SPA: true,
-		validators: zod(z.object({})),
-		dataType: 'json',
-		invalidateAll: false,
-		applyAction: false,
-		resetForm: false,
-		taintedMessage: false,
-		validationMethod: 'auto'
-	});
-
 	const popupFilter: PopupSettings = {
 		event: 'click',
 		target: 'popupFilter',
@@ -188,6 +170,28 @@
 	// Initialize filter values from URL search params
 	for (const field of filteredFields)
 		filterValues[field] = $page.url.searchParams.getAll(field).map((value) => ({ value }));
+
+	const filterInitialData: Record<string, string[]> = {};
+	// convert URL search params to filter initial data
+	for (const [key, value] of $page.url.searchParams) {
+		filterInitialData[key] ??= [];
+		filterInitialData[key].push(value);
+	}
+
+	const zodFiltersObject = {};
+	Object.keys(filters).forEach((k) => {
+		zodFiltersObject[k] = z.array(z.string());
+	});
+	const _form = superForm(defaults(filterInitialData, zod(z.object(zodFiltersObject))), {
+		SPA: true,
+		validators: zod(z.object(zodFiltersObject)),
+		dataType: 'json',
+		invalidateAll: false,
+		applyAction: false,
+		resetForm: false,
+		taintedMessage: false,
+		validationMethod: 'auto'
+	});
 
 	$: hideFilters = hideFilters || !Object.entries(filters).some(([_, filter]) => !filter.hide);
 
