6916daf3c3b362a6101178c32dfebc7d01442e3b
Nassim <71079376+nas-tabchiche@users.noreply.github.com>
2025-05-22 23:39:03 +0200

    feat(pro): expose audit logs UI on enterprise (#2022)
    * starting point
    
    * finish logentry list view
    
    * some l10n
    
    * detail starting point
    
    * fix logentry list
    
    * make logentries visible by admins only
    
    * log entry detail PoC
    
    * componentize log entry
    
    * translate audit log list page title
    
    * display logentry detail before actual diff
    
    * small ui changes
    
    * some l10n
    
    * simplify logentry component
    
    * properly reflect view_logentry perm
    
    * remove redundant i18n entry
    
    * handle logentry fetch errors
    
    * pad before and after values
    
    * move auditlog app up
    
    ---------
    
    Co-authored-by: Abder <smimite@gmail.com>

---
+++ enterprise/frontend/src/routes/(app)/(internal)/audit-log/[id]/+page.server.ts
@@ -0,0 +1,29 @@
+import { BASE_API_URL } from '$lib/utils/constants';
+import { m } from '$paraglide/messages';
+import { safeTranslate } from '$lib/utils/i18n'
+import type { PageServerLoad } from './$types';
+
+
+export const load = (async ({ fetch, params }) => {
+	const endpoint = `${BASE_API_URL}/log-entries/${params.id}/`;
+	try {
+		const res = await fetch(endpoint);
+		if (!res.ok) {
+			throw new Error(`Failed to fetch log entry: ${res.status}`);
+		}
+		const log = await res.json();
+		
+		return { 
+			log, 
+			title: m.logEntryRepr({
+				actor: log.actor.str, 
+				action: safeTranslate(log.action), 
+				timestamp: log.timestamp, 
+				object: log.object_repr
+			}) 
+		};
+	} catch (error) {
+		console.error('Error loading audit log entry:', error);
+		throw error;
+	}
+}) satisfies PageServerLoad;
