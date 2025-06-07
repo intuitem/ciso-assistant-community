f6bd40595f8a36d28cd3f31d7d8d1f4555ed7492
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-06-07 23:43:18 +0200

    update accordion item locator

--- frontend/src/lib/components/SideBar/SideBarCategory.svelte
+++ frontend/src/lib/components/SideBar/SideBarCategory.svelte
@@ -7,6 +7,9 @@
 	let { item }: Props = $props();
 </script>
 
-<span class="whitespace-nowrap text-primary-800 font-semibold uppercase tracking-tighter text-xs">
+<span
+	class="whitespace-nowrap text-primary-800 font-semibold uppercase tracking-tighter text-xs"
+	data-testid={'accordion-item-' + item.name.toLowerCase().replace(' ', '-')}
+>
 	{safeTranslate(item.name)}
 </span>
