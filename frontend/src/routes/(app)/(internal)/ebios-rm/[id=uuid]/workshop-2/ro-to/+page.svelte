c1cc48949ebc4869bc4a92f1640648ee8f61f8d9
melinoix <titouan.ameline@gmail.com>
2025-06-20 19:34:52 +0200

    fix: add button border unsatisfying correction (#2127)
    add button border unsatisfying

--- frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/workshop-2/ro-to/+page.svelte
+++ frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/workshop-2/ro-to/+page.svelte
@@ -73,7 +73,7 @@
 		<div>
 			<span class="inline-flex overflow-hidden rounded-md border bg-white shadow-xs">
 				<button
-					class="inline-block border-e p-3 btn-mini-primary w-12 focus:relative"
+					class="inline-block p-3 btn-mini-primary w-12 focus:relative"
 					data-testid="add-button"
 					title={safeTranslate('add-' + data.model.localName)}
 					onclick={modalCreateForm}
