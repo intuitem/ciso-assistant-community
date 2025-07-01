03137b6c29dee6bff956a96458629d3e734a80e3
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-07-01 18:43:45 +0200

    update language selector functional tests

--- frontend/tests/functional/nav.test.ts
+++ frontend/tests/functional/nav.test.ts
@@ -64,20 +64,26 @@ test('sidebar navigation tests', async ({ logedPage, analyticsPage, sideBar, pag
 
 	await test.step('translation panel is working properly', async () => {
 		await analyticsPage.goto();
-		const locales_ = [...locales];
-		const index = locales_.indexOf('en');
+		const allLocales = [...locales];
+		const index = allLocales.indexOf('en');
 		if (index !== -1) {
-			locales_.splice(index, 1);
-			locales_.push('en');
+			allLocales.splice(index, 1);
+			allLocales.push('en');
 		}
-		for (const getLocale of locales_) {
+		for (const locale of allLocales) {
 			await sideBar.moreButton.click();
 			await expect(sideBar.morePanel).not.toHaveAttribute('inert');
 			await expect(sideBar.languageSelect).toBeVisible();
-			setLocale(getLocale);
-			await sideBar.languageSelect.selectOption(getLocale);
-			await logedPage.hasTitle(m.analytics());
+			setLocale(locale);
+			await sideBar.languageSelect.selectOption(locale);
+			await logedPage.hasTitle(m.analytics({}, { locale }));
 		}
+		await sideBar.moreButton.click();
+		await expect(sideBar.morePanel).not.toHaveAttribute('inert');
+		await expect(sideBar.languageSelect).toBeVisible();
+		setLocale('en');
+		await sideBar.languageSelect.selectOption('en');
+		await logedPage.hasTitle(m.analytics({}, { locale: 'en' }));
 	});
 
 	await test.step('about panel is working properly', async () => {
