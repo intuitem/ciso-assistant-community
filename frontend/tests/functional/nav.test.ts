dfb3fd48f8aff570995081b01a2215106c4a4888
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-07-01 21:50:36 +0200

    split nav tests

--- frontend/tests/functional/nav.test.ts
+++ frontend/tests/functional/nav.test.ts
@@ -38,7 +38,9 @@ test('sidebar navigation tests', async ({ logedPage, analyticsPage, sideBar, pag
 			}
 		}
 	});
+});
 
+test('more panel components work properly', async ({ logedPage, sideBar, page }) => {
 	await test.step('user email is showing properly', async () => {
 		await expect(sideBar.userEmailDisplay).toHaveText(logedPage.email);
 		//TOD0 test also that user name and first name are displayed instead of the email when sets
@@ -61,7 +63,9 @@ test('sidebar navigation tests', async ({ logedPage, analyticsPage, sideBar, pag
 
 		await expect(sideBar.docsButton).toBeVisible();
 	});
+});
 
+test('switching locale works properly', async ({ logedPage, analyticsPage, sideBar, page }) => {
 	await test.step('translation panel is working properly', async () => {
 		await analyticsPage.goto();
 		const allLocales = [...locales];
@@ -85,7 +89,9 @@ test('sidebar navigation tests', async ({ logedPage, analyticsPage, sideBar, pag
 		await sideBar.languageSelect.selectOption('en');
 		await logedPage.hasTitle(m.analytics({}, { locale: 'en' }));
 	});
+});
 
+test('about panel works properly', async ({ logedPage, sideBar, page }) => {
 	await test.step('about panel is working properly', async () => {
 		await sideBar.moreButton.click();
 		await expect(sideBar.morePanel).not.toHaveAttribute('inert');
