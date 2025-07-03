75fd67bdc6b771282339194126054bb56dcac506
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2025-07-03 21:47:21 +0200

    Update +page.svelte
    make chevrons consistent with sidebar convention

--- frontend/src/routes/(app)/(third-party)/compliance-assessments/[id=uuid]/table-mode/+page.svelte
+++ frontend/src/routes/(app)/(third-party)/compliance-assessments/[id=uuid]/table-mode/+page.svelte
@@ -352,9 +352,9 @@
 								</div>
 								<button onclick={() => toggleSuggestion(requirementAssessment.id)}>
 									{#if !hideSuggestionHashmap[requirementAssessment.id]}
-										<i class="fa-solid fa-chevron-up"></i>
-									{:else}
 										<i class="fa-solid fa-chevron-down"></i>
+									{:else}
+										<i class="fa-solid fa-chevron-right"></i>
 									{/if}
 								</button>
 							</h2>
