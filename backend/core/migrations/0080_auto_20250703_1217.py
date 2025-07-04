842e21bea74476a5500bb894ce7dc54169aee787
Mohamed-Hacene <90701924+Mohamed-Hacene@users.noreply.github.com>
2025-07-04 10:46:24 +0200

    Update 0080_auto_20250703_1217.py

--- backend/core/migrations/0080_auto_20250703_1217.py
+++ backend/core/migrations/0080_auto_20250703_1217.py
@@ -10,10 +10,6 @@ class Migration(migrations.Migration):
         Qualification = apps.get_model("core", "Qualification")
         Qualification.objects.update(is_published=True)
 
-    def reverse_update_is_published(apps, schema_editor):
-        Qualification = apps.get_model("core", "Qualification")
-        Qualification.objects.update(is_published=False)
-
     operations = [
-        migrations.RunPython(update_is_published, reverse_update_is_published),
+        migrations.RunPython(update_is_published),
     ]
