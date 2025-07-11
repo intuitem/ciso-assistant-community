7ad62295159fa13f781be6667b54e7bc81478c99
Abderrahmane Smimite <smimite@gmail.com>
2025-07-11 13:53:39 +0200

    fixup

--- frontend/src/lib/utils/table.ts
+++ frontend/src/lib/utils/table.ts
@@ -1009,7 +1009,7 @@ export const listViewFields = {
 		}
 	},
 	'elementary-actions': {
-		head: ['ref_id', 'domain', 'icon', 'name', 'attack_stage', 'threat'],
+		head: ['ref_id', 'domain', '', 'name', 'attack_stage', 'threat'],
 		body: ['ref_id', 'domain', 'icon_fa_class', 'name', 'attack_stage', 'threat']
 	},
 	'operating-modes': {
