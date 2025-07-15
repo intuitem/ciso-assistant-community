308595ddf26ef9e88f6d2a94d203bbc37cc5cf48
Monsieur S <143810744+monsieurswag@users.noreply.github.com>
2025-07-15 12:30:27 +0200

    fix: Detail view not updated on ModelTable row click (#2283)
    * fix: Detail view not updated on ModelTable row click
    
    * Formatter

--- frontend/src/lib/components/DetailView/DetailView.svelte
+++ frontend/src/lib/components/DetailView/DetailView.svelte
@@ -75,15 +75,16 @@
 		return model.reverseForeignKeyFields.findIndex((o) => o.urlModel === relatedModel.urlModel);
 	};
 
-	let filteredData = $state(data.data);
-
-	if (data.model?.detailViewFields) {
-		filteredData = Object.fromEntries(
-			Object.entries(data.data).filter(
-				([key, _]) => data.model.detailViewFields.filter((field) => field.field === key).length > 0
-			)
-		);
-	}
+	let filteredData = $derived(
+		data.model?.detailViewFields
+			? Object.fromEntries(
+					Object.entries(data.data).filter(
+						([key, _]) =>
+							data.model.detailViewFields.filter((field) => field.field === key).length > 0
+					)
+				)
+			: data.data
+	);
 
 	function handleKeydown(event: KeyboardEvent) {
 		if (event.metaKey || event.ctrlKey) return;
