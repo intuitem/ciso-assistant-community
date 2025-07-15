4a1547879e63a8b63fb1af5f2ad561776502f2d1
monsieurswag <mael.joumier@intuitem.com>
2025-07-15 11:55:23 +0200

    Same fix for audits

--- frontend/src/routes/(app)/(internal)/compliance-assessments/[id=uuid]/action-plan/+page.svelte
+++ frontend/src/routes/(app)/(internal)/compliance-assessments/[id=uuid]/action-plan/+page.svelte
@@ -15,7 +15,7 @@
 		expiry_date: 'expiryDate',
 		effort: 'effort',
 		cost: 'cost',
-		'requirements-assessments': 'matchingRequirements'
+		'requirements_assessments': 'matchingRequirements'
 	};
 
 	const appliedControls: TableSource = {
