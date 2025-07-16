c56098d21524ce457eaacb48c47c1f391717c9d2
Poulereaper <104522240+Poulereaper@users.noreply.github.com>
2025-07-16 18:03:39 +0200

    test: doc and username edit (#2293)
    * feat/doc for E2E tests
    
    * fix/function to avoid welcome pop up after login
    
    * fix/add username test
    
    * fix: linter

--- frontend/tests/utils/test-utils.ts
+++ frontend/tests/utils/test-utils.ts
@@ -279,6 +279,7 @@ export const test = base.extend<Fixtures>({
 		const loginPage = new LoginPage(page);
 		await loginPage.goto();
 		await loginPage.login();
+		await loginPage.skipWelcome();
 		await use(loginPage);
 	},
 
