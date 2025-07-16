a58f56e165c38b9335861149786952099dbf31e4
Abder <smimite@gmail.com>
2025-07-16 22:07:03 +0200

    fix: multiple GDPR processings improvements (#2297)
    * fix: GDPR module multiple improvements
    
    * fixup to avoid Namibia appearing as N/A
    
    * wip
    
    * multiple fixes
    
    * formatting
    
    * fix typo

--- frontend/src/lib/utils/crud.ts
+++ frontend/src/lib/utils/crud.ts
@@ -806,7 +806,27 @@ export const URL_MODEL_MAP: ModelMap = {
 			{ field: 'processing', urlModel: 'purposes' },
 			{ field: 'processing', urlModel: 'data-recipients' },
 			{ field: 'processing', urlModel: 'data-contractors' },
-			{ field: 'processing', urlModel: 'data-transfers' }
+			{ field: 'processing', urlModel: 'data-transfers' },
+			{
+				field: 'processings',
+				urlModel: 'applied-controls',
+				disableCreate: true,
+				disableDelete: true
+			}
+		],
+		detailViewFields: [
+			{ field: 'id' },
+			{ field: 'name' },
+			{ field: 'description' },
+			{ field: 'status' },
+			{ field: 'legal_basis' },
+			{ field: 'dpia_required' },
+			{ field: 'dpia_reference' },
+			{ field: 'folder' },
+			{ field: 'nature' },
+			{ field: 'created_at' },
+			{ field: 'updated_at' },
+			{ field: 'filtering_labels' }
 		]
 	},
 	'processing-natures': {
