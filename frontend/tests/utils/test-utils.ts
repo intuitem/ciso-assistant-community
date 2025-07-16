915e099df5f5dfcf6f7187c36fb4bbaf47c445cf
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2025-07-16 17:04:21 +0200

    feat: update tests

--- frontend/tests/utils/test-utils.ts
+++ frontend/tests/utils/test-utils.ts
@@ -570,7 +570,7 @@ export class TestContent {
 					assets: [vars.folderName + '/' + vars.assetName + ' Support'],
 					current_proba: 'High',
 					current_impact: 'Medium',
-					applied_controls: [vars.folderName + '/' + vars.appliedControlName],
+					applied_controls: [vars.folderName + '/' + vars.appliedControlName + ' ' + vars.referenceControlName],
 					residual_proba: 'Medium',
 					residual_impact: 'Low',
 					justification: 'Test comments'
