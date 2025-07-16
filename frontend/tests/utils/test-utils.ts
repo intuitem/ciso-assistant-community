ba7e3a475dcb76a7a15c9b4e3b39a4b97e7df891
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2025-07-16 17:01:01 +0200

    chore: fix test typo

--- frontend/tests/utils/test-utils.ts
+++ frontend/tests/utils/test-utils.ts
@@ -123,7 +123,7 @@ export const test = base.extend<Fixtures>({
 	},
 
 	evidencesPage: async ({ page }, use) => {
-		const ePage = new PageContent(page, '/evidences', 'Evidences', [
+		const ePage = new PageContent(page, '/evidences', 'Evidence', [
 			{ name: 'name', type: type.TEXT },
 			{ name: 'description', type: type.TEXT },
 			{ name: 'attachment', type: type.FILE },
@@ -514,7 +514,7 @@ export class TestContent {
 				}
 			},
 			evidencesPage: {
-				displayName: 'Evidences',
+				displayName: 'Evidence',
 				dependency: vars.framework,
 				build: {
 					name: vars.evidenceName,
