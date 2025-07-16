4fc4b43a1f14ebced45e2117907cb183338e0307
Abder <smimite@gmail.com>
2025-07-16 22:07:03 +0200

    fix: multiple GDPR processings improvements (#2297)
    * fix: GDPR module multiple improvements
    
    * fixup to avoid Namibia appearing as N/A
    
    * wip
    
    * multiple fixes
    
    * formatting
    
    * fix typo

--- frontend/src/routes/(app)/(internal)/processings/[id=uuid]/+page.svelte
+++ frontend/src/routes/(app)/(internal)/processings/[id=uuid]/+page.svelte
@@ -11,14 +11,4 @@
 	let { data }: Props = $props();
 </script>
 
-<DetailView {data}>
-	<!-- <div slot="actions" class="flex flex-col space-y-2"> -->
-	<!-- 	<Anchor -->
-	<!-- 		href={`${page.url.pathname}/summary`} -->
-	<!-- 		class="btn variant-filled-primary h-fit" -->
-	<!-- 		breadcrumbAction="push"><i class="fa-solid fa-heart-pulse mr-2" />{m.summary()}</Anchor -->
-	<!-- 	> -->
-	<!-- </div> -->
-	<!---->
-	<!-- <div slot="widgets" class="h-full flex flex-col space-y-4 bg-slate-100"></div> -->
-</DetailView>
+<DetailView {data}></DetailView>
