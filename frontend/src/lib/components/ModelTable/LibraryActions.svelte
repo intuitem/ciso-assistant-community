1b0426a708004e86f72cb4b4fae1f639339de017
Axxiar <galoualexis@gmail.com>
2025-07-16 15:59:43 +0200

    hide arrow after update

--- frontend/src/lib/components/ModelTable/LibraryActions.svelte
+++ frontend/src/lib/components/ModelTable/LibraryActions.svelte
@@ -17,21 +17,21 @@
 	let library = $derived(meta);
 	let loading = $state({ form: false, library: '' });
 
-	async function handleSubmit(event: { currentTarget: EventTarget & HTMLFormElement }) {
-		const data = new FormData(event.currentTarget);
-
-		const response = await fetch(event.currentTarget.action, {
-			method: 'POST',
-			body: data
-		});
-
-		const result: ActionResult = deserialize(await response.text());
-
-		if (result.type === 'success') {
-			await invalidateAll();
-		}
-		applyAction(result);
-	}
+	// async function handleSubmit(event: { currentTarget: EventTarget & HTMLFormElement }) {
+	// 	const data = new FormData(event.currentTarget);
+	//
+	// 	const response = await fetch(event.currentTarget.action, {
+	// 		method: 'POST',
+	// 		body: data
+	// 	});
+	//
+	// 	const result: ActionResult = deserialize(await response.text());
+	//
+	// 	if (result.type === 'success') {
+	// 		await invalidateAll();
+	// 	}
+	// 	applyAction(result);
+	// }
 </script>
 
 {#if actionsURLModel === 'stored-libraries' && Object.hasOwn(library, 'is_loaded') && !library.is_loaded}
@@ -117,10 +117,12 @@
 					return async ({ update }) => {
 						loading.form = false;
 						loading.library = '';
-						update();
+						await update();
+						Object.values($tableHandlers).forEach((handler) => {
+							handler.invalidate();
+						});
 					};
 				}}
-				onsubmit={handleSubmit}
 			>
 				<button title={m.updateThisLibrary()} onclick={(e) => e.stopPropagation()}>
 					<i class="fa-solid fa-circle-up"></i>
