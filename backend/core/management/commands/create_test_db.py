66106768b32fbd83194eeb248c5fda962d0572f9
monsieurswag <mael.joumier@intuitem.com>
2025-07-24 19:17:21 +0200

    Creates db/tests directory and makes the code slighly better

--- backend/core/management/commands/create_test_db.py
+++ backend/core/management/commands/create_test_db.py
@@ -17,6 +17,8 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 TEST_ADMIN_EMAIL = "admin@tests.com"
 TEST_ADMIN_PASSWORD = "1234"
 INITIAL_BACKUP_FNAME = "initial-db-backup.sqlite3"
+DB_DIRECTORY = settings.DATABASES["default"]["NAME"].parent
+TEST_DB_DIRECTORY = DB_DIRECTORY / "tests"
 BLANK_DB_NAME = "blank-test-database.sqlite3"
 
 # mapping(test_name => list of objects/lazy-loaded objects to create)
@@ -25,13 +27,13 @@ TEST_DATA: dict[str, list[models.Model | Callable[[], models.Model]]] = {
         # Lambda functions serve as lazy-loaded objects
         lambda: Folder(
             name="Compliance-Assessments Test Domain", parent_folder=_get_root_folder()
-        ),
+        )
     ]
 }
 
 
 class Command(BaseCommand):
-    help = "Create functional test databases"
+    help = "Create functional test databases (if this command corrupted the main database use the backup stored at initial-db-backup.sqlite3)"
 
     def add_arguments(self, parser) -> None:
         parser.add_argument(
@@ -45,8 +47,10 @@ class Command(BaseCommand):
         keep_blank_db = options["keep_blank_db"]
 
         sqlite_file_path = settings.DATABASES["default"]["NAME"]
-        backup_file_path = sqlite_file_path.parent / INITIAL_BACKUP_FNAME
-        blank_test_db_file_path = sqlite_file_path.parent / "tests" / BLANK_DB_NAME
+        backup_file_path = DB_DIRECTORY / INITIAL_BACKUP_FNAME
+        if not TEST_DB_DIRECTORY.exists():
+            TEST_DB_DIRECTORY.mkdir()
+        blank_test_db_file_path = TEST_DB_DIRECTORY / BLANK_DB_NAME
 
         shutil.copy(sqlite_file_path, backup_file_path)
         if keep_blank_db and blank_test_db_file_path.exists():
