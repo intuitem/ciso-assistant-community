621f596f187e7129239f438ad8e4947e8d758949
Abder <smimite@gmail.com>
2025-07-31 19:27:16 +0200

    fix: add user observation during creation (#2367)
    * fix: add user observation during creation
    
    * fixup for extra fields on user

--- backend/iam/models.py
+++ backend/iam/models.py
@@ -335,6 +335,7 @@ class UserManager(BaseUserManager):
             last_name=extra_fields.get("last_name", ""),
             is_superuser=extra_fields.get("is_superuser", False),
             is_active=extra_fields.get("is_active", True),
+            observation=extra_fields.get("observation"),
             folder=_get_root_folder(),
             keep_local_login=extra_fields.get("keep_local_login", False),
         )
