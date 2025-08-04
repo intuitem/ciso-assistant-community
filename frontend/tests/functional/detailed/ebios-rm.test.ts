5800e433917043d3b5e7cb6a4794faacc6998278
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-08-04 19:32:57 +0200

    longer timeout for ebios rm tests

--- frontend/tests/functional/detailed/ebios-rm.test.ts
+++ frontend/tests/functional/detailed/ebios-rm.test.ts
@@ -49,7 +49,7 @@ test('ebios rm study', async ({
 	appliedControlsPage,
 	page
 }) => {
-	test.slow();
+	test.setTimeout(600_000);
 	await test.step('create required folder', async () => {
 		await foldersPage.goto();
 		await foldersPage.hasUrl();
