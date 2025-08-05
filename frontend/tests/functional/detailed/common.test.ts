6f7bd3c40409ac50a3ddd580d1476d457dd7b386
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-08-05 10:28:43 +0200

    Revert "set 10m timeout for common test"
    This reverts commit 23f1995ccf337b43c9decf94703171c49e9f823c.

--- frontend/tests/functional/detailed/common.test.ts
+++ frontend/tests/functional/detailed/common.test.ts
@@ -88,7 +88,7 @@ for (const key of testPages) {
 				pages,
 				page
 			}, testInfo) => {
-				test.setTimeout(600_000);
+				test.slow();
 				const editedValues = await pages[key].itemDetail.editItem(
 					items[key].build,
 					items[key].editParams
