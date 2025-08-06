b3c0e8d820280f25319ade190eab4332fdab114e
Abder <smimite@gmail.com>
2025-08-06 10:19:37 +0200

    feat: more shortcuts on applied controls context menu and experimental priority review matrix (#2370)
    * starting point
    
    * change impact PoC
    
    * add effort
    
    * add separator below change status context menu action
    
    * add priority
    
    * fixup
    
    * fixup
    
    * starter for impact-effort matrix
    
    * fixup with asymmetric layout
    
    * fixup
    
    * fixup and attempt with modelTable
    
    * invalidate applied controls table on click
    
    * plug to api
    
    * cleanup
    
    * fix logic
    
    * fixup
    
    * fixup
    
    * formatting
    
    * typing
    
    ---------
    
    Co-authored-by: Nassim Tabchiche <n.tabchiche2@gmail.com>

--- frontend/src/lib/components/ContextMenu/applied-controls/ChangeImpact.svelte
+++ frontend/src/lib/components/ContextMenu/applied-controls/ChangeImpact.svelte
@@ -19,17 +19,17 @@
 	let options: { label: string; value: string }[] = $state([]);
 
 	onMount(async () => {
-		options = await fetch('/applied-controls/status').then((r) => r.json());
+		options = await fetch('/applied-controls/control_impact').then((r) => r.json());
 	});
 
-	async function changeStatus(newStatus: string) {
-		const endpoint = `/applied-controls/${row?.meta?.id}/status`;
+	async function changeImpact(newImpact: string) {
+		const endpoint = `/applied-controls/${row?.meta?.id}/control_impact`;
 		const requestInit = {
 			method: 'PATCH',
 			headers: {
 				'Content-Type': 'application/json'
 			},
-			body: JSON.stringify({ status: newStatus })
+			body: JSON.stringify({ control_impact: newImpact })
 		};
 		try {
 			const response = await fetch(endpoint, requestInit);
@@ -47,7 +47,7 @@
 				type: 'error',
 				message: m.errorUpdatingObject({ object: m.appliedControl().toLowerCase() })
 			});
-			console.error('Error changing status:', error);
+			console.error('Error changing impact:', error);
 		}
 	}
 </script>
@@ -56,7 +56,7 @@
 	<ContextMenu.SubTrigger
 		class="flex h-10 select-none items-center rounded-button py-3 pl-3 pr-1.5 text-sm font-medium outline-hidden ring-0! ring-transparent! data-highlighted:bg-muted data-[state=open]:bg-surface-50"
 	>
-		<div class="flex items-center">{m.changeStatus()}</div>
+		<div class="flex items-center">{m.changeImpact()}</div>
 	</ContextMenu.SubTrigger>
 	<ContextMenu.SubContent
 		class="z-50 w-full max-w-[209px] outline-hidden card bg-white px-1 py-1.5 shadow-md cursor-default data-highlighted:bg-surface-50"
@@ -65,12 +65,10 @@
 		{#each options as option}
 			<ContextMenu.Item
 				class="flex h-10 select-none items-center rounded-xs py-3 pl-3 pr-1.5 text-sm font-medium outline-hidden ring-0! ring-transparent! hover:bg-surface-50"
-				on:click={async () => await changeStatus(option.value)}
+				on:click={async () => await changeImpact(option.value)}
 			>
 				{safeTranslate(option.label)}
 			</ContextMenu.Item>
 		{/each}
 	</ContextMenu.SubContent>
 </ContextMenu.Sub>
-
-<ContextMenu.Separator class="-mx-1 my-1 block h-px bg-surface-100" />
