2a902ffe14bd7a330e6733c3dcd625271de8a772
Abderrahmane Smimite <smimite@gmail.com>
2025-08-07 10:01:15 +0200

    formatter

--- frontend/src/routes/(app)/(internal)/x-rays/+page.svelte
+++ frontend/src/routes/(app)/(internal)/x-rays/+page.svelte
@@ -60,255 +60,255 @@
 			<span class="text-2xl">{m.xRaysEmptyMessage()}</span>
 		{/if}
 		{#each perimeters as perimeter, index}
-		{@const compliance_assessments = Object.values(perimeter.compliance_assessments.objects)}
-		{@const risk_assessments = Object.values(perimeter.risk_assessments.objects)}
-		<div>
-			<span class="text-2xl">💡</span>
-			<Anchor
-				class="text-2xl font-bold mb-1 hover:underline text-blue-600"
-				href="/perimeters/{perimeter.perimeter.id}"
-			>
-				{perimeter.perimeter.folder.str}/{perimeter.perimeter.name}
-			</Anchor>
-			<Tabs
-				value={tabStates[perimeter.id] || 'compliance_assessments'}
-				onValueChange={(e) => {
-					if (!tabStates[perimeter.id]) {
-						tabStates[perimeter.id] = 'compliance_assessments';
-					}
-					tabStates[perimeter.id] = e.value;
-				}}
-				listJustify="justify-center"
-			>
-				{#snippet list()}
-					<Tabs.Control value="compliance_assessments" labelBase="inert px-2"
-						>{m.complianceAssessments()}
-						{#if perimeter.compliance_assessments.errors.length > 0}
-							<span class="badge preset-tonal-error"
-								>{perimeter.compliance_assessments.errors.length}</span
-							>
-						{/if}
-						{#if perimeter.compliance_assessments.warnings.length > 0}
-							<span class="badge preset-tonal-warning"
-								>{perimeter.compliance_assessments.warnings.length}</span
-							>
-						{/if}
-						{#if perimeter.compliance_assessments.info.length > 0}
-							<span class="badge preset-tonal-secondary"
-								>{perimeter.compliance_assessments.info.length}</span
-							>
-						{/if}
-					</Tabs.Control>
-					<Tabs.Control value="risk_assessments" labelBase="inert px-2"
-						>{m.riskAssessments()}
-						{#if perimeter.risk_assessments.errors.length > 0}
-							<span class="badge preset-tonal-error"
-								>{perimeter.risk_assessments.errors.length}</span
-							>
-						{/if}
-						{#if perimeter.risk_assessments.warnings.length > 0}
-							<span class="badge preset-tonal-warning"
-								>{perimeter.risk_assessments.warnings.length}</span
-							>
-						{/if}
-						{#if perimeter.risk_assessments.info.length > 0}
-							<span class="badge preset-tonal-secondary"
-								>{perimeter.risk_assessments.info.length}</span
-							>
-						{/if}
-					</Tabs.Control>
-				{/snippet}
-				{#snippet content()}
-					<Tabs.Panel value="compliance_assessments">
-						<ul class="list-none pl-4 text-sm space-y-2">
-							{#each compliance_assessments as compliance_assessment, index}
-								<li class="h4 font-semibold mb-1">
-									<Anchor
-										href="/compliance-assessments/{compliance_assessment.object.id}"
-										class="hover:underline text-blue-600"
-										>{compliance_assessment.object.name}</Anchor
-									>
-								</li>
-								{@const quality_check = compliance_assessment.quality_check}
-								<div class="flex flex-col space-y-3">
-									{#if quality_check.errors.length > 0}
-										<div class="space-y-2">
-											<div class="preset-tonal-error rounded-base px-2 py-1">
-												<i class="fa-solid fa-bug mr-1"></i>
-												{#if quality_check.errors.length === 1}
-													<span class="font-bold">{quality_check.errors.length}</span>
-													{m.errorsFound({ s: '' })}
-												{:else}
-													<span class="font-bold">{quality_check.errors.length}</span>
-													{m.errorsFound({ s: '' })}
-												{/if}
+			{@const compliance_assessments = Object.values(perimeter.compliance_assessments.objects)}
+			{@const risk_assessments = Object.values(perimeter.risk_assessments.objects)}
+			<div>
+				<span class="text-2xl">💡</span>
+				<Anchor
+					class="text-2xl font-bold mb-1 hover:underline text-blue-600"
+					href="/perimeters/{perimeter.perimeter.id}"
+				>
+					{perimeter.perimeter.folder.str}/{perimeter.perimeter.name}
+				</Anchor>
+				<Tabs
+					value={tabStates[perimeter.id] || 'compliance_assessments'}
+					onValueChange={(e) => {
+						if (!tabStates[perimeter.id]) {
+							tabStates[perimeter.id] = 'compliance_assessments';
+						}
+						tabStates[perimeter.id] = e.value;
+					}}
+					listJustify="justify-center"
+				>
+					{#snippet list()}
+						<Tabs.Control value="compliance_assessments" labelBase="inert px-2"
+							>{m.complianceAssessments()}
+							{#if perimeter.compliance_assessments.errors.length > 0}
+								<span class="badge preset-tonal-error"
+									>{perimeter.compliance_assessments.errors.length}</span
+								>
+							{/if}
+							{#if perimeter.compliance_assessments.warnings.length > 0}
+								<span class="badge preset-tonal-warning"
+									>{perimeter.compliance_assessments.warnings.length}</span
+								>
+							{/if}
+							{#if perimeter.compliance_assessments.info.length > 0}
+								<span class="badge preset-tonal-secondary"
+									>{perimeter.compliance_assessments.info.length}</span
+								>
+							{/if}
+						</Tabs.Control>
+						<Tabs.Control value="risk_assessments" labelBase="inert px-2"
+							>{m.riskAssessments()}
+							{#if perimeter.risk_assessments.errors.length > 0}
+								<span class="badge preset-tonal-error"
+									>{perimeter.risk_assessments.errors.length}</span
+								>
+							{/if}
+							{#if perimeter.risk_assessments.warnings.length > 0}
+								<span class="badge preset-tonal-warning"
+									>{perimeter.risk_assessments.warnings.length}</span
+								>
+							{/if}
+							{#if perimeter.risk_assessments.info.length > 0}
+								<span class="badge preset-tonal-secondary"
+									>{perimeter.risk_assessments.info.length}</span
+								>
+							{/if}
+						</Tabs.Control>
+					{/snippet}
+					{#snippet content()}
+						<Tabs.Panel value="compliance_assessments">
+							<ul class="list-none pl-4 text-sm space-y-2">
+								{#each compliance_assessments as compliance_assessment, index}
+									<li class="h4 font-semibold mb-1">
+										<Anchor
+											href="/compliance-assessments/{compliance_assessment.object.id}"
+											class="hover:underline text-blue-600"
+											>{compliance_assessment.object.name}</Anchor
+										>
+									</li>
+									{@const quality_check = compliance_assessment.quality_check}
+									<div class="flex flex-col space-y-3">
+										{#if quality_check.errors.length > 0}
+											<div class="space-y-2">
+												<div class="preset-tonal-error rounded-base px-2 py-1">
+													<i class="fa-solid fa-bug mr-1"></i>
+													{#if quality_check.errors.length === 1}
+														<span class="font-bold">{quality_check.errors.length}</span>
+														{m.errorsFound({ s: '' })}
+													{:else}
+														<span class="font-bold">{quality_check.errors.length}</span>
+														{m.errorsFound({ s: '' })}
+													{/if}
+												</div>
+												<ul class="list-disc pl-4 text-sm">
+													{#each quality_check.errors as error}
+														<li>
+															{#if error.object.name}<Anchor class="anchor" href={error.link}
+																	>{error.object.name}</Anchor
+																>:{/if}
+															{safeTranslate(error.msgid)}
+														</li>
+													{/each}
+												</ul>
 											</div>
-											<ul class="list-disc pl-4 text-sm">
-												{#each quality_check.errors as error}
-													<li>
-														{#if error.object.name}<Anchor class="anchor" href={error.link}
-																>{error.object.name}</Anchor
-															>:{/if}
-														{safeTranslate(error.msgid)}
-													</li>
-												{/each}
-											</ul>
-										</div>
-									{/if}
-									{#if quality_check.warnings.length > 0}
-										<div class="space-y-2">
-											<div class="preset-tonal-warning rounded-base px-2 py-1">
-												<i class="fa-solid fa-triangle-exclamation mr-1"></i>
-												{#if quality_check.warnings.length === 1}
-													<span class="font-bold">{quality_check.warnings.length}</span>
-													{m.warningsFound({ s: '' })}
-												{:else}
-													<span class="font-bold">{quality_check.warnings.length}</span>
-													{m.warningsFound({ s: 's' })}
-												{/if}
+										{/if}
+										{#if quality_check.warnings.length > 0}
+											<div class="space-y-2">
+												<div class="preset-tonal-warning rounded-base px-2 py-1">
+													<i class="fa-solid fa-triangle-exclamation mr-1"></i>
+													{#if quality_check.warnings.length === 1}
+														<span class="font-bold">{quality_check.warnings.length}</span>
+														{m.warningsFound({ s: '' })}
+													{:else}
+														<span class="font-bold">{quality_check.warnings.length}</span>
+														{m.warningsFound({ s: 's' })}
+													{/if}
+												</div>
+												<ul class="list-disc pl-4 text-sm">
+													{#each quality_check.warnings as warning}
+														<li>
+															{#if warning.object.name}
+																<Anchor class="anchor" href={warning.link}
+																	>{warning.object.name}</Anchor
+																>:
+															{/if}
+															{safeTranslate(warning.msgid)}
+														</li>
+													{/each}
+												</ul>
 											</div>
-											<ul class="list-disc pl-4 text-sm">
-												{#each quality_check.warnings as warning}
-													<li>
-														{#if warning.object.name}
-															<Anchor class="anchor" href={warning.link}
-																>{warning.object.name}</Anchor
-															>:
-														{/if}
-														{safeTranslate(warning.msgid)}
-													</li>
-												{/each}
-											</ul>
-										</div>
-									{/if}
-									{#if quality_check.info.length > 0}
-										<div class="space-y-2">
-											<div class="preset-tonal-secondary rounded-base px-2 py-1">
-												<i class="fa-solid fa-circle-info mr-1"></i>
-												{#if quality_check.info.length === 1}
-													<span class="font-bold">{quality_check.info.length}</span>
-													{m.infosFound({ s: '' })}
-												{:else}
-													<span class="font-bold">{quality_check.info.length}</span>
-													{m.infosFound({ s: 's' })}
-												{/if}
+										{/if}
+										{#if quality_check.info.length > 0}
+											<div class="space-y-2">
+												<div class="preset-tonal-secondary rounded-base px-2 py-1">
+													<i class="fa-solid fa-circle-info mr-1"></i>
+													{#if quality_check.info.length === 1}
+														<span class="font-bold">{quality_check.info.length}</span>
+														{m.infosFound({ s: '' })}
+													{:else}
+														<span class="font-bold">{quality_check.info.length}</span>
+														{m.infosFound({ s: 's' })}
+													{/if}
+												</div>
+												<ul class="list-disc pl-4 text-sm">
+													{#each quality_check.info as info}
+														<li>
+															{#if info.object.name}<Anchor class="anchor" href={info.link}
+																	>{info.object.name}</Anchor
+																>:{/if}
+															{safeTranslate(info.msgid)}
+														</li>
+													{/each}
+												</ul>
 											</div>
-											<ul class="list-disc pl-4 text-sm">
-												{#each quality_check.info as info}
-													<li>
-														{#if info.object.name}<Anchor class="anchor" href={info.link}
-																>{info.object.name}</Anchor
-															>:{/if}
-														{safeTranslate(info.msgid)}
-													</li>
-												{/each}
-											</ul>
-										</div>
+										{/if}
+									</div>
+									{#if index != compliance_assessments.length - 1}
+										<hr />
 									{/if}
-								</div>
-								{#if index != compliance_assessments.length - 1}
-									<hr />
-								{/if}
-							{/each}
-						</ul>
-					</Tabs.Panel>
-					<Tabs.Panel value="risk_assessments">
-						<ul class="list-none pl-4 text-sm space-y-2">
-							{#each risk_assessments as risk_assessment, index}
-								<li class="h4 font-semibold mb-1">
-									<Anchor
-										href="/risk-assessments/{risk_assessment.object.id}"
-										class="hover:underline text-blue-600">{risk_assessment.object.name}</Anchor
-									>
-								</li>
-								{@const quality_check = risk_assessment.quality_check}
-								<div class="flex flex-col space-y-3">
-									{#if quality_check.errors.length > 0}
-										<div class="space-y-2">
-											<div class="preset-tonal-error rounded-base px-2 py-1">
-												<i class="fa-solid fa-bug mr-1"></i>
-												{#if quality_check.errors.length === 1}
-													<span class="font-bold">{quality_check.errors.length}</span>
-													{m.errorsFound({ s: '' })}
-												{:else}
-													<span class="font-bold">{quality_check.errors.length}</span>
-													{m.errorsFound({ s: '' })}
-												{/if}
+								{/each}
+							</ul>
+						</Tabs.Panel>
+						<Tabs.Panel value="risk_assessments">
+							<ul class="list-none pl-4 text-sm space-y-2">
+								{#each risk_assessments as risk_assessment, index}
+									<li class="h4 font-semibold mb-1">
+										<Anchor
+											href="/risk-assessments/{risk_assessment.object.id}"
+											class="hover:underline text-blue-600">{risk_assessment.object.name}</Anchor
+										>
+									</li>
+									{@const quality_check = risk_assessment.quality_check}
+									<div class="flex flex-col space-y-3">
+										{#if quality_check.errors.length > 0}
+											<div class="space-y-2">
+												<div class="preset-tonal-error rounded-base px-2 py-1">
+													<i class="fa-solid fa-bug mr-1"></i>
+													{#if quality_check.errors.length === 1}
+														<span class="font-bold">{quality_check.errors.length}</span>
+														{m.errorsFound({ s: '' })}
+													{:else}
+														<span class="font-bold">{quality_check.errors.length}</span>
+														{m.errorsFound({ s: '' })}
+													{/if}
+												</div>
+												<ul class="list-disc pl-4 text-sm">
+													{#each quality_check.errors as error}
+														<li>
+															{#if error.object.name}<Anchor class="anchor" href={error.link}
+																	>{error.object.name}</Anchor
+																>:{/if}
+															{safeTranslate(error.msgid)}
+														</li>
+													{/each}
+												</ul>
 											</div>
-											<ul class="list-disc pl-4 text-sm">
-												{#each quality_check.errors as error}
-													<li>
-														{#if error.object.name}<Anchor class="anchor" href={error.link}
-																>{error.object.name}</Anchor
-															>:{/if}
-														{safeTranslate(error.msgid)}
-													</li>
-												{/each}
-											</ul>
-										</div>
-									{/if}
-									{#if quality_check.warnings.length > 0}
-										<div class="space-y-2">
-											<div class="preset-tonal-warning rounded-base px-2 py-1">
-												<i class="fa-solid fa-triangle-exclamation mr-1"></i>
-												{#if quality_check.warnings.length === 1}
-													<span class="font-bold">{quality_check.warnings.length}</span>
-													{m.warningsFound({ s: '' })}
-												{:else}
-													<span class="font-bold">{quality_check.warnings.length}</span>
-													{m.warningsFound({ s: 's' })}
-												{/if}
+										{/if}
+										{#if quality_check.warnings.length > 0}
+											<div class="space-y-2">
+												<div class="preset-tonal-warning rounded-base px-2 py-1">
+													<i class="fa-solid fa-triangle-exclamation mr-1"></i>
+													{#if quality_check.warnings.length === 1}
+														<span class="font-bold">{quality_check.warnings.length}</span>
+														{m.warningsFound({ s: '' })}
+													{:else}
+														<span class="font-bold">{quality_check.warnings.length}</span>
+														{m.warningsFound({ s: 's' })}
+													{/if}
+												</div>
+												<ul class="list-disc pl-4 text-sm">
+													{#each quality_check.warnings as warning}
+														<li>
+															{#if warning.object.name}<Anchor class="anchor" href={warning.link}
+																	>{warning.object.name}</Anchor
+																>:{/if}
+															{safeTranslate(warning.msgid)}
+														</li>
+													{/each}
+												</ul>
 											</div>
-											<ul class="list-disc pl-4 text-sm">
-												{#each quality_check.warnings as warning}
-													<li>
-														{#if warning.object.name}<Anchor class="anchor" href={warning.link}
-																>{warning.object.name}</Anchor
-															>:{/if}
-														{safeTranslate(warning.msgid)}
-													</li>
-												{/each}
-											</ul>
-										</div>
-									{/if}
-									{#if quality_check.info.length > 0}
-										<div class="space-y-2">
-											<div class="preset-tonal-secondary rounded-base px-2 py-1">
-												<i class="fa-solid fa-circle-info mr-1"></i>
-												{#if quality_check.info.length === 1}
-													<span class="font-bold">{quality_check.info.length}</span>
-													{m.infosFound({ s: '' })}
-												{:else}
-													<span class="font-bold">{quality_check.info.length}</span>
-													{m.infosFound({ s: 's' })}
-												{/if}
+										{/if}
+										{#if quality_check.info.length > 0}
+											<div class="space-y-2">
+												<div class="preset-tonal-secondary rounded-base px-2 py-1">
+													<i class="fa-solid fa-circle-info mr-1"></i>
+													{#if quality_check.info.length === 1}
+														<span class="font-bold">{quality_check.info.length}</span>
+														{m.infosFound({ s: '' })}
+													{:else}
+														<span class="font-bold">{quality_check.info.length}</span>
+														{m.infosFound({ s: 's' })}
+													{/if}
+												</div>
+												<ul class="list-disc pl-4 text-sm">
+													{#each quality_check.info as info}
+														<li>
+															{#if info.object.name}<Anchor class="anchor" href={info.link}
+																	>{info.object.name}</Anchor
+																>:{/if}
+															{safeTranslate(info.msgid)}
+														</li>
+													{/each}
+												</ul>
 											</div>
-											<ul class="list-disc pl-4 text-sm">
-												{#each quality_check.info as info}
-													<li>
-														{#if info.object.name}<Anchor class="anchor" href={info.link}
-																>{info.object.name}</Anchor
-															>:{/if}
-														{safeTranslate(info.msgid)}
-													</li>
-												{/each}
-											</ul>
-										</div>
+										{/if}
+									</div>
+									{#if index != risk_assessments.length - 1}
+										<hr />
 									{/if}
-								</div>
-								{#if index != risk_assessments.length - 1}
-									<hr />
-								{/if}
-							{/each}
-						</ul>
-					</Tabs.Panel>
-				{/snippet}
-			</Tabs>
-		</div>
-		{#if index != perimeters.length - 1}
-			<hr />
-		{/if}
-	{/each}
+								{/each}
+							</ul>
+						</Tabs.Panel>
+					{/snippet}
+				</Tabs>
+			</div>
+			{#if index != perimeters.length - 1}
+				<hr />
+			{/if}
+		{/each}
 	{:catch error}
 		<div class="flex flex-col items-center justify-center py-8">
 			<p class="text-red-500">Error loading data</p>
