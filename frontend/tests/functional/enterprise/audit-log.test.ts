dc0e920d6c83f15e333028b402845099e949fe12
Axxiar <galoualexis@gmail.com>
2025-08-13 16:43:02 +0200

    rewrite utilsv2

--- frontend/tests/functional/enterprise/audit-log.test.ts
+++ frontend/tests/functional/enterprise/audit-log.test.ts
@@ -1,8 +1,11 @@
-import { test } from '../../utils/test-utils.js';
+import { test, expect } from '../../utilsv2/core/base';
 
-test('Logging is working properly', async ({ logedPage, auditsLogPage }) => {
+test('Logs are working properly', async ({ loginPage, auditLogPage }) => {
 	await test.step('original logs show up', async () => {
-		await auditsLogPage.goto();
-		await auditsLogPage.hasUrl();
+		await loginPage.gotoSelf();
+		await loginPage.doLoginAdminP();
+		await auditLogPage.gotoSelf();
+		await auditLogPage.checkSelf(expect);
+		await auditLogPage.doCloseModal();
 	});
 });
