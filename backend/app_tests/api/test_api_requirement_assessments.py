15c0d47364840d0cbac1d7c106e390d08e3cbe02
Abderrahmane Smimite <smimite@gmail.com>
2025-08-21 08:38:54 +0200

    fixup

--- backend/app_tests/api/test_api_requirement_assessments.py
+++ backend/app_tests/api/test_api_requirement_assessments.py
@@ -123,6 +123,8 @@ class TestRequirementAssessmentsAuthenticated:
                 "compliance_assessment": {
                     "id": str(compliance_assessment.id),
                     "str": compliance_assessment.name,
+                    "is_locked": False,
+                    "name": compliance_assessment.name,
                 },
                 "requirement": {
                     "id": str(RequirementNode.objects.all()[0].id),
