b71346b5b791faa8269e60794974193edda703e4
Axxiar <galoualexis@gmail.com>
2025-08-25 17:43:02 +0200

    audit-logs table.ts support

--- enterprise/frontend/src/routes/(app)/(internal)/audit-log/+page.svelte
+++ enterprise/frontend/src/routes/(app)/(internal)/audit-log/+page.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
 	import ModelTable from '$lib/components/ModelTable/ModelTable.svelte';
+	import type { PageData } from './$types';
+
+	interface Props {
+		data: PageData;
+	}
+
+	let { data }: Props = $props();
 </script>
 
 <main class="bg-white card p-4">
 	<ModelTable
-		source={{
-			head: {
-				actor: 'actor',
-				action: 'action',
-				content_type: 'content_type',
-				timestamp: 'timestamp'
-			},
-			body: [],
-			meta: []
-		}}
+		source={{ ...data.table }}
 		URLModel="audit-log"
 		baseEndpoint="/audit-log"
-		fields={['actor', 'action', 'content_type', 'timestamp']}
+		fields={['actor', 'action', 'content_type', 'timestamp', 'folder']}
 	/>
 </main>
