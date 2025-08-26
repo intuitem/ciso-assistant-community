6745e285606c3a110304368c03ce198ec173c043
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-08-26 13:02:09 +0200

    write initial serializers

---
+++ backend/crq/serializers.py
@@ -0,0 +1,75 @@
+from core.serializers import (
+    BaseModelSerializer,
+)
+from core.serializer_fields import FieldsRelatedField, HashSlugRelatedField
+from core.models import RiskMatrix
+from .models import (
+    QuantitativeRiskStudy,
+    QuantitativeRiskScenario,
+    QuantitativeRiskHypothesis,
+    QuantitativeRiskAggregation,
+)
+from rest_framework import serializers
+
+
+class QuantitativeRiskStudyWriteSerializer(BaseModelSerializer):
+    class Meta:
+        model = QuantitativeRiskStudy
+        exclude = ["created_at", "updated_at"]
+
+
+class QuantitativeRiskStudyReadSerializer(BaseModelSerializer):
+    folder = FieldsRelatedField()
+    authors = FieldsRelatedField(many=True)
+    reviewers = FieldsRelatedField(many=True)
+
+    class Meta:
+        model = QuantitativeRiskStudy
+        fields = "__all__"
+
+
+class QuantitativeRiskScenarioWriteSerializer(BaseModelSerializer):
+    class Meta:
+        model = QuantitativeRiskScenario
+        exclude = ["created_at", "updated_at"]
+
+
+class QuantitativeRiskScenarioReadSerializer(BaseModelSerializer):
+    quantitative_risk_study = FieldsRelatedField()
+    assets = FieldsRelatedField(many=True)
+    threats = FieldsRelatedField(many=True)
+    vulnerabilities = FieldsRelatedField(many=True)
+    qualifications = FieldsRelatedField(many=True)
+
+
+class QuantitativeRiskHypothesisWriteSerializer(BaseModelSerializer):
+    class Meta:
+        model = QuantitativeRiskHypothesis
+        exclude = ["created_at", "updated_at"]
+
+
+class QuantitativeRiskHypothesisReadSerializer(BaseModelSerializer):
+    quantitative_risk_study = FieldsRelatedField()
+    quantitative_risk_scenario = FieldsRelatedField()
+    quantitative_risk_aggregations = FieldsRelatedField(many=True)
+    existing_applied_controls = FieldsRelatedField(many=True)
+    added_applied_controls = FieldsRelatedField(many=True)
+    removed_applied_controls = FieldsRelatedField(many=True)
+
+    class Meta:
+        model = QuantitativeRiskHypothesis
+        fields = "__all__"
+
+
+class QuantitativeRiskAggregationWriteSerializer(BaseModelSerializer):
+    class Meta:
+        model = QuantitativeRiskAggregation
+        exclude = ["created_at", "updated_at"]
+
+
+class QuantitativeRiskAggregationReadSerializer(BaseModelSerializer):
+    quantitative_risk_study = FieldsRelatedField()
+
+    class Meta:
+        model = QuantitativeRiskAggregation
+        fields = "__all__"
