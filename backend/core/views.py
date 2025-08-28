40122b9935f63aa549adc31fe5a19053f3c8f70c
Axxiar <galoualexis@gmail.com>
2025-08-28 19:10:28 +0200

    avoid duplication of nodes

--- backend/core/views.py
+++ backend/core/views.py
@@ -2029,23 +2029,25 @@ class AppliedControlViewSet(BaseModelViewSet):
             idx_cnt += 1
             # attached requirement_assessments
             for req in RequirementAssessment.objects.filter(applied_controls__id=ac.id):
-                nodes.append(
-                    {
-                        "name": req.requirement.ref_id
-                        or req.requirement.safe_display_str,
-                        "value": req.requirement.description
-                        or req.requirement.safe_display_str,
-                        "category": 7,
-                        "symbol": "triangle",
-                    }
-                )
-                indexes[req.id] = (
-                    idx_cnt  # not good - even if the probability of collision is low
-                )
-                idx_cnt += 1
+                if req.id not in indexes:
+                    # Add RequirementAssessment node only if it doesn't exist
+                    nodes.append(
+                        {
+                            "name": req.requirement.ref_id
+                            or req.requirement.safe_display_str,
+                            "value": req.requirement.description
+                            or req.requirement.safe_display_str,
+                            "category": 7,
+                            "symbol": "triangle",
+                        }
+                    )
+                    indexes[req.id] = (
+                        idx_cnt  # not good - even if the probability of collision is low
+                    )
+                    idx_cnt += 1
 
                 audit = req.compliance_assessment
-                if indexes.get(audit.id) is None:
+                if audit.id not in indexes:
                     nodes.append(
                         {
                             "name": audit.name,
@@ -2057,22 +2059,23 @@ class AppliedControlViewSet(BaseModelViewSet):
                     indexes[audit.id] = idx_cnt
                     idx_cnt += 1
                 links.append({"source": indexes[audit.id], "target": indexes[req.id]})
-
                 links.append({"source": indexes[ac.id], "target": indexes[req.id]})
+
             for sc in RiskScenario.objects.filter(applied_controls__id=ac.id):
-                nodes.append(
-                    {
-                        "name": sc.ref_id,
-                        "value": sc.name,
-                        "category": 8,
-                        "symbol": "diamond",
-                    }
-                )
-                indexes[sc.id] = idx_cnt
-                idx_cnt += 1
+                if sc.id not in indexes:
+                    nodes.append(
+                        {
+                            "name": sc.ref_id,
+                            "value": sc.name,
+                            "category": 8,
+                            "symbol": "diamond",
+                        }
+                    )
+                    indexes[sc.id] = idx_cnt
+                    idx_cnt += 1
 
                 ra = sc.risk_assessment
-                if indexes.get(ra.id) is None:
+                if ra.id not in indexes:
                     nodes.append(
                         {
                             "name": ra.name,
@@ -2084,7 +2087,6 @@ class AppliedControlViewSet(BaseModelViewSet):
                     indexes[ra.id] = idx_cnt
                     idx_cnt += 1
                 links.append({"source": indexes[ra.id], "target": indexes[sc.id]})
-
                 links.append({"source": indexes[ac.id], "target": indexes[sc.id]})
 
         return Response({"nodes": nodes, "categories": categories, "links": links})
