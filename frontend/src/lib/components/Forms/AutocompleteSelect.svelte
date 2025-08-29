d7ad96ac15836d9306e04646006afc754526382b
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2025-08-29 17:21:22 +0200

    fix: revert changes

--- frontend/src/lib/components/Forms/AutocompleteSelect.svelte
+++ frontend/src/lib/components/Forms/AutocompleteSelect.svelte
@@ -437,7 +437,6 @@
 			duplicates={false}
 			key={JSON.stringify}
 			filterFunc={fastFilter}
-			data-testid="form-input-{field.replaceAll('_', '-')}"
 		>
 			{#snippet option({ option })}
 				{#if option.infoString?.position === 'prefix'}
