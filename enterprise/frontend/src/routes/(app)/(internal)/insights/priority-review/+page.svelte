b49fdd8203db9dceda23894d0582555af1535f14
Abder <smimite@gmail.com>
2025-08-30 18:50:11 +0200

    feat(pro): controls impact vs effort review to help with prioritisation (#2467)
    * feat: controls impact vs effort review to help with prioritisation
    
    * white bg
    
    * missing ts import

--- enterprise/frontend/src/routes/(app)/(internal)/insights/priority-review/+page.svelte
+++ enterprise/frontend/src/routes/(app)/(internal)/insights/priority-review/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+	import ImpactEffortMatrix from './ImpactEffortMatrix.svelte';
 	import type { PageData } from './$types';
-	import PriorityRadarChart from '$lib/components/Chart/PriorityRadarChart.svelte';
 	interface Props {
 		data: PageData;
 	}
@@ -8,8 +8,4 @@
 	let { data }: Props = $props();
 </script>
 
-<div class="bg-white shadow flex overflow-x-auto">
-	<div class="w-full h-screen">
-		<PriorityRadarChart title="" name="c_priority" data={data.data} />
-	</div>
-</div>
+<ImpactEffortMatrix data={data.data} />
