2a20e9705778f3a0b9081b662daf8aff8bce584a
Abderrahmane Smimite <smimite@gmail.com>
2025-08-30 16:20:38 +0200

    feat: controls impact vs effort review to help with prioritisation

--- enterprise/frontend/src/routes/(app)/(internal)/insights/priority-review/+page.svelte
+++ enterprise/frontend/src/routes/(app)/(internal)/insights/priority-review/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import PriorityRadarChart from '$lib/components/Chart/PriorityRadarChart.svelte';
+	import ImpactEffortMatrix from './ImpactEffortMatrix.svelte';
 	interface Props {
 		data: PageData;
 	}
@@ -8,8 +7,4 @@
 	let { data }: Props = $props();
 </script>
 
-<div class="bg-white shadow flex overflow-x-auto">
-	<div class="w-full h-screen">
-		<PriorityRadarChart title="" name="c_priority" data={data.data} />
-	</div>
-</div>
+<ImpactEffortMatrix data={data.data} />
