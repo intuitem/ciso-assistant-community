d9b1f8aa9215bc338920b0c084ff5897da491705
Axxiar <galoualexis@gmail.com>
2025-09-03 11:57:20 +0200

    new style for table mode too

--- frontend/src/routes/(app)/(third-party)/compliance-assessments/[id=uuid]/table-mode/+page.svelte
+++ frontend/src/routes/(app)/(third-party)/compliance-assessments/[id=uuid]/table-mode/+page.svelte
@@ -339,14 +339,23 @@
 				class="flex flex-col items-center justify-center border px-4 py-2 shadow-sm rounded-xl space-y-2"
 			>
 				{#if requirementAssessment.description}
-					<div class="flex w-full font-semibold">
+					<div class="card w-full font-light text-lg p-4 preset-tonal-primary">
+						<h2 class="font-semibold text-lg flex flex-row justify-between">
+							<div>
+								<i class="fa-solid fa-file-lines mr-2"></i>{m.description()}
+							</div>
+						</h2>
 						<MarkdownRenderer content={requirementAssessment.description} />
 					</div>
+
+					<!-- <div class="flex w-full font-semibold"> -->
+					<!-- 	<MarkdownRenderer content={requirementAssessment.description} /> -->
+					<!-- </div> -->
 				{/if}
 				{#if requirementAssessment.assessable}
 					{#if data.requirements[i].annotation || data.requirements[i].typical_evidence || requirementAssessment.mapping_inference.result}
 						<div
-							class="card p-4 preset-tonal-primary text-sm flex flex-col justify-evenly cursor-auto w-full"
+							class="card p-4 preset-tonal-secondary text-sm flex flex-col justify-evenly cursor-auto w-full"
 						>
 							<h2 class="font-semibold text-lg flex flex-row justify-between">
 								<div>
@@ -354,9 +363,9 @@
 								</div>
 								<button onclick={() => toggleSuggestion(requirementAssessment.id)}>
 									{#if !hideSuggestionHashmap[requirementAssessment.id]}
-										<i class="fa-solid fa-chevron-down"></i>
+										<i class="fa-solid fa-eye"></i>
 									{:else}
-										<i class="fa-solid fa-chevron-right"></i>
+										<i class="fa-solid fa-eye-slash"></i>
 									{/if}
 								</button>
 							</h2>
