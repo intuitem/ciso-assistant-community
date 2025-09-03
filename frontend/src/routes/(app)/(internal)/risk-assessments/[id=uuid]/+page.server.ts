0e9e1190feeccbb55c68763ece07a7e6e0567bf7
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-09-03 19:46:42 +0200

    prepare sveltekit actions and endpoints

--- frontend/src/routes/(app)/(internal)/risk-assessments/[id=uuid]/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/risk-assessments/[id=uuid]/+page.server.ts
@@ -59,5 +59,43 @@ export const actions: Actions = {
 		);
 
 		return { form };
+	},
+	syncToActions: async (event) => {
+		const formData = await event.request.formData();
+
+		if (!formData) {
+			return fail(400, { form: null });
+		}
+
+		const schema = z.object({ id: z.string().uuid() });
+		const form = await superValidate(formData, zod(schema));
+
+		const response = await event.fetch(
+			`${BASE_API_URL}/risk-assessment/${event.params.id}/sync-to-actions/?dry_run=false`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		);
+		if (response.ok) {
+			setFlash(
+				{
+					type: 'success',
+					message: m.syncToAppliedControlsSuccess()
+				},
+				event
+			);
+		} else {
+			setFlash(
+				{
+					type: 'error',
+					message: m.syncToAppliedControlsError()
+				},
+				event
+			);
+		}
+		return { form, message: { riskScenarios: await response.json() } };
 	}
 };
