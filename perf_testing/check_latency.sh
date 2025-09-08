9ca2069bef7e147747a0c8062700949117803c6d
Abderrahmane Smimite <smimite@gmail.com>
2025-09-08 20:34:10 +0200

    chores: tooling for latency simulation with ToxiProxy

---
+++ perf_testing/check_latency.sh
@@ -0,0 +1,4 @@
+# check_latency.sh
+#!/bin/bash
+echo "Current toxics on postgres proxy:"
+curl -s http://localhost:8474/proxies/postgres/toxics | jq '.' || curl -s http://localhost:8474/proxies/postgres/toxics
