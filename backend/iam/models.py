4a6ae4542b00ce263d4281c9a80d1c5ef4ac0cb9
Abder <smimite@gmail.com>
2025-09-08 19:59:16 +0200

    chores: permissions perf optimisation  (#2494)
    permissions optim

--- backend/iam/models.py
+++ backend/iam/models.py
@@ -818,15 +818,26 @@ class RoleAssignment(NameDescriptionMixin, FolderMixin):
         Also retrieve published objects in view
         """
         class_name = object_type.__name__.lower()
-        permission_view = Permission.objects.get(codename="view_" + class_name)
-        permission_change = Permission.objects.get(codename="change_" + class_name)
-        permission_delete = Permission.objects.get(codename="delete_" + class_name)
+        permissions_map = {
+            p.codename: p
+            for p in Permission.objects.filter(
+                codename__in=[
+                    f"view_{class_name}",
+                    f"change_{class_name}",
+                    f"delete_{class_name}",
+                    "view_folder",
+                ]
+            )
+        }
+        permission_view = permissions_map[f"view_{class_name}"]
+        permission_change = permissions_map[f"change_{class_name}"]
+        permission_delete = permissions_map[f"delete_{class_name}"]
         permissions = set([permission_view, permission_change, permission_delete])
         result_view = set()
         result_change = set()
         result_delete = set()
 
-        ref_permission = Permission.objects.get(codename="view_folder")
+        ref_permission = permissions_map["view_folder"]
         perimeter = {folder} | set(folder.get_sub_folders())
         # Process role assignments
         role_assignments = [
@@ -906,11 +917,18 @@ class RoleAssignment(NameDescriptionMixin, FolderMixin):
     @staticmethod
     def get_role_assignments(principal: AbstractBaseUser | AnonymousUser | UserGroup):
         """get all role assignments attached to a user directly or indirectly"""
-        assignments = list(principal.roleassignment_set.all())
+        assignments = list(
+            principal.roleassignment_set.select_related("role").prefetch_related(
+                "role__permissions", "perimeter_folders"
+            )
+        )
         if hasattr(principal, "user_groups"):
             for user_group in principal.user_groups.all():
-                assignments += list(user_group.roleassignment_set.all())
-        assignments += list(principal.roleassignment_set.all())
+                assignments += list(
+                    user_group.roleassignment_set.select_related(
+                        "role"
+                    ).prefetch_related("role__permissions", "perimeter_folders")
+                )
         return assignments
 
     @staticmethod
