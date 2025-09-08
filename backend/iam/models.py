2960038ea0b8b7bbed2c5b2908949afa6967f114
Abderrahmane Smimite <smimite@gmail.com>
2025-09-08 16:18:38 +0200

    wip

--- backend/iam/models.py
+++ backend/iam/models.py
@@ -917,11 +917,12 @@ class RoleAssignment(NameDescriptionMixin, FolderMixin):
     def get_permissions(principal: AbstractBaseUser | AnonymousUser | UserGroup):
         """get all permissions attached to a user directly or indirectly"""
         permissions = {}
+        query_filter = models.Q(user=principal)
+        if hasattr(principal, "user_groups"):
+            query_filter |= models.Q(user_group__in=principal.user_groups.all())
+
         permission_rows = (
-            RoleAssignment.objects.filter(
-                models.Q(user=principal)
-                | models.Q(user_group__in=principal.user_groups.all())
-            )
+            RoleAssignment.objects.filter(query_filter)
             .values_list("role__permissions__codename", "role__permissions__name")
             .distinct()
         )
