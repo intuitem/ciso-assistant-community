76d29a6fa964353062954887ff4bd87ce00c0126
Axxiar <70155662+Axxiar@users.noreply.github.com>
2025-09-11 21:18:59 +0200

    feat: filters on autditlogs table (#2484)
    * ModelTable: support filter for enterprise
    * Thfilters for actor content_type and domain
    * stabliize filters and Thfilters
    * actor & action translation
    * folder ordering
    
    ---------
    
    Co-authored-by: eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>

--- frontend/src/lib/components/ModelTable/types.ts
+++ frontend/src/lib/components/ModelTable/types.ts
@@ -7,4 +7,6 @@ export interface TableSource {
 	meta?: any;
 	/** The formatted table footer values. */
 	foot?: string[];
+	/** The table filters. It can be used if the URLModel is not in listViewFields (table.ts) */
+	filters?: any;
 }
