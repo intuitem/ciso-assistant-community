3c20fc3f2f132e35e8f333e0f0a21478e573f09e
monsieurswag <mael.joumier@intuitem.com>
2025-09-15 18:23:19 +0200

    feat: Support comma characters for floats

--- frontend/src/lib/components/Forms/ModelForm/QuantitativeRiskStudyForm.svelte
+++ frontend/src/lib/components/Forms/ModelForm/QuantitativeRiskStudyForm.svelte
@@ -131,17 +131,18 @@
 					{form}
 					field="risk_tolerance.points.point1.probability"
 					label="Point 1 - Probability"
+					commaSupported={true}
 					min={0.01}
 					max={0.99}
-					step={0.01}
+					floatPrecision={2}
 					helpText="Probability value (0.01-0.99). You can start with 0.99 for the most frequent acceptable issues"
 				/>
 				<NumberField
 					{form}
 					field="risk_tolerance.points.point1.acceptable_loss"
 					label="Point 1 - Tolerable Loss ({displayCurrency})"
+					commaSupported={true}
 					min={1}
-					step={1}
 					helpText="Acceptable loss amount for point 1"
 				/>
 			</div>
@@ -150,17 +151,18 @@
 					{form}
 					field="risk_tolerance.points.point2.probability"
 					label="Point 2 - Probability"
+					commaSupported={true}
 					min={0.01}
 					max={0.99}
-					step={0.01}
+					floatPrecision={2}
 					helpText="Probability value (0.01-0.99), You can close with 0.01 for the most rare acceptable cases"
 				/>
 				<NumberField
 					{form}
 					field="risk_tolerance.points.point2.acceptable_loss"
 					label="Point 2 - Tolerable Loss ({displayCurrency})"
+					commaSupported={true}
 					min={1}
-					step={1}
 					helpText="Acceptable loss amount for point 2"
 				/>
 			</div>
--- frontend/src/lib/components/Forms/NumberField.svelte
+++ frontend/src/lib/components/Forms/NumberField.svelte
@@ -8,7 +8,9 @@
 	interface Props {
 		class?: string;
 		label?: string | undefined;
-		step?: number;
+		max?: number;
+		allowNegative?: boolean;
+		floatPrecision?: number;
 		field: string;
 		valuePath?: any; // the place where the value is stored in the form. This is useful for nested objects
 		helpText?: string | undefined;
@@ -18,13 +20,16 @@
 		hidden?: boolean;
 		disabled?: boolean;
 		required?: boolean;
+		commaSupported?: boolean;
 		[key: string]: any;
 	}
 
 	let {
 		class: _class = '',
 		label = $bindable(),
-		step = 1,
+		max = Infinity,
+		allowNegative = false,
+		floatPrecision = 0,
 		field,
 		valuePath = field,
 		helpText = undefined,
@@ -37,6 +42,7 @@
 		hidden = false,
 		disabled = false,
 		required = false,
+		commaSupported = false,
 		...rest
 	}: Props = $props();
 
@@ -56,6 +62,21 @@
 		const cacheResult = await cacheLock.promise;
 		if (cacheResult) $value = cacheResult;
 	});
+
+	let stringifiedValueRegex =
+		floatPrecision > 0
+			? new RegExp(`^${allowNegative ? '\\-?' : ''}[0-9]((\\.|,)([0-9]{1,${floatPrecision}})?)?$`)
+			: new RegExp(`^${allowNegative ? '\\-?' : ''}[0-9]+$`);
+	let stringifiedValue = $state(`${$value === undefined ? '' : $value}`);
+
+	if (commaSupported) {
+		$effect(() => {
+			if (stringifiedValue) {
+				const newValue = Number(stringifiedValue.replace(',', '.'));
+				value.set(newValue);
+			}
+		});
+	}
 
 	let classesTextField = $derived((errors: string[] | undefined) => (errors ? 'input-error' : ''));
 	let classesDisabled = $derived((d: boolean) => (d ? 'opacity-50' : ''));
@@ -81,20 +102,59 @@
 		{/if}
 	</div>
 	<div class="control">
-		<input
-			type="number"
-			{step}
-			class="{'input ' + _class} {classesTextField($errors)}"
-			data-testid="form-input-{field.replaceAll('_', '-')}"
-			name={field}
-			aria-invalid={$errors ? 'true' : undefined}
-			placeholder=""
-			bind:value={$value}
-			{...$constraints}
-			{...rest}
-			{disabled}
-			{required}
-		/>
+		{#if commaSupported}
+			<input
+				type="text"
+				class="{'input ' + _class} {classesTextField($errors)}"
+				data-testid="form-input-{field.replaceAll('_', '-')}"
+				name={field}
+				aria-invalid={$errors ? 'true' : undefined}
+				placeholder=""
+				bind:value={stringifiedValue}
+				onbeforeinput={(e) => {
+					if (e.inputType === 'deleteContentBackward') return;
+
+					const targetElem = e.target as HTMLInputElement;
+					const selectionStart = targetElem.selectionStart;
+					const selectionEnd = targetElem.selectionEnd;
+
+					const currentText = e.target.value;
+					// const newText = currentText + e.data;
+
+					const newText =
+						currentText.slice(0, selectionStart) + e.data + currentText.slice(selectionEnd);
+					console.log('===>', newText);
+
+					const newNumber = Number(newText);
+
+					if (newNumber !== newNumber || newNumber > max) {
+						e.preventDefault();
+					}
+					if (newText && !stringifiedValueRegex.test(newText)) {
+						e.preventDefault();
+					}
+				}}
+				{...$constraints}
+				{...rest}
+				{disabled}
+				{required}
+			/>
+		{:else}
+			<!-- { step }-->
+			<input
+				type="number"
+				class="{'input ' + _class} {classesTextField($errors)}"
+				data-testid="form-input-{field.replaceAll('_', '-')}"
+				name={field}
+				aria-invalid={$errors ? 'true' : undefined}
+				placeholder=""
+				bind:value={$value}
+				{...$constraints}
+				{...rest}
+				{disabled}
+				{required}
+			/>
+		{/if}
 	</div>
 	{#if helpText}
 		<p class="text-sm text-gray-500">{helpText}</p>
