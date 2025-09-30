23ac68118687568f09118978e25ad99144343239
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2025-09-30 09:48:19 +0200

    fix: api tests

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1641,6 +1641,8 @@ class RequirementAssessmentReadSerializer(BaseModelSerializer):
 
 
 class RequirementAssessmentWriteSerializer(BaseModelSerializer):
+    requirement = serializers.PrimaryKeyRelatedField(read_only=True)
+
     def validate(self, attrs):
         compliance_assessment = self.get_compliance_assessment()
 
@@ -1684,7 +1686,7 @@ class RequirementAssessmentWriteSerializer(BaseModelSerializer):
 
     class Meta:
         model = RequirementAssessment
-        exclude = ["created_at", "updated_at", "requirement"]
+        exclude = ["created_at", "updated_at"]
 
 
 class RequirementMappingSetReadSerializer(BaseModelSerializer):
