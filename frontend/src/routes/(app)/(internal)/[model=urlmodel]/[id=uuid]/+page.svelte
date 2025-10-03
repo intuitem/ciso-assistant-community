bb707b92ab18bf0f95860cf5190bdf16da5f838a
melinoix <titouan.ameline@gmail.com>
2025-10-03 14:26:54 +0200

    feat: Graph explorer enhancement  (#2621)
    * Traduction + flashmessage
    
    * fix
    
    * fixup
    
    ---------
    
    Co-authored-by: Abderrahmane Smimite <smimite@gmail.com>

--- frontend/src/routes/(app)/(internal)/[model=urlmodel]/[id=uuid]/+page.svelte
+++ frontend/src/routes/(app)/(internal)/[model=urlmodel]/[id=uuid]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import DetailView from '$lib/components/DetailView/DetailView.svelte';
 	import type { PageData, ActionData } from './$types';
+	import { m } from '$paraglide/messages';
 
 	interface Props {
 		data: PageData;
@@ -16,7 +17,7 @@
 	<div class="card my-4 p-4 bg-white">
 		<span class="bg-purple-700 text-white px-2 py-1 rounded-sm text-sm font-semibold">beta</span><a
 			class="ml-2 hover:text-purple-700"
-			href={`/experimental/mapping/${data.data.id}`}>View on graph explorer</a
+			href={`/experimental/mapping/${data.data.id}`}>{m.viewOnGraphExplorer()}</a
 		>
 	</div>
 {/if}
--- frontend/src/routes/(app)/(internal)/experimental/+page.svelte
+++ frontend/src/routes/(app)/(internal)/experimental/+page.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
 	import Article from '$lib/components/DataViz/Article.svelte';
 	import { pageTitle } from '$lib/utils/stores';
+	import { m } from '$paraglide/messages';
 
 	$pageTitle = 'Experimental';
 </script>
 
 <div class="bg-white grid grid-cols-2 p-4 gap-4">
 	<Article
-		title="Frameworks mapping"
-		desc="Visualize applied mapping data as a graph"
+		title={m.frameworksMapping()}
+		desc={m.visualizeAppliedMapping()}
 		link="/experimental/mapping"
 		tags={['analysis', 'mapping']}
 	/>
 	<Article
-		title="Assets explorer"
-		desc="Visualize assets and their relationships"
+		title={m.assetsExplorer()}
+		desc={m.visualizeAssetsRelationships()}
 		link="/assets/graph"
 		tags={['analysis', 'assets']}
 	/>
 	<Article
-		title="Requirements explorer"
-		desc="Visualize all requirements across all audits"
+		title={m.requirementsExplorer()}
+		desc={m.visualizeRequirements()}
 		link="/requirement-assessments"
 		tags={['analysis', 'assets']}
 	/>
