cbad30ca38b43cb5e9af3a748511aef6eec2e892
tchoumi313 <75470956+tchoumi313@users.noreply.github.com>
2025-10-04 01:42:06 +0200

    feat(ui): Adding Audit due date to the calendar  (#2640)
    Adding Audit due date to the calendar just like TASKS,APPLIEDCONTROL and RISACCEPTANCE

--- frontend/src/routes/(app)/(internal)/calendar/[year]/[month]/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/calendar/[year]/[month]/+page.server.ts
@@ -12,6 +12,7 @@ const nextMonth = (month: number): string => {
 export const load = (async ({ fetch, locals, params }) => {
 	const appliedControlsEndpoint = `${BASE_API_URL}/applied-controls/?eta__year=${params.year}&eta__month=${params.month}`;
 	const riskAcceptancesEndpoint = `${BASE_API_URL}/risk-acceptances/?expiry_date__year=${params.year}&expiry_date__month=${params.month}`;
+	const auditsEndpoint = `${BASE_API_URL}/compliance-assessments/?due_date__year=${params.year}&due_date__month=${params.month}`;
 	const tasksEndpoint = `${BASE_API_URL}/task-templates/calendar/${params.year}-${params.month.padStart(2, '0')}-01/${nextMonth(parseInt(params.month)) === '01' ? parseInt(params.year) + 1 : params.year}-${nextMonth(parseInt(params.month))}-01`;
 
 	const appliedControlsResponse = await fetch(appliedControlsEndpoint);
@@ -20,12 +21,16 @@ export const load = (async ({ fetch, locals, params }) => {
 	const riskAcceptancesResponse = await fetch(riskAcceptancesEndpoint);
 	const riskAcceptances = await riskAcceptancesResponse.json().then((res) => res.results);
 
+	const auditsResponse = await fetch(auditsEndpoint);
+	const audits = await auditsResponse.json().then((res) => res.results);
+
 	const tasksResponse = await fetch(tasksEndpoint);
 	const tasks = await tasksResponse.json();
 
 	return {
 		appliedControls,
 		riskAcceptances,
+		audits,
 		tasks,
 		title: 'calendar'
 	};
