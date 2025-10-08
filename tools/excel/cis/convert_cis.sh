fb87de4dcfd4156d5ee332f156b4bbbab3202955
Tarkadia <161824332+tarkadia@users.noreply.github.com>
2025-10-08 11:37:18 +0200

    feat(lib): Reference Controls for CIS v8.x.x Framework (#2594)
    * Add Reference Controls from Requirements
    
    - Requirements are now also available as Reference Controls.
    NOTE: Ref. IDs in "Framework" and "Reference Controls" sheets contain periods "." instead of commas "," because, for the "reference_controls" column, commas in IDs were considered as separators.
    - Updated README.md
    - Updated scrpit (.sh & .bat) so we can actually define a packager name and not use a predefined one
    
    * docs: improve cis doc
    
    ---------
    
    Co-authored-by: Mohamed-Hacene <mohamedhacene.b@gmail.com>

--- tools/excel/cis/convert_cis.sh
+++ tools/excel/cis/convert_cis.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
-if [ -z "$1" ]
+if [[ -z "$1" || -z "$2" ]]
 then
-    echo "Usage: convert_cis.sh cis_file.xlsx"
+    echo "Usage: $0 cis_file.xlsx <packager>"
     exit 1
 fi
 
-packager="personal"
-
 echo "➡️ [STEP 1] Extract Excel file data..."
-python prep_cis.py $1 $packager || { echo "❌ Step 1 failed"; exit 1; }
+python prep_cis.py $1 $2 || { echo "❌ Step 1 failed"; exit 1; }
 echo "➡️ [STEP 2] Convert extracted Excel file to v2..."
 python ../../convert_v1_to_v2.py cis-controls-v8.xlsx || { echo "❌ Step 2 failed"; exit 1; }
 echo "➡️ [STEP 3] Convert Excel v2 file to YAML..."
