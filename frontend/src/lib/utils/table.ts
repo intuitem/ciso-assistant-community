4f2538028fd8ea275c1da8e5a4cab79e26c87e5d
Abder <smimite@gmail.com>
2025-10-10 13:38:10 +0200

    feat: add owner on evidences table (#2688)

--- frontend/src/lib/utils/table.ts
+++ frontend/src/lib/utils/table.ts
@@ -1327,8 +1327,8 @@ export const listViewFields = {
 		}
 	},
 	evidences: {
-		head: ['name', 'file', 'size', 'folder', 'status', 'updatedAt', 'labels'],
-		body: ['name', 'attachment', 'size', 'folder', 'status', 'updated_at', 'filtering_labels'],
+		head: ['name', 'file', 'folder', 'owner', 'status', 'updatedAt', 'labels'],
+		body: ['name', 'attachment', 'folder', 'owner', 'status', 'updated_at', 'filtering_labels'],
 		filters: {
 			folder: DOMAIN_FILTER,
 			filtering_labels: LABELS_FILTER,
