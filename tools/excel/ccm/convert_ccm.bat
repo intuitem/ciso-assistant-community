28884f1369d8682ab7871d5c7f6ffbf690de7d5d
Tarkadia <161824332+tarkadia@users.noreply.github.com>
2025-10-24 10:54:41 +0200

    fix(lib): CCM Framework + CIS Framework (#2720)
    * Update CCM Framework Scripts & Doc
    
    - Updated ".sh" script to use Excel v2 structure before converting to YAML file.
    - Created ".bat" version of shell script for Windows users.
    - Added "requirements.txt".
    - Added "README.md".
    
    * Fix CIS Framework Script & Doc
    
    - Fixed "convert_cis.bat" so it can work on Windows instances using "pyenv-win"
    - Fixed syntax in "requirements.txt"
    - Fixed small typo in "README.md"
    
    * Fix Documentation
    
    - Fixed "README.md" for CIS Framework and CCM Framework

--- tools/excel/ccm/convert_ccm.bat
+++ tools/excel/ccm/convert_ccm.bat
@@ -1,33 +1,33 @@
 @echo off
 REM Check if an argument is provided
 IF "%~1"=="" (
-    echo Usage: convert_cis.bat cis_file.xlsx ^<packager^>
+    echo Usage: convert_ccm.bat ccm_file.xlsx ^<packager^>
     exit /b 1
 )
 IF "%~2"=="" (
-    echo Usage: convert_cis.bat cis_file.xlsx ^<packager^>
+    echo Usage: convert_ccm.bat ccm_file.xlsx ^<packager^>
     exit /b 1
 )
 
 
 REM Run the Python scripts
 echo ==^> [STEP 1] Extract Excel file data...
-call python prep_cis.py %1 %2
+call python convert_ccm.py %1 %2
 if %errorlevel% neq 0 (
     echo ==^> [ERROR] Step 1 failed
     exit /b 1
 )
 echo ==^> [STEP 2] Convert extracted Excel file to v2...
-call python ..\..\convert_v1_to_v2.py cis-controls-v8.xlsx
+call python ..\..\convert_v1_to_v2.py ccm-controls-v4.xlsx
 if %errorlevel% neq 0 (
     echo ==^> [ERROR] Step 2 failed
     exit /b 1
 )
 echo ==^> [STEP 3] Convert Excel v2 file to YAML...
-call python ..\..\convert_library_v2.py cis-controls-v8_new.xlsx
+call python ..\..\convert_library_v2.py ccm-controls-v4_new.xlsx
 if %errorlevel% neq 0 (
     echo ==^> [ERROR] Step 3 failed
     exit /b 1
 )
 
-echo ==^> [OK] Resulting file is available at cis-controls-v8_new.yaml
+echo ==^> [OK] Resulting file is available at ccm-controls-v4_new.yaml
