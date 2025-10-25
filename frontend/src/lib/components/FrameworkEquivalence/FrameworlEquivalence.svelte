95b4a48aecb3a426e186fc8ed5f8ec536eeae85d
Nassim <71079376+nas-tabchiche@users.noreply.github.com>
2025-10-25 17:33:06 +0200

    feat: experiment dynamic mappings (#2719)
    * Create mapping.py
    
    experimentation for dynamic mappings
    
    * Update mapping.py
    
    ruff
    
    * make it more modular with reusable code
    
    * keep only direct path if it exists
    
    This is more relevant, no reason to make an exception
    We can add later a way to disable mappings that are not welcome
    
    * Update mapping.py
    
    compress mappings in RAM
    
    * some reorganizing
    
    * only allow direct mappings for now
    
    * feat: first step to the mapping generation using RAM
    
    * result PoC working
    
    * use python 3.9 typing for collections
    
    * expand engine to include any requirement assessment field
    
    * simplify
    
    * bring all relevant fields
    
    * feat: frameworks and mappings in global vars
    
    * feat: set correcly frameworks and mappings in engine ram
    
    * feat: reload engine after creating a new requirementmappingset
    
    * fix: ruff format
    
    * add assets, exceptions, evidences, applied_control; to load_audit_fields
    
    * fix: ruff
    
    * cleanup
    
    * use global engine
    
    * add some type hints
    
    * lil todo
    
    * m2m mapping naive implementation
    
    * remove commented block and re-enable batch controls creation
    
    * framework results distribution endpoint
    
    * framework results distribution in Autocomplete PoC
    
    * add additionalMultiselectOptions prop
    
    * hacky way to add padding to multiselect options
    
    * add applied controls to subset and interset
    
    * add mapping_inference to m2m_fields
    
    * import engine at different places to avoid engine loading on makemigrations
    
    * add startup.py comments
    
    * add function to get direct paths and coverage from source framework
    
    * more safety on rms looping
    
    * add views for coverage view and +safety in loop
    
    * Visual implementation of coverage equivalence between a mapping and destinations
    
    * set global mapping max depth
    
    * remove coverage equivalence table and add equivalence of requirements in barchart
    
    * format
    
    * prettier
    
    * duplicated data in metrics init
    
    * some l10n
    
    * remove unused imports
    
    * fix variable/function name typo
    
    * keep existing selected IG on an audit
    
    * respect the selected IG on the mapping form
    
    * remove the accordion mapping chart for now
    
    * fix the collision and missing controls
    
    ---------
    
    Co-authored-by: eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
    Co-authored-by: Martin Cremers <martin.cremers@intuitem.com>
    Co-authored-by: martinzerty <martinus.cremers@outlook.fr>
    Co-authored-by: Abderrahmane Smimite <smimite@gmail.com>

---
+++ frontend/src/lib/components/FrameworkEquivalence/FrameworlEquivalence.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	let { coverages } = $props();
+	coverages = coverages.response;
+</script>
+
+<table class="table caption-bottom table text-xs table-interactive">
+	<thead class="table-head">
+		<tr>
+			<td class="cursor-pointer select-none uppercase bg-white text-gray-700 hover:text-black">
+				URN
+			</td>
+			<td> Partial coverage </td>
+			<td> Full coverage </td>
+		</tr>
+	</thead>
+	<tbody class="w-full border-b border-b-surface-100-900 bg-white">
+		{#each Object.entries(coverages) as [urn, cov]}
+			<tr class="hover:preset-tonal-primary even:bg-surface-50 cursor-pointer">
+				<td>
+					{urn}
+				</td>
+				<td>
+					{cov[0]}
+				</td>
+				<td>
+					{cov[1]}
+				</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
