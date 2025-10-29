81825c866fdd92e3fa10e9c22fc2d0cf45ac652d
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-10-29 15:10:17 +0100

    wip

--- frontend/src/routes/(app)/(internal)/settings/+page.svelte
+++ frontend/src/routes/(app)/(internal)/settings/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	import { page } from '$app/state';
 	import ModelForm from '$lib/components/Forms/ModelForm.svelte';
 	import { SSOSettingsSchema, GeneralSettingsSchema, FeatureFlagsSchema } from '$lib/utils/schemas';
 	import { m } from '$paraglide/messages';
 	import { Tabs } from '@skeletonlabs/skeleton-svelte';
 
-	let group = $state('integrations');
+	let group = $state('general');
 
 	let { data } = $props();
 </script>
@@ -18,7 +17,7 @@
 	active="bg-primary-100 text-primary-800 border-b border-primary-800"
 >
 	{#snippet list()}
-		<Tabs.Control value="instance"><i class="fa-solid fa-globe"></i> {m.general()}</Tabs.Control>
+		<Tabs.Control value="general"><i class="fa-solid fa-globe"></i> {m.general()}</Tabs.Control>
 		<Tabs.Control value="sso"><i class="fa-solid fa-key"></i> {m.sso()}</Tabs.Control>
 		<Tabs.Control value="featureFlags"
 			><i class="fa-solid fa-flag"></i> {m.featureFlags()}</Tabs.Control
@@ -28,7 +27,7 @@
 		>
 	{/snippet}
 	{#snippet content()}
-		<Tabs.Panel value="instance">
+		<Tabs.Panel value="general">
 			<div>
 				<span class="text-gray-500">{m.generalSettingsDescription()}</span>
 				<ModelForm
@@ -64,35 +63,5 @@
 				/>
 			</div>
 		</Tabs.Panel>
-		<Tabs.Panel value="integrations">
-			<div>
-				<span class="text-gray-500">{m.configureIntegrations()}</span>
-				<div class="flow-root">
-					<dl class="divide-y divide-surface-100 text-sm">
-						<div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-							<dt class="font-medium">{m.itsm()}</dt>
-							<dd class="text-surface-900 sm:col-span-2">
-								<div class="card p-4 bg-inherit flex flex-col space-y-3">
-									<a class="unstyled" href="/settings/integrations/jira">
-										<div class="flex flex-col space-y-2 hover:bg-primary-50 card p-4">
-											<span class="flex flex-row justify-between text-xl">
-												<i class="text-blue-700 fab fa-jira"></i>
-												{#if page.data.settings?.enabled_integrations.filter((integration: Record<string, any>) => integration.name === 'jira' && integration.configurations)}
-													<i class="fa-solid fa-circle-check text-success-600-400"></i>
-												{/if}
-											</span>
-											<span class="flex flex-row space-x-2">
-												<h6 class="h6 base-font-color">{m.jira()}</h6>
-											</span>
-										</div>
-									</a>
-								</div>
-								<hr />
-							</dd>
-						</div>
-					</dl>
-				</div>
-			</div></Tabs.Panel
-		>
 	{/snippet}
 </Tabs>
