2d97856c2e80c87d918231647f4a2c064c1aa561
AZERTY9463 <64594930+martinzerty@users.noreply.github.com>
2025-10-30 11:58:50 +0100

    feat: add implementation group in requirement assessments view (#2716)
    * feat: add implementation group in requirement assessments view
    
    * fix: add implementation_groups to the API tests
    
    * fix: format test code
    
    * bring the full IG name and safe check for requirement without IG
    
    * fixup
    
    * fix test
    
    * remove redundant serialized field
    
    ---------
    
    Co-authored-by: Martin Cremers <martin.cremers@intuitem.com>
    Co-authored-by: Abderrahmane Smimite <smimite@gmail.com>

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1754,13 +1754,21 @@ class RequirementAssessmentReadSerializer(BaseModelSerializer):
                 "associated_threats",
                 "parent_requirement",
                 "questions",
+                "implementation_groups",
             ]
 
     name = serializers.CharField(source="__str__")
     description = serializers.CharField(source="get_requirement_description")
     evidences = FieldsRelatedField(many=True)
     compliance_assessment = FieldsRelatedField(
-        ["id", "name", "is_locked", "min_score", "max_score"]
+        [
+            "id",
+            "name",
+            "is_locked",
+            "min_score",
+            "max_score",
+            {"framework": ["implementation_groups_definition"]},
+        ]
     )
     folder = FieldsRelatedField()
     perimeter = FieldsRelatedField(source="compliance_assessment.perimeter")
