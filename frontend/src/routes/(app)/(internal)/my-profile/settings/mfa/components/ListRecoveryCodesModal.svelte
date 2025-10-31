b67ed9d9f516c047ce6f606977df788b5f6da60e
Abder <smimite@gmail.com>
2025-10-31 13:18:42 +0100

    fix: safety check for TOTP and PAT modals (#2801)

--- frontend/src/routes/(app)/(internal)/my-profile/settings/mfa/components/ListRecoveryCodesModal.svelte
+++ frontend/src/routes/(app)/(internal)/my-profile/settings/mfa/components/ListRecoveryCodesModal.svelte
@@ -35,21 +35,21 @@
 				<span class="flex flex-row space-x-2 justify-end">
 					<form method="POST" action="?/regenerateRecoveryCodes" use:enhance>
 						<input type="hidden" name="regenerate" value="1" />
-						<button type="submit" class="btn px-2 py-1 {parent.buttonNeutral}"
+						<button type="submit" class="btn px-2 py-1 {parent?.buttonNeutral ?? ''}"
 							><i class="fa-solid fa-sync mr-2"></i>{m.regenerateRecoveryCodes()}</button
 						>
 					</form>
 					<button
 						type="button"
-						class="btn px-2 py-1 {parent.buttonNeutral}"
+						class="btn px-2 py-1 {parent?.buttonNeutral ?? ''}"
 						use:copy={{ text: $recoveryCodes.unused_codes.join(' ') }}
 						><i class="fa-solid fa-copy mr-2"></i>{m.copy()}</button
 					>
 				</span>
 			</div>
 		{/if}
-		<footer class="modal-footer {parent.regionFooter}">
-			<button type="button" class="btn {parent.buttonPositive}" onclick={parent.onClose}
+		<footer class="modal-footer {parent?.regionFooter ?? ''}">
+			<button type="button" class="btn {parent?.buttonPositive ?? ''}" onclick={parent?.onClose}
 				>{m.ok()}</button
 			>
 		</footer>
