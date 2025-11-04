18f6dba4765800bd49086b85f868b39c71ffd1b6
monsieurswag <mael.joumier@intuitem.com>
2025-11-04 18:51:38 +0100

    Attempt to fix backend code coverage

--- backend/serdes/utils.py
+++ backend/serdes/utils.py
@@ -411,10 +411,10 @@ def get_domain_export_objects(domain: Folder):
         | Q(ebios_rm_studies__in=ebios_rm_studies)
     ).distinct()
     requirement_assessments = RequirementAssessment.objects.filter(
-        compliance_assessments__in=compliance_assessments
+        compliance_assessment__in=compliance_assessments
     ).distinct()
     frameworks = Framework.objects.filter(
-        Q(folder__in=folders) | Q(compliance_assessments__in=compliance_assessments)
+        Q(folder__in=folders) | Q(compliance_assessment__in=compliance_assessments)
     ).distinct()
 
     entities = Entity.objects.filter(
