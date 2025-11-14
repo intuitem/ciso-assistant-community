d0df747200c250f5b0d738d733cc1a320dd8b1f9
monsieurswag <mael.joumier@intuitem.com>
2025-11-14 09:26:25 +0100

    Log failed login in logger

--- enterprise/backend/enterprise_core/signals.py
+++ enterprise/backend/enterprise_core/signals.py
@@ -3,9 +3,11 @@ from django.dispatch import receiver
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
 from auditlog.models import LogEntry
+import structlog
 
 from enterprise_core.models import LogEntryAction
 
+logger = structlog.get_logger(__name__)
 User = get_user_model()
 
 
@@ -15,6 +17,12 @@ def log_login_failed(sender, credentials, request, **kwargs):
     if username is None:
         return
     remote_addr = request.META.get("REMOTE_ADDR") if request else None
+
+    logger.info(
+        "Failed login attempt",
+        remote_addr=remote_addr,
+        username=username,
+    )
 
     LogEntry.objects.create(
         action=LogEntryAction.LOGIN_FAILED,
