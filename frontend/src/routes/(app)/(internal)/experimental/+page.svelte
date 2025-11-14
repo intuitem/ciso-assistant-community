5f9d7d0d4f0ab36d3de631fb25ca6c2113a083c8
Abder <smimite@gmail.com>
2025-11-14 23:58:10 +0100

    feat: shortcut on experimental page to all findings combined (#2876)

--- frontend/src/routes/(app)/(internal)/experimental/+page.svelte
+++ frontend/src/routes/(app)/(internal)/experimental/+page.svelte
@@ -8,21 +8,15 @@
 
 <div class="bg-white grid grid-cols-2 p-4 gap-4">
 	<Article
-		title={m.frameworksMapping()}
-		desc={m.visualizeAppliedMapping()}
-		link="/experimental/mapping"
-		tags={['analysis', 'mapping']}
-	/>
-	<Article
-		title={m.assetsExplorer()}
-		desc={m.visualizeAssetsRelationships()}
-		link="/assets/graph"
-		tags={['analysis', 'assets']}
-	/>
-	<Article
 		title={m.requirementsExplorer()}
 		desc={m.visualizeRequirements()}
 		link="/requirement-assessments"
-		tags={['analysis', 'assets']}
+		tags={['analysis', 'audit']}
+	/>
+	<Article
+		title={m.findings()}
+		desc={m.findingsExplorer()}
+		link="/findings"
+		tags={['analysis', 'findings']}
 	/>
 </div>
