9c4a6c99d2986ade8b7e30a4a6543fc2d08acfaf
monsieurswag <mael.joumier@intuitem.com>
2025-11-14 09:20:37 +0100

    feat: Log failed logins in auditlog

---
+++ enterprise/backend/enterprise_core/signals.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.signals import user_login_failed
+from django.dispatch import receiver
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import get_user_model
+from auditlog.models import LogEntry
+
+from enterprise_core.models import LogEntryAction
+
+User = get_user_model()
+
+
+@receiver(user_login_failed)
+def log_login_failed(sender, credentials, request, **kwargs):
+    username = credentials.get("username", None)
+    if username is None:
+        return
+    remote_addr = request.META.get("REMOTE_ADDR") if request else None
+
+    LogEntry.objects.create(
+        action=LogEntryAction.LOGIN_FAILED,
+        content_type=ContentType.objects.get_for_model(User),
+        object_repr=username,
+        remote_addr=remote_addr,
+        additional_data={"username": username},
+    )
