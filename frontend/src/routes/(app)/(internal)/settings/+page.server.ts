13148c1d64361c6e16123d02107d14e694134060
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-11-17 21:37:22 +0100

    delete webhook endpoint form action

--- frontend/src/routes/(app)/(internal)/settings/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/settings/+page.server.ts
@@ -1,17 +1,19 @@
 import { handleErrorResponse } from '$lib/utils/actions';
 import { BASE_API_URL } from '$lib/utils/constants';
 import { getModelInfo } from '$lib/utils/crud';
+import { m } from '$paraglide/messages';
+import { safeTranslate } from '$lib/utils/i18n';
 import {
-	SSOSettingsSchema,
-	GeneralSettingsSchema,
 	FeatureFlagsSchema,
+	GeneralSettingsSchema,
+	SSOSettingsSchema,
 	webhookEndpointSchema
 } from '$lib/utils/schemas';
-import { m } from '$paraglide/messages';
 import { fail, type Actions } from '@sveltejs/kit';
 import { setFlash } from 'sveltekit-flash-message/server';
-import { superValidate } from 'sveltekit-superforms';
+import { message, setError, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
+import { z } from 'zod';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
@@ -239,5 +241,47 @@ export const actions: Actions = {
 		);
 
 		return { form };
+	},
+	deleteWebhookEndpoint: async (event) => {
+		const formData = await event.request.formData();
+		const schema = z.object({ id: z.string() });
+		const deleteForm = await superValidate(formData, zod(schema));
+		const id = deleteForm.data.id;
+		const endpoint = `${BASE_API_URL}/webhooks/endpoints/${id}/`;
+
+		if (!deleteForm.valid) {
+			console.error(deleteForm.errors);
+			return message(deleteForm, { status: 400 });
+		}
+
+		const requestInitOptions: RequestInit = {
+			method: 'DELETE'
+		};
+		const res = await event.fetch(endpoint, requestInitOptions);
+		if (!res.ok) {
+			const response = await res.json();
+			if (response.error) {
+				const errorMessages = Array.isArray(response.error) ? response.error : [response.error];
+				errorMessages.forEach((error) => {
+					setFlash({ type: 'error', message: safeTranslate(error) }, event);
+				});
+				return message(deleteForm, { status: res.status });
+			}
+			if (response.non_field_errors) {
+				setError(deleteForm, 'non_field_errors', response.non_field_errors);
+			}
+			return message(deleteForm, { status: res.status });
+		}
+		setFlash(
+			{
+				type: 'success',
+				message: m.successfullyDeletedObject({
+					object: m.webhookEndpoint().toLowerCase()
+				})
+			},
+			event
+		);
+
+		return message(deleteForm, { status: res.status });
 	}
 };
