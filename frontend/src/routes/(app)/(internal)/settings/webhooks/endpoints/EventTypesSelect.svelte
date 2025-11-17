e65d98875b33e1e27467da214a61fdca005f4c13
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-11-17 15:04:30 +0100

    simpler and more robust event types select

--- frontend/src/routes/(app)/(internal)/settings/webhooks/endpoints/EventTypesSelect.svelte
+++ frontend/src/routes/(app)/(internal)/settings/webhooks/endpoints/EventTypesSelect.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import LoadingSpinner from '$lib/components/utils/LoadingSpinner.svelte';
+	import { URL_MODEL_MAP } from '$lib/utils/crud';
 	import { safeTranslate } from '$lib/utils/i18n';
 	import { formFieldProxy, type SuperForm } from 'sveltekit-superforms';
 
@@ -37,6 +38,47 @@
 
 	const { value, errors, constraints } = formFieldProxy(form, valuePath);
 
+	const getModelName = (option: Option): string | null => {
+		if (typeof option.value !== 'string' || !option.value.includes('.')) {
+			console.warn('Skipping option with invalid value:', option);
+			return null;
+		}
+		return option.value.split('.')[0];
+	};
+
+	const optionsByModel = (options: Option[]) => {
+		return options.reduce((acc: Record<string, Option[]>, option) => {
+			const modelName = getModelName(option);
+
+			if (modelName) {
+				(acc[modelName] = acc[modelName] || []).push(option);
+			}
+
+			return acc;
+		}, {});
+	};
+
+	const modelNameLocaleMap = (options: Option[]) => {
+		return Object.values(URL_MODEL_MAP).reduce((acc: Record<string, any>, model) => {
+			if (!model?.name) {
+				console.warn('Skipping model with no name:', model);
+				return acc;
+			}
+
+			if (acc?.[model.name]) {
+				// skip duplicates (e.g. policies)
+				return acc;
+			}
+
+			acc[model.name] = {
+				i18nName: model.localName,
+				options: optionsByModel(options)[model.name] || []
+			};
+
+			return acc;
+		}, {});
+	};
+
 	let classesDisabled = $derived((d: boolean) => (d ? 'opacity-50' : ''));
 </script>
 
@@ -59,23 +101,27 @@
 			</div>
 		{/if}
 	</div>
-	{#if options}
-		{#each options as option}
-			<div class="flex items-center mb-2 {classes} {classesDisabled(disabled)}">
-				<input
-					type="checkbox"
-					name={field}
-					id={option.value}
-					value={option.value}
-					checked={$value.includes(option.value)}
-					bind:group={$value}
-					{disabled}
-					class="mr-2"
-				/>
-				<label for={option.value}>{option.label}</label>
-			</div>
-		{:else}
-			<LoadingSpinner />
+	{#if options.length}
+		{#each Object.values(modelNameLocaleMap(options)).filter((e: Record<string, any>) => e?.options?.length) as model}
+			<p class="font-medium">{safeTranslate(model.i18nName)}</p>
+			{#each model.options as option}
+				{@const action = option.value.split('.')[1]}
+				<div class="flex items-center mb-2 {classes} {classesDisabled(disabled)}">
+					<input
+						type="checkbox"
+						name={field}
+						id={option.value}
+						value={option.value}
+						checked={$value?.includes(option.value)}
+						bind:group={$value}
+						{disabled}
+						class="mr-2"
+					/>
+					<label for={option.value}>{safeTranslate(action)}</label>
+				</div>
+			{/each}
 		{/each}
+	{:else}
+		<LoadingSpinner />
 	{/if}
 </div>
