a0ac1e1de9e18492c33df3eae5812d9189fa7847
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-11-18 14:40:11 +0100

    populate secret field on secret generation + nicer ui

--- frontend/src/lib/components/Forms/WebhookSecretGenerator.svelte
+++ frontend/src/lib/components/Forms/WebhookSecretGenerator.svelte
@@ -11,6 +11,8 @@
 
 	let { form, field = 'secret' }: Props = $props();
 
+	const formStore = form?.form;
+
 	let generatedSecret = $state('');
 
 	function generateSecret() {
@@ -21,28 +23,39 @@
 			.replace(/\//g, '_')
 			.replace(/=+$/, '');
 		generatedSecret = `whsec_${randomString}`;
+		$formStore.secret = generatedSecret;
 	}
 </script>
 
-<div class="flex flex-col items-center gap-2">
-	<div class="flex flex-row">
+<div class="w-full flex flex-col items-center gap-2">
+	<div class="w-full flex flex-row gap-2 items-center">
 		<TextField
 			{form}
 			{field}
 			type="password"
 			label={m.secret()}
 			helpText={m.webhookSecretHelpText()}
+			classesContainer="w-full"
+			autocomplete="new-password"
 		/>
-		<button type="button" onclick={generateSecret}>{m.generate()}</button>
+		<button
+			type="button"
+			class="btn px-2 py-1 preset-tonal-surface border border-surface-500"
+			onclick={generateSecret}>{m.generate()}</button
+		>
 	</div>
 	{#if generatedSecret}
-		<span class="flex flex-row gap-2 preset-tonal items-center card pl-2">
-			<pre class="max-w-[40ch] overflow-x-scroll">{generatedSecret}</pre>
-			<button
-				type="button"
-				class="btn px-2 py-1 preset-tonal-surface border border-surface-500 rounded-l-none"
-				use:copy={{ text: generatedSecret }}><i class="fa-solid fa-copy mr-2"></i>{m.copy()}</button
-			></span
-		>
+		<div class="flex flex-col card p-2 preset-tonal-warning w-full">
+			<p>{m.webhookSharedSecretShownOnce()}</p>
+			<span class="flex flex-row gap-2 preset-tonal items-center card pl-2">
+				<pre class="overflow-x-scroll">{generatedSecret}</pre>
+				<button
+					type="button"
+					class="btn px-2 py-1 preset-tonal-surface border border-surface-500 rounded-l-none"
+					use:copy={{ text: generatedSecret }}
+					><i class="fa-solid fa-copy mr-2"></i>{m.copy()}</button
+				></span
+			>
+		</div>
 	{/if}
 </div>
