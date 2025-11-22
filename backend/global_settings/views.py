9c4c7d79eadd11b7e6d9138a928e13acdb48dbe1
tchoumi313 <75470956+tchoumi313@users.noreply.github.com>
2025-11-22 01:42:07 +0100

    feat: mapping max depth setting implementation (#2888)
    * Mapping max depth setting implementation
    
    * update
    
    * fix wrong imports
    
    * ruff
    
    ---------
    
    Co-authored-by: eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>

--- backend/global_settings/views.py
+++ backend/global_settings/views.py
@@ -101,6 +101,7 @@ class GeneralSettingsViewSet(viewsets.ModelViewSet):
             "risk_matrix_swap_axes": False,
             "risk_matrix_flip_vertical": False,
             "risk_matrix_labels": "ISO",
+            "mapping_max_depth": 3,
         }
 
         settings, created = GlobalSettings.objects.get_or_create(name="general")
