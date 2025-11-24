259b4e1388900f9624cac330674f0bd3db73aac5
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-11-24 12:33:31 +0100

    remove debug log

--- frontend/src/routes/(app)/(internal)/settings/webhooks/+layout.server.ts
+++ frontend/src/routes/(app)/(internal)/settings/webhooks/+layout.server.ts
@@ -2,7 +2,6 @@ import type { LayoutServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-	console.log(locals?.featureflags?.outgoing_webhooks);
 	if (!locals?.featureflags?.outgoing_webhooks) {
 		redirect(302, '/settings');
 	}
