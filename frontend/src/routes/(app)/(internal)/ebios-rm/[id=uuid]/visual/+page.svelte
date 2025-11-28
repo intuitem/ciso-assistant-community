292f037658fb46d812ca10439c9b6c10e5c5d823
Abder <smimite@gmail.com>
2025-11-28 07:36:39 +0100

    feat: optimizations on ebios workshops titles and propagating the feared events (#2950)
    * add feared events to strategic scenarios
    
    * pass titles to ebios workshops
    
    * fixup
    
    * better detailview items
    
    * fixup

--- frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/visual/+page.svelte
+++ frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/visual/+page.svelte
@@ -3,6 +3,8 @@
 	import GraphExplorer from '$lib/components/DataViz/GraphExplorer.svelte';
 	import { pageTitle } from '$lib/utils/stores';
 	import { m } from '$paraglide/messages';
+	import { page } from '$app/state';
+	import Anchor from '$lib/components/Anchor/Anchor.svelte';
 
 	interface Props {
 		data: PageData;
@@ -19,10 +21,22 @@
 		'#fc8452',
 		'#ff006e',
 		'#3a86ff',
-		'#3d348b'
+		'#3d348b',
+		'#9a60b4'
 	];
 	const zoom = 1.5;
 </script>
+
+<div class="flex items-center justify-between mb-4">
+	<Anchor
+		breadcrumbAction="push"
+		href={`/ebios-rm/${page.params.id}`}
+		class="flex items-center space-x-2 text-primary-800 hover:text-primary-600"
+	>
+		<i class="fa-solid fa-arrow-left"></i>
+		<p>{m.goBackToEbiosRmStudy()}</p>
+	</Anchor>
+</div>
 
 <div class="bg-white shadow-sm flex overflow-x-auto">
 	<div class="w-full h-screen">
