7a121d30142eb5fc9873c95519cf241f7341bc25
Abderrahmane Smimite <smimite@gmail.com>
2025-12-03 20:25:14 +0100

    fixup

--- frontend/src/lib/utils/crud.ts
+++ frontend/src/lib/utils/crud.ts
@@ -534,8 +534,13 @@ export const URL_MODEL_MAP: ModelMap = {
 				disableDelete: true
 			},
 			{ field: 'assets', urlModel: 'vulnerabilities' },
-			{ field: 'assets', urlModel: 'risk-scenarios' },
-			{ field: 'assets', urlModel: 'quantitative-risk-scenarios' },
+			{ field: 'assets', urlModel: 'risk-scenarios', disableCreate: true, disableDelete: true },
+			{
+				field: 'assets',
+				urlModel: 'quantitative-risk-scenarios',
+				disableCreate: true,
+				disableDelete: true
+			},
 			{ field: 'assets', urlModel: 'solutions', disableCreate: true, disableDelete: true },
 			{ field: 'assets', urlModel: 'personal-data', disableCreate: true, disableDelete: true },
 			{ field: 'assets', urlModel: 'incidents' },
