de53b53f295adc69fdcd920dea9db333a51437be
Abder <smimite@gmail.com>
2025-12-05 12:25:38 +0100

    feat: allow changing domain for evidences and tasks while updating the children domains (#3008)

--- frontend/src/lib/components/Forms/ModelForm/EvidenceForm.svelte
+++ frontend/src/lib/components/Forms/ModelForm/EvidenceForm.svelte
@@ -71,22 +71,15 @@
 		allowedExtensions={'*'}
 	/>
 {/if}
-{#if !(initialData.applied_controls || initialData.requirement_assessments)}
-	<AutocompleteSelect
-		{form}
-		optionsEndpoint="folders"
-		field="folder"
-		pathField="path"
-		cacheLock={cacheLocks['folder']}
-		bind:cachedValue={formDataCache['folder']}
-		label={m.domain()}
-		hidden={initialData.applied_controls ||
-			initialData.requirement_assessments ||
-			initialData.folder}
-	/>
-{:else}
-	<HiddenInput {form} field="folder" />
-{/if}
+<AutocompleteSelect
+	{form}
+	optionsEndpoint="folders"
+	field="folder"
+	pathField="path"
+	cacheLock={cacheLocks['folder']}
+	bind:cachedValue={formDataCache['folder']}
+	label={m.domain()}
+/>
 {#if context !== 'edit'}
 	<TextField
 		{form}
