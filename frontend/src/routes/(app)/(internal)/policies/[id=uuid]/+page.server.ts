36ce8884eceae70594a9222b6e02cc5715f6408d
Abderrahmane Smimite <smimite@gmail.com>
2025-12-11 16:57:49 +0100

    code refactoring

--- frontend/src/routes/(app)/(internal)/policies/[id=uuid]/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/policies/[id=uuid]/+page.server.ts
@@ -1,12 +1,8 @@
 import { getModelInfo } from '$lib/utils/crud';
-import { loadDetail } from '$lib/utils/load';
-import { BASE_API_URL } from '$lib/utils/constants';
+import { loadDetail, loadValidationFlowFormData } from '$lib/utils/load';
 import type { PageServerLoad } from './$types';
 import { type Actions } from '@sveltejs/kit';
 import { nestedDeleteFormAction } from '$lib/utils/actions';
-import { modelSchema } from '$lib/utils/schemas';
-import { superValidate } from 'sveltekit-superforms';
-import { zod } from 'sveltekit-superforms/adapters';
 
 export const load: PageServerLoad = async (event) => {
 	const detailData = await loadDetail({
@@ -15,41 +11,12 @@ export const load: PageServerLoad = async (event) => {
 		id: event.params.id
 	});
 
-	// Validation flow form with preset data
-	const validationFlowSchema = modelSchema('validation-flows');
-	const validationFlowInitialData = {
-		folder: detailData.data.folder?.id || detailData.data.folder,
-		policies: [event.params.id],
-		ref_id: ''
-	};
-	const validationFlowForm = await superValidate(
-		validationFlowInitialData,
-		zod(validationFlowSchema),
-		{
-			errors: false
-		}
-	);
-	const validationFlowModel = getModelInfo('validation-flows');
-
-	// Populate selectOptions for validation flow model
-	const validationFlowSelectOptions: Record<string, any> = {};
-	if (validationFlowModel.selectFields) {
-		for (const selectField of validationFlowModel.selectFields) {
-			const url = `${BASE_API_URL}/validation-flows/${selectField.field}/`;
-			const response = await event.fetch(url);
-			if (response.ok) {
-				validationFlowSelectOptions[selectField.field] = await response.json().then((data) =>
-					Object.entries(data).map(([key, value]) => ({
-						label: value,
-						value: selectField.valueType === 'number' ? parseInt(key) : key
-					}))
-				);
-			} else {
-				console.error(`Failed to fetch data for ${selectField.field}: ${response.statusText}`);
-			}
-		}
-	}
-	validationFlowModel.selectOptions = validationFlowSelectOptions;
+	const { validationFlowForm, validationFlowModel } = await loadValidationFlowFormData({
+		event,
+		folderId: detailData.data.folder?.id || detailData.data.folder,
+		targetField: 'policies',
+		targetIds: [event.params.id]
+	});
 
 	return {
 		...detailData,
