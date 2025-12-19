133883af29394286f61c0f931800a8d2d24b13cf
Monsieur S <143810744+monsieurswag@users.noreply.github.com>
2025-12-19 19:08:27 +0100

    feat: Unify library model tables (#3040)
    * feat: Unify library model tables
    
    * Apply missing translations
    
    * Attempt to fix codefactor
    
    * Remove debug line
    
    * Fix wrong error message
    
    * Complete missing fields
    
    * Fix migration conflict
    
    * Formatter
    
    * Add missing is_published value set
    
    * Add LibraryFilteringLabel + some filters
    
    * Add library list view related dead code
    
    * Formatter
    
    * Simplest fix attempt
    
    * Real good fix for library loading
    
    * Better UI and make quick filters compatible with filter persistance
    
    * Fix functional tests
    
    * Fix function tests
    
    * Improve spanish translation
    
    * Naive test fix attempt
    
    * Remove mapping_suggested filter
    
    * Rename YAML filtering_labels key to labels
    
    * Remove wrong dead code
    
    * Add labels in detail view
    
    * Delete fake stored libraries on unload
    
    * Remove useless
    
    * Fix wrong snippet typing
    
    * Better exception handling

--- backend/library/management/commands/storelibraries.py
+++ backend/library/management/commands/storelibraries.py
@@ -3,9 +3,10 @@ from pathlib import Path
 
 import structlog
 from django.core.management.base import BaseCommand
+from django.db.models import Exists, OuterRef
 
 from ciso_assistant.settings import LIBRARIES_PATH
-from core.models import StoredLibrary
+from core.models import StoredLibrary, LoadedLibrary
 
 logger = structlog.getLogger(__name__)
 
@@ -37,5 +38,46 @@ class Command(BaseCommand):
                         filename=fname,
                         library=library,
                     )
-            except:
+            except Exception:
                 logger.error("Invalid library file", filename=fname)
+
+        invisible_libraries = (
+            LoadedLibrary.objects.filter(
+                builtin=False,
+            )
+            .annotate(
+                is_stored=Exists(StoredLibrary.objects.filter(urn=OuterRef("urn")))
+            )
+            .filter(is_stored=False)
+        )
+
+        # If a user loaded a custom library with an urn U and then deleted the stored library with an urn U
+        # Then the user would not be able to see the library in the frontend libraries list view as it fetches from the stored-libraries endpoint, making the library effectively "invisible".
+        # This fixes this problem by recreating a StoredLibrary for every "orphaned" custom library.
+        # The library will fail to be reloaded if the user ever unloads it (because content is empty(== dict()))
+        # In that case the user will have to delete it and reupload it again to get back a sane version of the StoredLibrary.
+        for library in invisible_libraries:
+            StoredLibrary.objects.create(
+                urn=library.urn,
+                locale=library.locale,
+                name=library.name,
+                description=library.description,
+                is_published=True,
+                annotation=library.annotation,
+                copyright=library.copyright,
+                provider=library.provider,
+                packager=library.packager,
+                publication_date=library.publication_date,
+                translations=library.translations,
+                builtin=library.builtin,
+                objects_meta=library.objects_meta,
+                dependencies=[
+                    dependency.urn for dependency in library.dependencies.all()
+                ],
+                version=library.version,
+                ref_id=library.ref_id,
+                is_loaded=True,
+                hash_checksum="0" * 64,
+                content=dict(),
+                autoload=True,
+            )
