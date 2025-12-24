8f4971c6d94f7b52f4bdad7339fd605a24df2ca2
Abder <smimite@gmail.com>
2025-12-24 16:03:36 +0100

    fix: make parent domain clearly mandatory on EE with better fallback for CE (#3060)
    * fix: make parent domain clearly mandatory with better fallback
    
    * update tests
    
    * fixup
    
    * revert tests changes
    
    * align forms
    
    * simpler workaround for tests
    
    * format
    
    * fix order
    
    * manage when it's disabled
    
    * different attempt

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1398,6 +1398,14 @@ class FolderWriteSerializer(BaseModelSerializer):
             )
         return value
 
+    def validate_parent_folder(self, value):
+        """
+        If parent_folder is empty or None, default to the root folder.
+        """
+        if not value:
+            return Folder.get_root_folder()
+        return value
+
 
 class FolderReadSerializer(BaseModelSerializer):
     path = PathField(read_only=True)
