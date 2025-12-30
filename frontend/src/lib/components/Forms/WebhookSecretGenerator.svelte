d1440b78a78bb6df878013d917c2b08d9bfae786
melinoix <titouan.ameline@gmail.com>
2025-12-30 09:50:38 +0100

    feat: enhance and fix webhook form (#3105)
    enhance and fix webhook form

--- frontend/src/lib/components/Forms/WebhookSecretGenerator.svelte
+++ frontend/src/lib/components/Forms/WebhookSecretGenerator.svelte
@@ -3,6 +3,10 @@
 	import { copy } from '@svelte-put/copy';
 	import type { SuperForm } from 'sveltekit-superforms';
 	import TextField from './TextField.svelte';
+	import { page } from '$app/stores';
+
+	import { getFlash } from 'sveltekit-flash-message';
+	const flash = getFlash(page);
 
 	interface Props {
 		field: string;
@@ -25,6 +29,17 @@
 		generatedSecret = `whsec_${randomString}`;
 		$formStore.secret = generatedSecret;
 	}
+
+	function copySecret() {
+		if (!generatedSecret) return;
+
+		navigator.clipboard.writeText(generatedSecret).then(() => {
+			flash.set({
+				type: 'success',
+				message: m.secretCopiedToClipboard()
+			});
+		});
+	}
 </script>
 
 <div class="w-full flex flex-col items-center gap-2">
@@ -40,7 +55,7 @@
 		/>
 		<button
 			type="button"
-			class="btn px-2 py-1 preset-tonal-surface border border-surface-500"
+			class="btn px-2 py-1 preset-tonal-surface border border-surface-500 transition-transform active:scale-95"
 			onclick={generateSecret}>{m.generate()}</button
 		>
 	</div>
@@ -48,12 +63,12 @@
 		<div class="flex flex-col card p-2 preset-tonal-warning w-full">
 			<p>{m.webhookSharedSecretShownOnce()}</p>
 			<span class="flex flex-row gap-2 preset-tonal items-center card pl-2">
-				<pre class="overflow-x-scroll">{generatedSecret}</pre>
+				<pre class="overflow-x-auto flex-grow break-all">{generatedSecret}</pre>
 				<button
 					type="button"
-					class="btn px-2 py-1 preset-tonal-surface border border-surface-500 rounded-l-none"
+					class="btn px-2 py-1 preset-tonal-surface border border-surface-500 rounded-l-none transition-transform active:scale-95"
 					use:copy={{ text: generatedSecret }}
-					><i class="fa-solid fa-copy mr-2"></i>{m.copy()}</button
+					onclick={copySecret}><i class="fa-solid fa-copy mr-2"></i>{m.copy()}</button
 				></span
 			>
 		</div>
