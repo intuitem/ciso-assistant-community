24d1c343e01da352c2ec8fc7e7949ccc16390443
Abder <smimite@gmail.com>
2026-01-09 23:04:24 +0100

    feat: user can decide to include enclaves or not on the organisation tree (#3160)
    * feat: user can decide to include enclaves or not on the organisation tree
    
    * fix coderabbit feedbacks
    
    * use slash for the path

--- frontend/src/routes/(app)/(internal)/x-rays/inspect/+page.server.ts
+++ frontend/src/routes/(app)/(internal)/x-rays/inspect/+page.server.ts
@@ -2,11 +2,12 @@ import { BASE_API_URL } from '$lib/utils/constants';
 import { m } from '$paraglide/messages';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ fetch }) => {
-	const endpoint = `${BASE_API_URL}/folders/org_tree/`;
+export const load = (async ({ fetch, url }) => {
+	const includeEnclaves = url.searchParams.get('include_enclaves') === 'true';
+	const endpoint = `${BASE_API_URL}/folders/org_tree/?include_enclaves=${includeEnclaves ? 'true' : 'false'}`;
 
 	const res = await fetch(endpoint);
 	const data = await res.json();
 
-	return { data, title: m.inspect() };
+	return { data, title: m.inspect(), includeEnclaves };
 }) satisfies PageServerLoad;
