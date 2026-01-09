451d5d5dc90321f8c9f41e27c075bc50915678a9
tchoumi313 <tchouminzikeubd@gmail.com>
2026-01-09 12:55:53 +0100

    checking for RBAC on target folder before updating

--- backend/core/views.py
+++ backend/core/views.py
@@ -758,6 +758,46 @@ class BaseModelViewSet(viewsets.ModelViewSet):
         dispatch_webhook_event(instance, "updated", serializer=serializer)
         return instance
 
+    def _validate_folder_change(self, request: Request, instance) -> None:
+        if not hasattr(instance, "folder"):
+            return
+        if "folder" not in request.data:
+            return
+        folder_value = request.data.get("folder")
+        if isinstance(folder_value, list):
+            if not folder_value:
+                return
+            folder_value = folder_value[0]
+        if isinstance(folder_value, dict):
+            folder_value = folder_value.get("id")
+        if not folder_value:
+            return
+        try:
+            target_id = UUID(str(folder_value))
+        except (TypeError, ValueError):
+            return
+        if instance.folder_id and str(instance.folder_id) == str(target_id):
+            return
+        target_folder = Folder.objects.filter(id=target_id).first()
+        if not target_folder:
+            return
+        model = self.model or instance.__class__
+        perm = Permission.objects.get(
+            codename=f"add_{model._meta.model_name}",
+            content_type__app_label=model._meta.app_label,
+            content_type__model=model._meta.model_name,
+        )
+        if not RoleAssignment.is_access_allowed(
+            user=request.user,
+            perm=perm,
+            folder=target_folder,
+        ):
+            raise PermissionDenied(
+                {
+                    "folder": "You do not have permission to create objects in this folder"
+                }
+            )
+
     def create(self, request: Request, *args, **kwargs) -> Response:
         self._process_request_data(request)
         if request.data.get("filtering_labels"):
@@ -773,6 +813,8 @@ class BaseModelViewSet(viewsets.ModelViewSet):
         return super().create(request, *args, **kwargs)
 
     def update(self, request: Request, *args, **kwargs) -> Response:
+        instance = self.get_object()
+        self._validate_folder_change(request, instance)
         # Experimental: process evidences on TaskTemplate update
         if request.data.get("evidences") and self.model == TaskTemplate:
             folder = Folder.objects.get(id=request.data.get("folder"))
@@ -808,6 +850,8 @@ class BaseModelViewSet(viewsets.ModelViewSet):
         return super().update(request, *args, **kwargs)
 
     def partial_update(self, request: Request, *args, **kwargs) -> Response:
+        instance = self.get_object()
+        self._validate_folder_change(request, instance)
         self._process_request_data(request)
         return super().partial_update(request, *args, **kwargs)
 
