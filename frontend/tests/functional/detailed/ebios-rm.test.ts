66e70ad78212eefef9bb2f1b1db8b46a9327edea
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2026-01-14 01:31:01 +0100

    Update ebios-rm.test.ts
    workaround for test issue

--- frontend/tests/functional/detailed/ebios-rm.test.ts
+++ frontend/tests/functional/detailed/ebios-rm.test.ts
@@ -183,7 +183,7 @@ test('ebios rm study', async ({
 			await page.getByTestId('form-input-qualifications').getByRole('textbox').click();
 			await page.getByRole('option', { name: 'Authenticity' }).click();
 			await page.getByRole('option', { name: 'Availability' }).click();
-			await page.getByTestId('save-button').click();
+			await page.getByTestId('save-button').click({ force: true }); // the menu hides the button, so forcing is the simplest solution
 			await expect(page.getByTestId('modal-title')).not.toBeVisible();
 			await page.getByTestId('add-button').click();
 			await page.getByTestId('form-input-name').click();
