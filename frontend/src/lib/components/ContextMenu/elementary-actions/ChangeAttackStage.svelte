801eade6cb83efedf1af0922f5f401e2f7bddee8
Abder <smimite@gmail.com>
2026-01-18 15:13:56 +0100

    feat: add delete option to standard context menu and dependencies upgrade (#3205)
    * chores(deps): pin devalue for transient dependencies issue
    
    * feat(ui): delete option on context menu (right click)
    
    * update bits-ui while at it
    
    * recovery of other frontend deps
    
    * attempt for nanoid
    
    * better width, contrast and consistent ordering for --

--- frontend/src/lib/components/ContextMenu/elementary-actions/ChangeAttackStage.svelte
+++ frontend/src/lib/components/ContextMenu/elementary-actions/ChangeAttackStage.svelte
@@ -59,13 +59,13 @@
 		<div class="flex items-center">{m.changeAttackStage()}</div>
 	</ContextMenu.SubTrigger>
 	<ContextMenu.SubContent
-		class="z-50 w-full max-w-[209px] outline-hidden card bg-white px-1 py-1.5 shadow-md cursor-default data-highlighted:bg-surface-50"
+		class="z-50 w-full min-w-[180px] max-w-[209px] outline-hidden card bg-white px-1 py-1.5 shadow-md border border-surface-200 cursor-default data-highlighted:bg-surface-50"
 		sideOffset={10}
 	>
 		{#each options as option}
 			<ContextMenu.Item
 				class="flex h-10 select-none items-center rounded-xs py-3 pl-3 pr-1.5 text-sm font-medium outline-hidden ring-0! ring-transparent! hover:bg-surface-50"
-				on:click={async () => await changeAttackStage(option.value)}
+				onclick={async () => await changeAttackStage(option.value)}
 			>
 				{safeTranslate(option.label)}
 			</ContextMenu.Item>
