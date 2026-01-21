507a87710be1d94c340d40370d369598345a3662
martinzerty <martinus.cremers@outlook.fr>
2026-01-21 16:37:37 +0100

    fix: better with linting

--- frontend/src/routes/(app)/(third-party)/requirement-assessments/[id=uuid]/edit/+page.svelte
+++ frontend/src/routes/(app)/(third-party)/requirement-assessments/[id=uuid]/edit/+page.svelte
@@ -411,33 +411,33 @@
 							><i class="fa-solid fa-circle-info"></i> {m.mappingInferenceHelpText()}</span
 						>
 						<div>
-						<ul class="list-disc ml-4 {!expandedInferences ? 'hidden' : ''}">
-							{#each Object.entries(mappingInference.sourceRequirementAssessments) as [source_urn, source_requirement_assessment]}
-								<li>
-									<p>
-										<a
-											class="anchor"
-											href="/requirement-assessments/{source_requirement_assessment.id}"
-										>
-											{source_requirement_assessment.str}
-										</a>
-									</p>
-									<p class="whitespace-pre-line py-1">
-										<span class="italic">{m.coverageColon()}</span>
-										<span class="badge h-fit">
-											{safeTranslate(source_requirement_assessment.coverage)}
-										</span>
-									</p>
-									<p class="whitespace-pre-line py-1">
-										<span class="italic">{m.framework()}</span>
-										<a
-											class="anchor badge h-fit"
-											href="/frameworks/{source_requirement_assessment.source_framework.id}"
-										>
-											{source_requirement_assessment.source_framework.name}
-										</a>
-									</p>
-									<p class="whitespace-pre-line py-1">
+							<ul class="list-disc ml-4 {!expandedInferences ? 'hidden' : ''}">
+								{#each Object.entries(mappingInference.sourceRequirementAssessments) as [source_urn, source_requirement_assessment]}
+									<li>
+										<p>
+											<a
+												class="anchor"
+												href="/requirement-assessments/{source_requirement_assessment.id}"
+											>
+												{source_requirement_assessment.str}
+											</a>
+										</p>
+										<p class="whitespace-pre-line py-1">
+											<span class="italic">{m.coverageColon()}</span>
+											<span class="badge h-fit">
+												{safeTranslate(source_requirement_assessment.coverage)}
+											</span>
+										</p>
+										<p class="whitespace-pre-line py-1">
+											<span class="italic">{m.framework()}</span>
+											<a
+												class="anchor badge h-fit"
+												href="/frameworks/{source_requirement_assessment.source_framework.id}"
+											>
+												{source_requirement_assessment.source_framework.name}
+											</a>
+										</p>
+										<p class="whitespace-pre-line py-1">
 											<span class="italic">{m.mapping()}</span>
 											<a
 												class="anchor badge h-fit"
@@ -447,34 +447,36 @@
 												{source_requirement_assessment.used_mapping_set?.name}
 											</a>
 										</p>
-									{#if source_requirement_assessment.is_scored}
+										{#if source_requirement_assessment.is_scored}
+											<p class="whitespace-pre-line py-1">
+												<span class="italic">{m.scoreSemiColon()}</span>
+												<span class="badge h-fit">
+													{safeTranslate(source_requirement_assessment.score)}
+												</span>
+											</p>
+										{/if}
 										<p class="whitespace-pre-line py-1">
-											<span class="italic">{m.scoreSemiColon()}</span>
-											<span class="badge h-fit">
-												{safeTranslate(source_requirement_assessment.score)}
+											<span class="italic">{m.suggestionColon()}</span>
+											<span
+												class="badge {classesText} h-fit"
+												style="background-color: {complianceResultColorMap[
+													mappingInference.result
+												]};"
+											>
+												{safeTranslate(mappingInference.result)}
 											</span>
 										</p>
-									{/if}
-									<p class="whitespace-pre-line py-1">
-										<span class="italic">{m.suggestionColon()}</span>
-										<span
-											class="badge {classesText} h-fit"
-											style="background-color: {complianceResultColorMap[mappingInference.result]};"
-										>
-											{safeTranslate(mappingInference.result)}
-										</span>
-									</p>
-									{#if mappingInference.annotation}
-										<p class="whitespace-pre-line py-1">
-											<span class="italic">{m.annotationColon()}</span>
-											{mappingInference.annotation}
-										</p>
-									{/if}
-								</li>
-							{/each}
-						</ul>
-					</div>
-					<button
+										{#if mappingInference.annotation}
+											<p class="whitespace-pre-line py-1">
+												<span class="italic">{m.annotationColon()}</span>
+												{mappingInference.annotation}
+											</p>
+										{/if}
+									</li>
+								{/each}
+							</ul>
+						</div>
+						<button
 							onclick={() => (expandedInferences = !expandedInferences)}
 							class="m-5 text-blue-800"
 							aria-expanded={expandedInferences}
@@ -488,8 +490,7 @@
 							{/if}
 							({Object.keys(mappingInference.sourceRequirementAssessments).length})
 						</button>
-											</div>
-
+					</div>
 				{/if}
 			{/if}
 		</div>
