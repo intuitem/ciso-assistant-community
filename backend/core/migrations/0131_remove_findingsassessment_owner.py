07673842dbd8a629adb71c302d9907c4edb93a6c
martinzerty <martinus.cremers@outlook.fr>
2026-01-22 10:57:07 +0100

    fix: lint migration

--- backend/core/migrations/0131_remove_findingsassessment_owner.py
+++ backend/core/migrations/0131_remove_findingsassessment_owner.py
@@ -4,14 +4,13 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('core', '0130_remove_organisationobjective_new_assigned_to_and_more'),
+        ("core", "0130_remove_organisationobjective_new_assigned_to_and_more"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='findingsassessment',
-            name='owner',
+            model_name="findingsassessment",
+            name="owner",
         ),
     ]
