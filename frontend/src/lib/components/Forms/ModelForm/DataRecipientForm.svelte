5d2e349f15e94af0507060ee261e4686b0f4d2e6
Abder <smimite@gmail.com>
2026-01-27 17:56:00 +0100

    fix: multiple improvements and simplifications on GDPR module (#3301)
    * fix: gdpr translation issue and clickable items that shouldn't be
    
    * wip
    
    * fixup
    
    * fixup
    
    * fixup
    
    * manage uniqueness
    
    * fixup
    
    * cleanup
    
    * chore: squash migrations
    
    * chore: ruff format
    
    * Revert "chore: ruff format"
    
    This reverts commit 1b85bfa03d416454efe28cf00ca08c9ca527901e.
    
    * Revert "chore: squash migrations"
    
    This reverts commit 486afaa620d6392ca7878caf0bc5bf3abbdf9cca.
    
    * squash migrations manually
    
    ---------
    
    Co-authored-by: Nassim Tabchiche <n.tabchiche2@gmail.com>

--- frontend/src/lib/components/Forms/ModelForm/DataRecipientForm.svelte
+++ frontend/src/lib/components/Forms/ModelForm/DataRecipientForm.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import AutocompleteSelect from '../AutocompleteSelect.svelte';
 	import TextField from '$lib/components/Forms/TextField.svelte';
-	import TextArea from '$lib/components/Forms/TextArea.svelte';
-	import Select from '../Select.svelte';
+	import MarkdownField from '$lib/components/Forms/MarkdownField.svelte';
+	import Dropdown from '$lib/components/Dropdown/Dropdown.svelte';
 	import type { SuperValidated } from 'sveltekit-superforms';
 	import type { ModelInfo, CacheLock } from '$lib/utils/types';
 	import * as m from '$paraglide/messages.js';
@@ -24,13 +24,6 @@
 	}: Props = $props();
 </script>
 
-<TextField
-	{form}
-	field="ref_id"
-	label={m.refId()}
-	cacheLock={cacheLocks['ref_id']}
-	bind:cachedValue={formDataCache['ref_id']}
-/>
 <AutocompleteSelect
 	{form}
 	field="category"
@@ -48,3 +41,27 @@
 	label={m.processing()}
 	hidden={initialData.processing}
 />
+
+<Dropdown open={false} style="hover:text-primary-700" icon="fa-solid fa-list" header={m.more()}>
+	<TextField
+		{form}
+		field="ref_id"
+		label={m.refId()}
+		cacheLock={cacheLocks['ref_id']}
+		bind:cachedValue={formDataCache['ref_id']}
+	/>
+	<TextField
+		{form}
+		field="name"
+		label={m.name()}
+		cacheLock={cacheLocks['name']}
+		bind:cachedValue={formDataCache['name']}
+	/>
+	<MarkdownField
+		{form}
+		field="description"
+		label={m.description()}
+		cacheLock={cacheLocks['description']}
+		bind:cachedValue={formDataCache['description']}
+	/>
+</Dropdown>
