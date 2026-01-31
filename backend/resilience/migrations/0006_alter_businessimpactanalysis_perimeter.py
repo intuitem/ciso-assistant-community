d5ed16de28697cd7eda4855651c74764986f26cb
Abderrahmane Smimite <smimite@gmail.com>
2026-01-31 08:57:09 +0100

    fix migration

--- backend/resilience/migrations/0006_alter_businessimpactanalysis_perimeter.py
+++ backend/resilience/migrations/0006_alter_businessimpactanalysis_perimeter.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(
                 blank=True,
                 null=True,
-                on_delete=django.db.models.deletion.CASCADE,
+                on_delete=django.db.models.deletion.SET_NULL,
                 to="core.perimeter",
                 verbose_name="Perimeter",
             ),
