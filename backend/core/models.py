93a4b2464f757879867b33bd6dc4958e74566450
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2026-02-13 15:21:15 +0100

    fix: revert useless changes

--- backend/core/models.py
+++ backend/core/models.py
@@ -821,13 +821,6 @@ class LibraryUpdater:
                         for k, v in requirement_node.items()
                         if k not in ["urn", "depth", "reference_controls", "threats"]
                     }
-                    if (
-                        "parent_urn" in requirement_node_dict
-                        and requirement_node_dict["parent_urn"]
-                    ):
-                        requirement_node_dict["parent_urn"] = requirement_node_dict[
-                            "parent_urn"
-                        ].lower()
                     requirement_node_dict["order_id"] = order_id
                     order_id += 1
 
@@ -840,27 +833,11 @@ class LibraryUpdater:
                             requirement_node_object
                         )
                     else:
-                        existing_node = RequirementNode.objects.filter(urn=urn).first()
-                        if (
-                            existing_node
-                            and existing_node.framework_id != new_framework.id
-                        ):
-                            raise ValidationError("requirementNodeUrnConflict")
-                        requirement_node_object, _ = (
-                            RequirementNode.objects.update_or_create(
-                                urn=urn,
-                                defaults={
-                                    "framework": new_framework,
-                                    **self.referential_object_dict,
-                                    **requirement_node_dict,
-                                },
-                                create_defaults={
-                                    "framework": new_framework,
-                                    **self.referential_object_dict,
-                                    **self.i18n_object_dict,
-                                    **requirement_node_dict,
-                                },
-                            )
+                        requirement_node_object = RequirementNode.objects.create(
+                            urn=urn,
+                            framework=new_framework,
+                            **self.referential_object_dict,
+                            **requirement_node_dict,
                         )
                         for ca in compliance_assessments:
                             requirement_assessment_objects_to_create.append(
