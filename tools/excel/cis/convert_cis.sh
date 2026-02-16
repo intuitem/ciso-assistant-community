8f9c8d038fc074983a18f01ba1fcd84a68a5b6e5
Tarkadia <161824332+tarkadia@users.noreply.github.com>
2026-02-16 17:28:09 +0100

    Fix CIS v8 Automatic Scripts

--- tools/excel/cis/convert_cis.sh
+++ tools/excel/cis/convert_cis.sh
@@ -6,9 +6,9 @@ then
 fi
 
 echo "➡️ [STEP 1] Extract Excel file data..."
-python prep_cis.py $1 $2 || { echo "❌ Step 1 failed"; exit 1; }
-echo "➡️ [STEP 2] Convert extracted Excel file to v2..."
-python ../../convert_v1_to_v2.py cis-controls-v8.xlsx || { echo "❌ Step 2 failed"; exit 1; }
-echo "➡️ [STEP 3] Convert Excel v2 file to YAML..."
-python ../../convert_library_v2.py cis-controls-v8_new.xlsx || { echo "❌ Step 3 failed"; exit 1; }
-echo "✅ Resulting file is available at cis-controls-v8_new.yaml"
+python prep_cis_v2.py $1 -p $2 || { echo "❌ Step 1 failed"; exit 1; }
+echo ""
+echo "➡️ [STEP 2] Convert Excel v2 file to YAML..."
+python ../../convert_library_v2.py cis-controls-v8-v2.xlsx || { echo "❌ Step 2 failed"; exit 1; }
+echo ""
+echo "✅ Resulting file is available at cis-controls-v8-v2.yaml"
