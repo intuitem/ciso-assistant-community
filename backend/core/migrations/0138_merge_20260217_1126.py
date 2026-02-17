2882c1d9501356b184c8bedcf48bf7266cb4afc3
tchoumi313 <tchouminzikeubd@gmail.com>
2026-02-17 12:51:55 +0100

    formated

--- backend/core/migrations/0138_merge_20260217_1126.py
+++ backend/core/migrations/0138_merge_20260217_1126.py
@@ -4,11 +4,9 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('core', '0137_add_auto_sync_to_risk_compliance_assessments'),
-        ('core', '0137_add_threats_to_finding'),
+        ("core", "0137_add_auto_sync_to_risk_compliance_assessments"),
+        ("core", "0137_add_threats_to_finding"),
     ]
 
-    operations = [
-    ]
+    operations = []
