60a72b20beb135bf7f7be274b71d0ff350410a46
martinzerty <martinus.cremers@outlook.fr>
2026-02-18 09:59:33 +0100

    fix: raise error when related object to the vulnerability does not exist in the corresponding domain

--- backend/data_wizard/views.py
+++ backend/data_wizard/views.py
@@ -2469,6 +2469,15 @@ class LoadFileView(APIView):
                     logger.warning(f"Error creating control {control_name}: {str(e)}")
 
         return control_mapping
+    
+    def _add_missing_reference_for_vulnerability(self, results, record, object_name, name, folder_id):
+        results["failed"] +=1
+        results["errors"].append(
+            {
+                "record": record,
+                "details": f"No object \'{object_name}\' named \'{name}\' exists in folder {folder_id}."
+            }
+        )
 
     def _process_vulnerability_record(self, records, folder_id, request):
         results = {"successfull": 0, "failed": 0, "errors": []}
@@ -2485,41 +2494,35 @@ class LoadFileView(APIView):
                     record.get("severity", ""), -1
                 )
 
+                applied_controls = []
                 if record.get("applied_controls"):
                     applied_controls = [
-                        AppliedControl.objects.filter(name=ap_name, folder_id=folder_id)
-                        .first()
-                        .id
+                        ap.id if (ap := AppliedControl.objects.filter(name=ap_name, folder_id=folder_id).first()) else self._add_missing_reference_for_vulnerability(results, record, "applied_controls", ap_name, folder_id)
                         for ap_name in record.get("applied_controls", "").split("\n")
                     ]
                 else:
                     applied_controls = []
 
+
+                assets = []
                 if record.get("assets"):
                     assets = [
-                        Asset.objects.filter(name=asset_name, folder_id=folder_id)
-                        .first()
-                        .id
+                        asset.id if (asset := Asset.objects.filter(name=asset_name, folder_id=folder_id).first()) else self._add_missing_reference_for_vulnerability(results, record, "assets", asset_name, folder_id)
                         for asset_name in record.get("assets", "").split("\n")
                     ]
-                else:
-                    assets = []
 
                 filtering_labels = []
                 if record.get("filtering_labels"):
                     for filter_name in record.get("filtering_labels", "").split("\n"):
                         filtering_label, _ = FilteringLabel.objects.get_or_create(
-                            label=filter_name
+                            label=filter_name,
+                            folder=folder_id
                         )
                         filtering_labels.append(filtering_label.id)
 
                 if record.get("security_exceptions"):
                     security_exceptions = [
-                        SecurityException.objects.filter(
-                            name=se_name, folder_id=folder_id
-                        )
-                        .first()
-                        .id
+                        security_exception.id if (security_exception := SecurityException.objects.filter(name=se_name, folder_id=folder_id).first()) else self._add_missing_reference_for_vulnerability(results, record, "security_exceptions", se_name, folder_id)
                         for se_name in record.get("security_exceptions", "").split("\n")
                     ]
                 else:
