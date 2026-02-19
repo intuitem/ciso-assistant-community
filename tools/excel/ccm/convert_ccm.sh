0c86ee558bfbeda4509769e71413d601a58c379d
Tarkadia <161824332+tarkadia@users.noreply.github.com>
2026-02-19 18:47:58 +0100

    Small Fix

--- tools/excel/ccm/convert_ccm.sh
+++ tools/excel/ccm/convert_ccm.sh
@@ -13,4 +13,5 @@ python3 convert_ccm_v2.py $1 -p $2 || { echo "❌ Step 1 failed"; exit 1; }
 echo ""
 echo "➡️ [STEP 2] Convert Excel v2 file to YAML..."
 python3 ../../convert_library_v2.py $filenamev2 || { echo "❌ Step 2 failed"; exit 1; }
+echo ""
 echo "✅ Resulting file is available at $filenamev2_YAML"
