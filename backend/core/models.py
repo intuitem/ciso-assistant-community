03c8cfb8d70a30385a7d6fde2d797334e3231068
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2026-02-19 14:49:51 +0100

    chore: fix comment

--- backend/core/models.py
+++ backend/core/models.py
@@ -7716,7 +7716,7 @@ class TaskTemplate(NameDescriptionMixin, FolderMixin):
         if self.schedule and "end_date" in self.schedule:
             end_date = self.schedule["end_date"]
             end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
-            # Delete TaskNode instances whose original scheduled date is after the end date
+            # Delete TaskNode instances whose due date is after the end date
             TaskNode.objects.filter(
                 task_template=self, due_date__gt=end_date
             ).delete()
