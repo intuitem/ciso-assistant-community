c626bca2ca758508ba17adda1d5cabff9e47c671
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2026-02-19 13:50:15 +0100

    Update tprm.test.ts
    try fix test failure

--- frontend/tests/functional/detailed/tprm.test.ts
+++ frontend/tests/functional/detailed/tprm.test.ts
@@ -157,12 +157,15 @@ test('user can create representatives, solutions and entity assessments inside e
 
 	await test.step('check that third parties overview cards can be flipped', async () => {
 		const cards = page.getByTestId('cards-list').locator('div');
-		await cards.first().getByTestId('flip-button-front').click();
-		await expect(cards.first()).toHaveClass(/rotate-x-180/);
+		const firstCard = cards.first();
+		await expect(firstCard).toBeVisible();
+		await expect(firstCard.getByTestId('flip-button-front')).toBeEnabled();
+		await firstCard.getByTestId('flip-button-front').click();
+		await expect(firstCard).toHaveClass(/rotate-x-180/);
 
 		// flip back to front
-		await cards.first().getByTestId('flip-button-back').click();
-		await expect(cards.first()).not.toHaveClass(/rotate-x-180/);
+		await firstCard.getByTestId('flip-button-back').click();
+		await expect(firstCard).not.toHaveClass(/rotate-x-180/);
 	});
 });
 
