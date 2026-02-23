18af6906eed94c1d73d6274b960d5cc0e5979a65
Abderrahmane Smimite <smimite@gmail.com>
2026-02-23 13:32:22 +0100

    fix coderabbit feedback

--- frontend/src/lib/components/SideBar/SideBarFooter.svelte
+++ frontend/src/lib/components/SideBar/SideBarFooter.svelte
@@ -93,9 +93,8 @@
 					>
 						<a
 							href="/my-profile"
-							onclick={(e) => {
+							onclick={() => {
 								openState = false;
-								window.location.href = e.target.href;
 							}}
 							class="unstyled cursor-pointer flex items-center gap-2 w-full px-4 py-2.5 text-left text-sm hover:bg-gray-100 disabled:text-gray-500 text-gray-800"
 							data-testid="profile-button"
