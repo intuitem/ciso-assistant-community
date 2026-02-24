f2c5ac74a9e18b44338752ffc383315e55df5a6e
martinzerty <martinus.cremers@outlook.fr>
2026-02-24 16:06:15 +0100

    Revert "sort measures by assets list"
    This reverts commit c87740f55a9fec2ac68904cef1320f9b22211403.

--- frontend/src/lib/components/ModelTable/handler.ts
+++ frontend/src/lib/components/ModelTable/handler.ts
@@ -20,7 +20,7 @@ export const loadTableData = async ({
 }: LoadTableDataParams) => {
 	const url = new URL(endpoint, window.location.origin);
 	const params = new URLSearchParams(url.search);
-	const newParams = getParams(state, URLModel);
+	const newParams = getParams(state);
 
 	newParams.forEach((value, key) => params.append(key, value));
 	url.search = params.toString();
@@ -57,7 +57,7 @@ export const loadTableData = async ({
 	});
 };
 
-const getParams = ({ offset, rowsPerPage, search, sort, filters }: State, URLModel: urlModel) => {
+const getParams = ({ offset, rowsPerPage, search, sort, filters }: State) => {
 	const params = new URLSearchParams();
 	params.set('offset', offset.toString() ?? '0');
 	params.set('limit', rowsPerPage.toString() ?? '10');
@@ -65,9 +65,7 @@ const getParams = ({ offset, rowsPerPage, search, sort, filters }: State, URLMod
 		params.set('search', search);
 	}
 	if (sort) {
-		const mappedOrderBy =
-			URLModel === 'applied-controls' && sort.orderBy === 'assets' ? 'assets__name' : sort.orderBy;
-		params.set('ordering', `${sort.direction === 'desc' ? '-' : ''}${mappedOrderBy}`);
+		params.set('ordering', `${sort.direction === 'desc' ? '-' : ''}${sort.orderBy}`);
 	}
 	if (filters) {
 		for (const filter of filters) {
