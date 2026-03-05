42d0b72231529776d9f91dcbf033f6b1d77a6d23
Abder <smimite@gmail.com>
2026-03-05 06:30:28 +0100

    feat(deploy): support AWS_LOCATION for object-storage based deployments (#3605)

--- backend/ciso_assistant/settings.py
+++ backend/ciso_assistant/settings.py
@@ -219,6 +219,7 @@ if USE_S3:
     if AWS_S3_REGION_NAME:
         logger.info("AWS_S3_REGION_NAME: %s", AWS_S3_REGION_NAME)
 
+    AWS_LOCATION = os.getenv("AWS_LOCATION", "")
     AWS_S3_FILE_OVERWRITE = False
 
 else:
