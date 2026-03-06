ae051644fd4a28c006b48c73a31c5fa1d57bd286
monsieurswag <mael.joumier@intuitem.com>
2026-03-06 05:56:12 +0100

    fix: Date formatting not using user's locale

--- frontend/src/lib/utils/datetime.ts
+++ frontend/src/lib/utils/datetime.ts
@@ -1,14 +1,53 @@
-export function formatDateOrDateTime(isoString: string, locale = 'en-US'): string {
+import type { Locale } from '$lib/utils/locales';
+import { getCookie } from '$lib/utils/cookies';
+import { browser } from '$app/environment';
+
+const dateFormatLocales: Record<Locale, string> = {
+	fr: 'fr-FR',
+	en: 'en-US',
+	ar: 'ar-SA',
+	pt: 'pt-PT',
+	es: 'es-ES',
+	nl: 'nl-NL',
+	de: 'de-DE',
+	it: 'it-IT',
+	pl: 'pl-PL',
+	ro: 'ro-RO',
+	hi: 'hi-IN',
+	ur: 'ur-PK',
+	cs: 'cs-CZ',
+	sv: 'sv-SE',
+	id: 'id-ID',
+	da: 'da-DK',
+	hu: 'hu-HU',
+	uk: 'uk-UA',
+	el: 'el-GR',
+	tr: 'tr-TR',
+	hr: 'hr-HR',
+	zh: 'zh-CN',
+	lt: 'lt-LT'
+};
+
+export function formatDateOrDateTime(isoString: string, locale: Locale | null = null): string {
 	if (typeof isoString !== 'string') {
 		return isoString;
 	}
-	const hasTime = isoString.includes('T');
 
+	if (locale === null) {
+		if (browser) {
+			locale = (getCookie('LOCALE') as Locale) ?? null;
+		}
+	}
+	locale ??= 'en';
+
+	const dateFormatLocale = dateFormatLocales[locale] ?? 'en-US';
 	const date = new Date(isoString);
 
+	const hasTime = isoString.includes('T');
+
 	if (hasTime) {
-		return date.toLocaleString(locale);
+		return date.toLocaleString(dateFormatLocale);
 	} else {
-		return date.toLocaleDateString(locale);
+		return date.toLocaleDateString(dateFormatLocale);
 	}
 }
