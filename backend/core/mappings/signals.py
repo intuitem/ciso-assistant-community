24fbb7d8a8d4110d7fc9afa71834fd5f9dbcf740
Nassim Tabchiche <n.tabchiche2@gmail.com>
2026-03-09 09:13:47 +0100

    invalidate cache only if rms or frameworks in lib content

--- backend/core/mappings/signals.py
+++ backend/core/mappings/signals.py
@@ -1,11 +1,28 @@
 from django.db import transaction
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from core.models import StoredLibrary
+from core.models import StoredLibrary, Framework
 from core.mappings.engine import engine
+
 
 @receiver(post_save, sender=StoredLibrary)
 @receiver(post_delete, sender=StoredLibrary)
 def update_mapping_engine_cache(sender, instance, **kwargs):
-    # Reload RMS data whenever a library is saved (potentially loaded/unloaded) or deleted.
+    # Reload RMS data whenever a relevant library is saved (potentially loaded/unloaded) or deleted.
+    content = instance.content
+    if isinstance(content, dict):
+        if any(
+            key in content
+            for key in [
+                "requirement_mapping_set",
+                "requirement_mapping_sets",
+                "framework",
+                "frameworks",
+            ]
+        ):
+            transaction.on_commit(engine.reload_cache)
+
+
+@receiver(post_delete, sender=Framework)
+def update_mapping_engine_cache_on_framework_delete(sender, instance, **kwargs):
     transaction.on_commit(engine.reload_cache)
