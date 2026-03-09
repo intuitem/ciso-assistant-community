24801e8f62c16adf358714c72cca88cdc59b73d6
Nassim Tabchiche <n.tabchiche2@gmail.com>
2026-03-09 09:00:37 +0100

    load rms data on transaction commit

--- backend/core/mappings/signals.py
+++ backend/core/mappings/signals.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from core.models import StoredLibrary
@@ -7,4 +8,4 @@ from core.mappings.engine import engine
 @receiver(post_delete, sender=StoredLibrary)
 def update_mapping_engine_cache(sender, instance, **kwargs):
     # Reload RMS data whenever a library is saved (potentially loaded/unloaded) or deleted.
-    engine.load_rms_data()
+    transaction.on_commit(lambda: engine.load_rms_data())
