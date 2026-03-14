f95c1f7563e1c0fe800dd7cdd53cc6ad2b20fb14
Abder <smimite@gmail.com>
2026-03-14 12:45:36 +0100

    feat(pro): allow customizing emails and word report templates (#3662)
    * feat(pro): allow customizing emails and word report templates
    
    * fixup
    
    * good progress
    
    * fixup
    
    * moving html templates to markdown in yaml
    
    * manage templates name and desc i18n
    
    * improve styling
    
    * fix webhook issue on enterprise
    
    * recover webhook page styling
    
    * update tests
    
    * wip
    
    * format
    
    * uniqueness check with fields_to_check
    
    * coderabbit feedback
    
    * fix migration
    
    * coderabbit feedback
    
    * coderabbit fixes
    
    * fixup
    
    * add markdown package
    
    ---------
    
    Co-authored-by: eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>

---
+++ enterprise/frontend/src/routes/fe-api/custom-word-templates/+server.ts
@@ -0,0 +1,35 @@
+import { BASE_API_URL } from '$lib/utils/constants';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ fetch, url }) => {
+	const endpoint = `${BASE_API_URL}/custom-word-templates/${url.search}`;
+	const res = await fetch(endpoint);
+	const responseData = await res.text();
+
+	return new Response(responseData, {
+		status: res.status,
+		headers: {
+			'Content-Type': res.headers.get('Content-Type') ?? 'application/json'
+		}
+	});
+};
+
+export const POST: RequestHandler = async ({ fetch, request }) => {
+	const body = await request.text();
+	const endpoint = `${BASE_API_URL}/custom-word-templates/`;
+	const res = await fetch(endpoint, {
+		method: 'POST',
+		body,
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	const responseData = await res.text();
+
+	return new Response(responseData, {
+		status: res.status,
+		headers: {
+			'Content-Type': res.headers.get('Content-Type') ?? 'application/json'
+		}
+	});
+};
