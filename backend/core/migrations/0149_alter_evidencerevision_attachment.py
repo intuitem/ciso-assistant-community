53d6a69606cd461ae422c09efd9d4037997d0676
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2026-03-17 23:08:57 +0100

    Fix migration issue in main

--- backend/core/migrations/0149_alter_evidencerevision_attachment.py
+++ backend/core/migrations/0149_alter_evidencerevision_attachment.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("core", "0146_organisationobjective_closing_date_and_more"),
+        ("core", "0148_alter_asset_dora_licenced_activity"),
     ]
 
     operations = [
