b77d4d883e8dfee39a14c411331acd0cc3929a7c
Nassim Tabchiche <n.tabchiche2@gmail.com>
2026-03-26 10:13:29 +0100

    update tests

--- backend/app_tests/api/test_api_requirement_assessments.py
+++ backend/app_tests/api/test_api_requirement_assessments.py
@@ -154,6 +154,9 @@ class TestRequirementAssessmentsAuthenticated:
                     "implementation_groups": RequirementNode.objects.all()[
                         0
                     ].implementation_groups,
+                    "display_mode": RequirementNode.objects.all()[
+                        0
+                    ].display_mode,
                     "parent_requirement": {
                         "str": RequirementNode.objects.all()[0].parent_requirement.get(
                             "str"
@@ -303,6 +306,9 @@ class TestRequirementAssessmentsAuthenticated:
                     "implementation_groups": RequirementNode.objects.all()[
                         0
                     ].implementation_groups,
+                    "display_mode": RequirementNode.objects.all()[
+                        0
+                    ].display_mode,
                     "parent_requirement": {
                         "str": RequirementNode.objects.all()[0].parent_requirement.get(
                             "str"
