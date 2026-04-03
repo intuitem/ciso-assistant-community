d42ce785cc1a8d782d262e30a21331379c446de6
Abder <smimite@gmail.com>
2026-04-03 12:38:44 +0200

    fix: applied controls layout when reference control is attached (#3817)

--- frontend/src/routes/(app)/(internal)/applied-controls/[id=uuid]/+layout.server.ts
+++ frontend/src/routes/(app)/(internal)/applied-controls/[id=uuid]/+layout.server.ts
@@ -12,16 +12,18 @@ export const load: LayoutServerLoad = async (event) => {
 		id: event.params.id
 	});
 
-	const endpoint = `${BASE_API_URL}/applied-controls/${event.params.id}/sync-to-reference-control/?dry_run=true`;
-	const response = await event.fetch(endpoint, {
-		method: 'POST'
-	});
-
 	let dryRunData: [string, string][] = [];
-	if (response.ok) {
-		dryRunData = await response.json();
-	} else {
-		console.error('Failed to get dry run data.');
+	if (data.data.reference_control) {
+		const endpoint = `${BASE_API_URL}/applied-controls/${event.params.id}/sync-to-reference-control/?dry_run=true`;
+		const response = await event.fetch(endpoint, {
+			method: 'POST'
+		});
+
+		if (response.ok) {
+			dryRunData = await response.json();
+		} else {
+			console.error('Failed to get dry run data.');
+		}
 	}
 
 	data.dryRunData = dryRunData;
