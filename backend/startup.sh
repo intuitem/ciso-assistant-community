c432f2f1f84cac146d95d6ba5030d98b03ef64a3
Axxiar <github.spearmint788@passmail.net>
2026-04-10 12:25:01 +0200

    experiment without activating venv

--- backend/startup.sh
+++ backend/startup.sh
@@ -15,10 +15,10 @@ while ! python manage.py showmigrations iam >/dev/null; do
   echo "database not ready; waiting"
   sleep 15
 done
-uv run python manage.py migrate --settings="${DJANGO_SETTINGS_MODULE}"
-uv run python manage.py storelibraries --settings="${DJANGO_SETTINGS_MODULE}"
+python manage.py migrate --settings="${DJANGO_SETTINGS_MODULE}"
+python manage.py storelibraries --settings="${DJANGO_SETTINGS_MODULE}"
 if [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
-  uv run python manage.py createsuperuser --noinput --settings="${DJANGO_SETTINGS_MODULE}"
+  python manage.py createsuperuser --noinput --settings="${DJANGO_SETTINGS_MODULE}"
 fi
 
 # Set default values for Gunicorn configuration
