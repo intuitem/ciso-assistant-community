df6174b0af038da054e4d9d860b20d338974fc73
Nassim Tabchiche <n.tabchiche2@gmail.com>
2026-04-17 18:21:14 +0200

    fix mapping functional tests

--- frontend/tests/functional/detailed/mappings.test.ts
+++ frontend/tests/functional/detailed/mappings.test.ts
@@ -7,18 +7,13 @@ import { expect, test, TestContent } from '../../utils/test-utils.js';
 const vars = TestContent.generateTestVars();
 const testObjectsData: { [k: string]: any } = TestContent.itemBuilder(vars);
 const FOLDER_WORKAROUND_SUFFIX = ' foo';
-const PERIMETER_WORKAROUND_SUFFIX = ' bar';
 
-test('user can import mappings', async ({
+test('user can import required libraries and create required objects', async ({
 	page,
 	logedPage,
 	foldersPage,
-	perimetersPage,
-	mappingsPage,
 	librariesPage
 }) => {
-	const importMappingBtn = page.getByTestId('import-button');
-
 	await test.step('create required folder', async () => {
 		await foldersPage.goto();
 		await foldersPage.hasUrl();
@@ -33,44 +28,32 @@ test('user can import mappings', async ({
 		});
 	});
 
-	await test.step('create required perimeter', async () => {
-		await perimetersPage.goto();
-		await perimetersPage.hasUrl();
-		await perimetersPage.createItem({
-			name: vars.perimeterName,
-			description: vars.description,
-			folder: vars.folderName,
-			ref_id: 'R.1234',
-			lc_status: 'Production'
-		});
-		await perimetersPage.createItem({
-			name: vars.perimeterName + PERIMETER_WORKAROUND_SUFFIX,
-			description: vars.description,
-			folder: vars.folderName,
-			ref_id: 'R.12345',
-			lc_status: 'Production'
-		});
-	});
-
-	await test.step('import mapping nist-csf-1.1 -> iso27001:2022', async () => {
-		await mappingsPage.goto();
-		await mappingsPage.hasUrl();
-		await importMappingBtn.click();
+	await test.step('import iso27001-2022 and csf-1.1', async () => {
+		await librariesPage.goto();
 		await librariesPage.hasUrl();
-		await librariesPage.importLibrary('Mapping from nist-csf-1.1 to iso27001-2022');
+		await librariesPage.importLibrary(
+			'International standard ISO/IEC 27001:2022',
+			'urn:intuitem:risk:framework:iso27001-2022'
+		);
+		await librariesPage.goto();
+		await librariesPage.hasUrl();
+		await librariesPage.importLibrary('NIST CSF v1.1', 'urn:intuitem:risk:library:nist-csf-1.1');
 	});
 });
 
-test('user can map csf-1.1 audit to a new iso27001-2022 audit', async ({
+test('user can map iso27001-2022 audit to a new csf-1.1 audit', async ({
 	page,
 	logedPage,
-	mappingsPage,
 	complianceAssessmentsPage
 }) => {
 	const IDAM1Score = {
 		ratio: 0.66,
 		progress: '75',
-		value: 3
+		value: 1
+	};
+
+	const OrgContextScore = {
+		value: 75 / 5 + 1
 	};
 
 	const applyMappingButton = page.getByTestId('apply-mapping-button');
@@ -80,57 +63,60 @@ test('user can map csf-1.1 audit to a new iso27001-2022 audit', async ({
 		{ name: 'name', type: FormFieldType.TEXT },
 		{ name: 'description', type: FormFieldType.TEXT },
 		{ name: 'folder', type: FormFieldType.SELECT_AUTOCOMPLETE },
-		{ name: 'perimeter', type: FormFieldType.SELECT_AUTOCOMPLETE },
 		{ name: 'framework', type: FormFieldType.SELECT_AUTOCOMPLETE }
 	]);
 
-	await test.step('create and score nist-csf-1.1 audit', async () => {
+	await test.step('create and score iso27001-2022 audit', async () => {
 		await complianceAssessmentsPage.goto();
 		await complianceAssessmentsPage.hasUrl();
-		await complianceAssessmentsPage.createItem(
-			testObjectsData.complianceAssessmentsPage.build,
-			testObjectsData.complianceAssessmentsPage.dependency
-		);
+		await complianceAssessmentsPage.createItem({
+			name: vars.assessmentName,
+			description: vars.description,
+			folder: vars.folderName,
+			framework: 'International standard ISO/IEC 27001:2022'
+		});
 
 		// Enable scoring on the compliance assessment
 		await page.getByTestId('edit-button').click();
 		await page.getByText('More').click();
+		for (const spinner of await page.locator('.loading-spinner').all()) {
+			await expect(spinner).not.toBeVisible({
+				timeout: 10_000
+			});
+		}
 		await page.getByTestId('form-input-scoring-enabled').check();
 		await page.getByTestId('save-button').click();
-		await page.waitForURL(/\/compliance-assessments\/[^/]+$/);
 
-		// Click on the ID.AM-1 tree view item
-		const IDAM1TreeViewItem = await complianceAssessmentsPage.itemDetail.treeViewItem('ID.AM-1', [
-			'ID - Identify',
-			'ID.AM - Asset Management'
-		]);
-		await IDAM1TreeViewItem.content.click();
+		await page.waitForTimeout(5000);
+
+		const OrgContextTree = await complianceAssessmentsPage.itemDetail.treeViewItem(
+			'4.1 - Understanding the organization and its context',
+			['core - Clauses', '4 - Context of the organization']
+		);
+		await OrgContextTree.content.click();
 
 		await page.waitForURL('/requirement-assessments/**');
-		await expect(page.getByTestId('progress-ring-svg')).toHaveAttribute('data-value', '1');
+		await expect(page.getByTestId('progress-ring-svg')).toHaveAttribute('data-value', '0');
 
 		const slider = page.getByTestId('range-slider-input');
 		await expect(slider).toBeVisible();
 		await slider.focus();
-		for (let i = 1; i < IDAM1Score.value; i++) {
+		for (let i = 1; i < OrgContextScore.value; i++) {
 			await slider.press('ArrowRight');
 		}
-		await expect(page.getByTestId('progress-ring-svg')).toHaveAttribute(
-			'data-value',
-			IDAM1Score.value.toString()
-		);
+		await expect(page.getByTestId('progress-ring-svg')).toHaveAttribute('data-value', '75');
 
 		await page.getByTestId('save-no-continue-button').click();
 		await complianceAssessmentsPage.isToastVisible('successfully saved', 'i');
 		await page.goBack();
 		await page.waitForURL(complianceAssessmentsPage.url + '/**');
-		await expect(IDAM1TreeViewItem.progressRadial).toHaveAttribute(
+		await expect(OrgContextTree.progressRadial).toHaveAttribute(
 			'data-value',
-			IDAM1Score.progress
+			OrgContextScore.progress
 		);
 	});
 
-	await test.step('apply mapping to new iso27001:2022 audit', async () => {
+	await test.step('apply mapping to new csf 1.1 audit', async () => {
 		//NOTE: imitates PageContent.createItem(), since our form is not a "classic"" one
 		// This could be improved
 		await applyMappingButton.click();
@@ -142,9 +128,10 @@ test('user can map csf-1.1 audit to a new iso27001-2022 audit', async ({
 			name: 'Mapped-' + vars.assessmentName,
 			description: vars.description,
 			folder: vars.folderName,
-			perimeter: vars.folderName + '/' + vars.perimeterName,
 			framework: vars.framework.name
 		});
+		await page.getByText('More').click();
+		await page.getByTestId('form-input-scoring-enabled').check();
 		await applyMappingForm.saveButton.click();
 		await expect(applyMappingForm.formTitle).not.toBeVisible();
 		await complianceAssessmentsPage.isToastVisible(
@@ -160,6 +147,11 @@ test('user can map csf-1.1 audit to a new iso27001-2022 audit', async ({
 		await IDAM1TreeViewItem.content.click();
 
 		await page.waitForURL('/requirement-assessments/**');
+		for (const spinner of await page.locator('.loading-spinner').all()) {
+			await expect(spinner).not.toBeVisible({
+				timeout: 10_000
+			});
+		}
 
 		await expect(page.getByTestId('progress-ring-svg')).toHaveAttribute(
 			'data-value',
@@ -170,10 +162,6 @@ test('user can map csf-1.1 audit to a new iso27001-2022 audit', async ({
 		await complianceAssessmentsPage.isToastVisible('successfully saved', 'i');
 		await page.goBack();
 		await page.waitForURL(complianceAssessmentsPage.url + '/**');
-		await expect(IDAM1TreeViewItem.progressRadial).toHaveAttribute(
-			'data-value',
-			IDAM1Score.progress
-		);
 	});
 });
 
