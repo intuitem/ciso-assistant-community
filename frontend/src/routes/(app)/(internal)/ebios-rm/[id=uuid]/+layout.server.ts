fbfd985bf9cb815685890e1cfed411d501cc27df
monsieurswag <mael.joumier@intuitem.com>
2026-04-20 16:56:23 +0200

    Fix missing zod migration import

--- frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/+layout.server.ts
+++ frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/+layout.server.ts
@@ -1,5 +1,5 @@
 import { superValidate } from 'sveltekit-superforms';
-import { zod } from 'sveltekit-superforms/adapters';
+import { zod4 as zod } from 'sveltekit-superforms/adapters';
 
 import type { LayoutServerLoad } from './$types';
 import { loadDetail } from '$lib/utils/load';
