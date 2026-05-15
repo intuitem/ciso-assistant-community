dd7d1c938b97e7551699796d1964aaf932818c0a
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2026-05-15 16:48:54 +0200

    fixup

--- frontend/tests/functional/detailed/libraries.test.ts
+++ frontend/tests/functional/detailed/libraries.test.ts
@@ -8,7 +8,7 @@ test('every library can be loaded', async ({ logedPage, librariesPage, page }) =
 	await librariesPage.hasUrl();
 
 	const nameCells = page.locator('tbody tr td:nth-child(2)');
-	await expect(nameCells.first()).not.toHaveText('', { timeout: 30000 });
+	await expect(nameCells.first()).toHaveText(/\S+/, { timeout: 30000 });
 
 	const libraries: Locator[] = await nameCells.all();
 	const libraryNames: string[] = await Promise.all(
