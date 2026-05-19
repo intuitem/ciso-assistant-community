9113b1878d646ee14a7eacfabd415594e392000e
eric-intuitem <71850047+eric-intuitem@users.noreply.github.com>
2026-05-19 23:58:46 +0200

    Update RequirementLabel.svelte

--- frontend/src/lib/components/RequirementLabel.svelte
+++ frontend/src/lib/components/RequirementLabel.svelte
@@ -12,7 +12,7 @@
 {#if ref_id || name}
 	<span class={className}>
 		{#if ref_id}<span
-				class="inline-block px-1.5 py-0.5 rounded bg-gray-100 text-gray-700 border border-gray-200 font-mono text-xs leading-none align-middle"
+				class="inline-block px-1.5 py-0.5 rounded bg-gray-100 text-gray-700 border border-gray-200 font-mono leading-none align-middle"
 				>{ref_id}</span
 			>{/if}{#if ref_id && name}{' '}{/if}{#if name}<span class={bold ? 'font-semibold' : ''}
 				>{name}</span
