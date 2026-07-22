c17d3e38183a46ac0b97ec35253dd7812afda8d2
monsieurswag <mael.joumier@intuitem.com>
2026-07-22 15:05:13 +0200

    Remove dead import

--- backend/iam/models.py
+++ backend/iam/models.py
@@ -4,7 +4,7 @@ Inspired from Azure IAM model"""
 from __future__ import annotations
 from collections import defaultdict
 from dataclasses import dataclass
-from typing import Any, Generator, List, Literal, Optional, Iterable, ClassVar
+from typing import Any, List, Literal, Optional, Iterable, ClassVar
 from typing import TYPE_CHECKING, cast
 import itertools
 import uuid
