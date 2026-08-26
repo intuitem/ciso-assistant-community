6de5a3037e193cdb3d34726628e687b4bd397072
Abderrahmane Smimite <smimite@gmail.com>
2026-08-26 07:49:28 +0200

    format

--- backend/core/migrations/0184_commitment_remove_comment_comment_exactly_one_parent_and_more.py
+++ backend/core/migrations/0184_commitment_remove_comment_comment_exactly_one_parent_and_more.py
@@ -9,7 +9,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("contenttypes", "0002_remove_content_type_name"),
         ("core", "0183_cleanup_stale_permissions_from_model_renamed"),
