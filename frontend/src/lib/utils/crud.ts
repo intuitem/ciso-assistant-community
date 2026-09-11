c655988ad74f37c1fdbcedf1cffb2b4f645b9c18
Abderrahmane Smimite <smimite@gmail.com>
2026-09-11 20:28:55 +0200

    fix: multiple frontend glitches

--- frontend/src/lib/utils/crud.ts
+++ frontend/src/lib/utils/crud.ts
@@ -2987,7 +2987,7 @@ export const URL_MODEL_MAP: ModelMap = {
 		selectFields: [{ field: 'status' }, { field: 'kind' }],
 		foreignKeyFields: [
 			{ field: 'folder', urlModel: 'folders', urlParams: 'content_type=DO&content_type=GL' },
-			{ field: 'framework', urlModel: 'frameworks' },
+			{ field: 'frameworks', urlModel: 'frameworks' },
 			{ field: 'perimeters', urlModel: 'perimeters' },
 			{ field: 'entities', urlModel: 'entities' }
 		],
@@ -3022,7 +3022,7 @@ export const URL_MODEL_MAP: ModelMap = {
 		],
 		filters: [
 			{ field: 'status' },
-			{ field: 'framework' },
+			{ field: 'frameworks' },
 			{ field: 'folder' },
 			{ field: 'perimeters' }
 		]
