3dcac619c01bfa69cea9acce72ea59c2124d98f0
tchoumi313 <tchouminzikeubd@gmail.com>
2026-09-16 13:51:21 +0200

    updates

--- frontend/src/routes/(app)/(internal)/identity-providers/+layout.server.ts
+++ frontend/src/routes/(app)/(internal)/identity-providers/+layout.server.ts
@@ -7,7 +7,7 @@ import type { LayoutServerLoad } from './$types';
 const URLModel = 'identity-providers';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-	if (!locals?.featureflags?.service_accounts) {
+	if (!(await locals.getFeatureFlags())?.service_accounts) {
 		redirect(302, '/analytics');
 	}
 	const base = listViewFields[URLModel];
